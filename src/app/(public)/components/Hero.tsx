9dbce3eaf2109d4fc1c0e4bb67768347558c0233
João Pedro <joaopedro.nc@outlook.com>
2023-07-20 17:30:55 -0300

    ➕ Add Hero component

---
+++ src/app/(public)/components/Hero.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export function Hero() {
+  return (
+    <div className="space-y-6">
+      <Image src="/images/logo.svg" width={280} height={280} alt="StarDust" />
+      <p className="text-gray-100 text-base text-center">Aprenda enquanto se diverte!</p>
+      <Image src="/images/rocket.svg" width={280} height={280} alt="Rocket" />
+    </div>
+  )
+}
