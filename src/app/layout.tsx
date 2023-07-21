a1d2c8051d3dd395ec35f5e16f7cff74896894ce
João Pedro <joaopedro.nc@outlook.com>
2023-07-21 08:27:22 -0300

    ➕ Add providers to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import '../styles/global.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 
+import { Providers } from '@/providers'
+
 const poppins = Poppins({ weight: ['400', '500', '600'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -9,14 +11,17 @@ export const metadata: Metadata = {
   description: 'Aprenda lógica de programação de maneira gamificada',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="en">
-      <body className={`${poppins.className} bg-gray-900`}>{children}</body>
+      <body className={`${poppins.className} bg-gray-900`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
