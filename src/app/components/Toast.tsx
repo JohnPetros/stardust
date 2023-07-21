cba6c06cb2c74be0db8a19b31668677a98ea5fd1
João Pedro <joaopedro.nc@outlook.com>
2023-07-21 08:54:10 -0300

    💄 style: Add Toast component styles

--- src/app/components/Toast.tsx
+++ src/app/components/Toast.tsx
@@ -1,42 +1,45 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Prohibit, X } from '@phosphor-icons/react'
-import * as RadixToast from '@radix-ui/react-toast'
+import * as Container from '@radix-ui/react-toast'
 
 export function Toast() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(true)
 
+  
+ 
   return (
     <>
-      <RadixToast.Root
-        className="bg-red-700/30 absolute top-0 right-0"
-        open={isOpen}
-        onOpenChange={setIsOpen}
+      <Container.Viewport className="fixed top-4 right-4 max-w-[90vw] z-50 flex" />
+      <Container.Root
+        className="bg-red-700/50 rounded w-full"
+        open={true}
+        // onOpenChange={setIsOpen}
       >
-        <RadixToast.Title className="flex items-center gap-2 p-5">
-          <Prohibit
-            className="text-gray-100 bg-red-700"
-            width={20}
-            height={20}
-            weight="bold"
-          />
-          Error alert
-        </RadixToast.Title>
+        <div className="flex justify-between p-4 gap-6">
+          <Container.Title className="flex items-center gap-2 text-gray-100 font-medium">
+            <span className="text-gray-100 bg-red-700 rounded p-1">
+              <Prohibit className="" width={20} height={20} weight="bold" />
+            </span>
+            Usuário não encontrado
+          </Container.Title>
 
-        <RadixToast.Action
-          className="p-2"
-          asChild
-          altText="Close toast"
-        >
-          <button>
-            <X className="text-gray-400" width={16} height={16} weight="bold" />
-          </button>
-        </RadixToast.Action>
-        <div>
+          <Container.Action className="w-max" asChild altText="Close toast">
+            <button>
+              <X
+                className="text-gray-400"
+                width={20}
+                height={20}
+                weight="bold"
+              />
+            </button>
+          </Container.Action>
+        </div>
+
+        <div className='w-full rounded'>
           <div className="bg-red-700 w-1/4 h-1" />
         </div>
-      </RadixToast.Root>
-      <RadixToast.Viewport className="ToastViewport" />
+      </Container.Root>
     </>
   )
 }
