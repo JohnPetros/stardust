dd00b9405c66fbc08dbc471c8bab86355546b837
João Pedro <joaopedro.nc@outlook.com>
2023-07-24 19:04:38 -0300

    ➕ Add SidebarProvider to providers

--- src/providers/Client.tsx
+++ src/providers/Client.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { ReactNode } from 'react'
 import { Provider as ToastProvider } from '@radix-ui/react-toast'
+import { SidebarProvider } from '@/contexts/SidebarContext'
 
 interface ClientProps {
   children: ReactNode
@@ -8,6 +9,8 @@ interface ClientProps {
 
 export function Client({ children }: ClientProps) {
   return (
+    <SidebarProvider>
       <ToastProvider swipeDirection="right">{children}</ToastProvider>
+    </SidebarProvider>
   )
 }
