444a464e009cf7d2c5b45aa86ae1b9b7010c6291
João Pedro <joaopedro.nc@outlook.com>
2023-07-23 21:50:22 -0300

    🔧 chore: Add supabase auto gen types

--- src/types/collections.ts
+++ src/types/collections.ts
@@ -1,3 +1,5 @@
 import { Database } from "./supabase";
 
 export type User = Database["public"]["Tables"]["users"]["Row"];
+export type Planet = Database["public"]["Tables"]["planets"]["Row"];
+export type Star = Database["public"]["Tables"]["stars"]["Row"];
