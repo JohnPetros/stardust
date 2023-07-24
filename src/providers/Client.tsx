e4695b24d0aa9a403488453c55ef48b8ed3eeb8e
João Pedro <joaopedro.nc@outlook.com>
2023-07-24 17:44:16 -0300

    🔥 Remove React query provider

--- src/providers/Client.tsx
+++ src/providers/Client.tsx
@@ -1,25 +1,13 @@
 'use client'
 import { ReactNode } from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider as ToastProvider } from '@radix-ui/react-toast'
 
 interface ClientProps {
   children: ReactNode
 }
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      // staleTime: 1000 * 60 * 30
-    },
-  },
-})
-
 export function Client({ children }: ClientProps) {
   return (
-    <QueryClientProvider client={queryClient}>
       <ToastProvider swipeDirection="right">{children}</ToastProvider>
-    </QueryClientProvider>
   )
 }
