fb5b5237f8a7e395307b831fc6eff25a3f4f8116
João Pedro <joaopedro.nc@outlook.com>
2023-07-23 21:47:20 -0300

    🏷️ Add user type

---
+++ src/types/user.ts
@@ -0,0 +1,27 @@
+export type User = {
+  acquired_rockets: number
+  avatar_id: string
+  coins: number
+  completed_challenges: number
+  completed_planets: number
+  created_at: string
+  did_break_streak: boolean
+  did_complete_saturday: boolean
+  did_update_ranking: boolean
+  email: string
+  id: string
+  is_admin: boolean
+  is_loser: boolean | null
+  last_position: number | null
+  level: number
+  name: string
+  ranking_id: string
+  rocket_id: string
+  streak: number
+  study_time: string
+  unlocked_achievements: number
+  unlocked_stars: number
+  week_status: string[]
+  weekly_xp: number
+  xp: number
+}
