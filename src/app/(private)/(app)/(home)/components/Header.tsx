1bb47644a0db8841e8b5b80d1207579168835264
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 12:36:29 -0300

    💄 style: Repositing Header component

--- src/app/(private)/(app)/(home)/components/Header.tsx
+++ src/app/(private)/(app)/(home)/components/Header.tsx
@@ -12,38 +12,38 @@ export function Header() {
   const { toggle } = useSiderbar()
 
   return (
-    <header className="bg-gray-900 border-b border-green-800 px-6 py-3 flex justify-between">
-        <div className="flex items-center gap-3">
-          <button onClick={toggle}>
-            <List
-              width={24}
-              height={24}
-              className="text-green-400"
-              weight="bold"
-            />
-          </button>
-          <Image src="/images/logo.svg" width={80} height={80} alt="StarDust" />
+    <header className="fixed top-0 w-full z-40 bg-gray-900 border-b border-green-800 px-6 py-3 flex justify-between">
+      <div className="flex items-center gap-3">
+        <button onClick={toggle}>
+          <List
+            width={24}
+            height={24}
+            className="text-green-400"
+            weight="bold"
+          />
+        </button>
+        <Image src="/images/logo.svg" width={80} height={80} alt="StarDust" />
+      </div>
+
+      <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2">
+          <Image
+            src="/icons/coin.svg"
+            width={28}
+            height={28}
+            alt="Star Coins"
+          />
+          <span className="text-yellow-400 font-semibold text-lg">
+            {user?.coins}
+          </span>
         </div>
 
-        <div className="flex items-center gap-2">
-          <div className="flex items-center gap-2">
-            <Image
-              src="/icons/coin.svg"
-              width={28}
-              height={28}
-              alt="Star Coins"
-            />
-            <span className="text-yellow-400 font-semibold text-lg">
-              {user?.coins}
-            </span>
-          </div>
-
-          <div className="flex items-center">
-            <Animation src={StreakAnimation} size={32} hasLoop={false} />
-            <span className="text-green-500 font-semibold text-lg">
-              {user?.streak}
-            </span>
-          </div>
+        <div className="flex items-center">
+          <Animation src={StreakAnimation} size={32} hasLoop={false} />
+          <span className="text-green-500 font-semibold text-lg">
+            {user?.streak}
+          </span>
+        </div>
       </div>
     </header>
   )
