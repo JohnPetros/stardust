697a6161c0e7dbe4a1bb462347568cb451c95b63
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 14:07:41 -0300

    ♿ Add  sidebar closeClick to Home Page

--- src/app/(private)/(app)/(home)/components/Sidebar.tsx
+++ src/app/(private)/(app)/(home)/components/Sidebar.tsx
@@ -21,36 +21,36 @@ const sidebarVariants: Variants = {
 
 export function Sidebar() {
   const { user } = useAuth()
-  if (!user) return null
 
   const { isOpen } = useSiderbar()
 
-  return (
-    <AnimatePresence>
-      {isOpen && (
-        <motion.div
-          variants={sidebarVariants}
-          initial="close"
-          animate="open"
-          exit="close"
-          className="bg-gray-900 fixed h-screen left-0 w-80 z-20 mt-12 "
-        >
-          <div className="flex flex-col items-center justify-center gap-3 text-gray-100">
-            <UserAvatar avatarId={user.avatar_id} size={96} />
-            <strong>{user?.name}</strong>
-            <small className="text-sm">{user?.email}</small>
-            <Button className="bg-green-400 text-green-900 px-3 py-2 w-48 mx-aut mt-3">
-              Sair
-            </Button>
-          </div>
-
-          <div className="mt-3 overflow-y-auto h-full p-6 custom-scrollbar">
-            <div>
-              <AchievementsList />
+  if (user)
+    return (
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            variants={sidebarVariants}
+            initial="close"
+            animate="open"
+            exit="close"
+            className="bg-gray-900 fixed h-screen left-0 w-80 z-20 pt-24"
+          >
+            <div className="flex flex-col items-center justify-center gap-3 text-gray-100">
+              <UserAvatar avatarId={user.avatar_id} size={96} />
+              <strong>{user?.name}</strong>
+              <small className="text-sm">{user?.email}</small>
+              <Button className="bg-green-400 text-green-900 px-3 py-2 w-48 mx-aut mt-3">
+                Sair
+              </Button>
             </div>
-          </div>
-        </motion.div>
-      )}
-    </AnimatePresence>
-  )
+
+            <div className="mt-3 overflow-y-auto h-full p-6 custom-scrollbar">
+              <div>
+                <AchievementsList />
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    )
 }
--- src/app/(private)/(app)/(home)/page.tsx
+++ src/app/(private)/(app)/(home)/page.tsx
@@ -3,20 +3,30 @@
 import { useAuth } from '@/hooks/useAuth'
 import { usePlanet } from '@/hooks/usePlanet'
 import { Planet } from './components/Planet'
+import { useSiderbar } from '@/hooks/useSiderbar'
 
 export default function Home() {
   const { user } = useAuth()
   const { planets, lastUnlockedStarId } = usePlanet()
+  const { isOpen, toggle } = useSiderbar()
+
+  function handleClick() {
+    if (isOpen) toggle()
+  }
 
   return (
     <main
       style={{ backgroundImage: 'url("/images/space-background.png")' }}
-      className="bg-center flex flex-col items-center "
+      className="bg-center flex flex-col items-center"
+      onClick={handleClick}
     >
-      <ul className="w-[480px] max-w-[90vw] mt-8 flex flex-col items-center justify-center gap-12">
-        
+      <ul className="w-[480px] max-w-[90vw] mt-24 flex flex-col items-center justify-center gap-12">
         {planets?.map((planet) => (
-          <Planet key={planet.id} data={planet} />
+          <Planet
+            key={planet.id}
+            data={planet}
+            lastUnlockedStarId={lastUnlockedStarId}
+          />
         ))}
       </ul>
     </main>
