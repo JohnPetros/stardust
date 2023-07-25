41943c6ae7290167bae0c24c2523481318e7e87f
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 12:34:28 -0300

    ➕ Add verifyAchievement hook

---
+++ src/hooks/useAchievements.tsx
@@ -0,0 +1,70 @@
+import { api } from '@/services/api'
+import { Achievement } from '@/types/achievement'
+import { useMemo } from 'react'
+import useSWR from 'swr'
+
+async function getUserUnlockedAchievementsIds(userId: string) {
+  if (userId) {
+    return await api.getUserUnlockedAchievementsIds(userId)
+  }
+}
+
+async function getUserRescuableAchievementsIds(userId: string) {
+  if (userId) {
+    return await api.getUserRescuableAchievementsIds(userId)
+  }
+}
+
+function verifyAchievement(
+  achievement: Achievement,
+  userUnlockedAchievementsIds: string[],
+  userRescuableAchievementsIds: string[]
+) {
+  const isUnlocked = userUnlockedAchievementsIds.some(
+    (unlockedAchievementId) => unlockedAchievementId === achievement.id
+  )
+
+  const isRescuable = userRescuableAchievementsIds.some(
+    (rescuableachievementId) => rescuableachievementId === achievement.id
+  )
+
+  return { ...achievement, isUnlocked, isRescuable }
+}
+
+export function useAchievement(userId?: string) {
+  const { data: achievements } = useSWR('achievements', api.getAchievements)
+  const { data: userUnlockedAchievementsIds } = useSWR(
+    'achievements',
+    getUserUnlockedAchievementsIds
+  )
+  const { data: userRescuableAchievementsIds } = useSWR(
+    'achievements',
+    getUserRescuableAchievementsIds
+  )
+
+  const verifiedAchievements = useMemo(() => {
+    if (
+      userId &&
+      achievements &&
+      userUnlockedAchievementsIds &&
+      userRescuableAchievementsIds
+    ) {
+      return achievements.map((achievement) =>
+        verifyAchievement(
+          achievement,
+          userUnlockedAchievementsIds,
+          userRescuableAchievementsIds
+        )
+      )
+    }
+  }, [
+    userId,
+    achievements,
+    userUnlockedAchievementsIds,
+    userRescuableAchievementsIds,
+  ])
+
+  console.log(verifiedAchievements)
+
+  return { achievements: verifiedAchievements }
+}
