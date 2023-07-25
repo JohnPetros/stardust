611352720d7aad2f2049db3a3e456395984a69c5
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 20:26:12 -0300

    💫 Add TransitionPageAnimation to Home page

--- src/app/(private)/(app)/(home)/page.tsx
+++ src/app/(private)/(app)/(home)/page.tsx
@@ -4,15 +4,23 @@ import { useAuth } from '@/hooks/useAuth'
 import { usePlanet } from '@/hooks/usePlanet'
 import { Planet } from './components/Planet'
 import { useSiderbar } from '@/hooks/useSiderbar'
+import { useEffect, useState } from 'react'
+import { TransitionPageAnimation } from '../components/TransitionPageAnimation'
 
 export default function Home() {
-  const { user } = useAuth()
   const { planets, lastUnlockedStarId } = usePlanet()
   const { isOpen, toggle } = useSiderbar()
+  const [isTransitionVisible, setIsTransitionVisible] = useState(true)
 
   function handleClick() {
     if (isOpen) toggle()
   }
+
+  useEffect(() => {
+    if (planets?.length) {
+      setTimeout(() => setIsTransitionVisible(false), 3500)
+    }
+  }, [planets])
 
   return (
     <main
@@ -20,7 +28,8 @@ export default function Home() {
       className="bg-center flex flex-col items-center"
       onClick={handleClick}
     >
-      <ul className="w-[480px] max-w-[90vw] mt-24 flex flex-col items-center justify-center gap-12">
+      <TransitionPageAnimation isVisible={isTransitionVisible} />
+      <ul className=" max-w-[90vw] mt-24 flex flex-col items-start justify-center gap-12">
         {planets?.map((planet) => (
           <Planet
             key={planet.id}
