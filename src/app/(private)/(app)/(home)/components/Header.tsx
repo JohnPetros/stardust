1bb9b761cd978ef3889821a2f4509b1b6928534a
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 20:27:28 -0300

    📱 Adapt header to mobile

--- src/app/(private)/(app)/(home)/components/Header.tsx
+++ src/app/(private)/(app)/(home)/components/Header.tsx
@@ -6,13 +6,14 @@ import { List } from '@phosphor-icons/react'
 import Image from 'next/image'
 
 import StreakAnimation from '../../../../../../public/animations/streak.json'
+import { UserAvatar } from './UserAvatar'
 
 export function Header() {
   const { user } = useAuth()
   const { toggle } = useSiderbar()
 
   return (
-    <header className="fixed top-0 w-full z-40 bg-gray-900 h-16 px-6 py-3 flex justify-between md:justify-end">
+    <header className="fixed top-0 w-screen z-40 bg-gray-900 h-16 px-6 py-3 flex justify-between md:justify-end">
       <div className="flex items-center gap-3 md:hidden">
         <button onClick={toggle}>
           <List
@@ -45,6 +46,16 @@ export function Header() {
           </span>
         </div>
       </div>
+
+      {user && (
+        <div className="hidden md:flex items-center gap-6 ml-6">
+          <div className="flex flex-col items-center text-sm text-gray-100">
+            <strong>{user?.name}</strong>
+            <small>{user?.email}</small>
+          </div>
+          <UserAvatar avatarId={user?.avatar_id} size={48} />
+        </div>
+      )}
     </header>
   )
 }
