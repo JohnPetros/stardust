678b6ce2e7269111c0e84777b515ece90062a6e0
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 16:50:16 -0300

    📱 Adapt TabNav component

--- src/app/(private)/(app)/(home)/components/TabNav.tsx
+++ src/app/(private)/(app)/(home)/components/TabNav.tsx
@@ -2,9 +2,9 @@
 import { HOME_PAGES } from '@/utils/constants/home-pages'
 import { NavButton } from './NavButton'
 
-export function TabBar() {
+export function TabNav() {
   return (
-    <div className="fixed bottom-0 w-full border-t border-green-800 bg-gray-900 flex justify-center items-center px-6 py-3">
+    <div className="md:hidden fixed bottom-0 w-full border-t border-green-800 bg-gray-900 flex justify-center items-center px-6 py-3">
       <nav className="flex items-center w-full justify-between">
         {HOME_PAGES.map(({ path, icon, label }) => (
           <NavButton path={path} label={label} icon={icon} />
