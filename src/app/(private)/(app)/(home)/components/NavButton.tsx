20fa61c07d847413f00f1ed02a66b9fc42a8bf86
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 16:17:02 -0300

    💫 Animate NavButton component

--- src/app/(private)/(app)/(home)/components/NavButton.tsx
+++ src/app/(private)/(app)/(home)/components/NavButton.tsx
@@ -3,20 +3,33 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import { twMerge } from 'tailwind-merge'
+import { Variants, motion } from 'framer-motion'
 
 interface NavButtonProps {
   path: string
   label: string
   icon: string
-  isCol?: boolean
+  isColumn?: boolean
   isExpanded?: boolean
+}
+
+const labelVariants: Variants = {
+  shrink: {
+    width: 0,
+  },
+  expand: {
+    width: 'auto',
+    transition: {
+      delay: 0.05,
+    },
+  },
 }
 
 export function NavButton({
   path,
   label,
   icon,
-  isCol = true,
+  isColumn = true,
   isExpanded = true,
 }: NavButtonProps) {
   const pathname = usePathname()
@@ -26,34 +39,35 @@ export function NavButton({
     <Link
       href={path}
       className={twMerge(
-        'rounded-xl hover:bg-green-500/30 transition-colors duration-200 flex items-center justify-center gap-2',
-        isCol ? 'flex-col' : 'flex-row'
+        'rounded-xl hover:bg-green-700/20 transition-colors duration-200 flex items-center justify-center w-max p-3 gap-2',
+        isColumn ? 'flex-col' : 'flex-row'
       )}
     >
       <span
         className={twMerge(
-          'rounded-lg w-12 h-10 grid place-content-center',
+          'rounded-lg w-10 h-10 grid place-content-center',
           isActive ? 'bg-green-400' : 'bg-green-800'
         )}
       >
         <Image
           src={`/icons/${icon}`}
-          width={26}
-          height={26}
+          width={28}
+          height={28}
           className="text-gray-800"
           alt=""
         />
       </span>
-      {isExpanded && (
-        <span
-          className={twMerge(
-            'font-semibold text-sm',
-            isActive ? 'text-gray-100' : 'text-gray-400'
-          )}
-        >
-          {label}
-        </span>
-      )}
+      <motion.span
+        variants={labelVariants}
+        initial="shrink"
+        animate={isExpanded ? 'expand' : ''}
+        className={twMerge(
+          'block overflow-hidden font-semibold text-sm',
+          isActive ? 'text-gray-100' : 'text-gray-400'
+        )}
+      >
+        {label}
+      </motion.span>
     </Link>
   )
 }
