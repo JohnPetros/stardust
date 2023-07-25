33924774f76e36b0466ba4a8fd474a67096a8949
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 16:48:49 -0300

    💄 style:  Sidenav to Home layout

--- src/app/(private)/(app)/(home)/layout.tsx
+++ src/app/(private)/(app)/(home)/layout.tsx
@@ -2,11 +2,13 @@ import { ReactNode } from 'react'
 import { Header } from './components/Header'
 import { TabBar } from './components/TabBar'
 import { Sidebar } from './components/Sidebar'
+import { Sidenav } from './components/Sidenav'
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
+      <Sidenav />
       <Sidebar />
       {children}
       <TabBar />
