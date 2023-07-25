122317233bcae3418a6481001f4256380710f9df
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 16:16:23 -0300

    ➕ Add Sidenav component

--- src/app/(private)/(app)/(home)/components/Sidenav.tsx
+++ src/app/(private)/(app)/(home)/components/Sidenav.tsx
@@ -1,42 +1,86 @@
 'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
+
+import { CaretLeft, CaretRight, Flag, Power } from '@phosphor-icons/react'
+import { Button } from '@/app/components/Button'
 import { NavButton } from './NavButton'
 import { HOME_PAGES } from '@/utils/constants/home-pages'
-import { Flag, Power } from '@phosphor-icons/react'
+import { Variants, motion } from 'framer-motion'
 import SidenavButton from './SidenavButton'
-import { Button } from '@/app/components/Button'
+
+const sidenavVariants: Variants = {
+  shrink: {
+    width: 88,
+  },
+  expand: {
+    width: 164,
+  },
+}
 
 export function Sidenav() {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  function handleExpandClick() {
+    setIsExpanded(!isExpanded)
+  }
+
   return (
-    <div className="absolute left-0 bg-gray-900 w-20 h-full z-50 flex flex-col justify-between">
-      <div>
-        <div className="border-b border-green-700 p-3 grid place-content-center">
-          <Image src="/icons/rocket.svg" width={32} height={32} alt="" />
+    <motion.div
+      variants={sidenavVariants}
+      initial="shrink"
+      animate={isExpanded ? 'expand' : ''}
+      className="absolute left-0 bg-gray-900 h-full z-50"
+    >
+      <div className="reative flex flex-col justify-between h-full">
+        <button
+          onClick={handleExpandClick}
+          className="absolute top-20 -right-2 rounded-full bg-green-400 p-1 grid place-content-center"
+        >
+          {isExpanded ? (
+            <CaretRight className="text-gray-800 text-sm" weight="bold" />
+          ) : (
+            <CaretLeft className="text-gray-800 text-sm" weight="bold" />
+          )}
+        </button>
+        <div>
+          <div className="border-b border-green-700 h-16 mx-3 grid place-content-center">
+            {isExpanded ? (
+              <Image
+                src="/images/logo.svg"
+                width={96}
+                height={96}
+                alt="StarDust"
+              />
+            ) : (
+              <Image src="/icons/rocket.svg" width={30} height={30} alt="" />
+            )}
+          </div>
+
+          <div className="mt-12 flex flex-col px-3 gap-3">
+            {HOME_PAGES.map(({ path, icon, label }) => (
+              <NavButton
+                path={path}
+                label={label}
+                icon={icon}
+                isExpanded={isExpanded}
+                isColumn={false}
+              />
+            ))}
+          </div>
         </div>
 
-        <div className="mt-6 flex flex-col items-center gap-4">
-          {HOME_PAGES.map(({ path, icon, label }) => (
-            <NavButton
-              path={path}
-              label={label}
-              icon={icon}
-              isExpanded={false}
-            />
-          ))}
+        <div className="border-t border-green-700 flex flex-col mx-3 px-3 py-3">
+          <SidenavButton
+            icon={Flag}
+            title="Conquistas"
+            isExpanded={isExpanded}
+          />
+
+          <SidenavButton icon={Power} title="Sair" isExpanded={isExpanded} />
         </div>
       </div>
-
-      <div className="border-t border-green-700 flex flex-col items-center py-3">
-        <Button className="bg-transparent flex items-center gap-2 text-gray-100 text-sm h-auto w-max p-2 hover:bg-green-500/30 transition-colors duration-200">
-          <Flag className="text-green-400 text-xl" weight="bold" />
-          {/* {false && 'Conquistas'} */}
-        </Button>
-
-        <Button className="bg-transparent flex items-center gap-2 text-gray-100 text-sm h-auto w-max p-2 hover:bg-green-500/30 transition-colors duration-200">
-          <Power className="text-green-400 text-xl" weight="bold" />
-          {/* {false && 'Sair'} */}
-        </Button>
-      </div>
-    </div>
+    </motion.div>
   )
 }
