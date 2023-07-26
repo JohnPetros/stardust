389e6e51146df471974586df692ce956315fb727
João Pedro <joaopedro.nc@outlook.com>
2023-07-25 21:26:15 -0300

    💫  Animate  Achievement component

--- src/app/(private)/(app)/(home)/components/Achievement.tsx
+++ src/app/(private)/(app)/(home)/components/Achievement.tsx
@@ -4,8 +4,26 @@ import { Achievement as AchievementType } from '@/types/achievement'
 import { getImage } from '@/utils/functions'
 import Image from 'next/image'
 import * as Progress from '@radix-ui/react-progress'
+import { Variants, motion } from 'framer-motion'
 
 import Lock from '../../../../../../public/icons/lock.svg'
+
+const achievementVariants: Variants = {
+  hidden: {
+    x: -120,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      damping: 20,
+      stiffness: 80,
+      duration: 0.1,
+    },
+  },
+}
 
 interface AchievementProps {
   data: AchievementType
@@ -26,8 +44,11 @@ export function Achievement({
     currentProgress >= required_amount ? required_amount : currentProgress
 
   return (
-    <div className="grid grid-cols-[48px_1fr] gap-4 py-4 border-b border-green-500">
-      <div className="relative w-16 h-16 grid place-content-center">
+    <motion.div
+      variants={achievementVariants}
+      className="grid grid-cols-[48px_1fr] gap-4 py-4 border-b border-green-500"
+    >
+      <div className="relative w-12 h-12 grid place-content-center">
         {isUnlocked ? (
           <Image src={iconImage} fill alt="" />
         ) : (
@@ -36,8 +57,8 @@ export function Achievement({
       </div>
 
       <div className="flex flex-col gap-1">
-        <strong className="text-gray-100">{name}</strong>
-        <p className="text-gray-100">{description}</p>
+        <strong className="text-gray-100 text-sm">{name}</strong>
+        <p className="text-gray-100 text-xs">{description}</p>
         {!isUnlocked && (
           <div className="flex w-full items-center gap-2">
             <Progress.Root value={barWidth} className="bg-gray-400 h-1 w-full">
@@ -46,12 +67,12 @@ export function Achievement({
                 style={{ width: `${barWidth}%` }}
               />
             </Progress.Root>
-            <span className="text-gray-100">
+            <span className="text-gray-100 text-sm">
               {formatedCurrentProgress}/{required_amount}
             </span>
           </div>
         )}
       </div>
-    </div>
+    </motion.div>
   )
 }
