26f10bb407e7a9f59e6a9708477c885906e09474
João Pedro <joaopedro.nc@outlook.com>
2023-07-26 15:55:55 -0300

    🐛 fix: Add key to NavButton

--- src/app/(private)/(app)/(home)/components/TabNav.tsx
+++ src/app/(private)/(app)/(home)/components/TabNav.tsx
@@ -7,7 +7,7 @@ export function TabNav() {
     <div className="md:hidden fixed bottom-0 w-full border-t border-green-800 bg-gray-900 flex justify-center items-center px-6 py-3">
       <nav className="flex items-center w-full justify-between">
         {HOME_PAGES.map(({ path, icon, label }) => (
-          <NavButton path={path} label={label} icon={icon} />
+          <NavButton key={path} path={path} label={label} icon={icon} />
         ))}
       </nav>
     </div>
