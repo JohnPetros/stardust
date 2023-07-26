c8b9723f8e4e8964ca6d3f6a712f044db1ec67ca
João Pedro <joaopedro.nc@outlook.com>
2023-07-26 11:36:49 -0300

    🐛 fix: Fix TransitionPageAnimation to page

--- src/app/(private)/(app)/components/TransitionPageAnimation.tsx
+++ src/app/(private)/(app)/components/TransitionPageAnimation.tsx
@@ -41,7 +41,7 @@ export function TransitionPageAnimation({
           initial="initial"
           animate="visible"
           exit="hidden"
-          className="absolute top-0 left-0 right-0 bottom-0 bg-gray-900 z-50 flex items-center justify-center"
+          className="fixed top-0 left-0 right-0 bottom-0 bg-gray-900 z-50 flex items-center justify-center"
         >
           <motion.div variants={apolloVariants}>
             <Animation src={Transition} size={540} />
