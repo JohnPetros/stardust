d6cb3df553eb72566ec771ad85f7ba84799f6ab4
João Pedro <joaopedro.nc@outlook.com>
2023-07-26 15:57:31 -0300

    ➕ Add useRocket hook

--- src/hooks/useRocket.tsx
+++ src/hooks/useRocket.tsx
@@ -1,14 +1,57 @@
-import { api } from '@/services/api'
+import { useMemo } from 'react'
+import { useAuth } from './useAuth'
 import useSWR from 'swr'
 
+import { Rocket } from '@/types/rocket'
+import { api } from '@/services/api'
+
 export function useRocket(rocketId?: string) {
+  const { user } = useAuth()
+
+  function verifyRocketAcquirement(
+    rocket: Rocket,
+    userAcquiredRocketsIds: string[]
+  ) {
+    const isAcquired = userAcquiredRocketsIds.some(
+      (userAcquiredRocketsId) => userAcquiredRocketsId === rocket.id
+    )
+
+    return { ...rocket, isAcquired }
+  }
+
   async function getRocket() {
     if (rocketId) return await api.getRocket(rocketId)
   }
 
+  async function getRockets() {
+    if (!rocketId) return await api.getRockets()
+  }
+
+  async function getUserAcquiredRocketsIds() {
+    if (!rocketId && user?.id) {
+      return await api.getUserAcquiredRocketsIds(user.id)
+    }
+  }
+
   const { data: rocket } = useSWR(rocketId ? 'rocket' : null, getRocket)
+  const { data: rockets } = useSWR(!rocketId ? 'rocket' : null, getRockets)
+  const { data: userAcquiredRocketsIds } = useSWR(
+    !rocketId ? 'users_acquired_rockets_ids' : null,
+    getUserAcquiredRocketsIds
+  )
+
+  const verifiedRockets = useMemo(() => {
+    if (!rocketId && userAcquiredRocketsIds?.length) {
+      return rockets?.map((rocket) =>
+        verifyRocketAcquirement(rocket, userAcquiredRocketsIds)
+      )
+    }
+
+    return []
+  }, [rockets, userAcquiredRocketsIds])
 
   return {
     rocket,
+    rockets: verifiedRockets,
   }
 }
