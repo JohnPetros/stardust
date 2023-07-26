39a1f3b16bfddf5a08f204ef789c55034d3cf82c
João Pedro <joaopedro.nc@outlook.com>
2023-07-26 12:08:06 -0300

    💄 style: Add RocketAnimation component

---
+++ src/app/(public)/components/RocketAnimation.tsx
@@ -0,0 +1,48 @@
+'use client'
+import Lottie, { LottieRef } from 'lottie-react'
+import { motion, Variants } from 'framer-motion'
+
+import RocketLaunching from '../../../../public/animations/rocket-launching.json'
+import { ROCKET_ANIMATION_DURATION } from '@/utils/constants'
+
+const rocketVariants: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: ROCKET_ANIMATION_DURATION,
+      duration: 0.4,
+    },
+  },
+}
+
+interface RocketProps {
+  animationRef: LottieRef
+  isVisible: boolean
+  onAnimationEnd: () => void
+}
+
+export function RocketAnimation({
+  animationRef,
+  isVisible,
+  onAnimationEnd,
+}: RocketProps) {
+  return (
+    <motion.div
+      variants={rocketVariants}
+      initial="hidden"
+      animate={isVisible ? 'visible' : ''}
+      className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+    >
+      <Lottie
+        lottieRef={animationRef}
+        animationData={RocketLaunching}
+        style={{ width: 640, height: 640 }}
+        loop={false}
+        onComplete={onAnimationEnd}
+      />
+    </motion.div>
+  )
+}
