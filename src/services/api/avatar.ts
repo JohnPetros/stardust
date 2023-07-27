8630fbab8e72b776a398b920dc4f3e6862a06adb
João Pedro <joaopedro.nc@outlook.com>
2023-07-27 17:28:08 -0300

    ➕ Add avatar api methods

--- src/services/api/avatar.ts
+++ src/services/api/avatar.ts
@@ -16,4 +16,39 @@ export default {
     }
     return data
   },
+
+  getAvatars: async () => {
+    const { data, error } = await supabase
+      .from('avatars')
+      .select('*')
+      .order('price, name', { ascending: true })
+      .returns<Avatar[]>()
+    if (error) {
+      throw new Error(error.message)
+    }
+    return data
+  },
+
+  getUserAcquiredAvatarsIds: async (userId: string) => {
+    const { data, error } = await supabase
+      .from('users_acquired_avatars')
+      .select('avatar_id')
+      .eq('user_id', userId)
+
+    if (error) {
+      throw new Error(error.message)
+    }
+
+    return data.map((data) => data.avatar_id)
+  },
+
+  addUserAcquiredAvatar: async (avatarId: string, userId: string) => {
+    const { error } = await supabase
+      .from('users_acquired_avatars')
+      .insert([{ avatar_id: avatarId, user_id: userId }])
+
+    if (error) {
+      return error.message
+    }
+  },
 }
