693bba8fa7326f8bf5fdd565c8b49c2d1a4aa98b
João Pedro <joaopedro.nc@outlook.com>
2023-07-26 21:40:32 -0300

    ✨ List rockets on shop page

--- src/app/(private)/(app)/(home)/shop/page.tsx
+++ src/app/(private)/(app)/(home)/shop/page.tsx
@@ -2,20 +2,28 @@
 
 import { useRocket } from '@/hooks/useRocket'
 import { Rocket } from './components/Rocket'
+import { Loading } from '@/app/components/Loading'
+import { Suspense } from 'react'
 
 export default function Shop() {
-  const { rockets } = useRocket()
+  const { rockets, addUserAcquiredRocket } = useRocket()
 
   console.log(rockets)
 
   return (
-    <div className="w-full max-w-[1024px] mx-auto">
-      <h2 className="text-white">Foguetes</h2>
-      <div className="grid grid-cols-3 gap-6 mt-3">
-        {rockets?.map((rocket) => (
-          <Rocket key={rocket.id} data={rocket} />
-        ))}
-      </div>
+    <div className=" max-w-[1024px] mx-auto">
+      <Suspense fallback={<Loading isSmall={true} />}>
+        <h2 className="text-white font-semibold text-lg">Foguetes</h2>
+        <div className="px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-3">
+          {rockets.map((rocket) => (
+            <Rocket
+              key={rocket.id}
+              data={rocket}
+              addUserAcquiredRocket={addUserAcquiredRocket}
+            />
+          ))}
+        </div>
+      </Suspense>
     </div>
   )
 }
