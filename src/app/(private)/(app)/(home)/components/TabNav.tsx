2a144addf1d856e090dfe863348233bfa0f5fddb
João Pedro <joaopedro.nc@outlook.com>
2023-07-26 22:31:01 -0300

    📱 Adapt better NavButton to mobile

--- src/app/(private)/(app)/(home)/components/TabNav.tsx
+++ src/app/(private)/(app)/(home)/components/TabNav.tsx
@@ -5,7 +5,7 @@ import { NavButton } from './NavButton'
 export function TabNav() {
   return (
     <div className="md:hidden fixed bottom-0 w-full border-t border-green-800 bg-gray-900 flex justify-center items-center px-6 py-3">
-      <nav className="flex items-center w-full justify-between">
+      <nav className="flex items-center w-full justify-between max-w-lg md:max-w-0">
         {HOME_PAGES.map(({ path, icon, label }) => (
           <NavButton key={path} path={path} label={label} icon={icon} />
         ))}
