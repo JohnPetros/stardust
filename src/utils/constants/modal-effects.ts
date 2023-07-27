529e3ce3519f8517b2a5cd0d14b02d73b6893028
João Pedro <joaopedro.nc@outlook.com>
2023-07-27 16:11:46 -0300

    🚚 Add sound effect to Modal effects constant

--- src/utils/constants/modal-effects.ts
+++ src/utils/constants/modal-effects.ts
@@ -2,32 +2,39 @@ import Earning from '../../../public/animations/apollo-earning.json'
 import Crying from '../../../public/animations/apollo-crying.json'
 import Denying from '../../../public/animations/apollo-denying.json'
 import Asking from '../../../public/animations/apollo-asking.json'
+
 import { Type as ModalType } from '@/app/components/Modal'
 
 type ModalEffect = {
   id: ModalType
-  animation: unknown
+  animation: unknown | null
+  sound: string | null
 }
 
 export const MODAL_EFFECTS: ModalEffect[] = [
   {
     id: 'earning',
     animation: Earning,
+    sound: 'earning.wav',
   },
   {
     id: 'crying',
     animation: Crying,
+    sound: 'crying.wav',
   },
   {
     id: 'denying',
     animation: Denying,
+    sound: 'denying.wav',
   },
   {
     id: 'asking',
     animation: Asking,
+    sound: 'asking.wav',
   },
   {
     id: 'generic',
     animation: null,
+    sound: null,
   },
 ]
