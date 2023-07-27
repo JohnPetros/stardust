35995e57c67c4174adfe32d1c0efd78d9337a036
João Pedro <joaopedro.nc@outlook.com>
2023-07-27 18:10:06 -0300

    ♻️ refactor: Remove radix ui avatar form UserAvatr component

--- src/app/(private)/(app)/(home)/components/UserAvatar.tsx
+++ src/app/(private)/(app)/(home)/components/UserAvatar.tsx
@@ -1,8 +1,10 @@
+'use client'
 import { Loading } from '@/app/components/Loading'
 import { useAvatar } from '@/hooks/useAvatar'
 import { getImage } from '@/utils/functions'
 
 import * as Avatar from '@radix-ui/react-avatar'
+import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
 
 interface UserAvatarProps {
@@ -16,20 +18,15 @@ export function UserAvatar({ avatarId, size }: UserAvatarProps) {
 
   if (avatar)
     return (
-      <Avatar.Root>
-        <Avatar.Image
-          src={avatarImage}
-          width={size}
-          height={size}
-          className="rounded-full border border-green-700"
-          alt={avatar.name}
-        />
-        <Avatar.Fallback
-          className={twMerge('AvatarFallback', `w-[${size}px] h-[${size}px]`)}
-          delayMs={600}
-        >
+      <div
+        style={{ width: size, height: size }}
+        className="relative rounded-[50%] border border-green-700 bg-gray-300 overflow-hidden grid place-content-center"
+      >
+        {avatarImage ? (
+          <Image src={avatarImage} fill className="" alt={avatar.name} />
+        ) : (
           <Loading />
-        </Avatar.Fallback>
-      </Avatar.Root>
+        )}
+      </div>
     )
 }
