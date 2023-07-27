4af4b377f3b9a696e39189969f27f1ebe3ab1d53
João Pedro <joaopedro.nc@outlook.com>
2023-07-27 15:51:25 -0300

    📱 Adapt Modal to mobile

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -1,11 +1,25 @@
 'use client'
 import { ReactNode, forwardRef, useImperativeHandle, useState } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from '@phosphor-icons/react'
-import { MODAL_EFFECTS } from '@/utils/constants'
 import Lottie from 'lottie-react'
 
+import { AnimatePresence, motion, Variants } from 'framer-motion'
+
+import { X } from '@phosphor-icons/react'
+import { MODAL_EFFECTS } from '@/utils/constants'
+
 export type Type = 'earning' | 'crying' | 'denying' | 'asking' | 'generic'
+
+const modalVariants = {
+  close: {
+    opacity: 0,
+    scale: 0.9,
+  },
+  open: {
+    opacity: 1,
+    scale: 1,
+  },
+}
 
 export interface ModalRef {
   open: VoidFunction
@@ -19,6 +33,8 @@ interface ModalProps {
   body: ReactNode
   footer: ReactNode
 }
+
+export const Container = Dialog.Root
 
 export const Modal = forwardRef<ModalRef, ModalProps>(
   ({ type, canPlaySong = true, title, body, footer }, ref) => {
@@ -49,32 +65,41 @@ export const Modal = forwardRef<ModalRef, ModalProps>(
     )
 
     return (
-      <Dialog.Root open={isOpen}>
-        <Dialog.Overlay className="fixed inset-0 overflow-y-auto bg-black bg-opacity-50 z-50">
-          <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-y-auto max-h-screen w-[90%] max-w-md p-6 rounded-lg bg-gray-800">
-            <div className="flex items-start">
-              <Dialog.Title className="font-semibold text-white text-center mx-auto">
-                {title}
-              </Dialog.Title>
-              <Dialog.Close
-                onClick={close}
-                className="p-2 hover:bg-gray-700 transition-colors duration-200 rounded-full"
+      <Dialog.Root open={isOpen} onOpenChange={close}>
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed inset-0 overflow-y-auto bg-black bg-opacity-50 z-50">
+            <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-screen w-full max-w-lg p-6">
+              <motion.div
+                variants={modalVariants}
+                initial="close"
+                animate="open"
+                className="rounded-lg bg-gray-800 p-6"
               >
-                <X className="text-gray-500" weight="bold" />
-              </Dialog.Close>
-            </div>
-            <div className="flex justify-center">
-              <Lottie
-                animationData={animation}
-                style={{ width: 200 }}
-                loop={true}
-              />
-            </div>
+                <div className="flex items-start">
+                  <Dialog.Title className="font-semibold text-white text-center mx-auto">
+                    {title}
+                  </Dialog.Title>
+                  <Dialog.Close
+                    onClick={close}
+                    className="p-2 hover:bg-gray-700 transition-colors duration-200 rounded-full"
+                  >
+                    <X className="text-gray-500" weight="bold" />
+                  </Dialog.Close>
+                </div>
+                <div className="flex justify-center">
+                  <Lottie
+                    animationData={animation}
+                    style={{ width: 200 }}
+                    loop={true}
+                  />
+                </div>
 
-            {body}
-            {footer}
-          </Dialog.Content>
-        </Dialog.Overlay>
+                {body}
+                {footer}
+              </motion.div>
+            </Dialog.Content>
+          </Dialog.Overlay>
+        </Dialog.Portal>
       </Dialog.Root>
     )
   }
