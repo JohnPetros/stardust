6f978988fb1ace20fd436d5b582646fe2567160d
João Pedro <joaopedro.nc@outlook.com>
2023-07-28 13:25:16 -0300

    💄 style: Add User component styles

---
+++ src/app/(private)/(app)/(home)/profile/components/Status.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+interface StatusProps {
+  title: string
+  image: string
+  value: string
+}
+
+export function Status({ title, image, value }: StatusProps) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-3">
+      <dt className="text-green-500 text-lg">{title}</dt>
+      <div className="relative w-16 h-16">
+        <Image src={image} fill alt="" />
+      </div>
+      <dd className="text-gray-100">{value}</dd>
+    </div>
+  )
+}
