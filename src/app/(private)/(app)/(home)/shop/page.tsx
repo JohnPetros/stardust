41a8ab62e27a00b209b25236943de4bb32e7f493
João Pedro <joaopedro.nc@outlook.com>
2023-07-28 16:03:43 -0300

    📝 Correct tailnwind max-w class

--- src/app/(private)/(app)/(home)/shop/page.tsx
+++ src/app/(private)/(app)/(home)/shop/page.tsx
@@ -11,7 +11,7 @@ export default function Shop() {
   const { avatars, addUserAcquiredAvatar } = useAvatar()
 
   return (
-    <div className="px-6 max-w-[1024px] mx-auto">
+    <div className="px-6 max-w-5xl mx-auto">
       <>
         <h2 className="text-white font-semibold text-lg">Foguetes</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center gap-6 mt-3">
