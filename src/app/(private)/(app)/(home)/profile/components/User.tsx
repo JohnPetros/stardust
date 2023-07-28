73fa27106b1addc994e4886dcec034d47dfac423
João Pedro <joaopedro.nc@outlook.com>
2023-07-28 14:09:04 -0300

    📱 Adapt User component to desktop

--- src/app/(private)/(app)/(home)/profile/components/User.tsx
+++ src/app/(private)/(app)/(home)/profile/components/User.tsx
@@ -10,6 +10,8 @@ import { Status } from './Status'
 import { getImage } from '@/utils/functions'
 
 import { User as UserType } from '@/types/user'
+import { CalendarBlank, GearSix, Shield } from '@phosphor-icons/react'
+import Link from 'next/link'
 
 interface UserProps {
   data: UserType
@@ -30,20 +32,37 @@ export function User({
   const rocketImage = getImage('rockets', rocket.image)
 
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center gap-1">
+    <div className="flex flex-col md:flex-row md:justify-between md:gap-6 pb-6 border-b border-gray-300">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-3 md:gap-6">
         <UserAvatar avatarId={avatar_id} size={148} />
-        <strong className="text-green-500 text-xl text-center mt-3">
-          {name}
-        </strong>
-        <strong className="text-gray-100">
-          Nível {level} - {xp} xp
-        </strong>
-        <strong className="text-gray-100"></strong>
-        <p className="text-gray-300">Por aqui desde 16 de março de 2023</p>
+        <div className="flex flex-col md:items-start gap-2">
+          <strong className="text-green-500 text-lg text-center mt-3 truncate">
+            {name}
+          </strong>
+          <div className="flex items-center justify-center gap-2">
+            <Shield
+              className="text-green-500 text-lg hidden md:block"
+              weight="bold"
+            />
+            <strong className="text-gray-100 text-sm">
+              Nível {level} - {xp} xp
+            </strong>
+          </div>
+
+          <div className="flex items-center justify-center gap-2">
+            <CalendarBlank
+              className="text-green-500 text-lg hidden md:block"
+              weight="bold"
+            />
+            <p className="text-gray-300 text-sm">
+              Por aqui desde 16 de março de 2023
+            </p>
+          </div>
+        </div>
       </div>
 
-      <dl className="flex justify-between mt-6">
+      <dl className="flex md:flex-row md:gap-8 justify-between mt-6">
+        <span className="hidden md:block bg-gray-300 w-[1px] rounded-md"></span>
         <Status
           title="Ranking atual"
           image={rankingImage}
@@ -51,6 +70,12 @@ export function User({
         />
         <Status title="Ranking atual" image={rocketImage} value={rocket.name} />
       </dl>
+
+      <div className="w-6 flex justify-start  h-full">
+        <Link href="/settings">
+          <GearSix className="text-green-500 text-4xl hidden md:block" />
+        </Link>
+      </div>
     </div>
   )
 }
