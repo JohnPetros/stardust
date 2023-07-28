427f5c3e77eb01cf21fbb40ece01de0663fa2803
João Pedro <joaopedro.nc@outlook.com>
2023-07-28 16:05:16 -0300

    ✨ feat: Show user statistics and streak on profile page

--- src/app/(private)/(app)/(home)/profile/[userId]/page.tsx
+++ src/app/(private)/(app)/(home)/profile/[userId]/page.tsx
@@ -1,6 +1,7 @@
-import { NextPage } from 'next'
 import { api } from '@/services/api'
 import { User } from '../components/User'
+import { Statistics } from '../components/Statistics'
+import { Streak } from '../components/Streak'
 
 interface ProfileProps {
   params: { userId: string }
@@ -10,8 +11,12 @@ export default async function Profile({ params }: ProfileProps) {
   const user = await api.getUser(params.userId)
 
   return (
-    <div className="pt-8 max-w-sm md:max-w-[1024px] mx-auto">
+    <div className="pt-8 max-w-sm md:max-w-5xl mx-auto px-6">
       <User data={user} />
+      <div className='flex flex-col md:flex-row gap-6'>
+        <Statistics data={user} />
+        <Streak data={user} />
+      </div>
     </div>
   )
 }
