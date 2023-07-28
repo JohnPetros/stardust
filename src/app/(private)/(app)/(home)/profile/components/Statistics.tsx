839b05e539c6c55330f3243a63a60ba3a8728d5b
João Pedro <joaopedro.nc@outlook.com>
2023-07-28 16:31:45 -0300

    📱 Adapt Statistics component to desktop

--- src/app/(private)/(app)/(home)/profile/components/Statistics.tsx
+++ src/app/(private)/(app)/(home)/profile/components/Statistics.tsx
@@ -9,19 +9,19 @@ export function Statistics({
   data: { unlocked_stars, completed_planets, unlocked_achievements },
 }: StatisticsProps) {
   return (
-    <div className="flex gap-3 mt-6">
+    <div className="flex md:flex-col md:justify-between h-full gap-3 mt-6 md:py-6">
       <Statistic
         title="Estrelas completadas"
         image="/icons/coin.svg"
         value={unlocked_stars}
       />
-      <span className='bg-gray-300 w-[1px] rounded-md' />
+      <span className="bg-gray-300 w-[1px] md:h-[1px] md:w-full rounded-md" />
       <Statistic
         title="Planetas concluídos"
         image="/icons/planet.svg"
         value={completed_planets}
       />
-      <span className='bg-gray-300 w-[1px] rounded-md' />
+      <span className="bg-gray-300 w-[1px] md:h-[1px] md:w-full rounded-md" />
       <Statistic
         title="Conquistas adquiridas"
         image="/icons/flag.svg"
