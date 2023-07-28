2f73ecac9cf5ba9cdfb6176259f8645fbf8f924e
João Pedro <joaopedro.nc@outlook.com>
2023-07-28 14:08:50 -0300

    📱 Adapt status to desktop

--- src/app/(private)/(app)/(home)/profile/components/Status.tsx
+++ src/app/(private)/(app)/(home)/profile/components/Status.tsx
@@ -9,11 +9,11 @@ interface StatusProps {
 export function Status({ title, image, value }: StatusProps) {
   return (
     <div className="flex flex-col items-center justify-center gap-3">
-      <dt className="text-green-500 text-lg">{title}</dt>
+      <dt className="text-green-500 text-center">{title}</dt>
       <div className="relative w-16 h-16">
         <Image src={image} fill alt="" />
       </div>
-      <dd className="text-gray-100">{value}</dd>
+      <dd className="text-gray-100 text-sm text-center">{value}</dd>
     </div>
   )
 }
