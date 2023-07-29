fba32513ebfca8fc2c5d6f23505c5912d94b83c0
João Pedro <joaopedro.nc@outlook.com>
2023-07-29 09:30:16 -0300

    ➕  Add TabButton component props

--- src/app/(private)/(app)/(home)/profile/components/Tabs/TabButton.tsx
+++ src/app/(private)/(app)/(home)/profile/components/Tabs/TabButton.tsx
@@ -5,17 +5,20 @@ import { Icon } from '@phosphor-icons/react'
 interface TabButtonProps {
   title: string
   icon: Icon
+  value: string
   isActive: boolean
+  onClick: VoidFunction
 }
 
-export function TabButton({ title, icon: Icon, isActive }: TabButtonProps) {
+export function TabButton({ title, icon: Icon, value, isActive, onClick }: TabButtonProps) {
   return (
     <Tabs.Trigger
       className={twMerge(
-        'p-3 rounded-md flex items-center gap-1 text-gray-100 font-medium',
-        isActive ? 'bg-green-900 border border-green-600' : 'bg-transparent'
+        'p-2 rounded-md flex items-center gap-2 text-gray-100 font-medium border',
+        isActive ? 'bg-green-900 border-green-600' : 'bg-transparent border-transparent'
       )}
-      value="tab1"
+      value={value}
+      onClick={onClick}
     >
       <Icon className="text-lg text-green-500" />
       {title}
