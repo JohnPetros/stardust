fff2cbfdc1bf36548f3c6556fe653c26afc3fcbb
João Pedro <joaopedro.nc@outlook.com>
2023-07-29 09:29:40 -0300

    💄 style: Add SorterButton component styles

---
+++ src/app/(private)/(app)/(home)/profile/components/Tabs/SorterButton.tsx
@@ -0,0 +1,27 @@
+import { Icon } from '@phosphor-icons/react'
+import { twMerge } from 'tailwind-merge'
+
+interface SorterButtonProps {
+  title: string
+  isActive: boolean
+  icon: Icon
+}
+
+export function SorterButton({ title, isActive, icon: Icon }: SorterButtonProps) {
+  return (
+    <button
+      className={twMerge(
+        'text-sm p-2 hover:text-gray-100 flex items-center gap-1 group',
+        isActive ? 'text-gray-100' : 'text-gray-400'
+      )}
+    >
+      <Icon
+        className={twMerge(
+          'text-sm group-hover:text-gray-100',
+          isActive ? 'text-gray-100' : 'text-gray-400'
+        )}
+      />
+      {title}
+    </button>
+  )
+}
