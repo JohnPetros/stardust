d3afd586cafb813c5907446d7ffdfdfea4b77f09
João Pedro <joaopedro.nc@outlook.com>
2023-07-29 08:44:54 -0300

    ➕ Add ChallengesChart, Achievements and Tabs components to Profile page

--- src/app/(private)/(app)/(home)/profile/[userId]/page.tsx
+++ src/app/(private)/(app)/(home)/profile/[userId]/page.tsx
@@ -1,7 +1,10 @@
 import { api } from '@/services/api'
 import { User } from '../components/User'
 import { Statistics } from '../components/Statistics'
+import { Tabs } from '../components/Tabs'
 import { Streak } from '../components/Streak'
+import { ChallengesChart } from '../components/ChallengesChart'
+import { Achievements } from '../components/Achievements'
 
 interface ProfileProps {
   params: { userId: string }
@@ -13,10 +16,23 @@ export default async function Profile({ params }: ProfileProps) {
   return (
     <div className="pt-8 max-w-sm md:max-w-5xl mx-auto px-6">
       <User data={user} />
-      <div className='flex flex-col md:flex-row gap-6'>
+      <div className="grid grid-cols-1 md:grid-cols-[1fr_1.5fr] justify-center items-center md:flex-row gap-12 mt-10">
         <Statistics data={user} />
         <Streak data={user} />
       </div>
+      <div className="grid grid-cols-[1fr_1.5fr] gap-2 mt-10">
+        <div>
+          <h4 className="text-gray-100">Desafios concluídos</h4>
+          <ChallengesChart />
+        </div>
+        <div>
+          <h4 className="text-gray-100 text-center mb-3">
+            Conquistas adquiridas
+          </h4>
+          <Achievements userId={user.id} />
+        </div>
+      </div>
+      <Tabs />
     </div>
   )
 }
