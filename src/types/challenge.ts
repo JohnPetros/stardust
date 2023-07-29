cb2012c2728e8c4f4604a6e5ebd6ca418571707a
João Pedro <joaopedro.nc@outlook.com>
2023-07-29 19:31:57 -0300

    🏷️ Add Challenge type

---
+++ src/types/challenge.ts
@@ -0,0 +1,21 @@
+import type { Category } from "./category"
+
+type Difficulty = 'easy' | 'medium' | 'hard'
+
+export type Challenge = {
+  id: string
+  code: string
+  author: string
+  difficulty: Difficulty
+  created_at: string
+  function_name: string | null
+  upvotes: number
+  downvotes: number
+  texts: JSON
+  star_id: string | null
+  test_cases: JSON
+  title: string
+  topic_id: string
+  totalCompletitions: number
+  categories: Category[]
+}
