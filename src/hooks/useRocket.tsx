1e522041ccf0eb1212e0f01ced5cb7f1b5113893
João Pedro <joaopedro.nc@outlook.com>
2023-07-29 08:39:57 -0300

    🔧 Make useRocket as a client component

--- src/hooks/useRocket.tsx
+++ src/hooks/useRocket.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useAuth } from './useAuth'
 import useSWR, { mutate } from 'swr'
 
