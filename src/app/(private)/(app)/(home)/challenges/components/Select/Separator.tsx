e0f72b6b0701fa81109680d6e2912be86c9721f1
João Pedro <joaopedro.nc@outlook.com>
2023-07-30 14:21:30 -0300

    💄 style: Set fixed width to Separator component

--- src/app/(private)/(app)/(home)/challenges/components/Select/Separator.tsx
+++ src/app/(private)/(app)/(home)/challenges/components/Select/Separator.tsx
@@ -2,5 +2,7 @@
 import * as S from '@radix-ui/react-select'
 
 export function Separator() {
-  return <S.Separator className="rounded-md bg-green-600 h-[2px] w-[90%] mx-auto my-[2px]" />
+  return (
+    <S.Separator className="rounded-md bg-gray-500 h-[2px] w-[90%] mx-auto my-[2px]" />
+  )
 }
