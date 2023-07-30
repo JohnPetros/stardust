8549d8a9a7858a4c3fddd5ae3f04bcca2bef15d6
João Pedro <joaopedro.nc@outlook.com>
2023-07-30 14:12:48 -0300

    💄 style: Add select Trigger styles

---
+++ src/app/(private)/(app)/(home)/challenges/components/Select/Trigger.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { CaretDown } from '@phosphor-icons/react'
+import * as S from '@radix-ui/react-select'
+
+interface SelectTriggerProps {
+  value: string
+}
+
+export function Trigger({ value }: SelectTriggerProps) {
+  return (
+    <S.Trigger className="flex items-center gap-2 rounded-md overflow-hidden text-green-500 p-2 bg-gray-800 border border-gray-400">
+      <S.Value className="text-green-500" aria-value={value}>
+        {value}
+      </S.Value>
+      <S.Icon className="SelectIcon">
+        <CaretDown className="text-gray-400" />
+      </S.Icon>
+    </S.Trigger>
+  )
+}
