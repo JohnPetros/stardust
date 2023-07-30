581282619d8779aa14838fc4bb5d81ac93f421c5
João Pedro <joaopedro.nc@outlook.com>
2023-07-30 14:22:08 -0300

    💄 style: Add textTyle prop to Select Item

--- src/app/(private)/(app)/(home)/challenges/components/Select/Item.tsx
+++ src/app/(private)/(app)/(home)/challenges/components/Select/Item.tsx
@@ -1,18 +1,23 @@
 'use client'
 import { ReactNode } from 'react'
 import * as S from '@radix-ui/react-select'
+import { twMerge } from 'tailwind-merge'
 
 interface SelectItemProps {
   text: ReactNode
+  textStye?: string
   value: string
   icon?: ReactNode
 }
 
-export function Item({ text, icon, value }: SelectItemProps) {
+export function Item({ text, textStye, icon, value }: SelectItemProps) {
   return (
     <S.SelectItem
       value={value}
-      className="flex items-center gap-2 p-3 cursor-pointer text-gray-400 font-medium outline-gray-400 focus:text-gray-100 hover:text-gray-100"
+      className={twMerge(
+        'flex items-center gap-2 px-6 py-3 cursor-pointer text-gray-400 font-medium outline-gray-400 focus:text-gray-100 hover:text-gray-100',
+        textStye
+      )}
     >
       {icon}
       <S.SelectItemText>{text}</S.SelectItemText>
