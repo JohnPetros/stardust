19d1869d3f2322938ff79b6d9ecedb7f589f42c7
João Pedro <joaopedro.nc@outlook.com>
2023-07-30 14:09:29 -0300

    🚚 Remove Select Component

---
+++ src/app/(private)/(app)/(home)/challenges/components/Select/Container.tsx
@@ -0,0 +1,15 @@
+import { ReactNode, useState } from 'react'
+import * as S from '@radix-ui/react-select'
+
+interface SelectProps {
+  children: ReactNode
+  onValueChange: (value: string) => void
+}
+
+export function Container({ children, onValueChange }: SelectProps) {
+  return (
+    <S.Root onValueChange={(value: string) => onValueChange(value)}>
+      {children}
+    </S.Root>
+  )
+}
--- src/app/components/Select/Separator.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import * as S from '@radix-ui/react-select'
-
-export function Separator() {
-  return <S.Separator className="rounded-md bg-green-600 h-[2px] w-[90%] mx-auto my-[2px]" />
-}
