e9ade7cf525354d366ae2caa6cddcd383a6bd91c
João Pedro <joaopedro.nc@outlook.com>
2023-07-30 14:12:09 -0300

    💄 style: Add Select Separator styles

---
+++ src/app/(private)/(app)/(home)/challenges/components/Select/Separator.tsx
@@ -0,0 +1,6 @@
+'use client'
+import * as S from '@radix-ui/react-select'
+
+export function Separator() {
+  return <S.Separator className="rounded-md bg-green-600 h-[2px] w-[90%] mx-auto my-[2px]" />
+}
