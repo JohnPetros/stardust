6db0f4f4172e5011732659d5ccd5e41359f214dc
João Pedro <joaopedro.nc@outlook.com>
2023-07-31 17:25:06 -0300

    ♿ Add Loading to Challenges page

--- src/app/(private)/(app)/(home)/challenges/page.tsx
+++ src/app/(private)/(app)/(home)/challenges/page.tsx
@@ -3,6 +3,7 @@ import { useChallengesList } from '@/hooks/useChallengesList'
 
 import { Challenge } from './components/Challenge'
 import { Filters } from './components/Filters'
+import { Loading } from '@/app/components/Loading'
 
 export default function Challenges() {
   const { challenges } = useChallengesList()
@@ -12,17 +13,20 @@ export default function Challenges() {
   return (
     <div className="mx-auto max-w-2xl mt-10 pb-10">
       <Filters />
-
-      {challenges && (
-        <div className="space-y-6 mt-10">
-          {challenges.map((challenge) => (
-            <Challenge
-              key={challenge.id}
-              data={challenge}
-              isCompleted={false}
-            />
-          ))}
-        </div>
+      {challenges ? (
+        <>
+          <div className="space-y-6 mt-8">
+            {challenges.map((challenge) => (
+              <Challenge
+                key={challenge.id}
+                data={challenge}
+                isCompleted={false}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <Loading isSmall={true} />
       )}
     </div>
   )
