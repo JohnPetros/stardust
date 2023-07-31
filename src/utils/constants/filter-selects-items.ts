c4b84fab0fa2179c3ef0c60e359c83153b021780
João Pedro <joaopedro.nc@outlook.com>
2023-07-31 17:09:43 -0300

    🚚 Add FILTER_SELECTS_ITEMS constant

---
+++ src/utils/constants/filter-selects-items.ts
@@ -0,0 +1,48 @@
+'use client'
+import { Status, Difficulty } from '@/contexts/ChallengesListContext'
+import { CheckCircle, Circle, Icon, Minus } from '@phosphor-icons/react'
+
+type SelectsItem = {
+  value: Status | Difficulty
+  text: string
+  textStyles?: string
+  icon?: Icon
+  iconStyles?: string
+}
+
+export const FILTER_SELECTS_ITEMS: SelectsItem[] = [
+  {
+    value: 'all',
+    text: 'Todos',
+    icon: Minus,
+    iconStyles: 'text-gray-500 text-lg',
+  },
+  {
+    value: 'completed',
+    text: 'Resolvido',
+    icon: CheckCircle,
+    iconStyles: 'text-green-500 text-lg',
+  },
+  {
+    value: 'not-completed',
+    text: 'Não Resolvido',
+    icon: Circle,
+    iconStyles: 'text-red-700 text-lg',
+  },
+  {
+    value: 'easy',
+    text: 'Fácil',
+    textStyles: 'text-green-400',
+    icon: Circle,
+  },
+  {
+    value: 'medium',
+    text: 'Médio',
+    textStyles: 'text-yellow-400',
+  },
+  {
+    value: 'hard',
+    text: 'Difícil',
+    textStyles: 'text-red-700',
+  },
+]
