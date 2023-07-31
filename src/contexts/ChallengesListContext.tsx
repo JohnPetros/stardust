422ab57f08e9e8d87e176274d443f3d7ee3b785a
João Pedro <joaopedro.nc@outlook.com>
2023-07-31 14:26:21 -0300

    ➕ Add ChallengesListReducer hook

---
+++ src/contexts/ChallengesListContext.tsx
@@ -0,0 +1,60 @@
+import { ReactNode, createContext, useReducer } from 'react'
+
+type Status = 'all' | 'completed' | 'not-completed'
+type Difficulty = 'all' | 'easy' | 'medium' | 'hard'
+
+type ChallengesListAction =
+  | { type: 'setStatus'; payload: Status }
+  | { type: 'setDifficulty'; payload: Difficulty }
+
+type ChallengesListState = {
+  status: Status
+  difficulty: Difficulty
+}
+
+interface ChallengesListProviderProps {
+  children: ReactNode
+}
+
+interface ChallengesListContextValue {
+  state: ChallengesListState
+  dispatch: (action: ChallengesListAction) => void
+}
+
+const initialChallengesListState: ChallengesListState = {
+  status: 'all',
+  difficulty: 'all',
+}
+
+function ChallengesListReducer(
+  state: ChallengesListState,
+  action: ChallengesListAction
+) {
+  switch (action.type) {
+    case 'setStatus':
+      return { ...state, status: action.payload }
+    case 'setDifficulty':
+      return { ...state, difficulty: action.payload }
+    default:
+      return initialChallengesListState
+  }
+}
+
+export const ChallengesListContext = createContext(
+  {} as ChallengesListContextValue
+)
+
+export function ChallengesListProvider({
+  children,
+}: ChallengesListProviderProps) {
+  const [state, dispatch] = useReducer(
+    ChallengesListReducer,
+    initialChallengesListState
+  )
+
+  return (
+    <ChallengesListContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ChallengesListContext.Provider>
+  )
+}
