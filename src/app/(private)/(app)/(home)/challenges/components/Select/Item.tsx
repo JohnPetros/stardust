689ac48fe4a2773fe606e56ac03f8c0388397565
João Pedro <joaopedro.nc@outlook.com>
2023-07-31 17:10:24 -0300

    💄 style: Add textStye and iconStyle props to Select Item component

--- src/app/(private)/(app)/(home)/challenges/components/Select/Item.tsx
+++ src/app/(private)/(app)/(home)/challenges/components/Select/Item.tsx
@@ -2,24 +2,32 @@
 import { ReactNode } from 'react'
 import * as S from '@radix-ui/react-select'
 import { twMerge } from 'tailwind-merge'
+import { Icon } from '@phosphor-icons/react'
 
 interface SelectItemProps {
   text: ReactNode
   textStye?: string
   value: string
-  icon?: ReactNode
+  icon?: Icon
+  iconStyles?: string
 }
 
-export function Item({ text, textStye, icon, value }: SelectItemProps) {
+export function Item({
+  text,
+  textStye,
+  icon: Icon,
+  iconStyles,
+  value,
+}: SelectItemProps) {
   return (
     <S.SelectItem
       value={value}
       className={twMerge(
-        'flex items-center gap-2 px-6 py-3 cursor-pointer text-gray-400 font-medium outline-gray-400 focus:text-gray-100 hover:text-gray-100',
+        'flex items-center gap-2 px-4 py-2 cursor-pointer text-gray-400 font-medium outline-gray-400 focus:text-gray-100 hover:text-gray-100 text-sm',
         textStye
       )}
     >
-      {icon}
+      {Icon && <Icon className={iconStyles} />}
       <S.SelectItemText>{text}</S.SelectItemText>
     </S.SelectItem>
   )
