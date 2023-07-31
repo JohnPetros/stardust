49291733ccff5d67160d2259c9fffb7d9fa79427
João Pedro <joaopedro.nc@outlook.com>
2023-07-31 17:10:51 -0300

    ✨ feat: Add tags to challenges status filtering

--- src/app/(private)/(app)/(home)/challenges/components/Filters/index.tsx
+++ src/app/(private)/(app)/(home)/challenges/components/Filters/index.tsx
@@ -1,23 +1,77 @@
 'use client'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useChallengesList } from '@/hooks/useChallengesList'
-import { CheckCircle, Circle, Minus } from '@phosphor-icons/react'
+
 import { Select } from '../Select'
+import { Tag } from './Tag'
+import { CheckCircle, Circle, Icon, Minus } from '@phosphor-icons/react'
+
 import type { Difficulty, Status } from '@/contexts/ChallengesListContext'
+import { FILTER_SELECTS_ITEMS } from '@/utils/constants/filter-selects-items'
 
 export function Filters() {
   const { state, dispatch } = useChallengesList()
+  const [tags, setTags] = useState<string[]>([])
+  const statusTag = useRef<string | null>(null)
+  const difficultyTag = useRef<string | null>(null)
+  console.log(tags)
+
+  function setStatus(status: Status) {
+    dispatch({ type: 'setStatus', payload: status })
+  }
+
+  function setDifficulty(difficulty: Difficulty) {
+    dispatch({ type: 'setDifficulty', payload: difficulty })
+  }
+
+  function addTag(newTag: string) {
+    setTags((currentTags) => [...currentTags, newTag])
+  }
+
+  function removeTag(removedTag: string) {
+    setTags((currentTags) => currentTags.filter((tag) => tag !== removedTag))
+  }
+
+  function getTag(value: Status | Difficulty) {
+    return FILTER_SELECTS_ITEMS.find((item) => item.value === value)?.text
+  }
+
+  function handleTagClick(tagText: string, tagValue: string) {
+    removeTag(tagText)
+
+    if (['completed', 'not-completed'].includes(tagValue)) {
+      statusTag.current = 'all'
+      return
+    }
+
+    if (['easy', 'medium', 'hard'].includes(tagValue)) {
+      difficultyTag.current = 'all'
+      return
+    }
+  }
 
   function handleStatusChange(newStatus: Status) {
-    dispatch({ type: 'setStatus', payload: newStatus })
+    const tag = getTag(newStatus)
+    if (!tag) return
+
+    if (statusTag.current) {
+      removeTag(statusTag.current)
+    }
+
+    setStatus(newStatus)
+
+    if (tag !== 'todos') {
+      addTag(tag)
+      statusTag.current = tag
+    }
   }
 
   function handleDifficultyChange(newDifficulty: Difficulty) {
-    dispatch({ type: 'setDifficulty', payload: newDifficulty })
+    setDifficulty(newDifficulty)
   }
 
   return (
-    <div>
+    <div className="flex flex-col">
       <div className="flex items-center gap-6">
         <Select.Container
           onValueChange={(newStatus: string) =>
@@ -26,25 +80,20 @@ export function Filters() {
         >
           <Select.Trigger value="Status" />
           <Select.Content>
-            <Select.Item
-              value="all"
-              icon={<Minus className="text-gray-500 text-lg" weight="bold" />}
-              text="Todos"
-            />
-            <Select.Separator />
-            <Select.Item
-              value="completed"
-              icon={
-                <CheckCircle className="text-green-500 text-lg" weight="bold" />
-              }
-              text="Resolvido"
-            />
-            <Select.Separator />
-            <Select.Item
-              value="not-completed"
-              icon={<Circle className="text-red-700 text-lg" weight="bold" />}
-              text="Não Resolvido"
-            />
+            {FILTER_SELECTS_ITEMS.slice(0, 3).map((item, index, allItems) => {
+              const isLastItem = index === allItems.length - 1
+              return (
+                <>
+                  <Select.Item
+                    value={item.value}
+                    icon={item.icon}
+                    text={item.text}
+                    iconStyles={item.iconStyles}
+                  />
+                  {!isLastItem && <Select.Separator />}
+                </>
+              )
+            })}
           </Select.Content>
         </Select.Container>
 
@@ -68,7 +117,23 @@ export function Filters() {
         </Select.Container>
       </div>
 
-      
+      <div className="flex flex-wrap mt-6">
+        {tags.map((tag) => {
+          const item = FILTER_SELECTS_ITEMS.find((item) => item.text === tag)
+          return (
+            <Tag
+              key={tag}
+              name={tag}
+              nameStyles={item?.textStyles ?? null}
+              icon={item?.icon ?? null}
+              iconStyles={item?.iconStyles ?? null}
+              onClick={() =>
+                item?.value ? handleTagClick(tag, item.value) : null
+              }
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
