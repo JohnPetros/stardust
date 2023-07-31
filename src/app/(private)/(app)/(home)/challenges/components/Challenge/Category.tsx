cdc9f0c3927d16c7699c52fdc2ee360f5846974e
João Pedro <joaopedro.nc@outlook.com>
2023-07-31 20:10:27 -0300

    💄 style: Add Category component styles

--- src/app/(private)/(app)/(home)/challenges/components/Challenge/Category.tsx
+++ src/app/(private)/(app)/(home)/challenges/components/Challenge/Category.tsx
@@ -1,9 +1,11 @@
-import { string } from 'zod'
-
 interface CategoryProps {
   name: string
 }
 
 export function Category({ name }: CategoryProps) {
-  return <li className="rounded-md p-1 bg-gray-400 text-gray-600 text-sm">{name}</li>
+  return (
+    <li className="rounded-md p-1 bg-gray-400 text-gray-900 font-semibold text-xs">
+      {name}
+    </li>
+  )
 }
