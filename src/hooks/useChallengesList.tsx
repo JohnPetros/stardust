3401f1824da3972ead5b1ad65ea1dd3ef02c485b
João Pedro <joaopedro.nc@outlook.com>
2023-07-31 14:25:56 -0300

    ➕ Add useChallengesList hook

---
+++ src/hooks/useChallengesList.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useContext } from 'react'
+import { useAuth } from './useAuth'
+import useSWR from 'swr'
+
+import { ChallengesListContext } from '@/contexts/ChallengesListContext'
+import { api } from '@/services/api'
+import type { Challenge } from '@/types/challenge'
+
+export const useChallengesList = () => {
+  const { user } = useAuth()
+  const { state, dispatch } = useContext(ChallengesListContext)
+
+  if (!state) {
+    throw new Error(
+      'useChallengesList must be used inside ChallengesListProvider'
+    )
+  }
+
+  function sortChallengesByDifficulty(challenges: Challenge[]) {
+    const easyChallenges = challenges.filter(
+      (challenge) => challenge.difficulty === 'easy'
+    )
+    const mediumChallenges = challenges.filter(
+      (challenge) => challenge.difficulty === 'medium'
+    )
+    const hardChallenges = challenges.filter(
+      (challenge) => challenge.difficulty === 'hard'
+    )
+
+    return easyChallenges.concat(mediumChallenges, hardChallenges)
+  }
+
+  async function getChallenges() {
+    if (user) {
+      console.log(state.difficulty)
+
+      const challenges = await api.getFilteredChallenges({
+        userId: user?.id,
+        status: state.status,
+        difficulty: state.difficulty,
+        categoriesIds: [],
+        range: 0,
+      })
+
+      return sortChallengesByDifficulty(challenges)
+    }
+  }
+
+  const { data: challenges, error } = useSWR('/challenges', getChallenges)
+
+  console.log(challenges)
+
+  return {
+    state,
+    dispatch,
+    challenges,
+  }
+}
