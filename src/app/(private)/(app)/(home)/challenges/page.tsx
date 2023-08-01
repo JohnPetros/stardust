33492e7d8cccf5bfb3221c52b82aacefa6715fed
João Pedro <joaopedro.nc@outlook.com>
2023-08-01 14:37:52 -0300

    💄 Add initial loading to Challenges page

--- src/app/(private)/(app)/(home)/challenges/page.tsx
+++ src/app/(private)/(app)/(home)/challenges/page.tsx
@@ -4,25 +4,30 @@ import { useChallengesList } from '@/hooks/useChallengesList'
 import { Challenge } from './components/Challenge'
 import { Filters } from './components/Filters'
 import { Loading } from '@/app/components/Loading'
+import { useEffect, useState } from 'react'
 
 export default function Challenges() {
-  const { challenges, categories } = useChallengesList()
+  const { challenges, categories, isLoading } = useChallengesList()
+  const [isFirstRendering, setIsFirstRendering] = useState(
+    challenges.length === 0
+  )
 
-  console.log(challenges)
+  useEffect(() => {
+    setTimeout(() => {
+      setIsFirstRendering(false)
+    }, 1500)
+  }, [])
 
   return (
     <div className="mx-auto max-w-2xl mt-10 pb-10">
+      {isFirstRendering && <Loading isSmall={false} />}
       <Filters categories={categories} />
-      
-      {challenges ? (
+
+      {!isLoading ? (
         <>
           <div className="space-y-6">
             {challenges.map((challenge) => (
-              <Challenge
-                key={challenge.id}
-                data={challenge}
-                isCompleted={false}
-              />
+              <Challenge key={challenge.id} data={challenge} />
             ))}
           </div>
         </>
