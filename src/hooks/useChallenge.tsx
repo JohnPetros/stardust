ed2968b986cb9113a78e39e40429f3c7e62a4cfd
João Pedro <joaopedro.nc@outlook.com>
2023-08-01 18:21:29 -0300

    ➕ Add useChallenge hook

---
+++ src/hooks/useChallenge.tsx
@@ -0,0 +1,43 @@
+import { useMemo } from 'react'
+import useSWR from 'swr'
+
+import { api } from '@/services/api'
+import type { Challenge } from '@/types/challenge'
+
+export function useChallenge(
+  challengeId?: string | null,
+  userId?: string | null
+) {
+  function getChallenges() {
+    if (userId) {
+      return api.getChallenges(userId)
+    }
+  }
+
+  const { data: challenges, error } = useSWR(
+    userId ? '/challenges?user_id=' + userId : null,
+    getChallenges
+  )
+
+  function checkCompletition(challenge: Challenge) {
+    const isCompleted = challenge.users_completed_challenges.length > 0
+
+    return { ...challenge, isCompleted }
+  }
+
+  if (error) {
+    throw new Error(error)
+  }
+
+  const verifiedChallenges: Challenge[] = useMemo(() => {
+    if (challenges?.length) {
+      return challenges.map(checkCompletition)
+    }
+    return []
+  }, [challenges])
+
+
+  return {
+    challenges: verifiedChallenges,
+  }
+}
