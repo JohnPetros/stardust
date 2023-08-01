a2ec4a0d8cf2ef24d668d1b6fe58d690fde0ffd4
João Pedro <joaopedro.nc@outlook.com>
2023-08-01 15:26:51 -0300

    ➕ Add className prop to Search component

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,13 +1,19 @@
 import { MagnifyingGlass } from '@phosphor-icons/react'
 import { InputHTMLAttributes } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-interface SearchProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface SearchProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string
+}
 
-export function Search({ ...rest }: SearchProps) {
+export function Search({ className, ...rest }: SearchProps) {
   return (
     <label
       htmlFor="search"
-      className="flex items-center gap-1 p-2 w-full bg-gray-700 border-2 border-gray-400 rounded-md brightness-95 focus-within:brightness-125"
+      className={twMerge(
+        'flex items-center gap-1 p-2 w-full bg-gray-700 border-2 border-gray-400 rounded-md brightness-95 focus-within:brightness-125',
+        className
+      )}
     >
       <MagnifyingGlass className="text-gray-400" weight="bold" />
       <input
