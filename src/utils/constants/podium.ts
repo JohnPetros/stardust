d39257a75e8b611e8a37f1c88cf2aa0d358270ab
João Pedro <joaopedro.nc@outlook.com>
2023-08-02 14:10:55 -0300

    🚚 Add PODIUM constant

---
+++ src/utils/constants/podium.ts
@@ -0,0 +1,19 @@
+type Podium = {
+  position: number
+  icon: string
+}
+
+export const PODIUM: Podium[] = [
+  {
+    position: 1,
+    icon: 'first-place.svg',
+  },
+  {
+    position: 2,
+    icon: 'second-place.svg',
+  },
+  {
+    position: 3,
+    icon: 'third-place.svg',
+  },
+]
