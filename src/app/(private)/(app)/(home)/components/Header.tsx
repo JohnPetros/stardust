f77165ee19dcdd1edd1f71e78191c478ea1509c1
João Pedro <joaopedro.nc@outlook.com>
2023-08-01 21:02:49 -0300

    💄 style: Render Header if auth user exists

--- src/app/(private)/(app)/(home)/components/Header.tsx
+++ src/app/(private)/(app)/(home)/components/Header.tsx
@@ -27,55 +27,61 @@ export function Header() {
   const { user } = useAuth()
   const { toggle } = useSiderbar()
 
-  return (
-    <motion.header
-      variants={headerVariants}
-      initial="hidden"
-      animate="visible"
-      className="fixed top-0 w-screen z-40 bg-gray-900 h-16 px-6 py-3 flex justify-between md:justify-end border-b border-gray-700"
-    >
-      <div className="flex items-center gap-3 md:hidden">
-        <button onClick={toggle}>
-          <List
-            width={24}
-            height={24}
-            className="text-green-400"
-            weight="bold"
-          />
-        </button>
-        <Image src="/images/logo.svg" width={100} height={100} alt="StarDust" />
-      </div>
-
-      <div className="flex items-center gap-2">
-        <div className="flex items-center gap-2">
+  if (user?.id)
+    return (
+      <motion.header
+        variants={headerVariants}
+        initial="hidden"
+        animate="visible"
+        className="fixed top-0 w-screen z-40 bg-gray-900 h-16 px-6 py-3 flex justify-between md:justify-end border-b border-gray-700"
+      >
+        <div className="flex items-center gap-3 md:hidden">
+          <button onClick={toggle}>
+            <List
+              width={24}
+              height={24}
+              className="text-green-400"
+              weight="bold"
+            />
+          </button>
           <Image
-            src="/icons/coin.svg"
-            width={28}
-            height={28}
-            alt="Star Coins"
+            src="/images/logo.svg"
+            width={100}
+            height={100}
+            alt="StarDust"
           />
-          <span className="text-yellow-400 font-semibold text-lg">
-            {user?.coins}
-          </span>
         </div>
 
-        <div className="flex items-center">
-          <Animation src={StreakAnimation} size={32} hasLoop={false} />
-          <span className="text-green-500 font-semibold text-lg">
-            {user?.streak}
-          </span>
-        </div>
-      </div>
-
-      {user && (
-        <div className="hidden md:flex items-center gap-6 ml-6">
-          <div className="flex flex-col items-center text-sm text-gray-100">
-            <strong>{user?.name}</strong>
-            <small>{user?.email}</small>
+        <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2">
+            <Image
+              src="/icons/coin.svg"
+              width={28}
+              height={28}
+              alt="Star Coins"
+            />
+            <span className="text-yellow-400 font-semibold text-lg">
+              {user?.coins}
+            </span>
           </div>
-          <UserAvatar avatarId={user?.avatar_id} size={50} />
+
+          <div className="flex items-center">
+            <Animation src={StreakAnimation} size={32} hasLoop={false} />
+            <span className="text-green-500 font-semibold text-lg">
+              {user?.streak}
+            </span>
+          </div>
         </div>
-      )}
-    </motion.header>
-  )
+
+        {user && (
+          <div className="hidden md:flex items-center gap-6 ml-6">
+            <div className="flex flex-col items-center text-sm text-gray-100">
+              <strong>{user?.name}</strong>
+              <small>{user?.email}</small>
+            </div>
+            <UserAvatar avatarId={user?.avatar_id} size={50} />
+          </div>
+        )}
+      </motion.header>
+    )
 }
