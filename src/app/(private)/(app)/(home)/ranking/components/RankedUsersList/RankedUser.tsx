4093edb27d95333f0f3781628a96037196db3f8e
João Pedro <joaopedro.nc@outlook.com>
2023-08-02 17:57:03 -0300

    ✨ feat: Show ranked user data  on click

--- src/app/(private)/(app)/(home)/ranking/components/RankedUsersList/RankedUser.tsx
+++ src/app/(private)/(app)/(home)/ranking/components/RankedUsersList/RankedUser.tsx
@@ -6,6 +6,7 @@ import { PODIUM } from '@/utils/constants'
 import { UserAvatar } from '../../../components/UserAvatar'
 import { tv } from 'tailwind-variants'
 import { twMerge } from 'tailwind-merge'
+import Link from 'next/link'
 
 const positionStyles = tv({
   base: 'font-semibold p-2 w-12 grid place-content-center',
@@ -26,14 +27,14 @@ interface UserProps {
 }
 
 export function RankedUser({
-  data: { name, avatar_id, weekly_xp },
+  data: { id, name, avatar_id, weekly_xp },
   position,
   lastPositionsOffset,
+  isAuthUser,
 }: UserProps) {
   const isInPodium = position <= 3
   const isInSafeArea = position <= 5
   const isInDangerArea = position > lastPositionsOffset
-  const isAuthUser = false
 
   const icon =
     isInPodium && PODIUM.find((place) => place.position === position)?.icon
@@ -41,13 +42,14 @@ export function RankedUser({
   const color = isInSafeArea ? 'safe' : isInDangerArea ? 'danger' : 'neutral'
 
   return (
-    <div
+    <Link
+      href={`/profile/${id}`}
       className={twMerge(
-        'border-2 flex items-center justify-between rounded-md p-3 w-full',
+        'border-2 flex items-center justify-between rounded-md p-3 w-full group hover:border-gray-100/90',
         isAuthUser ? 'border-green-700' : 'border-gray-800'
       )}
     >
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 text-lg mr-2">
         <span className={positionStyles({ color })}>
           {isInPodium ? (
             <Image src={`/icons/${icon}`} width={32} height={32} alt="" />
@@ -58,9 +60,18 @@ export function RankedUser({
         <UserAvatar avatarId={avatar_id} size={48} />
       </div>
 
-      <strong className="text-gray-500">{name}</strong>
+      <strong
+        className={twMerge(
+          'text-gray-500 group-hover:text-gray-100 block truncate',
+          isAuthUser ? 'text-gray-100' : 'text-gray-500'
+        )}
+      >
+        {name}
+      </strong>
 
-      <strong className="text-gray-600 uppercase">{weekly_xp} xp</strong>
-    </div>
+      <strong className="text-gray-600 uppercase block w-12">
+        {weekly_xp} xp
+      </strong>
+    </Link>
   )
 }
