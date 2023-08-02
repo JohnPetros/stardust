5f797c25edabb51f45c17b8206d3dbfcbca772bf
João Pedro <joaopedro.nc@outlook.com>
2023-08-01 21:56:13 -0300

    🏷️ Add Folder type to getImage util function

--- src/utils/functions/getImage.ts
+++ src/utils/functions/getImage.ts
@@ -1,3 +1,5 @@
-export function getImage(folder: string, resource: string) {
+type Folder = 'avatars' | 'rockets' | 'rankings' | 'planets'
+
+export function getImage(folder: Folder, resource: string) {
   return `${process.env.NEXT_PUBLIC_CDN_URL}/${folder}/${resource}`
 }
