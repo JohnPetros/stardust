dbef50fa7de8f91c2ffa91019ad091d5efba475d
João Pedro <joaopedro.nc@outlook.com>
2023-08-01 21:04:50 -0300

    ♻️ refactor: Transform Profile page as client component

--- src/app/(private)/(app)/(home)/profile/[userId]/page.tsx
+++ src/app/(private)/(app)/(home)/profile/[userId]/page.tsx
@@ -1,46 +1,60 @@
-import { api } from '@/services/api'
+'use client'
+import { useParams } from 'next/navigation'
+import { useUser } from '@/hooks/useUser'
+
+import { Loading } from '@/app/components/Loading'
 import { User } from '../components/User'
 import { Statistics } from '../components/Statistics'
 import { Tabs } from '../components/Tabs'
 import { Streak } from '../components/Streak'
 import { ChallengesChart } from '../components/ChallengesChart'
 import { Achievements } from '../components/Achievements'
-import { Loading } from '@/app/components/Loading'
+import { useEffect, useState } from 'react'
 
-interface ProfileProps {
-  params: { userId: string }
-}
+export default function Profile() {
+  const params = useParams()
+  const { user } = useUser(String(params.userId))
+  const [isFistRendering, setIsFistRendering] = useState(true)
 
-export default async function Profile({ params }: ProfileProps) {
-  const user = await api.getUser(params.userId)
+  useEffect(() => {
+    if (user && isFistRendering) {
+      setTimeout(() => {
+        setIsFistRendering(false)
+      }, 1500)
+    }
+  }, [user])
 
-  if (user.id)
+  if (user?.id)
     return (
       <div className="pt-8 max-w-sm md:max-w-5xl mx-auto px-6">
-        <User data={user} />
-        <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] justify-center items-center md:flex-row gap-12 mt-10">
-          <Statistics data={user} />
-          <Streak data={user} />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-[1fr_1.5fr] gap-2 mt-10">
+        {isFistRendering && <Loading isSmall={false} />}
+        {user?.id && (
           <div>
-            <h4 className="text-gray-100">Desafios concluídos</h4>
-            <ChallengesChart userId={user.id} />
-          </div>
-          <div>
-            <h4 className="text-gray-100 text-center mb-3">
-              Conquistas adquiridas
-            </h4>
-            <div>
-              <Achievements userId={user.id} />
+            <User data={user} />
+            <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] justify-center items-center md:flex-row gap-12 mt-10">
+              <Statistics data={user} />
+              <Streak data={user} />
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-[1fr_1.5fr] gap-2 mt-10">
+              <div>
+                <h4 className="text-gray-100">Desafios concluídos</h4>
+                <ChallengesChart userId={user.id} />
+              </div>
+              <div>
+                <h4 className="text-gray-100 text-center mb-3">
+                  Conquistas adquiridas
+                </h4>
+                <div>
+                  <Achievements userId={user.id} />
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <Tabs />
             </div>
           </div>
-        </div>
-
-        <div className="mt-10">
-          <Tabs />
-        </div>
+        )}
       </div>
     )
-  else return <Loading isSmall={false} />
 }
--- src/app/loading-test.tsx
+++ src/app/loading-test.tsx
@@ -7,6 +7,7 @@ export default function Loading() {
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-900 z-50 flex items-center justify-center">
       <Animation src={Transition} size={540} />
+      <p>Loading</p>
     </div>
   )
 }
