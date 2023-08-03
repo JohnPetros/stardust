5e47822f0b9ce2f106c169e8a020cb1bd5a0dc25
João Pedro <joaopedro.nc@outlook.com>
2023-08-03 14:20:22 -0300

    🏷️ Add bgColor and order props to Podium type

--- src/utils/constants/podium.ts
+++ src/utils/constants/podium.ts
@@ -1,19 +1,27 @@
-type Podium = {
+export type Podium = {
   position: number
   icon: string
+  bgColor: string
+  order: '1st' | '2nd' | '3rd'
 }
 
 export const PODIUM: Podium[] = [
   {
     position: 1,
     icon: 'first-place.svg',
+    bgColor: 'bg-yellow-400',
+    order: '1st',
   },
   {
     position: 2,
     icon: 'second-place.svg',
+    bgColor: 'bg-gray-400',
+    order: '2nd',
   },
   {
     position: 3,
     icon: 'third-place.svg',
+    bgColor: 'bg-yellow-800',
+    order: '3rd',
   },
 ]
