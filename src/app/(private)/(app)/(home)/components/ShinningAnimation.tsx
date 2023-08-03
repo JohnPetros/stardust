beb86eb263724d63dd17d0f97f9ed0bbad790355
João Pedro <joaopedro.nc@outlook.com>
2023-08-03 20:11:45 -0300

    💄 Add ShinningAnimation component styles

---
+++ src/app/(private)/(app)/(home)/components/ShinningAnimation.tsx
@@ -0,0 +1,17 @@
+'use client'
+import Lottie from 'lottie-react'
+import RewardShinning from '../../../../../../public/animations/reward-shinning.json'
+
+interface ShinningAnimationProps {
+  size: number
+}
+
+export function ShinningAnimation({ size }: ShinningAnimationProps) {
+  return (
+    <Lottie
+      animationData={RewardShinning}
+      loop={true}
+      style={{ width: size, position: 'absolute', zIndex: 0 }}
+    />
+  )
+}
