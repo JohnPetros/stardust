4eae19b0631093d22d49c116b5e9e39fc8c68fa2
João Pedro <joaopedro.nc@outlook.com>
2023-08-03 17:24:28 -0300

    💄 style: Adjust BadgesList width

--- src/app/(private)/(app)/(home)/ranking/components/BadgesList/index.tsx
+++ src/app/(private)/(app)/(home)/ranking/components/BadgesList/index.tsx
@@ -16,7 +16,7 @@ export function BadgesList({ rankings, currentRanking }: BadgesListProps) {
     <div
       ref={badgesListRef}
       style={{ backgroundImage: 'url("/images/space-background.png")' }}
-      className="relative grid grid-cols-[repeat(6,140px)] md:gap-2 items-center md:justify-center rounded-md overflow-x-scroll custom-scrollbar p-4 z-50"
+      className="relative grid grid-cols-[repeat(6,140px)] md:gap-2 items-center md:justify-center rounded-md overflow-x-scroll custom-scrollbar p-4 z-30"
     >
       {rankings?.map(({ id, name, image }, index) => (
         <Badge
