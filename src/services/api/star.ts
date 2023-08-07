3c34fabec2791a735c8377f8a55b08fe6b45404b
João Pedro <joaopedro.nc@outlook.com>
2023-08-07 19:48:23 -0300

    ➕ Add star api methods

--- src/services/api/star.ts
+++ src/services/api/star.ts
@@ -1,8 +1,40 @@
 import { createClient } from '../supabase-browser'
+import type { Star } from '@/types/star'
 
 const supabase = createClient()
 
 export default {
+  getStar: async (starId: string) => {
+    const { data, error } = await supabase
+      .from('stars')
+      .select('*')
+      .eq('id', starId)
+      .single<Star>()
+
+    if (error) {
+      throw new Error(error.message)
+    }
+
+    return data
+  },
+
+  getNextStar: async (currentStar: Star) => {
+    const { data, error } = await supabase
+      .from('stars')
+      .select('*')
+      .match({
+        planet_id: currentStar.planet_id,
+        number: currentStar.number + 1,
+      })
+      .single<Star>()
+
+    if (error) {
+      throw new Error(error.message)
+    }
+
+    return data
+  },
+
   getUserUnlockedStars: async (userId: string) => {
     const { data, error } = await supabase
       .from('users_unlocked_stars')
