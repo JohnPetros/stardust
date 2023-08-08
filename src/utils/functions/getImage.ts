050d779d78167288df23a66fc96d203cc3882f73
João Pedro <joaopedro.nc@outlook.com>
2023-08-08 15:24:40 -0300

    🏷️ Add texts value to Folder type

--- src/utils/functions/getImage.ts
+++ src/utils/functions/getImage.ts
@@ -1,4 +1,4 @@
-type Folder = 'avatars' | 'rockets' | 'rankings' | 'planets' | 'achievements'
+type Folder = 'avatars' | 'rockets' | 'rankings' | 'planets' | 'achievements' | 'texts'
 
 export function getImage(folder: Folder, resource: string) {
   return `${process.env.NEXT_PUBLIC_CDN_URL}/${folder}/${resource}`
