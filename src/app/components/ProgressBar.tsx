94a20f6b23c6b5babe24c710c72e70bf5e453492
João Pedro <joaopedro.nc@outlook.com>
2023-08-08 16:27:04 -0300

    💄 Add ProgressBar component styles

---
+++ src/app/components/ProgressBar.tsx
@@ -0,0 +1,27 @@
+import * as Progress from '@radix-ui/react-progress'
+import Image from 'next/image'
+
+interface ProgessProps {
+  value: number
+  height: number
+  indicatorImage?: string
+}
+
+export function ProgressBar({ value, height, indicatorImage }: ProgessProps) {
+  return (
+    <Progress.Root
+      value={value}
+      className="bg-gray-400 w-full rounded-lg"
+      style={{ height: height }}
+    >
+      <Progress.Indicator
+        className="bg-green-400"
+        style={{ width: `${value}%` }}
+      />
+
+      <div className="relative w-6 h-6 rotate-90 -ml-1 -mt-1">
+        {indicatorImage && <Image src={indicatorImage} fill alt="" />}
+      </div>
+    </Progress.Root>
+  )
+}
