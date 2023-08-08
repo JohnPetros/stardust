af8b2e5cf50cfdaed187016378e8220fed3bb0a8
João Pedro <joaopedro.nc@outlook.com>
2023-08-08 15:23:28 -0300

    🚧 Add Lesson page sructutre

---
+++ src/app/(private)/(app)/lesson/[starId]/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useParams } from 'next/navigation'
+import { useStar } from '@/hooks/useStar'
+
+import { Theory } from '../components/Theory'
+import { TransitionPageAnimation } from '../../components/TransitionPageAnimation'
+
+import type { Star } from '@/types/star'
+
+export default function Lesson() {
+  const { starId } = useParams()
+  if (!starId) return null
+
+  const { star, getNextStar } = useStar(String(starId))
+  const [isTransitionVisible, setIsTransitionVisible] = useState(true)
+
+  useEffect(() => {
+    if (star) {
+      setTimeout(() => setIsTransitionVisible(false), 2500)
+    }
+  }, [star])
+
+  return (
+    <div>
+      <TransitionPageAnimation isVisible={isTransitionVisible} />
+      {star && <Theory title={star.name} number={star?.number} />}
+    </div>
+  )
+}
