87dc49b61daaaa5329fd59203eb4bc9742a92da1
João Pedro <joaopedro.nc@outlook.com>
2023-08-08 16:28:22 -0300

    💄 style: Add Star component styles

--- src/app/(private)/(app)/lesson/components/Theory/Star.tsx
+++ src/app/(private)/(app)/lesson/components/Theory/Star.tsx
@@ -8,12 +8,8 @@ interface StarProps {
 export function Star({ number }: StarProps) {
   return (
     <div className="relative">
-      <Lottie
-        animationData={UnlockedStar}
-        style={{ width: 120 }}
-        loop={false}
-      />
-      <span className="absolute block text-lg font-semibold top-[52%] -translate-x-1/2 -translate-y-1/2 text-yellow-700">
+      <Lottie animationData={UnlockedStar} style={{ width: 80 }} loop={false} />
+      <span className="absolute block text-lg font-semibold top-[51%] left-[51%] -translate-x-1/2 -translate-y-1/2 text-yellow-700">
         {number}
       </span>
     </div>
