fae87d5ae68bd8a3fe6e86438fa8b0487d38e7d5
João Pedro <joaopedro.nc@outlook.com>
2023-08-09 16:06:41 -0300

    ✨ feat Add delay and speed props to TypeWritter component

--- src/app/components/Text/TypeWritter.tsx
+++ src/app/components/Text/TypeWritter.tsx
@@ -4,27 +4,53 @@ import { useEffect, useRef, useState } from 'react'
 
 interface TypeWritterProps {
   children: string
-  canType: boolean
+  canType?: boolean
+  delay?: number
+  speed?: number
 }
 
-export function TypeWritter({ children, canType }: TypeWritterProps) {
+export function TypeWritter({
+  children,
+  canType = true,
+  delay = 0,
+  speed = 25,
+}: TypeWritterProps) {
   if (!children) return null
+
+  if (!canType) return children
 
   const [text, setText] = useState('')
   const index = useRef(0)
+  const hasDelay = useRef(!!delay)
   const timer = useRef<NodeJS.Timeout | number>(0)
 
-  useEffect(() => {
+  async function delayAnimation() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        hasDelay.current = false
+        resolve(true)
+      }, delay)
+    })
+  }
+
+  async function initAnimation() {
     if (text.length === children.length) {
       clearInterval(timer.current)
       return
     }
 
+    if (hasDelay.current) {
+      await delayAnimation()
+    }
+
     timer.current = setTimeout(() => {
       setText(text + children.charAt(index.current))
       index.current += 1
-    }, 20)
+    }, speed)
+  }
 
+  useEffect(() => {
+    initAnimation()
     return () => clearInterval(timer.current)
   }, [index.current])
 
