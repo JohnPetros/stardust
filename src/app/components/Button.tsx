29127ff9c9baa2ccfe31e42f14e0198bef54b8e7
João Pedro <joaopedro.nc@outlook.com>
2023-08-09 16:57:39 -0300

    ➕ Add buttonRef prop to Button component

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { ButtonHTMLAttributes, ReactNode } from 'react'
+import { ButtonHTMLAttributes, ReactNode, RefObject } from 'react'
 
 import { Loading } from './Loading'
 
@@ -7,12 +7,14 @@ import { twMerge } from 'tailwind-merge'
 import { MotionProps, motion } from 'framer-motion'
 
 type ButtonProps = {
+  buttonRef?: RefObject<HTMLButtonElement>
   children: ReactNode
   className?: string
   isLoading?: boolean
 }
 
 export function Button({
+  buttonRef,
   children,
   className,
   isLoading = false,
@@ -20,6 +22,7 @@ export function Button({
 }: ButtonProps & ButtonHTMLAttributes<HTMLButtonElement> & MotionProps) {
   return (
     <motion.button
+      ref={buttonRef}
       whileTap={{ scale: 0.99 }}
       className={twMerge(
         'h-10 w-full flex justify-center items-center rounded bg-green-400 font-semibold relative overflow-hidden hover:brightness-75 transition-[filter] duration-400',
