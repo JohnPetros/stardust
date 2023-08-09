b91989c29ad7ae721bc75aae4a920741fd65a274
João Pedro <joaopedro.nc@outlook.com>
2023-08-09 20:48:29 -0300

    🚧 Add Quiz component base structure

--- src/app/(private)/(app)/lesson/components/Quiz/index.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/index.tsx
@@ -1,6 +1,51 @@
+import { useLesson } from '@/hooks/useLesson'
+import { SelectionQuestion } from './SelectionQuestion'
+import { VerificationButton } from './VerificationButton'
 
 export function Quiz() {
+  const {
+    state: {
+      currentQuestionIndex,
+      questions,
+      answerHandler,
+      isAnswerWrong,
+      isAnswerVerified,
+      isAnswered,
+    },
+    dispatch,
+  } = useLesson()
+
+  const currentQuestion = questions[currentQuestionIndex]
+  
+  console.log(currentQuestion)
+
   return (
-    <h2 className="text-gray-100">index</h2>
+    <div>
+      <div className="flex items-center justify-center mt-20">
+        {currentQuestion.type === 'selection' && (
+          <SelectionQuestion data={currentQuestion} />
+        )}
+
+        {/* {currentQuestion.type === 'checkbox' && (
+        <CheckboxQuestion data={currentQuestion} />
+      )}
+      {currentQuestion.type === 'open' && (
+        <OpenQuestion data={currentQuestion} />
+      )}
+      {currentQuestion.type === 'drag-and-drop-click' && (
+        <DragAndDropClickQuestion data={currentQuestion} />
+      )}
+      {currentQuestion.type === 'drag-and-drop-list' && (
+        <DragAndDropListQuestion data={currentQuestion} />
+      )} */}
+      </div>
+
+      <VerificationButton
+        answerHandler={answerHandler}
+        isAnswerWrong={isAnswerWrong}
+        isAnswerVerified={isAnswerVerified}
+        isAnswered={isAnswered}
+      />
+    </div>
   )
 }
