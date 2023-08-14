895cb873b8368fa9c7abf80090b88908692487ff
João Pedro <joaopedro.nc@outlook.com>
2023-08-14 18:49:55 -0300

    ✨ feat: Add Drag aand Drop logic to Drag and Drop List Question

--- src/app/(private)/(app)/lesson/components/Quiz/DragAndDropListQuestion/Item.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/DragAndDropListQuestion/Item.tsx
@@ -1,15 +1,54 @@
+'use client'
+
 import { List } from '@phosphor-icons/react'
-import Image from 'next/image'
+
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import { twMerge } from 'tailwind-merge'
 
 interface ItemProps {
+  id: number
   label: string
+  isActive: boolean
+  isOverlay?: boolean
 }
 
-export function Item({ label }: ItemProps) {
+export function Item({ id, label, isActive }: ItemProps) {
+  const {
+    isDragging,
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
   return (
-    <div className="rounded-md flex items-center justify-between bg-purple-700 border-2 p-3 w-full max-w-sm mx-auto">
-      <strong className="text-medium text-gray-100">{label}</strong>
-      <List className="text-gray-100 text-lg" />
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className={twMerge(
+        'rounded-md flex items-center justify-between bg-purple-700 border-2 p-3 w-full max-w-sm mx-auto borde text-medium',
+        isActive
+          ? 'border-blue-300 text-blue-300'
+          : 'border-gray-100 text-gray-100',
+        isDragging && 'opacity-0'
+      )}
+    >
+      <strong>{label}</strong>
+      <List
+        className={twMerge(
+          'text-lg',
+          isActive ? 'text-blue-300' : 'text-gray-100'
+        )}
+      />
     </div>
   )
 }
--- src/app/(private)/(app)/lesson/components/Quiz/DragAndDropListQuestion/index.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/DragAndDropListQuestion/index.tsx
@@ -1,6 +1,29 @@
-import { DragAndDropList } from '@/types/question'
+'use client'
+import { useState } from 'react'
+
+import { DragAndDropList, Item as SortableItem } from '@/types/question'
 import { QuestionTitle } from '../QuestionTitle'
 import { Item } from './Item'
+
+import {
+  DndContext,
+  DragStartEvent,
+  DragEndEvent,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+  DragOverlay,
+} from '@dnd-kit/core'
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+  arrayMove,
+} from '@dnd-kit/sortable'
+import {
+  restrictToVerticalAxis,
+  restrictToWindowEdges,
+} from '@dnd-kit/modifiers'
 
 interface DragAndDropListQuestionProps {
   data: DragAndDropList
@@ -9,15 +32,77 @@ interface DragAndDropListQuestionProps {
 export function DragAndDropListQuestion({
   data: { title, items, picture },
 }: DragAndDropListQuestionProps) {
-  return (
-    <div className="mx-auto mt-16 w-full max-w-xl flex flex-col items-center justify-center cursor-grab">
-      <QuestionTitle picture={picture}>{title}</QuestionTitle>
+  const [sortableItems, setSortableItems] = useState<SortableItem[]>(items)
+  const [activeItemId, setActiveEventId] = useState<number | null>(null)
 
-      <div className="mx-auto w-full space-y-2 mt-8">
-        {items.map((item) => (
-          <Item key={item.id} label={item.label} />
-        ))}
+  const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor))
+
+  function handleDragEnd(event: DragEndEvent) {
+    setActiveEventId(null)
+
+    const { active, over } = event
+
+    if (over && active.id !== over?.id) {
+      setSortableItems((sortableItems) => {
+        const overIndex = sortableItems.findIndex(
+          (item) => item.id === active.id
+        )
+        const activeIndex = sortableItems.findIndex(
+          (item) => item.id === over.id
+        )
+        return arrayMove(sortableItems, overIndex, activeIndex)
+      })
+    }
+  }
+
+  function handleDragStart(event: DragStartEvent) {
+    setActiveEventId(Number(event.active.id))
+  }
+
+  function handleDragCancel() {
+    setActiveEventId(null)
+  }
+
+  return (
+    <DndContext
+      sensors={sensors}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragCancel={handleDragCancel}
+      modifiers={[restrictToVerticalAxis, restrictToWindowEdges]}
+    >
+      <div className="mx-auto mt-16 w-full max-w-xl flex flex-col items-center justify-center cursor-grab">
+        <QuestionTitle picture={picture}>{title}</QuestionTitle>
+
+        <SortableContext
+          items={sortableItems}
+          strategy={verticalListSortingStrategy}
+        >
+          <div className="mx-auto w-full space-y-2 mt-8">
+            {sortableItems.map((item) => (
+              <Item
+                key={item.id}
+                id={item.id}
+                label={item.label}
+                isActive={activeItemId === item.id}
+              />
+            ))}
+          </div>
+        </SortableContext>
+
+        <DragOverlay>
+          {activeItemId ? (
+            <Item
+              id={activeItemId}
+              label={
+                sortableItems.find((item) => item.id === activeItemId)?.label ??
+                ''
+              }
+              isActive={true}
+            />
+          ) : null}
+        </DragOverlay>
       </div>
-    </div>
+    </DndContext>
   )
 }
