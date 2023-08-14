20660f24ab24dca42ea51e916a52b1c6eaf0961f
João Pedro <joaopedro.nc@outlook.com>
2023-08-14 11:32:40 -0300

    🔥 Remove TypeWritter component form Title component

---
+++ src/app/(private)/(app)/lesson/components/Quiz/Title.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Image from 'next/image'
+import { TypeWritter } from '@/app/components/Text/TypeWritter'
+
+import { getImage } from '@/utils/functions'
+
+interface TitleProps {
+  children: string
+  picture?: string
+}
+
+export function Title({ children, picture }: TitleProps) {
+  const image = picture ? getImage('texts', picture) : ''
+
+  return (
+    <>
+      {image && (
+        <Image
+          src={image}
+          width={64}
+          height={64}
+          className="rounded-md"
+          alt=""
+        />
+      )}
+      <p className="text-gray-100 text-center font-medium mt-4">{children}</p>
+    </>
+  )
+}
