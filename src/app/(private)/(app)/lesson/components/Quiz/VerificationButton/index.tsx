ca6ac851af1bcf5603dbeffb90d869eee5299803
João Pedro <joaopedro.nc@outlook.com>
2023-08-14 12:14:32 -0300

    💄 style: Add animations to  VerificationButton

--- src/app/(private)/(app)/lesson/components/Quiz/VerificationButton/index.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/VerificationButton/index.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/app/components/Button'
 import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
 import { tv } from 'tailwind-variants'
-import { Variants, motion } from 'framer-motion'
+import { AnimatePresence, Variants, motion } from 'framer-motion'
 
 const buttonStyles = tv({
   base: 'w-64',
@@ -15,11 +15,15 @@ const buttonStyles = tv({
 })
 
 const feedbackAnimations: Variants = {
-  hidden: {
+  down: {
     height: 0,
   },
-  visible: {
-    height: '100%',
+  up: {
+    height: 80,
+    transition: {
+      type: 'spring',
+      duration: 0.4,
+    },
   },
 }
 
@@ -53,41 +57,47 @@ export function VerificationButton({
   return (
     <div
       className={twMerge(
-        'fixed bottom-0 border-t w-full px-6 py-6 flex flex-col items-center justify-center gap-6',
+        'fixed bottom-0 border-t w-full px-6 py-6 flex flex-col items-center justify-center gap-3',
         isAnswerVerified && !isAnswerCorrect
           ? 'border-red-700 bg-green-900'
           : 'border-green-500 bg-green-900'
       )}
     >
-      {isAnswerVerified && (
-        <motion.div
-          variants={feedbackAnimations}
-          initial="hidden"
-          animate="visible"
-          className="flex flex-col items-center gap-1"
-        >
-          <Image
-            src={`/icons/${
-              isAnswerVerified && !isAnswerCorrect
-                ? 'incorrect-answer.svg'
-                : 'correct-answer.svg'
-            }`}
-            width={48}
-            height={48}
-            alt=""
-          />
-          <strong
-            className={twMerge(
-              'text-lg',
-              isAnswerVerified && !isAnswerCorrect
-                ? 'text-red-700'
-                : 'text-green-400'
-            )}
-          >
-            {isAnswerCorrect ? 'Oops, tente denovo!' : 'Correto, parabéns!'}
-          </strong>
-        </motion.div>
-      )}
+      <div>
+        <AnimatePresence>
+          {isAnswerVerified && (
+            <motion.div
+              variants={feedbackAnimations}
+              initial="down"
+              animate="up"
+              exit="down"
+              className="flex flex-col items-center gap-1 overflow-hidden h-20"
+            >
+              <Image
+                src={`/icons/${
+                  isAnswerVerified && !isAnswerCorrect
+                    ? 'incorrect-answer.svg'
+                    : 'correct-answer.svg'
+                }`}
+                width={48}
+                height={48}
+                alt=""
+              />
+              <strong
+                className={twMerge(
+                  'text-lg',
+                  isAnswerVerified && !isAnswerCorrect
+                    ? 'text-red-700'
+                    : 'text-green-400'
+                )}
+              >
+                {isAnswerCorrect ? 'Oops, tente denovo!' : 'Correto, parabéns!'}
+              </strong>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+
       <Button
         onClick={handleButtonClick}
         className={buttonStyles({
