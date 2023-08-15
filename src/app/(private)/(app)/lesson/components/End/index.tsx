ae7256d5049a7b765ecfaa8ae9fa354ad4240229
João Pedro <joaopedro.nc@outlook.com>
2023-08-15 15:12:40 -0300

    ✨ feat: Calculate user metrics on lesson end stage

--- src/app/(private)/(app)/lesson/components/End/index.tsx
+++ src/app/(private)/(app)/lesson/components/End/index.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { useEffect, useRef } from 'react'
+import { useLesson } from '@/hooks/useLesson'
 import { useAuth } from '@/hooks/useAuth'
 
 import { Metric } from './Metric'
@@ -6,8 +8,57 @@ import { Metric } from './Metric'
 import ApolloContratulating from '../../../../../../../public/animations/apollo-congratulating.json'
 import Lottie from 'lottie-react'
 
-export function End() {
+interface EndProps {
+  isAlreadyCompleted: boolean
+}
+
+export function End({ isAlreadyCompleted }: EndProps) {
   const { user, updateUser } = useAuth()
+  const {
+    state: { questions, incorrectAnswersAmount, secondsAmount },
+    dispatch,
+  } = useLesson()
+
+  const coins = useRef(0)
+  const xp = useRef(0)
+  const time = useRef('')
+  const accurance = useRef('')
+
+  function formatSecondsToTime(seconds: number) {
+    const date = new Date(0);
+    date.setSeconds(seconds);
+    const time = date.toISOString().substring(14, 19);
+    return time;
+  }
+
+  function getAccurance() {
+    const accurance =
+      ((questions.length - incorrectAnswersAmount) / questions.length) * 100
+    return accurance === 0 ? '100%' : accurance.toFixed(1) + '%'
+  }
+
+  function getCoins() {
+    let maxCoins = isAlreadyCompleted ? 5 : 10
+    for (let i = 0; i < incorrectAnswersAmount; i++) {
+      maxCoins -= isAlreadyCompleted ? 1 : 2
+    }
+    return maxCoins
+  }
+
+  function getXp() {
+    let maxXp = isAlreadyCompleted ? 10 : 20
+    for (let i = 0; i < incorrectAnswersAmount; i++) {
+      maxXp -= isAlreadyCompleted ? 2 : 5
+    }
+    return maxXp
+  }
+
+  useEffect(() => {
+    xp.current = getXp()
+    coins.current = getCoins()
+    accurance.current = getAccurance()
+    time.current = formatSecondsToTime(secondsAmount)
+  }, [])
 
   return (
     <div className="flex flex-col items-center justify-center mt-24 mx-auto px-6 w-full max-w-lg">
@@ -18,44 +69,45 @@ export function End() {
         style={{ width: 320 }}
         loop={true}
       />
+
       <div className="flex flex-col items-center justify-center mt-3">
         <div className="mx-auto">
           <Metric
             title="Poeira estelar"
-            amount={300}
+            amount={coins.current}
             color="yellow"
             icon="coin.svg"
             isLarge={true}
-            delay={300}
+            delay={1.4}
           />
         </div>
 
         <div className="grid grid-cols-3 gap-3 mt-6">
           <Metric
             title="Total de xp"
-            amount={42}
+            amount={xp.current}
             color="green"
             icon="xp.svg"
             isLarge={false}
-            delay={300}
+            delay={2.4}
           />
 
           <Metric
             title="Tempo"
-            amount={42}
+            amount={secondsAmount}
             color="blue"
             icon="clock.svg"
             isLarge={false}
-            delay={300}
+            delay={3.4}
           />
 
           <Metric
             title="Acertos"
-            amount={42}
+            amount={accurance.current}
             color="red"
             icon="percent.svg"
             isLarge={false}
-            delay={300}
+            delay={4.4}
           />
         </div>
       </div>
