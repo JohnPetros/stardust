e4d39816bcdab54b20448a77b7235d6bd4ce5074
João Pedro <joaopedro.nc@outlook.com>
2023-08-15 18:16:23 -0300

    ♻️ refactor: Move End logic up to Lesson Component

--- src/app/(private)/(app)/lesson/[starId]/page.tsx
+++ src/app/(private)/(app)/lesson/[starId]/page.tsx
@@ -11,20 +11,61 @@ import { Theory } from '../components/Theory'
 import { Quiz } from '../components/Quiz'
 import { End } from '../components/End'
 
-import type { Star } from '@/types/star'
-
 export default function Lesson() {
   const { starId } = useParams()
   if (!starId) return null
 
   const { star, nextStar } = useStar(String(starId))
-  const { state, dispatch } = useLesson()
+  const {
+    state: { currentStage, questions, incorrectAnswersAmount, secondsAmount },
+    dispatch,
+  } = useLesson()
   const [isTransitionVisible, setIsTransitionVisible] = useState(true)
   const scrollRef = useRef<HTMLDivElement>(null)
 
+  const [coins, setCoins] = useState(0)
+  const [xp, setXp] = useState(0)
+  const [time, setTime] = useState('')
+  const [accurance, setAccurance] = useState('')
+
+  function formatSecondsToTime(seconds: number) {
+    const date = new Date(0)
+    date.setSeconds(seconds)
+    const time = date.toISOString().substring(14, 19)
+
+    return time
+  }
+
+  function getAccurance() {
+    const accurance =
+      ((questions.length - incorrectAnswersAmount) / questions.length) * 100
+    return accurance === 0 ? '100%' : accurance.toFixed(1) + '%'
+  }
+
+  function getCoins() {
+    if (!nextStar) return 0
+
+    let maxCoins = nextStar.isUnlocked ? 5 : 10
+    for (let i = 0; i < incorrectAnswersAmount; i++) {
+      maxCoins -= nextStar.isUnlocked ? 1 : 2
+    }
+    return maxCoins
+  }
+
+  function getXp() {
+    if (!nextStar) return 0
+
+    let maxXp = nextStar.isUnlocked ? 10 : 20
+    for (let i = 0; i < incorrectAnswersAmount; i++) {
+      maxXp -= nextStar.isUnlocked ? 2 : 5
+    }
+
+    return maxXp
+  }
+
   useEffect(() => {
     setTimeout(() => dispatch({ type: 'incrementSecondsAmount' }), 1000)
-  }, [state.secondsAmount])
+  }, [secondsAmount])
 
   useEffect(() => {
     if (star && nextStar) {
@@ -34,6 +75,15 @@ export default function Lesson() {
     }
   }, [star, nextStar])
 
+  useEffect(() => {
+    if (currentStage) {
+      setXp(getXp())
+      setCoins(getCoins())
+      setAccurance(getAccurance())
+      setTime(formatSecondsToTime(secondsAmount))
+    }
+  }, [currentStage])
+
   return (
     <div>
       <TransitionPageAnimation isVisible={isTransitionVisible} />
@@ -41,12 +91,12 @@ export default function Lesson() {
         <Header />
         {star && nextStar && (
           <>
-            {state.currentStage === 'theory' && (
+            {currentStage === 'theory' && (
               <Theory title={star.name} number={star.number} />
             )}
-            {state.currentStage === 'quiz' && <Quiz />}
-            {state.currentStage === 'end' && (
-              <End isAlreadyCompleted={nextStar.isUnlocked} />
+            {currentStage === 'quiz' && <Quiz />}
+            {currentStage === 'end' && (
+              <End coins={coins} xp={xp} time={time} accurance={accurance} />
             )}
           </>
         )}
