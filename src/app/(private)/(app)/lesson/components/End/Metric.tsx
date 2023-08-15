f5900b1debe832d4406afa0232e5576e9f4afdbb
João Pedro <joaopedro.nc@outlook.com>
2023-08-15 15:37:24 -0300

    💄 style: Animate Metric entrance

--- src/app/(private)/(app)/lesson/components/End/Metric.tsx
+++ src/app/(private)/(app)/lesson/components/End/Metric.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
+
+import { motion, Variants } from 'framer-motion'
 
 const colors = {
   bg: {
@@ -19,7 +23,7 @@ const colors = {
 
 interface MetricProps {
   title: string
-  amount: number
+  amount: number | string
   icon: string
   color: 'green' | 'blue' | 'red' | 'yellow'
   isLarge: boolean
@@ -34,21 +38,42 @@ export function Metric({
   isLarge,
   delay,
 }: MetricProps) {
+  
+  const metricAniamtions: Variants = {
+    down: {
+      opacity: 0,
+      y: 24,
+    },
+    up: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.4,
+        delay,
+      },
+    },
+  }
+
   return (
-    <div
+    <motion.div
+      variants={metricAniamtions}
+      initial="down"
+      animate="up"
       className={twMerge(
         'rounded-md overflow-hidden border',
         isLarge ? 'w-64 h-20' : 'w-32 h-20',
         colors.border[color]
       )}
     >
-      <div className={twMerge('grid place-content-center p-1', colors.bg[color])}>
+      <div
+        className={twMerge('grid place-content-center p-1', colors.bg[color])}
+      >
         <strong className="text-gray-900">{title}</strong>
       </div>
       <div className="flex items-center justify-center gap-2 p-3">
         <Image src={`/icons/${icon}`} width={24} height={24} alt="" />
         <span className="text-gray-100 text-lg font-medium">{amount}</span>
       </div>
-    </div>
+    </motion.div>
   )
 }
