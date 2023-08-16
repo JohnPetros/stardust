39073c2db45ef556a38692f3c557411c1cc57de7
João Pedro <joaopedro.nc@outlook.com>
2023-08-16 12:24:54 -0300

    ➕ Add getNextStarFromNextPlanet function to useStar hook

--- src/hooks/useStar.tsx
+++ src/hooks/useStar.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { updateUserDataParam } from '@/app/(private)/(app)/lesson/components/End'
 import { useAuth } from './useAuth'
 import { useApi } from '@/services/api'
+import { User } from '@/types/user'
 import useSWR from 'swr'
 
 export function useStar(starId?: string) {
@@ -36,8 +38,56 @@ export function useStar(starId?: string) {
     throw new Error(nextStarError)
   }
 
+  async function getNextStarFromNextPlanet() {
+    if (!star || !user) return
+
+    return await api.getNextStarFromNextPlanet(star.planet_id, user.id)
+  }
+
+  async function addUnlockedStar(UnlockedstarId: string) {
+    if (!user) return
+
+    await api.addUnlockedStar(UnlockedstarId, user.id)
+  }
+  
+  async function updateUserData({
+    newCoins,
+    newXp,
+    user,
+  }: updateUserDataParam): Promise<Partial<User>> {
+    const updatedCoins = newCoins + user.coins
+    const updatedXp = newXp + user.xp
+    const updatedWeeklyXp = newXp + user.weekly_xp
+    // const updatedLevel = getUpdatedLevel(updatedXp)
+
+    let completedPlanets = user.completed_planets
+    let updatedUnlockedStars = user.unlocked_stars
+
+    let _nextStar = nextStar
+
+    if (!_nextStar) {
+      _nextStar = await getNextStarFromNextPlanet()
+      completedPlanets += _nextStar ? 1 : 0
+    }
+
+    if (_nextStar && !_nextStar.isUnlocked) {
+      await addUnlockedStar(_nextStar.id)
+      updatedUnlockedStars++
+    }
+
+    return {
+      coins: updatedCoins,
+      xp: updatedXp,
+      weekly_xp: updatedWeeklyXp,
+      // level: updatedLevel,
+      unlocked_stars: updatedUnlockedStars,
+      completed_planets: completedPlanets,
+    }
+  }
+
   return {
     star,
     nextStar,
+    updateUserData,
   }
 }
