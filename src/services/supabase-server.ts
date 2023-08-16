668f9708001de3d4f101e34edf7c95b166132987
João Pedro <joaopedro.nc@outlook.com>
2023-08-16 13:32:05 -0300

    🐛 fix: Resolve cookies bug from Dynamic server usage

--- src/services/supabase-server.ts
+++ src/services/supabase-server.ts
@@ -2,6 +2,9 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
 import type { Database } from '@/types/supabase'
+import { cache } from 'react'
 
-export const createClient = () =>
-  createServerComponentClient<Database>({ cookies })
+export const createClient = cache(() => {
+  const cookieStore = cookies()
+  return createServerComponentClient<Database>({ cookies: () => cookieStore })
+})
