a9fa1eca7c092fd28f5196adfdef172823f0fdfc
João Pedro <joaopedro.nc@outlook.com>
2023-08-16 12:50:56 -0300

    🐛 fix: Resolve missing display name definition of Modal component

--- src/app/components/Tooltip/index.tsx
+++ src/app/components/Tooltip/index.tsx
@@ -1,5 +1,11 @@
 'use client'
-import { ReactNode, forwardRef, useImperativeHandle, useState } from 'react'
+import {
+  ForwardedRef,
+  ReactNode,
+  forwardRef,
+  useImperativeHandle,
+  useState,
+} from 'react'
 import * as T from '@radix-ui/react-tooltip'
 import { Variants, motion, AnimatePresence } from 'framer-motion'
 
@@ -34,52 +40,55 @@ export interface TooltipRef {
   hide: VoidFunction
 }
 
-export const TooltipContent = forwardRef<TooltipRef, TooltipProps>(
-  ({ text, direction = 'bottom' }: TooltipProps, ref) => {
-    const [isVisible, setIsVisible] = useState(false)
+export const TooltipComponent = (
+  { text, direction = 'bottom' }: TooltipProps,
+  ref: ForwardedRef<TooltipRef>
+) => {
+  const [isVisible, setIsVisible] = useState(false)
 
-    function show() {
-      setIsVisible(true)
-    }
-
-    function hide() {
-      setIsVisible(false)
-    }
-
-    useImperativeHandle(
-      ref,
-      () => {
-        return {
-          show,
-          hide,
-        }
-      },
-      []
-    )
-
-    return (
-      <T.Portal>
-        <AnimatePresence>
-          {isVisible && (
-            <T.Content
-              className="max-w-sm z-50"
-              sideOffset={1}
-              side={direction}
-              forceMount
-            >
-              <motion.p
-                variants={contentVariants}
-                initial="hidden"
-                animate="visible"
-                className="bg-green-900 border border-gray-400 rounded-md p-2 shadow-md text-gray-100 text-sm "
-              >
-                {text}
-                <T.TooltipArrow className="fill-gray-400" />
-              </motion.p>
-            </T.Content>
-          )}
-        </AnimatePresence>
-      </T.Portal>
-    )
+  function show() {
+    setIsVisible(true)
   }
-)
+
+  function hide() {
+    setIsVisible(false)
+  }
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        show,
+        hide,
+      }
+    },
+    []
+  )
+
+  return (
+    <T.Portal>
+      <AnimatePresence>
+        {isVisible && (
+          <T.Content
+            className="max-w-sm z-50"
+            sideOffset={1}
+            side={direction}
+            forceMount
+          >
+            <motion.p
+              variants={contentVariants}
+              initial="hidden"
+              animate="visible"
+              className="bg-green-900 border border-gray-400 rounded-md p-2 shadow-md text-gray-100 text-sm "
+            >
+              {text}
+              <T.TooltipArrow className="fill-gray-400" />
+            </motion.p>
+          </T.Content>
+        )}
+      </AnimatePresence>
+    </T.Portal>
+  )
+}
+
+export const TooltipContent = forwardRef(TooltipComponent)
