ffcb722e9f98de3f80f129ae6f6f06e2ddbbbb8e
João Pedro <joaopedro.nc@outlook.com>
2023-08-16 12:51:07 -0300

    🐛 fix: Resolve missing display name definition of Tooltip component

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 import {
+  ForwardedRef,
   ReactNode,
   forwardRef,
   useEffect,
@@ -43,79 +44,73 @@ interface ModalProps {
 
 export const Container = Dialog.Root
 
-export const Modal = forwardRef<ModalRef, ModalProps>(
-  ({ type, canPlaySong = true, title, body, footer }, ref) => {
-    const [isOpen, setIsOpen] = useState(false)
+const ModalComponent = (
+  { type, canPlaySong = true, title, body, footer }: ModalProps,
+  ref: ForwardedRef<ModalRef>
+) => {
+  const [isOpen, setIsOpen] = useState(false)
 
-    const { animation, sound } = MODAL_EFFECTS.find(
-      (animation) => animation.id === type.toLocaleLowerCase()
-    )!
+  const { animation, sound } = MODAL_EFFECTS.find(
+    (animation) => animation.id === type.toLocaleLowerCase()
+  )!
 
-    function open() {
-      setIsOpen(true)
-    }
-
-    function close() {
-      setIsOpen(false)
-    }
-
-    useImperativeHandle(
-      ref,
-      () => {
-        return {
-          open,
-          close,
-        }
-      },
-      []
-    )
-
-    useEffect(() => {
-      if (sound && isOpen && type !== 'generic' && canPlaySong) {
-        playSound(sound)
-      }
-    }, [isOpen])
-
-    return (
-      <Dialog.Root open={isOpen} onOpenChange={close}>
-        <Dialog.Portal>
-          <Dialog.Overlay className="fixed inset-0 overflow-y-auto bg-black bg-opacity-50 z-50">
-            <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-screen w-full max-w-lg p-6">
-              <motion.div
-                variants={modalVariants}
-                initial="close"
-                animate="open"
-                className="rounded-lg bg-gray-800 p-6 border border-gray-700"
-              >
-                <div className="flex items-center border-b border-gray-700 pb-2">
-                  <Dialog.Title className="mx-auto font-semibold text-white text-center flex items-center justify-center">
-                    {title}
-                  </Dialog.Title>
-                </div>
-                <div className="flex justify-center mt-3">
-                  <Lottie
-                    animationData={animation}
-                    style={{ width: 200 }}
-                    loop={true}
-                  />
-                </div>
-
-                {body}
-                {footer}
-              </motion.div>
-            </Dialog.Content>
-          </Dialog.Overlay>
-        </Dialog.Portal>
-      </Dialog.Root>
-    )
+  function open() {
+    setIsOpen(true)
   }
-)
-// import { Trigger, Title, Close } from '@radix-ui/react-dialog'
-// import { Content } from "./Content";
 
-// export const Modal = {
-//   Content,
-//   Trigger,
-//   Title,
-//   Close
-// }
+  function close() {
+    setIsOpen(false)
+  }
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        open,
+        close,
+      }
+    },
+    []
+  )
+
+  useEffect(() => {
+    if (sound && isOpen && type !== 'generic' && canPlaySong) {
+      playSound(sound)
+    }
+  }, [isOpen])
+
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={close}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 overflow-y-auto bg-black bg-opacity-50 z-50">
+          <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-screen w-full max-w-lg p-6">
+            <motion.div
+              variants={modalVariants}
+              initial="close"
+              animate="open"
+              className="rounded-lg bg-gray-800 p-6 border border-gray-700"
+            >
+              <div className="flex items-center border-b border-gray-700 pb-2">
+                <Dialog.Title className="mx-auto font-semibold text-white text-center flex items-center justify-center">
+                  {title}
+                </Dialog.Title>
+              </div>
+              <div className="flex justify-center mt-3">
+                <Lottie
+                  animationData={animation}
+                  style={{ width: 200 }}
+                  loop={true}
+                />
+              </div>
+
+              {body}
+              {footer}
+            </motion.div>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
+
+export const Modal = forwardRef(ModalComponent)
