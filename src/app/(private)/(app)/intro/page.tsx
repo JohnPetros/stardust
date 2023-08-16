cbe6a0d76405ad2e629692e27a02ba36f9fb22f5
João Pedro <joaopedro.nc@outlook.com>
2023-08-16 12:58:44 -0300

    🚚 Remove Intro page file

--- src/app/(private)/(app)/intro/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useAuth } from '@/hooks/useAuth'
-
-export function Intro() {
-  const { user } = useAuth()
-
-  return <div className='text-white'>intro {user?.name}</div>
-}
