768b0dd975d5bea1efcb2cd0349bcbbfbc2f0942
João Pedro <joaopedro.nc@outlook.com>
2023-08-17 13:20:29 -0300

    💡 Add rescue button to Achievement component

--- src/app/(private)/(app)/(home)/components/Achievement.tsx
+++ src/app/(private)/(app)/(home)/components/Achievement.tsx
@@ -1,9 +1,11 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import { useAchivementsContext } from '@/hooks/useAchievementContext'
 
 import Image from 'next/image'
 import * as Progress from '@radix-ui/react-progress'
+import { Button } from '@/app/components/Button'
 
 import { Variants, motion } from 'framer-motion'
 import { getImage } from '@/utils/functions'
@@ -25,6 +27,16 @@ const achievementAnimations: Variants = {
   },
 }
 
+const rescueButtonAnimations: Variants = {
+  bounce: {
+    scale: [1, 1.1, 1],
+    transition: {
+      repeat: Infinity,
+      duration: 0.8,
+    },
+  },
+}
+
 type Status = {
   formatedCurrentProgress: number
   barWidth: number
@@ -34,21 +46,31 @@ type Status = {
 interface AchievementProps {
   data: AchievementType
   isUnlocked: boolean
+  isRescuable: boolean
 }
 
 export function Achievement({
   data: {
+    id,
     name,
     description,
     icon,
     required_amount,
-    isRescuable,
     currentProgress,
+    reward,
   },
-  isUnlocked
+  isUnlocked,
+  isRescuable,
 }: AchievementProps) {
   const iconImage = getImage('achievements', icon)
   const [status, setStatus] = useState<Status | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { rescueAchivement } = useAchivementsContext()
+
+  function handleRescuButtonClick() {
+    rescueAchivement(id, name, reward)
+  }
 
   useEffect(() => {
     if (currentProgress || currentProgress === 0) {
@@ -79,22 +101,37 @@ export function Achievement({
       </div>
       <div className="flex flex-col gap-1">
         <strong className="text-gray-100 text-sm">{name}</strong>
-        <p className="text-gray-100 text-xs">{description}</p>
-        {!isUnlocked && (
-          <div className="flex w-full items-center gap-2">
-            <Progress.Root
-              value={status?.barWidth}
-              className="bg-gray-400 h-1 w-full"
+        {isRescuable ? (
+          <motion.div variants={rescueButtonAnimations} animate="bounce">
+            <Button
+              className="h-8 w-32 ml-4 text-sm"
+              onClick={handleRescuButtonClick}
+              // isLoading={isLoading}
+              // isDisabled={isLoading}
             >
-              <Progress.Indicator
-                className="bg-green-400"
-                style={{ width: `${status?.barWidth}%` }}
-              />
-            </Progress.Root>
-            <span className="text-gray-100 text-sm">
-              {status?.formatedCurrentProgress}/{required_amount}
-            </span>
-          </div>
+              Resgatar
+            </Button>
+          </motion.div>
+        ) : (
+          <>
+            <p className="text-gray-100 text-xs">{description}</p>
+            {!isUnlocked && (
+              <div className="flex w-full items-center gap-2">
+                <Progress.Root
+                  value={status?.barWidth}
+                  className="bg-gray-400 h-1 w-full"
+                >
+                  <Progress.Indicator
+                    className="bg-green-400"
+                    style={{ width: `${status?.barWidth}%` }}
+                  />
+                </Progress.Root>
+                <span className="text-gray-100 text-sm">
+                  {status?.formatedCurrentProgress}/{required_amount}
+                </span>
+              </div>
+            )}
+          </>
         )}
       </div>
     </motion.div>
