b05898c507c33c9d007b9c6c93e1ff2a9404ed27
João Pedro <joaopedro.nc@outlook.com>
2023-08-18 15:12:22 -0300

    ✨ feat: Reload code snippet

--- src/app/(private)/(app)/lesson/components/Theory/CodeSnippet.tsx
+++ src/app/(private)/(app)/lesson/components/Theory/CodeSnippet.tsx
@@ -1,15 +1,20 @@
 'use client'
 
-import { CodeEditor } from '@/app/components/CodeEditor'
+import { useMemo, useRef } from 'react'
+import { CodeEditor, CodeEditorRef } from '@/app/components/CodeEditor'
 import { Button } from '@/app/components/Button'
 import { ArrowClockwise } from '@phosphor-icons/react'
-import { useMemo } from 'react'
 
 interface CodeSnippetProps {
   code: string
 }
 
 export function CodeSnippet({ code }: CodeSnippetProps) {
+  const codeEditorRef = useRef<CodeEditorRef>(null)
+
+  function handleReloadButtonClick() {
+    codeEditorRef.current?.reloadValue()
+  }
 
   const codeEditorHeight = useMemo(() => {
     return code.split('\n').length * 10 + 200
@@ -18,7 +23,7 @@ export function CodeSnippet({ code }: CodeSnippetProps) {
   return (
     <div className="rounded-md bg-gray-800 w-full">
       <div className="flex items-center justify-end gap-2 border-b border-gray-700 p-2">
-        <Button className="h-8 w-max px-4">
+        <Button className="h-8 w-max px-4" onClick={handleReloadButtonClick}>
           <ArrowClockwise className="text-green-900" weight="bold" />
         </Button>
         <Button className="h-8 w-max px-4 text-sm">
@@ -26,7 +31,7 @@ export function CodeSnippet({ code }: CodeSnippetProps) {
         </Button>
       </div>
 
-      <CodeEditor width="100%" height={codeEditorHeight} code={code} />
+      <CodeEditor ref={codeEditorRef} width="100%" height={codeEditorHeight} value={code} />
     </div>
   )
 }
