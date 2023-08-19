49aabd28284585ef47216b89c8f9a228f5bc0c5b
João Pedro <joaopedro.nc@outlook.com>
2023-08-18 22:08:52 -0300

    💄 style: Add OpenQuestion component styles

--- src/app/(private)/(app)/lesson/components/Quiz/OpenQuestion/index.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/OpenQuestion/index.tsx
@@ -1,5 +1,42 @@
+'use client'
 
+import { useLesson } from '@/hooks/useLesson'
 
-export function OpenQuestion() {
-  return <div>index</div>
+import { QuestionTitle } from '../QuestionTitle'
+import { Input } from './Input'
+
+import type { OpenQuestion as OpenQuestionData } from '@/types/quiz'
+import { useState } from 'react'
+
+interface OpenQuestion {
+  data: OpenQuestionData
+  isCurrentQuestion: boolean
+}
+
+export function OpenQuestion({
+  data: { title, picture, code, answer },
+}: OpenQuestion) {
+  const {
+    state: { isAnswerVerified, isAnswerCorrect, currentQuestionIndex },
+    dispatch,
+  } = useLesson()
+  const [userAnswer, setUserAnswer] = useState('')
+
+  return (
+    <>
+      <div className="mx-auto mt-4 w-full max-w-xl flex flex-col items-center justify-center">
+        <QuestionTitle picture={picture}>{title}</QuestionTitle>
+
+        <div className="mt-12">
+          <Input
+            value={userAnswer}
+            isAnswerCorrect={isAnswerCorrect}
+            isAnswerVerified={isAnswerVerified}
+            autoCapitalize="none"
+            onChange={({ currentTarget }) => setUserAnswer(currentTarget.value)}
+          />
+        </div>
+      </div>
+    </>
+  )
 }
