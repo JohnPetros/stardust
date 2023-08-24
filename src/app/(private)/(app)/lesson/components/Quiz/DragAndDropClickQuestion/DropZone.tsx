0d49cd2cdac07ef376c5f9c469b790bf44744404
João Pedro <joaopedro.nc@outlook.com>
2023-08-24 12:24:59 -0300

    ✨ feat: Add DropZone full logic

---
+++ src/app/(private)/(app)/lesson/components/Quiz/DragAndDropClickQuestion/DropZone.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { MutableRefObject, useEffect, useMemo } from 'react'
+import { useLesson } from '@/hooks/useLesson'
+import { useDroppable } from '@dnd-kit/core'
+
+import { DraggrableItem } from '@/types/quiz'
+import { DragItem } from './DragItem'
+
+import { twMerge } from 'tailwind-merge'
+
+interface DropZoneProps {
+  id: string
+  droppedItem: DraggrableItem | null
+  activeDraggableItemId: number | null
+  userDragItemsIdsSenquence: MutableRefObject<number[]>
+}
+
+export function DropZone({
+  id,
+  droppedItem,
+  activeDraggableItemId,
+  userDragItemsIdsSenquence,
+}: DropZoneProps) {
+  const {
+    state: { isAnswerVerified, isAnswerCorrect },
+  } = useLesson()
+
+  const { setNodeRef } = useDroppable({
+    id,
+    data: { type: 'zone', hasDroppedItem: !!droppedItem },
+  })
+
+  useEffect(() => {
+    if (!activeDraggableItemId && droppedItem) {
+      userDragItemsIdsSenquence.current.push(droppedItem.id)
+    }
+  }, [activeDraggableItemId])
+
+  const borderColor = useMemo(() => {
+    if (isAnswerVerified && isAnswerCorrect) {
+      return 'border-green-500'
+    } else if (isAnswerVerified) {
+      return 'border-red-700'
+    } else {
+      return 'border-gray-100'
+    }
+  }, [isAnswerVerified, isAnswerCorrect])
+
+  return (
+    <div
+      ref={setNodeRef}
+      className={twMerge(
+        'grid place-content-center rounded-md border border-gray-100 h-12 bg-green-900 min-w-4 text-blue-300',
+        droppedItem ? 'px-0' : 'px-6',
+        borderColor
+      )}
+    >
+      {droppedItem && (
+        <DragItem
+          id={droppedItem.id}
+          label={droppedItem.label}
+          isDroppedInZone={true}
+          isActive={false}
+        />
+      )}
+    </div>
+  )
+}
