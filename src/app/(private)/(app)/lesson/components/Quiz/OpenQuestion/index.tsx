bb2602191d26214e65fcca3b2e25447e573cc816
João Pedro <joaopedro.nc@outlook.com>
2023-08-24 20:43:52 -0300

    ♻️ refactor: Add code to open question

--- src/app/(private)/(app)/lesson/components/Quiz/OpenQuestion/index.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/OpenQuestion/index.tsx
@@ -6,23 +6,27 @@ import { useLesson } from '@/hooks/useLesson'
 import { QuestionTitle } from '../QuestionTitle'
 import { Input } from './Input'
 
-import type { OpenQuestion as OpenQuestionData } from '@/types/quiz'
+import type { Open } from '@/types/quiz'
 import { QuestionContainer } from '../QuestionContainer'
+import { CodeText } from '../CodeText'
+import { compareArrays } from '@/utils/functions'
 
 interface OpenQuestion {
-  data: OpenQuestionData
+  data: Open
   isCurrentQuestion: boolean
 }
 
 export function OpenQuestion({
-  data: { title, picture, code, answer },
+  data: { title, picture, code, answers, lines },
   isCurrentQuestion,
 }: OpenQuestion) {
   const {
     state: { isAnswerVerified, isAnswerCorrect, currentQuestionIndex },
     dispatch,
   } = useLesson()
-  const [userAnswer, setUserAnswer] = useState('')
+  const [userAnswers, setUserAnswers] = useState<string[]>([])
+  console.log(userAnswers)
+
   const hasAlreadyIncrementIncorrectAnswersAmount = useRef(false)
 
   function setIsAnswerVerified(isAnswerVerified: boolean) {
@@ -37,9 +41,9 @@ export function OpenQuestion({
 
     setIsAnswerCorrect(false)
 
-    const openQuestionAnswer = !Array.isArray(answer) ? [answer] : answer
+    const isUserAnswerCorrect = compareArrays(userAnswers, answers)
 
-    if (openQuestionAnswer.includes(userAnswer.trim().toLowerCase())) {
+    if (isUserAnswerCorrect) {
       setIsAnswerCorrect(true)
 
       if (isAnswerVerified) {
@@ -62,31 +66,59 @@ export function OpenQuestion({
     if (isAnswerVerified) dispatch({ type: 'decrementLivesAmount' })
   }
 
+  function handleInputChange(value: string, index: number) {
+    userAnswers[index] = value
+
+    setUserAnswers([...userAnswers])
+  }
+
   useEffect(() => {
-    dispatch({ type: 'setIsAnswered', payload: !!userAnswer })
-  }, [userAnswer])
+    console.log(userAnswers)
+
+    dispatch({
+      type: 'setIsAnswered',
+      payload: userAnswers.filter(answer => !!answer).length === answers.length,
+    })
+  }, [userAnswers])
 
   useEffect(() => {
     dispatch({
       type: 'setAnswerHandler',
       payload: handleAnswer,
     })
-  }, [isAnswerVerified, userAnswer])
+  }, [isAnswerVerified, userAnswers])
 
   return (
     <QuestionContainer>
       <QuestionTitle picture={picture}>{title}</QuestionTitle>
 
-      <div className="mt-8">
-        <Input
-          value={userAnswer}
-          isAnswerCorrect={isAnswerCorrect}
-          isAnswerVerified={isAnswerVerified}
-          autoCapitalize="none"
-          onChange={({ currentTarget }) => setUserAnswer(currentTarget.value)}
-          autoFocus
-        />
-      </div>
+      <ul className="mt-8">
+        {lines.map((line) => (
+          <li className="flex flex-row items-center gap-3">
+            {line.texts.map((text, index) => (
+              <div key={`${index}-${line.id}`}>
+                {text !== 'input' ? (
+                  <div className="flex gap-2">
+                    {text.split(' ').map((word) => (
+                      <CodeText>{word}</CodeText>
+                    ))}
+                  </div>
+                ) : (
+                  <Input
+                    value={userAnswers[index]}
+                    autoCapitalize="none"
+                    onChange={({ currentTarget }) =>
+                      handleInputChange(currentTarget.value, index)
+                    }
+                    answer={answers[index]}
+                    autoFocus
+                  />
+                )}
+              </div>
+            ))}
+          </li>
+        ))}
+      </ul>
     </QuestionContainer>
   )
 }
