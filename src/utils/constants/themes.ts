fa47cfa2bf9e4eb3695cd0af0ef8a5cabc405a17
João Pedro <joaopedro.nc@outlook.com>
2023-08-24 20:40:27 -0300

    🚚 Add THEMES constant

---
+++ src/utils/constants/themes.ts
@@ -0,0 +1,17 @@
+type Theme = {
+  [themeName: string]: {
+    [token: string]: string
+  }
+}
+
+export const THEMES: Theme = {
+  default: {
+    keywords: '#0FE983',
+    string: '#999999',
+    comments: '#F1FA8C',
+    operators: '#022F43',
+    numbers: '#00D1FF',
+    booleans: '#00D1FF',
+    typeKeywords: '#0ac899',
+  },
+}
