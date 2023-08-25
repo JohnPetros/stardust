853b8043e9ead6cc8b66205c26be27ee34fee331
João Pedro <joaopedro.nc@outlook.com>
2023-08-24 22:45:58 -0300

    💄 style:: Add CheckboxQuestion base styles

--- src/app/(private)/(app)/lesson/components/Quiz/CheckboxQuestion/index.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/CheckboxQuestion/index.tsx
@@ -1,6 +1,25 @@
+import { QuestionContainer } from '../QuestionContainer'
+import { QuestionTitle } from '../QuestionTitle'
 
-export function CheckboxQuestion() {
+import type { CheckboxQuestion as CheckboxQuestionData } from '@/types/quiz'
+import { Checkbox } from './Checkbox'
+
+interface CheckboxQuestionProps {
+  data: CheckboxQuestionData
+}
+
+export function CheckboxQuestion({
+  data: { title, picture, options },
+}: CheckboxQuestionProps) {
   return (
-    <div>index</div>
+    <QuestionContainer>
+      <QuestionTitle picture={picture}>{title}</QuestionTitle>
+
+      <ul className="space-y-2 mt-6">
+        {options.map((option) => (
+          <Checkbox key={option}>{option}</Checkbox>
+        ))}
+      </ul>
+    </QuestionContainer>
   )
 }
