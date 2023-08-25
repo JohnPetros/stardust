83d45cc45e00a32ceb6c663338f498da8c0fbc53
João Pedro <joaopedro.nc@outlook.com>
2023-08-24 22:57:41 -0300

    💄 style: Add animation to checkbox indicator

--- src/app/(private)/(app)/lesson/components/Quiz/CheckboxQuestion/Checkbox.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/CheckboxQuestion/Checkbox.tsx
@@ -1,24 +1,59 @@
+'use client'
+
 import { Check } from '@phosphor-icons/react'
 import * as C from '@radix-ui/react-checkbox'
 
+import { Variants, motion } from 'framer-motion'
 interface CheckboxProps {
   children: string
 }
 
+const checkboxAnimations: Variants = {
+  hover: {
+    scale: 1.02,
+  },
+  tap: {
+    scale: 0.99,
+  },
+}
+
+const indicatorAnimations: Variants = {
+  initial: {
+    rotate: 90,
+  },
+  rotate: {
+    rotate: 0,
+  },
+}
+
 export function Checkbox({ children }: CheckboxProps) {
   return (
-    <li className="rounded-md border border-gray-100 bg-purple-700">
-      <label htmlFor={children} className='flex items-center  p-3 w-full gap-3 cursor-pointer'>
+    <motion.li
+      variants={checkboxAnimations}
+      whileHover="hover"
+      whileTap="tap"
+      className="rounded-md border border-gray-100 bg-purple-700"
+    >
+      <label
+        htmlFor={children}
+        className="flex items-center  p-3 w-full gap-3 cursor-pointer"
+      >
         <C.Root
           id={children}
           className="rounded-md border border-gray-100 bg-transparent w-6 h-6"
         >
           <C.Indicator className="grid place-content-center">
-            <Check className="text-blue-300 text-lg" />
+            <motion.div
+              variants={indicatorAnimations}
+              initial="initial"
+              animate="rotate"
+            >
+              <Check className="text-blue-300 text-lg" weight='bold' />
+            </motion.div>
           </C.Indicator>
         </C.Root>
         <span className="text-gray-100">{children}</span>
       </label>
-    </li>
+    </motion.li>
   )
 }
