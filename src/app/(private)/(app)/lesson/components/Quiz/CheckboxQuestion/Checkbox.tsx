4665b0e0b36fc790008a11505333b27e160584d5
João Pedro <joaopedro.nc@outlook.com>
2023-08-24 22:49:59 -0300

    💄 style: Correct Checkbox component styles

--- src/app/(private)/(app)/lesson/components/Quiz/CheckboxQuestion/Checkbox.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/CheckboxQuestion/Checkbox.tsx
@@ -7,17 +7,17 @@ interface CheckboxProps {
 
 export function Checkbox({ children }: CheckboxProps) {
   return (
-    <li className="rounded-md border border-gray-100 bg-purple-700 flex items-center gap-3 p-3 w-full">
-      <C.Root
-        id={children}
-        className="rounded-md border border-gray-100 bg-transparent w-6 h-6"
-      >
-        <C.Indicator className="CIndicator">
-          <Check className="text-blue-300 text-lg" />
-        </C.Indicator>
-      </C.Root>
-      <label htmlFor={children} className="text-gray-100">
-        {children}
+    <li className="rounded-md border border-gray-100 bg-purple-700">
+      <label htmlFor={children} className='flex items-center  p-3 w-full gap-3 cursor-pointer'>
+        <C.Root
+          id={children}
+          className="rounded-md border border-gray-100 bg-transparent w-6 h-6"
+        >
+          <C.Indicator className="grid place-content-center">
+            <Check className="text-blue-300 text-lg" />
+          </C.Indicator>
+        </C.Root>
+        <span className="text-gray-100">{children}</span>
       </label>
     </li>
   )
