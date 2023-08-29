927813322f1d5335effbeae141647d3382016fcc
João Pedro <joaopedro.nc@outlook.com>
2023-08-29 20:13:25 -0300

    💄 style: Change operators default color

--- src/utils/constants/themes.ts
+++ src/utils/constants/themes.ts
@@ -9,7 +9,7 @@ export const THEMES: Theme = {
     keywords: '#0FE983',
     string: '#999999',
     comments: '#F1FA8C',
-    operators: '#022F43',
+    operators: '#1494cc',
     numbers: '#00D1FF',
     booleans: '#00D1FF',
     typeKeywords: '#0ac899',
