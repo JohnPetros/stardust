4c22990246b3b20c51a3fe3b1eedc15a64275c95
João Pedro <joaopedro.nc@outlook.com>
2023-08-30 18:04:49 -0300

    ➕ Add Code and Result components to Challenge page

--- src/app/(private)/(app)/(home)/challenges/[challengeId]/page.tsx
+++ src/app/(private)/(app)/(home)/challenges/[challengeId]/page.tsx
@@ -3,12 +3,14 @@
 import { useState } from 'react'
 import { useParams } from 'next/navigation'
 import { useChallenge } from '@/hooks/useChallenge'
+import { useAuth } from '@/hooks/useAuth'
 
 import { Slider } from '@/app/components/Slider'
 import { Slide } from '@/app/components/Slider/Slide'
 import { Header } from './components/Header'
 import { Problem } from './components/Problem'
-import { useAuth } from '@/hooks/useAuth'
+import { Code } from './components/Code'
+import { Result } from './components/Result'
 
 type Tab = 'problem' | 'code' | 'result'
 
@@ -21,17 +23,28 @@ export default function Challenge() {
     userId: user?.id,
   })
 
-  const [currentTab, setCurrentTab] = useState<Tab>('problem')
+  const [currentTab, setCurrentTab] = useState<Tab>('result')
 
   if (challenge)
     return (
-      <div>
+      <div className="h-full">
         <Header challengeTitle="Pedido de ajuda" />
-        <main>
+        <main className="h-full">
           <Slider onChange={null}>
             {currentTab === 'problem' && (
               <Slide id={'problem'}>
                 <Problem texts={challenge.texts} />
+              </Slide>
+            )}
+            {currentTab === 'code' && (
+              <Slide id={'code'}>
+                <Code code={challenge.code} />
+              </Slide>
+            )}
+
+            {currentTab === 'result' && (
+              <Slide id={'result'}>
+                <Result testCases={challenge.test_cases} />
               </Slide>
             )}
           </Slider>
