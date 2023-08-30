46ba628a3c88d8ec9fad4fbe485f6c4ab5a00d10
João Pedro <joaopedro.nc@outlook.com>
2023-08-30 09:20:03 -0300

    💄 style: Add animation to PopoverMenu component

--- src/app/components/PopoverMenu/index.tsx
+++ src/app/components/PopoverMenu/index.tsx
@@ -1,9 +1,25 @@
-import { ReactNode } from 'react'
+'use client'
+
+import { ReactNode, useState } from 'react'
 
 import * as Popover from '@radix-ui/react-popover'
 import { Checkbox } from './Checkbox'
 
+import { AnimatePresence, Variants, motion } from 'framer-motion'
 import { twMerge } from 'tailwind-merge'
+
+const popoverAnimation: Variants = {
+  up: {
+    opacity: 0,
+    y: -24,
+    x: 24,
+  },
+  down: {
+    opacity: 1,
+    y: 0,
+    x: 0,
+  },
+}
 
 export type PopoverMenuButton = {
   title: string
@@ -18,28 +34,53 @@ interface PopoverMenuProps {
 }
 
 export function PopoverMenu({ buttons, trigger }: PopoverMenuProps) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleTriggerClick() {
+    setIsOpen(true)
+  }
+
+  function handleOpenChange() {
+    if (isOpen) {
+      setIsOpen(false)
+    }
+  }
+
   return (
-    <Popover.Root>
-      <Popover.Trigger>{trigger}</Popover.Trigger>
-      <Popover.Content className="bg-gray-700 rounded-md p-3 w-40">
-        {buttons.map(({ title, isToggle, value }, index) => {
-          const isFirst = index === 0
+    <Popover.Root open={isOpen} onOpenChange={handleOpenChange}>
+      <Popover.Trigger onClick={handleTriggerClick}>{trigger}</Popover.Trigger>
+      <AnimatePresence>
+        {isOpen ? (
+          <Popover.Portal forceMount>
+            <Popover.Content className=" w-40">
+              <motion.div
+                variants={popoverAnimation}
+                initial="up"
+                animate="down"
+                exit="up"
+                className="bg-gray-700 rounded-md p-3 w-full h-full"
+              >
+                {buttons.map(({ title, isToggle, value }, index) => {
+                  const isFirst = index === 0
+                  return (
+                    <button
+                      key={title}
+                      className={twMerge(
+                        'flex items-center w-full text-left p-3 border-t text-gray-100 mr-auto',
+                        !isFirst ? 'border-green-400' : 'border-transparent'
+                      )}
+                    >
+                      {title}
 
-          return (
-            <button
-              key={title}
-              className={twMerge(
-                'flex items-center w-full text-left p-3 border-t text-gray-100 mr-auto',
-                !isFirst ? 'border-green-400' : 'border-transparent'
-              )}
-            >
-              {title}
-
-              {isToggle && <Checkbox isChecked={Boolean(value)} />}
-            </button>
-          )
-        })}
-      </Popover.Content>
+                      {isToggle && <Checkbox isChecked={Boolean(value)} />}
+                    </button>
+                  )
+                })}
+              </motion.div>
+            </Popover.Content>
+          </Popover.Portal>
+        ) : null}
+      </AnimatePresence>
     </Popover.Root>
   )
 }
