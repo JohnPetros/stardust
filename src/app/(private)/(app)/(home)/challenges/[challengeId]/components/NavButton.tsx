9e25ec4ef7cabb3f8952f01e7b40e8b5632e3f09
João Pedro <joaopedro.nc@outlook.com>
2023-08-30 09:57:17 -0300

    💄 style: Add NavButton styles

---
+++ src/app/(private)/(app)/(home)/challenges/[challengeId]/components/NavButton.tsx
@@ -0,0 +1,21 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface NavButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode
+  isActive: boolean
+}
+
+export function NavButton({ children, isActive, ...rest }: NavButtonProps) {
+  return (
+    <button
+      className={twMerge(
+        'bg-transparent text-center w-full py-3',
+        isActive ? 'text-green-400' : 'text-gray-100'
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
+}
