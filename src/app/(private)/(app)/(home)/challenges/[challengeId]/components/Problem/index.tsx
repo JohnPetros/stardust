9f95828647d81b997088130942e552fff11ed5a1
João Pedro <joaopedro.nc@outlook.com>
2023-08-30 14:44:14 -0300

    ➕ Add Texts to Problem component

---
+++ src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Problem/index.tsx
@@ -0,0 +1,18 @@
+import { Text } from '@/app/components/Text'
+import type { Text as TextData } from '@/types/text'
+
+interface ProblemProps {
+  texts: TextData[]
+}
+
+export function Problem({ texts }: ProblemProps) {
+  return (
+    <div className="p-6">
+      <div className='space-y-8'>
+        {texts.map((text, index) => (
+          <Text key={`text-${index}`} data={text} hasAnimation={true} />
+        ))}
+      </div>
+    </div>
+  )
+}
