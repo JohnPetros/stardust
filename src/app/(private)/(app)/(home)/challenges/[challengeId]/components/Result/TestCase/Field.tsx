2b34fe44901d3720b6c3259aea09fa95bcd26b77
João Pedro <joaopedro.nc@outlook.com>
2023-08-30 18:01:32 -0300

    💄 style:  Add Field component styles

---
+++ src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Result/TestCase/Field.tsx
@@ -0,0 +1,23 @@
+import { twMerge } from 'tailwind-merge'
+
+interface FieldProps {
+  label: string
+  value: string
+  isFromUser?: boolean
+}
+
+export function Field({ label, value, isFromUser }: FieldProps) {
+  return (
+    <>
+      <dt className='text-gray-300 font-medium'>{label}</dt>
+      <dd
+        className={twMerge(
+          'py-2 px-3 rounded-md font-semibold mt-1',
+          isFromUser ? 'bg-gray-500 text-gray-900' : 'bg-gray-700 text-gray-100'
+        )}
+      >
+        {value}
+      </dd>
+    </>
+  )
+}
