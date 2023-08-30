62823c520e232bd7995123f57e1efc1fb7a07b24
João Pedro <joaopedro.nc@outlook.com>
2023-08-30 20:58:31 -0300

    ♻️ refactor: Add Challenge context to Problem component

--- src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Result/index.tsx
+++ src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Result/index.tsx
@@ -1,18 +1,19 @@
+import { useChallengeContext } from '@/hooks/useChallengeContext'
 import { TestCase } from './TestCase'
-import type { TestCase as TestCaseData } from '@/types/challenge'
 
-interface ResultsProps {
-  testCases: TestCaseData[]
-}
+export function Result() {
+  const {
+    state: { challenge },
+  } = useChallengeContext()
 
-export function Result({ testCases }: ResultsProps) {
-  return (
-    <div className="w-full h-full bg-gray-800 p-6">
-      <div className="space-y-6">
-        {testCases.map((testCase) => (
-          <TestCase key={testCase.id} data={testCase} isCorrect={false} />
-        ))}
+  if (challenge)
+    return (
+      <div className="w-full h-full bg-gray-800 p-6">
+        <div className="space-y-6">
+          {challenge.test_cases.map((testCase) => (
+            <TestCase key={testCase.id} data={testCase} isCorrect={false} />
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
 }
