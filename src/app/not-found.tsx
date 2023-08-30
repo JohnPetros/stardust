d77f31948ab6127725cfbc787a73d0c6bebf859e
João Pedro <joaopedro.nc@outlook.com>
2023-08-30 07:48:26 -0300

    📱 Adapt not found page to mobile

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,26 +12,27 @@ export default function NotFound() {
 
   return (
     <main className="bg-gray-900 w-screen h-screen grid place-content-center">
-      <div className="w-full max-w-3xl items-center gap-10">
-        <div className="">
-          <h1 className="flex items-center gap-3 uppercase font-semibold text-green-400 text-6xl">
-            Código <span className='sr-only'>404</span>
-            <div className="-mt-[2rem]">
-              <Lottie animationData={PageNotFound} style={{ width: 240 }} />
-            </div>
+      <div className="w-full max-w-3xl">
+        <div className="flex flex-col sm:flex-row items-center gap-3 uppercase ">
+          <h1 className="font-semibold text-green-400 text-6xl">
+            Código <span className="sr-only">404</span>
           </h1>
 
-          <div className="-mt-6">
-            <p className="text-gray-300 text-lg leading-8">
-              Parece que você se perdeu no espaço! <br /> Clique abaixo para
-              puxarmos você de volta.
-            </p>
+          <div className="-mt-[2rem]">
+            <Lottie animationData={PageNotFound} style={{ width: 240 }} />
           </div>
-
-          <Button className="mt-6" onClick={handleButtonClick}>
-            Retornar
-          </Button>
         </div>
+
+        <div className="-mt-6">
+          <p className="text-gray-300 text-lg leading-8">
+            Parece que você se perdeu no espaço! <br /> Clique abaixo para
+            puxarmos você de volta.
+          </p>
+        </div>
+
+        <Button className="mt-6" onClick={handleButtonClick}>
+          Retornar
+        </Button>
       </div>
     </main>
   )
