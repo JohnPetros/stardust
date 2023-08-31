d47354d212b5cc060fdca361ece2df3e75763695
João Pedro <joaopedro.nc@outlook.com>
2023-08-30 21:01:02 -0300

    ➕ Add new Challenge Slider component

---
+++ src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Slider/index.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { useRef, useState } from 'react'
+
+import { register } from 'swiper/element/bundle'
+import { Swiper, SwiperSlide, SwiperRef } from 'swiper/react'
+import { Swiper as SwiperInstance } from 'swiper'
+
+import { NavButton } from './NavButton'
+import { Problem } from '../Problem'
+import { Code } from '../Code'
+import { Result } from '../Result'
+
+register()
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+export function Slider() {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0)
+  const sliderRef = useRef<SwiperRef>(null)
+
+  function handleNavButtonClick(slideIndex: number) {
+    sliderRef.current?.swiper.slideTo(slideIndex)
+  }
+
+  function handleSwipe(swiper: SwiperInstance) {
+    console.log(swiper.activeIndex)
+    setActiveSlideIndex(swiper.activeIndex)
+  }
+
+  return (
+    <div>
+      <nav className="bg-gray-900">
+        <ul className="grid grid-cols-3">
+          <li>
+            <NavButton
+              isActive={activeSlideIndex === 0}
+              onClick={() => handleNavButtonClick(0)}
+            >
+              Problema
+            </NavButton>
+          </li>
+          <li>
+            <NavButton
+              isActive={activeSlideIndex === 1}
+              onClick={() => handleNavButtonClick(1)}
+            >
+              Código
+            </NavButton>
+          </li>
+          <li>
+            <NavButton
+              isActive={activeSlideIndex === 2}
+              onClick={() => handleNavButtonClick(2)}
+            >
+              Resultado
+            </NavButton>
+          </li>
+        </ul>
+      </nav>
+
+      <Swiper ref={sliderRef} onSwiper={handleSwipe}>
+        <SwiperSlide>
+          <Problem />
+        </SwiperSlide>
+        
+        <SwiperSlide>
+          <Code />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <Result />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  )
+}
