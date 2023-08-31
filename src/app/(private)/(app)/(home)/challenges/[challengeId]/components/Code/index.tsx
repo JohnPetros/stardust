ebafe0c54aefa3ce3965781b5c502aeda94f56ef
João Pedro <joaopedro.nc@outlook.com>
2023-08-30 21:00:14 -0300

    ♻️ refactor: Add Challenge context to Code component

--- src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Code/index.tsx
+++ src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Code/index.tsx
@@ -10,35 +10,46 @@ import {
   Code as CodeIcon,
   Gear,
 } from '@phosphor-icons/react'
+import { useChallengeContext } from '@/hooks/useChallengeContext'
 
-interface CodeProps {
-  code: string
-}
+export function Code() {
+  const {
+    state: { challenge },
+  } = useChallengeContext()
 
-export function Code({ code }: CodeProps) {
-  return (
-    <div className="w-full h-full ">
-      <div className="flex items-center justify-between bg-gray-700 py-2 px-3">
-        <Button className="h-6 px-3 text-xs w-max">Executar</Button>
-        <ul className="flex items-center gap-3">
-          <li>
-            <IconButton icon={ArrowClockwise} onClick={() => {}} />
-          </li>
-          <li>
-            <IconButton icon={ArrowsOutSimple} onClick={() => {}} />
-          </li>
-          <li>
-            <IconButton icon={Command} onClick={() => {}} />
-          </li>
-          <li>
-            <IconButton icon={CodeIcon} onClick={() => {}} />
-          </li>
-          <li>
-            <IconButton icon={Gear} onClick={() => {}} />
-          </li>
-        </ul>
+  function handleCodeChange(value: string) {
+    console.log(value)
+  }
+
+  if (challenge)
+    return (
+      <div className="w-full h-full ">
+        <div className="flex items-center justify-between bg-gray-700 py-2 px-3">
+          <Button className="h-6 px-3 text-xs w-max">Executar</Button>
+          <ul className="flex items-center gap-3">
+            <li>
+              <IconButton icon={ArrowClockwise} onClick={() => {}} />
+            </li>
+            <li>
+              <IconButton icon={ArrowsOutSimple} onClick={() => {}} />
+            </li>
+            <li>
+              <IconButton icon={Command} onClick={() => {}} />
+            </li>
+            <li>
+              <IconButton icon={CodeIcon} onClick={() => {}} />
+            </li>
+            <li>
+              <IconButton icon={Gear} onClick={() => {}} />
+            </li>
+          </ul>
+        </div>
+        <CodeEditor
+          value={challenge.code}
+          width="100%"
+          height="100%"
+          onChange={handleCodeChange}
+        />
       </div>
-      <CodeEditor value={code} width="100%" height="100%" />
-    </div>
-  )
+    )
 }
