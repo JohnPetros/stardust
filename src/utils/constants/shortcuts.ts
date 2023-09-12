52d6ebd27a5eb53c17e702d50d7773103e08dd5f
João Pedro <joaopedro.nc@outlook.com>
2023-09-12 20:58:44 -0300

    🚚 Add shortcuts constant

---
+++ src/utils/constants/shortcuts.ts
@@ -0,0 +1,26 @@
+export const SHORTCUTS = [
+  {
+    action: 'Executar código',
+    command: 'Ctrl + Enter',
+  },
+  {
+    action: 'Deletar linha + copiar código',
+    command: 'Ctrl + X',
+  },
+  {
+    action: 'Comentar código',
+    command: 'Ctrl + .',
+  },
+  {
+    action: 'Apagar último código digitado',
+    command: 'Ctrl + Z',
+  },
+  {
+    action: 'Rescrever último código digitado',
+    command: 'Ctrl + SHIFT + Z',
+  },
+  {
+    action: 'Exibir dicionário',
+    command: 'Ctrl + K',
+  },
+]
