fe63f734f9116f5584bb5cea1a174a4b9f54f4c9
João Pedro <joaopedro.nc@outlook.com>
2023-09-12 20:36:19 -0300

    ♿ Focus back to button after modal close

--- src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Code/IconButton.tsx
+++ src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Code/IconButton.tsx
@@ -1,13 +1,25 @@
+'use client'
+
+import { RefObject } from 'react'
 import { Icon } from '@phosphor-icons/react'
 
 interface IconButtonProps {
+  buttonRef: RefObject<HTMLButtonElement>
   icon: Icon
   onClick: VoidFunction
 }
 
-export function IconButton({ icon: Icon, onClick }: IconButtonProps) {
+export function IconButton({
+  buttonRef,
+  icon: Icon,
+  onClick,
+}: IconButtonProps) {
   return (
-    <button className='grid place-content-center' onClick={onClick}>
+    <button
+      ref={buttonRef}
+      className="grid place-content-center"
+      onClick={onClick}
+    >
       <Icon className="text-green-500 text-xl" weight="bold" />
     </button>
   )
