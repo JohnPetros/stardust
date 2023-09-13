6016bb0e0bbcc195bf7d2947b05a82e5b980e037
João Pedro <joaopedro.nc@outlook.com>
2023-09-13 12:54:01 -0300

    🏷️ Add ThemeName type to themes constant

--- src/utils/constants/themes.ts
+++ src/utils/constants/themes.ts
@@ -1,11 +1,13 @@
+import { ThemeName } from "@/types/themeName"
+
 type Theme = {
-  [themeName: string]: {
+  [themeName in ThemeName]: {
     [token: string]: string
   }
 }
 
 export const THEMES: Theme = {
-  default: {
+  darkSpace: {
     keywords: '#0FE983',
     string: '#999999',
     comments: '#F1FA8C',
