0b5b6d542224c25364a22c15190a44c28201abc3
João Pedro <joaopedro.nc@outlook.com>
2023-09-13 13:33:45 -0300

    ➕ Add useEditor hook

---
+++ src/hooks/useEditor.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useContext } from 'react'
+import { EditorContext } from '@/contexts/EditorContext'
+
+export function useEditor() {
+  const context = useContext(EditorContext)
+
+  if (!context) {
+    throw new Error('useEditorContext must be used inside EditorContext')
+  }
+
+  return context
+}
