c730ea2f3b86bd765b1bb7c0a868a0a5926fabde
João Pedro <joaopedro.nc@outlook.com>
2023-09-13 09:03:01 -0300

    ♻️ refactor: Add Modal component inside Dialog component

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -7,13 +7,13 @@ import {
   useImperativeHandle,
   useState,
 } from 'react'
-import * as Dialog from '@radix-ui/react-dialog'
 import Lottie from 'lottie-react'
 
 import { motion, Variants } from 'framer-motion'
 
 import { playSound } from '@/utils/functions'
 import { MODAL_EFFECTS } from '@/utils/constants'
+import { Dialog, DialogTitle } from '../Dialog'
 
 export type ModalType = 'earning' | 'crying' | 'denying' | 'asking' | 'generic'
 
@@ -40,8 +40,6 @@ interface ModalProps {
   body: ReactNode
   footer: ReactNode
 }
-
-export const Container = Dialog.Root
 
 const ModalComponent = (
   { type, canPlaySong = true, title, body, footer }: ModalProps,
@@ -79,36 +77,19 @@ const ModalComponent = (
   }, [isOpen])
 
   return (
-    <Dialog.Root open={isOpen} onOpenChange={close}>
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 overflow-y-auto bg-black bg-opacity-50 z-50">
-          <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-h-screen w-full max-w-lg p-6">
-            <motion.div
-              variants={modalAnimations}
-              initial="close"
-              animate="open"
-              className="rounded-lg bg-gray-800 p-6 border border-gray-700"
-            >
-              <div className="flex items-center border-b border-gray-700 pb-2">
-                <Dialog.Title className="mx-auto font-semibold text-white text-center flex items-center justify-center">
-                  {title}
-                </Dialog.Title>
-              </div>
-              <div className="flex justify-center mt-3">
-                <Lottie
-                  animationData={animation}
-                  style={{ width: 200 }}
-                  loop={true}
-                />
-              </div>
+    <Dialog isOpen={isOpen} onClose={close}>
+      <div className="flex items-center border-b border-gray-700 pb-2">
+        <DialogTitle className="mx-auto font-semibold text-white text-center flex items-center justify-center">
+          {title}
+        </DialogTitle>
+      </div>
+      <div className="flex justify-center mt-3">
+        <Lottie animationData={animation} style={{ width: 200 }} loop={true} />
+      </div>
 
-              {body}
-              {footer}
-            </motion.div>
-          </Dialog.Content>
-        </Dialog.Overlay>
-      </Dialog.Portal>
-    </Dialog.Root>
+      {body}
+      {footer}
+    </Dialog>
   )
 }
 
