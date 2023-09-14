1451af19bbeccf7c4c0161de55bd290635ed3eeb
João Pedro <joaopedro.nc@outlook.com>
2023-09-14 13:16:10 -0300

    🚚 Add CHALLENGE_EARNINGS_BY_DIFFICULTY constant

---
+++ src/utils/constants/challenge-earnings-by-difficulty.ts
@@ -0,0 +1,14 @@
+export const CHALLENGE_EARNINGS_BY_DIFFICULTY = {
+  easy: {
+    coins: 10,
+    xp: 20,
+  },
+  medium: {
+    coins: 20,
+    xp: 30,
+  },
+  hard: {
+    coins: 30,
+    xp: 40,
+  },
+};
