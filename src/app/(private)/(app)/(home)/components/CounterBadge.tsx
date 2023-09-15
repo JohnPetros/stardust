de646d7c9f5b07988fcde7594d27b91be87936f3
João Pedro <joaopedro.nc@outlook.com>
2023-09-15 20:26:38 -0300

    🐛 fix: show counter badge only when counter is greater than 0

--- src/app/(private)/(app)/(home)/components/CounterBadge.tsx
+++ src/app/(private)/(app)/(home)/components/CounterBadge.tsx
@@ -3,9 +3,10 @@ interface CounterBadgeProps {
 }
 
 export function CounterBadge({ count }: CounterBadgeProps) {
-  return (
-    <div className="grid place-content-center bg-green-400/90 w-4 h-4 font-semibold text-green-900 text-sm rounded-md absolute top-0 right-0">
-      {count}
-    </div>
-  )
+  if (count > 0)
+    return (
+      <div className="grid place-content-center bg-green-400/90 w-4 h-4 font-semibold text-green-900 text-sm rounded-md absolute top-0 right-0">
+        {count}
+      </div>
+    )
 }
