9ff82a05ac04187bcca5d0994c1fa0f6a23b82cb
João Pedro <joaopedro.nc@outlook.com>
2023-09-20 20:55:04 -0300

    ✨ feat: register home page scroll

--- src/app/(private)/(app)/(home)/page.tsx
+++ src/app/(private)/(app)/(home)/page.tsx
@@ -5,13 +5,24 @@ import { Planet } from './components/Planet'
 import { useSiderbar } from '@/hooks/useSiderbar'
 import { useEffect, useState } from 'react'
 import { TransitionPageAnimation } from '../components/TransitionPageAnimation'
+import { useMotionValueEvent, useScroll } from 'framer-motion'
+import { useSpace } from '@/hooks/useSpace'
 
 export default function Home() {
+  const { lastUnlockedStarRef } = useSpace()
   const { planets, lastUnlockedStarId } = usePlanet()
   const { isOpen, toggle, setIsAchievementsListVisible } = useSiderbar()
   const [isTransitionVisible, setIsTransitionVisible] = useState(
     !planets?.length
   )
+
+  const { scrollY } = useScroll()
+
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    console.log(lastUnlockedStarRef.current?.getBoundingClientRect().top)
+    
+    console.log('Page scroll: ', latest)
+  })
 
   function handleClick() {
     if (isOpen) toggle()
@@ -31,7 +42,7 @@ export default function Home() {
       onClick={handleClick}
     >
       <TransitionPageAnimation isVisible={isTransitionVisible} />
-      <ul className=" max-w-[90vw] mt-10 flex flex-col items-start justify-center gap-12">
+      <ul className=" max-w-[75vw] mt-10 flex flex-col items-start justify-center gap-12">
         {planets?.map((planet) => (
           <Planet
             key={planet.id}
