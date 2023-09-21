05756c20e10f8a465f469025c1435e8f0ce4c399
João Pedro <joaopedro.nc@outlook.com>
2023-09-20 22:35:00 -0300

    💄 style: add animation to FabButton  component

--- src/app/(private)/(app)/(home)/components/FabButton.tsx
+++ src/app/(private)/(app)/(home)/components/FabButton.tsx
@@ -1,17 +1,42 @@
-import { Icon } from '@phosphor-icons/react'
+'use client'
 
-interface FabButtonProps {
-  icon: Icon
-  onCLick: () => void
+import { Icon } from '@phosphor-icons/react'
+import { motion, AnimatePresence, Variants } from 'framer-motion'
+
+const fabButtonAnimations: Variants = {
+  hidden: {
+    scale: 0,
+  },
+  visible: {
+    scale: 1,
+  },
+  tap: {
+    scale: 0.8,
+  },
 }
 
-export function FabButton({ icon: Icon, onCLick }: FabButtonProps) {
+interface FabButtonProps {
+  isVisible: boolean
+  icon: Icon
+  onClick: VoidFunction
+}
+
+export function FabButton({ isVisible, icon: Icon, onClick }: FabButtonProps) {
   return (
-    <button
-      onCLick={onCLick}
-      className="fixed right-24 bottom-8 grid place-content-center w-12 h-12 rounded-md border-b-2 border-green-500 bg-gray-900"
-    >
-      <Icon className="text-green-500 text-xl" weight="bold" />
-    </button>
+    <AnimatePresence>
+      {isVisible && (
+        <motion.button
+          variants={fabButtonAnimations}
+          initial="hiden"
+          animate="visible"
+          exit="hidden"
+          whileTap="tap"
+          onClick={onClick}
+          className="fixed right-24 bottom-8 grid place-content-center w-12 h-12 rounded-md border-b-2 border-green-500 bg-gray-900"
+        >
+          <Icon className="text-green-500 text-xl" weight="bold" />
+        </motion.button>
+      )}
+    </AnimatePresence>
   )
 }
