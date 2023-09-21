8f2cc3b5703d2be393e9a5780d6fd47439bf39f2
João Pedro <joaopedro.nc@outlook.com>
2023-09-21 18:14:30 -0300

    ✨ feat: add toolbar to CodeSnippet component

--- src/app/components/Text/CodeSnippet.tsx
+++ src/app/components/Text/CodeSnippet.tsx
@@ -1,9 +1,12 @@
 'use client'
 
-import { useMemo, useRef } from 'react'
+import { useMemo, useRef, useState } from 'react'
 import { CodeEditor, CodeEditorRef } from '@/app/components/CodeEditor'
-import { Button } from '@/app/components/Button'
 import { ArrowClockwise } from '@phosphor-icons/react'
+import { Console } from '../Console/Index'
+
+import * as ToolBar from '@radix-ui/react-toolbar'
+import { execute } from '@/libs/delegua'
 
 interface CodeSnippetProps {
   code: string
@@ -12,6 +15,21 @@ interface CodeSnippetProps {
 
 export function CodeSnippet({ code, isRunnable = false }: CodeSnippetProps) {
   const codeEditorRef = useRef<CodeEditorRef>(null)
+  const [output, setOutput] = useState<string[]>([])
+
+  function handleOutput(output: string) {
+    setOutput(currentOutput => [...currentOutput, output]);
+    if (!output) setOutput(['Sem resultado']);
+  }
+
+ async function handleRunButtonClick() {
+    try {
+      const { erros } = await execute(code, handleOutput)
+      
+    } catch (error) {
+      
+    }
+  }
 
   function handleReloadButtonClick() {
     codeEditorRef.current?.reloadValue()
@@ -23,20 +41,25 @@ export function CodeSnippet({ code, isRunnable = false }: CodeSnippetProps) {
   }, [code])
 
   return (
-    <div className="rounded-md bg-gray-800 w-full">
-      <div className="flex items-center justify-end gap-2 border-b border-gray-700 p-2">
+    <div className="overflow-hidden relative h-64 rounded-md bg-gray-800 w-full">
+      <ToolBar.Root className="flex items-center justify-end gap-2 border-b border-gray-700 p-2">
         {isRunnable && (
           <>
-            <Button
-              className="h-8 w-max px-4"
+            <ToolBar.Button
+              className="h-6 w-max px-4 items-center rounded bg-green-400"
               onClick={handleReloadButtonClick}
             >
               <ArrowClockwise className="text-green-900" weight="bold" />
-            </Button>
-            <Button className="h-8 w-max px-4 text-sm">Executar</Button>
+            </ToolBar.Button>
+            <ToolBar.Button
+              className="h-6 w-max px-4 text-xs items-center rounded bg-green-400 font-semibold"
+              onClick={handleRunButtonClick}
+            >
+              Executar
+            </ToolBar.Button>
           </>
         )}
-      </div>
+      </ToolBar.Root>
 
       <CodeEditor
         ref={codeEditorRef}
@@ -45,6 +68,8 @@ export function CodeSnippet({ code, isRunnable = false }: CodeSnippetProps) {
         value={code}
         isReadOnly={!isRunnable}
       />
+
+      <Console result="Teste de resultado" />
     </div>
   )
 }
