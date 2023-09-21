15adf32fadcd7cff622cec7a361c88f9a09d5000
João Pedro <joaopedro.nc@outlook.com>
2023-09-21 20:31:01 -0300

    ✨ feat: add close button to Console component

--- src/app/components/Console/Index.tsx
+++ src/app/components/Console/Index.tsx
@@ -1,46 +1,105 @@
 'use client'
 
-import { useState } from 'react'
-import { PanInfo, Variants, motion } from 'framer-motion'
+import {
+  ForwardedRef,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from 'react'
+import { PanInfo, Variants, motion, useAnimation } from 'framer-motion'
+import { CaretDown } from '@phosphor-icons/react'
 
 const consoleAnimations: Variants = {
   closed: {
-    y: 250,
+    y: '100%',
   },
   open: {
-    y: 0,
+    y: '40%',
   },
 }
 
 interface ConsoleProps {
-  result: string
+  results: string[]
 }
 
-export function Console({ result }: ConsoleProps) {
-  const [isOpen, setIsOpen] = useState(true)
+export interface ConsoleRef {
+  open: VoidFunction
+  close: VoidFunction
+}
+
+export function ConsoleComponent(
+  { results }: ConsoleProps,
+  ref: ForwardedRef<ConsoleRef>
+) {
+  const [outputs, setOutputs] = useState<string[]>([])
+  const controls = useAnimation()
+
+  function open() {
+    controls.start('open')
+  }
+
+  function close() {
+    controls.start('closed')
+  }
 
   function handleDragEnd(_: any, info: PanInfo) {
-    if (info.offset.y >= 100) {
-      setIsOpen(false)
+    if (info.velocity.y > 20 && info.offset.y >= 50) {
+      close()
     }
   }
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        open,
+        close,
+      }
+    },
+    []
+  )
+
+  useEffect(() => {
+    setOutputs([])
+    if (!results.length) return
+
+    const outputs = results.filter((_, index) => index % 2 !== 0)
+
+    setOutputs(outputs)
+  }, [results])
 
   return (
     <motion.div
       variants={consoleAnimations}
       initial="closed"
-      animate={isOpen ? "open" : "closed"}
+      animate={controls}
       drag="y"
-      dragConstraints={{ top: 10, bottom: 0 }}
-      dragElastic={0.8}
+      dragConstraints={{ top: 0 }}
+      dragElastic={0.4}
       dragMomentum={false}
       onDragEnd={handleDragEnd}
-      className="absolute -bottom-4 bg-gray-700 rounded-t-lg w-full min-h-[12rem]"
+      className="absolute -bottom-4 bg-gray-700 rounded-t-lg w-full min-h-[20rem] cursor-pointer"
     >
-      <div className="p-3 border-b border-gray-400">
-        <strong className="text-gray-200">Resultado</strong>
+      <div className="px-6 py-3 border-b border-gray-400">
+        <span className="block rounded-md mx-auto h-[2px] bg-gray-400 w-1/6"></span>
+        <div className="mt-1 flex items-center justify-between">
+          <strong className="text-gray-200">Resultado</strong>
+          <button onClick={close}>
+            <CaretDown className="text-lg text-gray-400" weight="bold" />
+          </button>
+        </div>
       </div>
-      <div className="p-3 text-gray-300">{result}</div>
+
+      <ul className="px-6 py-3">
+        {outputs.map((output, index) => (
+          <li key={`result-${index}`} className="block text-gray-300 text-sm">
+            {output}
+          </li>
+        ))}
+      </ul>
     </motion.div>
   )
 }
+
+export const Console = forwardRef(ConsoleComponent)
