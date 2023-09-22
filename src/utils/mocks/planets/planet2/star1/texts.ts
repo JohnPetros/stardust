553c883a1d8dcf4631d563fa7d845a22613fdbf2
João Pedro <joaopedro.nc@outlook.com>
2023-09-21 21:47:37 -0300

    🧪 test: correct planet 2 star 1 texts

--- src/utils/mocks/planets/planet2/star1/texts.ts
+++ src/utils/mocks/planets/planet2/star1/texts.ts
@@ -1,15 +1,9 @@
 export const texts = [
   {
-    content: `escreva('Eu encontrei um ' + 'planeta')
-// Resultado: Eu encontrei um planeta`,
-    type: 'code',
-    isRunnable: true,
-  },
-  {
     type: 'default',
     content:
       'Parece que sua mensagem foi enviada com sucesso para o planeta que você encontrou, mas você não obteve nenhuma resposta :(',
-    picture: 'panda-andando-com-bambu.jpg',
+    picture: 'panda-confuso.jpg',
   },
   {
     content: 'Mas, por quê?',
@@ -19,7 +13,7 @@ export const texts = [
     type: 'default',
     content:
       'Isso dever ter acontecido por causa do tipo de dado que você estava usando.',
-    picture: 'panda-andando-com-bambu.jpg',
+    picture: 'panda.jpg',
   },
   {
     content: 'Pera aí, tipo de dado?',
@@ -29,26 +23,26 @@ export const texts = [
     content:
       'Isso mesmo! Em programação podemos usar vários tipos de dados diferentes. Os mais básicos são tipo de texto, número e lógico.',
     type: 'default',
-    picture: 'panda-andando-com-bambu.jpg',
+    picture: 'panda-deslumbrado.jpg',
   },
   {
     content:
       'Por enquanto, vamos falar sobre o tipo texto, que é o que mais usamos até agora.',
     type: 'alert',
-    picture: 'panda-andando-com-bambu.jpg',
+    picture: 'panda.jpg',
   },
   {
     content:
       'O tipo de texto é utilizado para representar valores que são texto, dã! Eles podem ser compostos por uma ou mais letras, como também números ou símbolos.',
     type: 'quote',
     title: 'Tipo texto',
-    picture: 'panda-andando-com-bambu.jpg',
+    picture: 'panda.jpg',
   },
   {
     content:
       'Para fazer com que o programa entenda que uma informação é do tipo texto, basta colocá-la entre aspas, podendo ser simples `(\' \')` ou duplas `(" ")` assim como nós já fizemos antes.',
     type: 'default',
-    picture: 'panda-andando-com-bambu.jpg',
+    picture: 'panda-segurando-bambu-de-pe.jpg',
   },
   {
     content: `var mensagem = "Olá, mundo!"
@@ -60,13 +54,13 @@ var simbolos = '$@%&'`,
   },
   {
     content:
-      'Cuidado, o `250` no exemplo acima não do tipo número, mas sim do tipo texto, justavemente porque ele está entre aspas.',
+      'Cuidado, o `250` no exemplo acima não do tipo número, mas sim do tipo texto, justamente porque ele está entre aspas.',
     type: 'alert',
     picture: 'panda-andando-com-bambu.jpg',
   },
   {
     content:
-      'É possível unir textos usando o operador de `adição (+)`, ato que chamamos de concatenação.',
+      'É possível unir textos usando o operador de `adição (+)`, ato que chamamos de `concatenação`.',
     type: 'quote',
     title: 'Concatenação de textos',
     picture: 'panda-andando-com-bambu.jpg',
@@ -95,20 +89,20 @@ escreva('Eu encontrei um ' + 'planeta' + ' e ele se chama ' + nomePlaneta)
     type: 'quote',
     title: 'Interpolação',
 
-    picture: 'panda-andando-com-bambu.jpg',
+    picture: 'panda-segurando-bambu-de-pe.jpg',
   },
   {
     content: `var nomePlaneta = "Datahon"
-escreva("Esse planeta \${nomePlaneta} não parece ser amigável")
-// Resultado: Eu encontrei um planeta e ele se chama Datahon`,
+escreva("Esse planeta \${nomePlaneta} parece ser amigável")
+// Resultado: Esse planeta Datahon parece ser amigável`,
     type: 'code',
     isRunnable: true,
   },
   {
     content:
-      'Cuidado também: se você usar aspas duplas dentro de outras aspas duplas, não vai dar muito certo.',
+      'Cuidado também: se você usar aspas duplas dentro de outras aspas duplas, vai haver conflito.',
     type: 'alert',
-    picture: 'panda-andando-com-bambu.jpg',
+    picture: 'panda-segurando-bambu-de-pe.jpg',
   },
   {
     content: `escreva("Essa mensagem foi "estranha"")
@@ -121,7 +115,7 @@ escreva("Esse planeta \${nomePlaneta} não parece ser amigável")
     content:
       'A solução para essa situação é escrever aspas simples dentro de aspas duplas ou vice-versa.',
     type: 'alert',
-    picture: 'panda-andando-com-bambu.jpg',
+    picture: 'panda-olhando-computador.jpg',
   },
   {
     content: `escreva("Essa mensagem foi 'extranha'")
