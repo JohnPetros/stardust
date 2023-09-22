37067a70bb34a44a640fb8417c58f3a23d464b09
João Pedro <joaopedro.nc@outlook.com>
2023-09-22 18:06:30 -0300

    💄 style: flex horizontaly QuestionTitle elements

--- src/app/(private)/(app)/lesson/components/Quiz/QuestionTitle.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/QuestionTitle.tsx
@@ -13,13 +13,20 @@ export function QuestionTitle({ children, picture }: TitleProps) {
   const image = picture ? getImage('theory', picture) : ''
 
   return (
-    <>
+    <div className="flex items-center justify-center gap-3 w-full">
       {image && (
-        <div className="relative w-16 h-16">
-          <Image src={image} fill sizes='(min-width: 375px) 4rem' className="rounded-md" alt="" priority />
+        <div className="relative w-20 h-12">
+          <Image
+            src={image}
+            fill
+            sizes="(min-width: 375px) 4rem"
+            className="rounded-md"
+            alt=""
+            priority
+          />
         </div>
       )}
       <p className="text-gray-100 text-center font-medium mt-4">{children}</p>
-    </>
+    </div>
   )
 }
