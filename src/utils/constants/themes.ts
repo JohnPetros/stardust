e445d6208a8fc2e076f8195a1537f2d6aff95bdc
João Pedro <joaopedro.nc@outlook.com>
2023-09-22 17:28:10 -0300

    🏷️ type: add ThemeName and Token types to THEMES constant

--- src/utils/constants/themes.ts
+++ src/utils/constants/themes.ts
@@ -1,19 +1,21 @@
-import { ThemeName } from "@/types/themeName"
+import type { ThemeName } from '@/types/themeName'
+import type { Token } from '@/types/token'
 
 type Theme = {
   [themeName in ThemeName]: {
-    [token: string]: string
+    [token in Token]: string
   }
 }
 
 export const THEMES: Theme = {
   darkSpace: {
     keywords: '#0FE983',
-    string: '#999999',
-    comments: '#F1FA8C',
+    strings: '#F1FA8C',
+    comments: '#999999',
     operators: '#1494cc',
     numbers: '#00D1FF',
     booleans: '#00D1FF',
     typeKeywords: '#0ac899',
+    rest: '#EBEBEB',
   },
 }
