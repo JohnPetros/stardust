43c6ef24adf41da2dde63dcb66f8457d78dc248b
João Pedro <joaopedro.nc@outlook.com>
2023-09-24 09:52:54 -0300

    💄 style: adjust verification button height

--- src/app/(private)/(app)/lesson/components/Quiz/VerificationButton/index.tsx
+++ src/app/(private)/(app)/lesson/components/Quiz/VerificationButton/index.tsx
@@ -81,7 +81,7 @@ export function VerificationButton({
   return (
     <div
       className={twMerge(
-        'fixed bottom-0 border-t w-full px-6 py-4 flex flex-col items-center justify-center gap-2 ',
+        'fixed bottom-0 border-t w-full px-6 py-3 flex flex-col items-center justify-center gap-2 ',
         isAnswerVerified && !isAnswerCorrect
           ? 'border-red-700 bg-green-900'
           : 'border-green-500 bg-green-900'
