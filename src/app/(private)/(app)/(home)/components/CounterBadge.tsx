d3231c91ad200be4fa7100f45397d2822ad879f1
João Pedro <joaopedro.nc@outlook.com>
2023-09-29 11:17:51 -0300

    💄 style: add ping animation to CounterBadge component

--- src/app/(private)/(app)/(home)/components/CounterBadge.tsx
+++ src/app/(private)/(app)/(home)/components/CounterBadge.tsx
@@ -5,7 +5,7 @@ interface CounterBadgeProps {
 export function CounterBadge({ count }: CounterBadgeProps) {
   if (count > 0)
     return (
-      <div className="grid place-content-center bg-green-400/90 w-4 h-4 font-semibold text-green-900 text-sm rounded-md absolute top-0 right-0">
+      <div className="grid place-content-center bg-green-400/90 w-4 h-4 font-semibold text-green-900 text-sm rounded-md absolute top-0 right-0 animate-ping">
         {count}
       </div>
     )
