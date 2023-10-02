b530a97211456ab7ca096d07eb83091679b9b3a6
João Pedro <joaopedro.nc@outlook.com>
2023-10-02 17:39:37 -0300

    🧪 test: mock planet 3 star 1 texts

---
+++ src/utils/mocks/planets/planet3/star1/questions.ts
@@ -0,0 +1,64 @@
+export const questions = [
+  {
+    title: 'Vamos verificar se há alguém por perto para ajudar. Para isso, precisamos dos operadores aritméticos. Quais os operadores abaixo estão escritos da maneira correta em um programa?',
+    type: 'checkbox',
+    options: ['/', '+', '÷', 'x'],
+    correctOptions: ['/', '+'],
+    picture: 'panda-olhando-computador.jpg',
+  },
+    {
+      title: 'O foguete detectou uma quantidade de seres lá fora, no caso: `5 + 3 * 2`. Qual é esse número?',
+      type: 'selection',
+      options: ['11', '16', '13', '15'],
+      picture: 'panda-olhando-de-lado.jpg',
+    },
+  {
+    title: 'Foi recebido uma mensagem em forma de operação usando `módulo`: 50 % 2. Qual é esse resultado?',
+    type: 'selection',
+    answer: '0',
+    options: ['0', '25', '1', '100'],
+    picture: 'panda-olhando-de-lado.jpg',
+  },
+  {
+    title:
+      'Complete a linha abaixo com os operadores corretos, de modo que indique que o número do nosso foguete é o `13`.',
+    type: 'drag-and-drop',
+    lines: [
+      {
+        id: 1,
+        texts: ['var numero = ', '5 ', 'dropZone', ' 2 ', 'dropZone', ' 4', ' = 13'],
+        identation: 0,
+      },
+    ],
+    dragItems: [
+      { id: 1, label: '/' },
+      { id: 2, label: '+' },
+      { id: 3, label: '*' },
+      { id: 4, label: 'x' },
+      { id: 5, label: '-' },
+    ],
+    picture: 'panda-olhando-de-lado.jpg',
+    correctItemsIdsSequence: [2, 3],
+  },
+  {
+    title:
+      'Agora complete a linha de código com os números certos para que o resultado seja igual a `10`, indicando que temos 10 assentos disponíveis.',
+    type: 'drag-and-drop',
+    lines: [
+      {
+        id: 1,
+        texts: ['var assentos = ', 'dropZone', ' + ', 'dropZone', ' / ', '2', ' = 10'],
+        indentLevel: 0,
+      },
+    ],
+    dragItems: [
+      { id: 1, label: '4' },
+      { id: 2, label: '12' },
+      { id: 3, label: '6' },
+      { id: 4, label: '-4' },
+      { id: 5, label: '10' },
+    ],
+    picture: 'panda-olhando-de-lado.jpg',
+    correctItemsIdsSequence: [1, 2],
+  },
+]
