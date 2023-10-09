d0e7e08588c0bd2509c0eb901b12bef08588f6ab
João Pedro <joaopedro.nc@outlook.com>
2023-10-08 22:54:20 -0300

    ✨ feat (ui): add TabContent component

---
+++ src/app/(private)/(app)/(home)/challenges/[challengeId]/components/Problem/TabContent.tsx
@@ -0,0 +1,47 @@
+import { ReactNode } from 'react'
+import * as Tabs from '@radix-ui/react-tabs'
+
+import { Variants, motion } from 'framer-motion'
+
+const tabAnimations: Variants = {
+  exit: {
+    opacity: 0,
+    x: -40,
+  },
+  left: {
+    opacity: 1,
+    x: 0,
+  },
+  right: {
+    opacity: 0,
+    x: 40,
+  },
+}
+
+interface TabContent {
+  children: ReactNode
+  value: string
+  tabRef: (ref: HTMLDivElement | null) => void
+}
+
+export default function TabContent({ children, value, tabRef }: TabContent) {
+  return (
+    <Tabs.Content
+      ref={tabRef}
+      value={value}
+      className="h-[calc(100vh-8rem)] overflow-y-scroll overflow-hidden"
+      forceMount
+    >
+      <motion.div
+        key={value}
+        variants={tabAnimations}
+        initial="right"
+        animate="left"
+        exit="exit"
+        className="h-full"
+      >
+        {children}
+      </motion.div>
+    </Tabs.Content>
+  )
+}
