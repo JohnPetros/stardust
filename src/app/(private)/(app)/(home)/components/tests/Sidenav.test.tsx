0616913b1ef165e017e867cc9ddb8bf14b4ddccc
João Pedro <joaopedro.nc@outlook.com>
2023-10-29 10:32:21 -0300

    🧪 test: Sidenav component

---
+++ src/app/(private)/(app)/(home)/components/tests/Sidenav.test.tsx
@@ -0,0 +1,101 @@
+import { ToastProvider } from '@radix-ui/react-toast'
+import { TooltipProvider } from '@radix-ui/react-tooltip'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+
+import { Sidenav } from '../Sidenav'
+
+import { userMock } from '@/__tests__/mocks/userMock'
+import { AuthContext, AuthContextValue } from '@/contexts/AuthContext'
+import { SidebarContext, SidebarContextValue } from '@/contexts/SidebarContext'
+import { SupabaseProvider } from '@/contexts/SupabaseContext'
+import { HOME_PAGES } from '@/utils/constants'
+
+function renderSidenav(isExpanded: boolean = false) {
+  const isAchievementsListVisibleMock = false
+
+  const signOutMock = jest.fn()
+  const setIsAchievementsListVisibleMock = jest.fn()
+  const toggleSidenavMock = jest
+    .fn()
+    .mockImplementationOnce(() =>
+      setIsAchievementsListVisibleMock(!isAchievementsListVisibleMock)
+    )
+
+  render(
+    <SupabaseProvider>
+      <TooltipProvider>
+        <ToastProvider>
+          <AuthContext.Provider
+            value={
+              {
+                user: userMock,
+                signOut: signOutMock,
+              } as unknown as AuthContextValue
+            }
+          >
+            <SidebarContext.Provider
+              value={
+                {
+                  isAchievementsListVisible: isAchievementsListVisibleMock,
+                  setIsAchievementsListVisible:
+                    setIsAchievementsListVisibleMock,
+                } as unknown as SidebarContextValue
+              }
+            >
+              <Sidenav
+                isExpanded={isExpanded}
+                toggleSidenav={toggleSidenavMock}
+              />
+            </SidebarContext.Provider>
+          </AuthContext.Provider>
+        </ToastProvider>
+      </TooltipProvider>
+    </SupabaseProvider>
+  )
+
+  return { toggleSidenavMock, signOutMock, setIsAchievementsListVisibleMock }
+}
+
+describe('Side nav component', () => {
+  it('should toggle to true', async () => {
+    const { toggleSidenavMock, setIsAchievementsListVisibleMock } =
+      renderSidenav(false)
+
+    const button = screen.getByLabelText(/Expandir barra de navegação lateral/i)
+
+    fireEvent.click(button)
+
+    expect(toggleSidenavMock).toHaveBeenCalled()
+    expect(setIsAchievementsListVisibleMock).toHaveBeenCalledWith(true)
+  })
+
+  it('should show achievements list', async () => {
+    const { setIsAchievementsListVisibleMock } = renderSidenav()
+
+    const button = screen.getByText(/Conquistas/i)
+
+    fireEvent.click(button)
+
+    expect(setIsAchievementsListVisibleMock).toHaveBeenCalledWith(true)
+  })
+
+  it('should open modal when user try to sign out', async () => {
+    renderSidenav()
+
+    const button = screen.getByText(/Sair/i)
+
+    fireEvent.click(button)
+
+    await waitFor(() => {
+      expect(screen.getByRole('dialog')).toBeVisible()
+    })
+  })
+
+  it.each(HOME_PAGES)(
+    'should render $label page link label when expanded',
+    async ({ label }) => {
+      renderSidenav(true)
+      expect(screen.getByText(label)).toBeInTheDocument()
+    }
+  )
+})
