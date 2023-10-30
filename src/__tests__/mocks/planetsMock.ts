ae259c87791eed958b806966c57e0cf13b2b10de
João Pedro <joaopedro.nc@outlook.com>
2023-10-30 17:49:03 -0300

    🧪 test: mock planets

---
+++ src/__tests__/mocks/planetsMock.ts
@@ -0,0 +1,30 @@
+import { starsMock } from './starsMock'
+
+import { Planet } from '@/@types/planet'
+
+export const planetsMock: Planet[] = [
+  {
+    id: '1',
+    name: 'Mercury',
+    icon: 'mercury-icon.png',
+    image: 'mercury-image.jpg',
+    position: 1,
+    stars: starsMock,
+  },
+  {
+    id: '2',
+    name: 'Venus',
+    icon: 'venus-icon.png',
+    image: 'venus-image.jpg',
+    position: 2,
+    stars: [],
+  },
+  {
+    id: '3',
+    name: 'Earth',
+    icon: 'earth-icon.png',
+    image: 'earth-image.jpg',
+    position: 3,
+    stars: [],
+  },
+]
