af9d9c4756b9cab7ecc83fe4dd47ffe6dd21068c
João Pedro <joaopedro.nc@outlook.com>
2023-10-30 21:08:02 -0300

    🧪 test: add rockets mock

---
+++ src/__tests__/mocks/rocketsMock.ts
@@ -0,0 +1,26 @@
+import { Rocket } from '@/@types/rocket'
+
+export const rocketsMock: Rocket[] = [
+  {
+    id: '1',
+    image: '/rocket1.jpg',
+    name: 'Falcon 9',
+    price: 1000000,
+    isAcquired: true,
+  },
+  {
+    id: '2',
+    image: '/rocket2.jpg',
+    name: 'Saturn V',
+    price: 2000000,
+    isAcquired: false,
+  },
+  {
+    id: '3',
+    image: '/rocket3.jpg',
+    name: 'SpaceX Starship',
+    price: 3000000,
+    isAcquired: false,
+  },
+  // Adicione mais rockets conforme necessário
+]
