82bd1b3eafb8fb5f2799e40e603fce26cd32782d
João Pedro <joaopedro.nc@outlook.com>
2023-11-03 12:32:26 -0300

    🐛 fix: Resolve Alert hydration failure

--- src/app/components/Alert.tsx
+++ src/app/components/Alert.tsx
@@ -6,12 +6,14 @@ import {
   ReactNode,
   useEffect,
   useImperativeHandle,
+  useRef,
   useState,
 } from 'react'
 import * as AlertDialog from '@radix-ui/react-alert-dialog'
 import Lottie from 'lottie-react'
 
 import { DialogAnimation } from './Dialog'
+import { Hydration } from './Hydration'
 
 import { MODAL_EFFECTS } from '@/utils/constants'
 import { playSound } from '@/utils/helpers'
@@ -26,6 +28,7 @@ export interface AlertRef {
 interface AlertProps {
   type: AlertType
   canPlaySong?: boolean
+  canForceMount?: boolean
   title: string
   body: ReactNode
   action: ReactNode
@@ -48,6 +51,9 @@ const AlertComponent = (
   ref: ForwardedRef<AlertRef>
 ) => {
   const [isOpen, setIsOpen] = useState(false)
+
+  const [isRendered, setIsRendered] = useState(false)
+  const containerRef = useRef<HTMLElement | null>(null)
 
   const { animation, sound } = MODAL_EFFECTS.find(
     (animation) => animation.id === type.toLocaleLowerCase()
@@ -78,38 +84,45 @@ const AlertComponent = (
     }
   }, [isOpen, canPlaySong, type, sound])
 
-  return (
-    <AlertDialog.Root open={isOpen} onOpenChange={setIsOpen}>
-      <AlertDialog.Portal container={document.body}>
-        <AlertDialog.Overlay className="fixed inset-0 z-50 overflow-y-auto bg-black bg-opacity-50" />
-        <AlertDialog.Content
-          onCloseAutoFocus={onClose}
-          className="fixed left-1/2 top-1/2 z-50 max-h-screen w-full max-w-lg -translate-x-1/2 -translate-y-1/2 p-6"
-        >
-          <DialogAnimation>
-            <div className="flex items-center border-b border-gray-700 pb-2">
-              <AlertDialog.Title className="mx-auto flex items-center justify-center text-center font-semibold text-white">
-                {title}
-              </AlertDialog.Title>
-            </div>
-            <div className="mt-3 flex justify-center">
-              <Lottie
-                animationData={animation}
-                style={{ width: 200 }}
-                loop={true}
-              />
-            </div>
+  useEffect(() => {
+    containerRef.current = document.body
+    setIsRendered(true)
+  }, [])
 
-            {body}
-            <div className="mt-3 flex justify-center gap-2">
-              <AlertDialog.Action asChild>{action}</AlertDialog.Action>
-              <AlertDialog.Cancel>{cancel}</AlertDialog.Cancel>
-            </div>
-          </DialogAnimation>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-      <AlertDialog.Trigger>{children}</AlertDialog.Trigger>
-    </AlertDialog.Root>
+  return (
+    <Hydration>
+      <AlertDialog.Root open={isOpen} onOpenChange={setIsOpen}>
+        <AlertDialog.Portal container={containerRef.current}>
+          <AlertDialog.Overlay className="fixed inset-0 z-50 overflow-y-auto bg-black bg-opacity-50" />
+          <AlertDialog.Content
+            onCloseAutoFocus={onClose}
+            className="fixed left-1/2 top-1/2 z-50 max-h-screen w-full max-w-lg -translate-x-1/2 -translate-y-1/2 p-6"
+          >
+            <DialogAnimation>
+              <div className="flex items-center border-b border-gray-700 pb-2">
+                <AlertDialog.Title className="mx-auto flex items-center justify-center text-center font-semibold text-white">
+                  {title}
+                </AlertDialog.Title>
+              </div>
+              <div className="mt-3 flex justify-center">
+                <Lottie
+                  animationData={animation}
+                  style={{ width: 200 }}
+                  loop={true}
+                />
+              </div>
+
+              {body}
+              <div className="mt-3 flex justify-center gap-2">
+                <AlertDialog.Action asChild>{action}</AlertDialog.Action>
+                <AlertDialog.Cancel asChild>{cancel}</AlertDialog.Cancel>
+              </div>
+            </DialogAnimation>
+          </AlertDialog.Content>
+        </AlertDialog.Portal>
+        <AlertDialog.Trigger>{children}</AlertDialog.Trigger>
+      </AlertDialog.Root>
+    </Hydration>
   )
 }
 
