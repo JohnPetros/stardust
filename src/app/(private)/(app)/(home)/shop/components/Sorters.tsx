26c7a3e19005923db867e1de636bd89d1c735509
João Pedro <joaopedro.nc@outlook.com>
2023-11-03 15:07:56 -0300

    ✨ feat (ui): add Sorters component

---
+++ src/app/(private)/(app)/(home)/shop/components/Sorters.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+import { Order } from '@/@types/order'
+import { Select } from '@/app/components/Select'
+
+type PriceOrder = 'Menor preço' | 'Maior preço'
+
+interface SortersProps {
+  onPriceOrderChange: (order: Order) => void
+}
+
+export function Sorters({ onPriceOrderChange }: SortersProps) {
+  const [priceOrder, setPriceOrder] = useState<PriceOrder>('Menor preço')
+
+  function handlePriceOrderSelectChange(order: Order) {
+    setPriceOrder(order === 'ascending' ? 'Menor preço' : 'Maior preço')
+    onPriceOrderChange(order)
+  }
+
+  return (
+    <div>
+      <Select.Container
+        onValueChange={(value) => handlePriceOrderSelectChange(value as Order)}
+      >
+        <Select.Trigger value={priceOrder} />
+        <Select.Content>
+          <Select.Item value={'ascending'} text={'Menor preço'} />
+          <Select.Separator />
+          <Select.Item value={'descending'} text={'Maior preço'} />
+        </Select.Content>
+      </Select.Container>
+    </div>
+  )
+}
