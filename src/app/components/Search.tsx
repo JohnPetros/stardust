8056ce6317252527eb44f5b73c9d075769542561
João Pedro <joaopedro.nc@outlook.com>
2023-11-03 14:26:22 -0300

    💫 perf: add debounce on search input change

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,12 +1,23 @@
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, useState } from 'react'
 import { MagnifyingGlass } from '@phosphor-icons/react'
 import { twMerge } from 'tailwind-merge'
 
+import { useDebounce } from '@/hooks/useDebounce'
+
 interface SearchProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string
+  onSearchChange: (...args: any[]) => void  // eslint-disable-line
 }
 
-export function Search({ className, ...rest }: SearchProps) {
+export function Search({ className, onSearchChange, ...rest }: SearchProps) {
+  const [value, setValue] = useState('')
+  const debouceChange = useDebounce(onSearchChange, 450)
+
+  function handleValueChange(value: string) {
+    setValue(value)
+    debouceChange(value)
+  }
+
   return (
     <label
       htmlFor="search"
@@ -21,6 +32,8 @@ export function Search({ className, ...rest }: SearchProps) {
         type="search"
         id="search"
         className="w-full bg-transparent text-sm text-gray-300 outline-none placeholder:text-gray-400"
+        value={value}
+        onChange={({ target }) => handleValueChange(target.value)}
         {...rest}
       />
     </label>
