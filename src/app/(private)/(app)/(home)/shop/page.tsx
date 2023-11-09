ed9f82cd1d6921e022ace76b45a0c5dfc86f1411
João Pedro <joaopedro.nc@outlook.com>
2023-11-08 22:53:46 -0300

    ✨ feat (ux): add loading to shop page

--- src/app/(private)/(app)/(home)/shop/page.tsx
+++ src/app/(private)/(app)/(home)/shop/page.tsx
@@ -1,32 +1,26 @@
 'use client'
 
-import { Avatar } from './components/Avatar'
+import { useEffect, useState } from 'react'
+
+import { AvatarsSection } from './components/AvatarsSection'
 import { RocketsSection } from './components/RocketsSection'
 
 import { Loading } from '@/app/components/Loading'
-import { useAvatar } from '@/hooks/useAvatar'
 
 export default function Shop() {
-  const { avatars, addUserAcquiredAvatar } = useAvatar()
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3500)
+  }, [])
 
   return (
-    <div className="mx-auto max-w-5xl px-6">
-      <>
-        <RocketsSection />
-
-        <h2 className="mt-8 text-lg font-semibold text-white">Avatares</h2>
-        <div className="mt-3 grid grid-cols-1 justify-center gap-8 pb-12 sm:grid-cols-2 lg:grid-cols-2">
-          {avatars.map((avatar) => (
-            <Avatar
-              key={avatar.id}
-              data={avatar}
-              addUserAcquiredAvatar={addUserAcquiredAvatar}
-            />
-          ))}
-        </div>
-      </>
-
-      {/* {!rockets.length && <Loading isSmall={false} />} */}
+    <div className="mx-auto max-w-5xl space-y-12 px-6 pb-6">
+      {isLoading && <Loading isSmall={false} />}
+      <RocketsSection />
+      <AvatarsSection />
     </div>
   )
 }
