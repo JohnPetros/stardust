47669e7e6ea6631ba82cad58e8c6264800681cd8
João Pedro <joaopedro.nc@outlook.com>
2023-11-08 22:51:47 -0300

    🏷️ type: add IAvatarService interface

---
+++ src/services/api/interfaces/IAvatarService.ts
@@ -0,0 +1,19 @@
+import { Avatar } from '@/@types/avatar'
+import { FilterOptions } from '@/@types/filterOptions'
+import { Order } from '@/@types/order'
+
+type SortingOptions = {
+  priceOrder: Order
+}
+
+export interface IAvatarService {
+  getAvatars(
+    filterAndSortingOptions: FilterOptions & SortingOptions
+  ): Promise<{ avatars: Avatar[]; count: number | null }>
+  getAvatar(avatarId: string): Promise<Avatar>
+  getUserAcquiredAvatarsIds(userId: string): Promise<string[]>
+  addUserAcquiredAvatar(
+    avatarId: string,
+    userId: string
+  ): Promise<string | undefined>
+}
