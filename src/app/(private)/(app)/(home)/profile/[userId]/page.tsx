2b1c9275a6856f57031d991cb9b7a8aaed171ed9
João Pedro <joaopedro.nc@outlook.com>
2023-11-13 08:41:04 -0300

    💄 style: adapt padding bottom to mobile device

--- src/app/(private)/(app)/(home)/profile/[userId]/page.tsx
+++ src/app/(private)/(app)/(home)/profile/[userId]/page.tsx
@@ -28,7 +28,7 @@ export default function Profile() {
 
   if (user?.id)
     return (
-      <div className="mx-auto max-w-sm px-6 pb-12 pt-8 md:max-w-5xl">
+      <div className="mx-auto max-w-sm px-6 pb-32 pt-8 md:max-w-5xl md:pb-12">
         {isFistRendering && <Loading isSmall={false} />}
         {user?.id && (
           <div>
@@ -40,7 +40,7 @@ export default function Profile() {
                 streakAmount={user.streak}
               />
             </div>
-            <div className="mt-10 grid grid-cols-1 gap-2 md:grid-cols-[1fr_1.5fr]">
+            <div className="mt-10 grid grid-cols-1 gap-4 md:grid-cols-[1fr_1.5fr]">
               <div>
                 <h4 className="text-gray-100">Desafios concluídos</h4>
                 <ChallengesChart userId={user.id} />
