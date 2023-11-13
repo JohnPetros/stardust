70fb8444103aae25baf4431aeae21a9dcb1663e0
João Pedro <joaopedro.nc@outlook.com>
2023-11-13 08:40:44 -0300

    💄 style: adapt user achievements list to mobile

--- src/app/(private)/(app)/(home)/profile/components/Achievements.tsx
+++ src/app/(private)/(app)/(home)/profile/components/Achievements.tsx
@@ -19,7 +19,7 @@ export function Achievements({ userId }: AchievementsProps) {
 
   if (unlockedAchievements?.length) {
     return (
-      <div className="custom-scroll grid h-72 grid-cols-2 gap-4 overflow-y-auto">
+      <div className="custom-scroll grid h-72 grid-cols-1 gap-4 overflow-y-auto px-1 md:grid-cols-2">
         {unlockedAchievements.map((achivement) => (
           <>
             <Achievement
