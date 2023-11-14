d8b900e930a0cce161b98145276712c4b099ba4b
João Pedro <joaopedro.nc@outlook.com>
2023-11-14 15:55:29 -0300

    🏷️ type: add supabase type

--- src/contexts/SupabaseContext.tsx
+++ src/contexts/SupabaseContext.tsx
@@ -1,12 +1,10 @@
 'use client'
 
 import { createContext, ReactNode, useState } from 'react'
-import {
-  createClientComponentClient,
-  type SupabaseClient,
-} from '@supabase/auth-helpers-nextjs'
+import { SupabaseClient } from '@supabase/supabase-js'
+import { createClient } from 'supabase/supabase-browser'
 
-import type { Database } from '@/@types/supabase'
+import type { Database } from '@/@types/database'
 
 type SupabaseContextValue = {
   supabase: SupabaseClient<Database>
@@ -19,7 +17,7 @@ interface SupabaseProviderProps {
 export const SupabaseContext = createContext({} as SupabaseContextValue)
 
 export function SupabaseProvider({ children }: SupabaseProviderProps) {
-  const [supabase] = useState(() => createClientComponentClient())
+  const [supabase] = useState(() => createClient())
 
   return (
     <SupabaseContext.Provider value={{ supabase }}>
