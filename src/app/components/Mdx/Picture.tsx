068aad219c6a562bc41950d8b8082393e0bac3b2
João Pedro <joaopedro.nc@outlook.com>
2023-11-20 17:13:15 -0300

    ✨ feat (ui): add Picture mdx component

---
+++ src/app/components/Mdx/Picture.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+
+import { getImage } from '@/utils/helpers'
+
+interface PictureProps {
+  url: string
+}
+
+export function Picture({ url }: PictureProps) {
+  const image = getImage('theory', url)
+
+  return (
+    <div className="relative mr-3 overflow-hidden rounded-md md:h-16 md:w-24 ">
+      <Image
+        src={image}
+        alt="Panda"
+        className="skeleton h-auto w-auto"
+        sizes="(min-width: 375px) 5rem, (min-width: 769px) 6rem"
+        priority
+        fill
+        style={{ objectFit: 'cover' }}
+        onLoadingComplete={(image) => image.classList.remove('skeleton')}
+      />
+    </div>
+  )
+}
