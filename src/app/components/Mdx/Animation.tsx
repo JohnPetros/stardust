53db96a5cbbea8743d18e84e74034926148fa087
João Pedro <joaopedro.nc@outlook.com>
2023-11-20 17:13:25 -0300

    ✨ feat (ui): add Animation mdx component

---
+++ src/app/components/Mdx/Animation.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react'
+import { motion, Variants } from 'framer-motion'
+
+const textAnimations: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 24,
+    scale: 0.99,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    transition: {
+      delay: 0.3,
+      duration: 0.3,
+      type: 'tween',
+      ease: 'linear',
+    },
+  },
+}
+
+interface AnimationProps {
+  children: ReactNode
+  hasAnimation: boolean
+}
+
+export function Animation({ hasAnimation, children }: AnimationProps) {
+  return (
+    <motion.div
+      variants={textAnimations}
+      initial={hasAnimation && 'hidden'}
+      animate={hasAnimation && 'visible'}
+    >
+      {children}
+    </motion.div>
+  )
+}
