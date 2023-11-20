63d7d5e45b8e8d6b88ae4bb933020522fc9be7f9
João Pedro <joaopedro.nc@outlook.com>
2023-11-20 17:10:13 -0300

    ♻️ refactor: use ROUTES constant on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextRequest, NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 
+import { ROUTES } from './utils/constants'
 import { checkIsPublicRoute } from './utils/helpers'
 
-import type { Database } from '@/@types/supabase'
+import type { Database } from '@/@types/database'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
@@ -15,11 +16,11 @@ export async function middleware(req: NextRequest) {
   } = await supabase.auth.getUser()
 
   if (user && isPublicRoute) {
-    return NextResponse.redirect(new URL('/', req.url))
+    return NextResponse.redirect(new URL(ROUTES.private.home, req.url))
   }
 
   if (!user && !isPublicRoute) {
-    return NextResponse.redirect(new URL('/sign-in', req.url))
+    return NextResponse.redirect(new URL(ROUTES.public.signIn, req.url))
   }
 
   return res
