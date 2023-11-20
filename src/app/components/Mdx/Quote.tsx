e6fd69f1e4b278d3ff4c211ac579cca1060e4777
João Pedro <joaopedro.nc@outlook.com>
2023-11-20 17:12:52 -0300

    ✨ feat (ui): add Quote mdx component

---
+++ src/app/components/Mdx/Quote.tsx
@@ -0,0 +1,36 @@
+import { Animation } from './Animation'
+import { Content } from './Content'
+import { Picture } from './Picture'
+import { Title } from './Title'
+
+import { slugify } from '@/utils/helpers'
+
+interface TextProps {
+  title: string
+  picture: string
+  children: string
+  hasAnimation?: boolean
+}
+
+export function Quote({
+  title,
+  picture,
+  children,
+  hasAnimation = true,
+}: TextProps) {
+  return (
+    <Animation hasAnimation={hasAnimation}>
+      <div className="flex w-full items-center">
+        {title && (
+          <section id={slugify(title)} className="mb-4">
+            <Title>{title}</Title>
+          </section>
+        )}
+        {picture && <Picture url={picture} />}
+        <Content type="quote" hasAnimation={hasAnimation}>
+          {children}
+        </Content>
+      </div>
+    </Animation>
+  )
+}
