924ce0a861bcf215f5ef3da11e231b87d44269be
João Pedro <joaopedro.nc@outlook.com>
2023-11-20 17:16:02 -0300

    ♻️ refactor: fetch star data from server side

--- src/app/(private)/(app)/lesson/[starId]/page.tsx
+++ src/app/(private)/(app)/lesson/[starId]/page.tsx
@@ -1,147 +1,18 @@
-'use client'
+import { createClient } from 'supabase/supabase-server'
 
-import { useEffect, useRef, useState } from 'react'
-import { useParams, useRouter } from 'next/navigation'
+import { Layout } from '../components/Layout'
 
-import { PageTransitionAnimation } from '../../../../components/PageTransitionAnimation'
-import { End } from '../components/End'
-import { Header } from '../components/Header'
-import { Quiz } from '../components/Quiz'
-import { Theory } from '../components/Theory'
+import { StarService } from '@/services/api/starService'
 
-import { Alert, AlertRef } from '@/app/components/Alert'
-import { Button } from '@/app/components/Button'
-import { useStar } from '@/hooks/useStar'
-import { useLessonStore } from '@/stores/lessonStore'
-import { formatSecondsToTime } from '@/utils/helpers'
+type LessonProps = {
+  params: { starId: string }
+}
 
-export default function Lesson() {
-  const { starId } = useParams()
-  const { star, nextStar, updateUserData } = useStar(String(starId))
+export default async function Lesson({ params }: LessonProps) {
+  const supabase = createClient()
+  const starService = StarService(supabase)
 
-  const {
-    state: { currentStage, questions, incorrectAnswersAmount, secondsAmount },
-    actions: { incrementSecondsAmount, setQuestions, resetState },
-  } = useLessonStore()
+  const star = await starService.getStar(params.starId)
 
-  const [isTransitionVisible, setIsTransitionVisible] = useState(true)
-  const scrollRef = useRef<HTMLDivElement>(null)
-  const alertRef = useRef<AlertRef>(null)
-
-  const [coins, setCoins] = useState(0)
-  const [xp, setXp] = useState(0)
-  const [time, setTime] = useState('')
-  const [accurance, setAccurance] = useState('')
-
-  const router = useRouter()
-
-  function leaveLesson() {
-    router.push('/')
-  }
-
-  function getAccurance() {
-    const accurance =
-      ((questions.length - incorrectAnswersAmount) / questions.length) * 100
-    return accurance === 0 ? '100%' : accurance.toFixed(1) + '%'
-  }
-
-  function getCoins() {
-    if (!nextStar) return 0
-
-    let maxCoins = nextStar.isUnlocked ? 5 : 10
-    for (let i = 0; i < incorrectAnswersAmount; i++) {
-      maxCoins -= nextStar.isUnlocked ? 1 : 2
-    }
-    return maxCoins
-  }
-
-  function getXp() {
-    if (!nextStar) return 0
-
-    let maxXp = nextStar.isUnlocked ? 10 : 20
-    for (let i = 0; i < incorrectAnswersAmount; i++) {
-      maxXp -= nextStar.isUnlocked ? 2 : 5
-    }
-
-    return maxXp
-  }
-
-  useEffect(() => {
-    setTimeout(() => incrementSecondsAmount(), 1000)
-  }, [secondsAmount])
-
-  useEffect(() => {
-    let timer: NodeJS.Timeout
-
-    if (star) {
-      // dispatch({ type: 'setTexts', payload: star.texts })
-      setQuestions(star.questions)
-      timer = setTimeout(() => setIsTransitionVisible(false), 1000)
-    }
-
-    return () => {
-      resetState()
-      clearTimeout(timer)
-    }
-  }, [star])
-
-  useEffect(() => {
-    if (currentStage === 'end') {
-      setXp(getXp())
-      setCoins(getCoins())
-      setAccurance(getAccurance())
-      setTime(formatSecondsToTime(secondsAmount))
-    }
-  }, [currentStage])
-
-  return (
-    <>
-      <PageTransitionAnimation isVisible={isTransitionVisible} />
-      <main ref={scrollRef} className="relative overflow-x-hidden">
-        {currentStage !== 'end' && (
-          <Header onLeaveLesson={() => alertRef.current?.open()} />
-        )}
-
-        {star && nextStar && (
-          <>
-            {currentStage === 'theory' && (
-              <Theory title={star.name} number={star.number} />
-            )}
-            {currentStage === 'quiz' && <Quiz leaveLesson={leaveLesson} />}
-            {currentStage === 'end' && (
-              <End
-                coins={coins}
-                xp={xp}
-                time={time}
-                accurance={accurance}
-                userDataUpdater={updateUserData}
-                onExit={leaveLesson}
-              />
-            )}
-          </>
-        )}
-      </main>
-
-      <Alert
-        ref={alertRef}
-        type="crying"
-        title="Deseja mesmo sair da sua lição?"
-        canPlaySong={false}
-        body={null}
-        action={
-          <Button
-            className="w-32 bg-red-700 text-gray-100"
-            onClick={leaveLesson}
-          >
-            Sair
-          </Button>
-        }
-        cancel={
-          <Button className="w-32 bg-green-400 text-green-900" autoFocus>
-            Cancelar
-          </Button>
-        }
-      />
-    </>
-  )
+  return <Layout star={star} />
 }
---
+++ src/app/(private)/(app)/lesson/components/Layout.tsx
@@ -0,0 +1,159 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+
+import { PageTransitionAnimation } from '../../../../components/PageTransitionAnimation'
+
+import { End } from './End'
+import { Header } from './Header'
+import { Quiz } from './Quiz'
+import { Theory } from './Theory'
+
+import { Star } from '@/@types/star'
+import { Alert, AlertRef } from '@/app/components/Alert'
+import { Button } from '@/app/components/Button'
+import { useStar } from '@/hooks/useStar'
+import { useLessonStore } from '@/stores/lessonStore'
+import { formatSecondsToTime } from '@/utils/helpers'
+
+interface LayoutProps {
+  star: Star
+}
+
+export function Layout({ star }: LayoutProps) {
+  const { starId } = useParams()
+  const { nextStar, updateUserData } = useStar(String(starId))
+
+  const {
+    state: { currentStage, questions, incorrectAnswersAmount, secondsAmount },
+    actions: { incrementSecondsAmount, setTexts, setQuestions, resetState },
+  } = useLessonStore()
+
+  const [isTransitionVisible, setIsTransitionVisible] = useState(true)
+  const scrollRef = useRef<HTMLDivElement>(null)
+  const alertRef = useRef<AlertRef>(null)
+
+  const [coins, setCoins] = useState(0)
+  const [xp, setXp] = useState(0)
+  const [time, setTime] = useState('')
+  const [accurance, setAccurance] = useState('')
+
+  const router = useRouter()
+
+  function leaveLesson() {
+    router.push('/')
+  }
+
+  useEffect(() => {
+    setTimeout(() => incrementSecondsAmount(), 1000)
+  }, [secondsAmount, incrementSecondsAmount])
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout
+
+    if (star) {
+      setTexts(star.texts)
+      setQuestions(star.questions)
+      timer = setTimeout(() => setIsTransitionVisible(false), 1000)
+    }
+
+    return () => {
+      resetState()
+      clearTimeout(timer)
+    }
+  }, [star, resetState, setTexts, setQuestions])
+
+  useEffect(() => {
+    function getAccurance() {
+      const accurance =
+        ((questions.length - incorrectAnswersAmount) / questions.length) * 100
+      return accurance === 0 ? '100%' : accurance.toFixed(1) + '%'
+    }
+
+    function getCoins() {
+      if (!nextStar) return 0
+
+      let maxCoins = nextStar.isUnlocked ? 5 : 10
+      for (let i = 0; i < incorrectAnswersAmount; i++) {
+        maxCoins -= nextStar.isUnlocked ? 1 : 2
+      }
+      return maxCoins
+    }
+
+    function getXp() {
+      if (!nextStar) return 0
+
+      let maxXp = nextStar.isUnlocked ? 10 : 20
+      for (let i = 0; i < incorrectAnswersAmount; i++) {
+        maxXp -= nextStar.isUnlocked ? 2 : 5
+      }
+
+      return maxXp
+    }
+
+    if (currentStage === 'end') {
+      setXp(getXp())
+      setCoins(getCoins())
+      setAccurance(getAccurance())
+      setTime(formatSecondsToTime(secondsAmount))
+    }
+  }, [
+    currentStage,
+    incorrectAnswersAmount,
+    nextStar,
+    questions.length,
+    secondsAmount,
+  ])
+
+  return (
+    <>
+      <PageTransitionAnimation isVisible={isTransitionVisible} />
+      <main ref={scrollRef} className="relative overflow-x-hidden">
+        {currentStage !== 'end' && (
+          <Header onLeaveLesson={() => alertRef.current?.open()} />
+        )}
+
+        {star && nextStar && (
+          <>
+            {currentStage === 'theory' && (
+              <Theory title={star.name} number={star.number} />
+            )}
+            {currentStage === 'quiz' && <Quiz leaveLesson={leaveLesson} />}
+            {currentStage === 'end' && (
+              <End
+                coins={coins}
+                xp={xp}
+                time={time}
+                accurance={accurance}
+                userDataUpdater={updateUserData}
+                onExit={leaveLesson}
+              />
+            )}
+          </>
+        )}
+      </main>
+
+      <Alert
+        ref={alertRef}
+        type="crying"
+        title="Deseja mesmo sair da sua lição?"
+        canPlaySong={false}
+        body={null}
+        action={
+          <Button
+            className="w-32 bg-red-700 text-gray-100"
+            onClick={leaveLesson}
+          >
+            Sair
+          </Button>
+        }
+        cancel={
+          <Button className="w-32 bg-green-400 text-green-900" autoFocus>
+            Cancelar
+          </Button>
+        }
+      />
+    </>
+  )
+}
