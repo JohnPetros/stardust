10a3397fc5737801e7c5982aae5d52c98ab4281a
João Pedro <joaopedro.nc@outlook.com>
2023-11-30 07:46:29 -0300

    💫 perf: ensure mdx is compiled when useMdx hook is mounted

--- src/hooks/useMdx.tsx
+++ src/hooks/useMdx.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { MDXRemoteSerializeResult } from 'next-mdx-remote'
 
 import { useApi } from '@/services/api'
@@ -9,15 +9,21 @@ export function useMdx(content: string) {
   const api = useApi()
   const [source, setSource] = useState<MDXRemoteSerializeResult | null>(null)
 
-  const fetchSource = useCallback(async () => {
-    const source = await api.compileMdx(content)
-
-    setSource(source)
-  }, [content])
-
   useEffect(() => {
-    fetchSource()
-  }, [fetchSource])
+    let isMounted = true
+
+    async function fetchSource() {
+      const source = await api.compileMdx(content)
+
+      setSource(source)
+    }
+
+    if (isMounted) fetchSource()
+
+    return () => {
+      isMounted = false
+    }
+  }, [content, api])
 
   return source
 }
