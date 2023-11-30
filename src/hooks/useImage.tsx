cb68b3ba8dc865c3462e277f5b26bc97402cb572
João Pedro <joaopedro.nc@outlook.com>
2023-11-30 09:29:16 -0300

    🐛 fix: ensure fetch resource image when resource is not undefined

--- src/hooks/useImage.tsx
+++ src/hooks/useImage.tsx
@@ -1,5 +1,6 @@
 import { Folder } from '@/@types/folder'
 
-export function useImage(folder: Folder, resource: string) {
-  return `${process.env.NEXT_PUBLIC_CDN_URL}/${folder}/${resource}`
+export function useImage(folder: Folder, resource: string | undefined) {
+  if (folder && resource)
+    return `${process.env.NEXT_PUBLIC_CDN_URL}/${folder}/${resource}`
 }
