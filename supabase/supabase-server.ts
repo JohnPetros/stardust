bf7bbd10418f86fd32fa929f7cec471232c97b07
João Pedro <joaopedro.nc@outlook.com>
2023-12-04 09:31:27 -0300

    ✨feat: handle supabase env vars

--- supabase/supabase-server.ts
+++ supabase/supabase-server.ts
@@ -4,17 +4,20 @@ import { cookies } from 'next/headers'
 
 import type { Database } from '@/@types/database'
 
+const URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
 export const createClient = cache(() => {
+  if (!URL || !ANON_KEY) {
+    throw new Error('Supabase url and anon key must be provided')
+  }
+
   const cookieStore = cookies()
-  return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
+  return createServerClient<Database>(URL, ANON_KEY, {
+    cookies: {
+      get(name: string) {
+        return cookieStore.get(name)?.value
       },
-    }
-  )
+    },
+  })
 })
