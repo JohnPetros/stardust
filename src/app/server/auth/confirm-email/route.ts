4eb2e22e9b01231dd556cadc8967534511bc5724
João Pedro <joaopedro.nc@outlook.com>
2023-12-04 12:18:46 -0300

    ✨ feat (server): add confirmation email server route

--- src/app/server/auth/confirm-email/route.ts
+++ src/app/server/auth/confirm-email/route.ts
@@ -2,22 +2,30 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
+import { AuthService } from '@/services/api/authService'
 import { ROUTES } from '@/utils/constants'
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+  const token = requestUrl.searchParams.get('token')
 
-  console.log({ code })
+  if (!token)
+    return NextResponse.redirect(new URL(ROUTES.public.signIn, request.url))
 
-  // try {
-  //   if (code) {
-  //     const supabase = createRouteHandlerClient({ cookies })
-  //     await supabase.auth.exchangeCodeForSession(code)
-  //   }
-  // } catch (error) {
-  //   console.error('email confirmation', error)
-  // }
+  try {
+    if (token) {
+      const cookieStore = cookies()
+      const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+      const authService = AuthService(supabase)
+      const user = await authService.confirmEmail(token)
 
-  return NextResponse.redirect(ROUTES.public.signIn)
+      console.log(user)
+    }
+  } catch (error) {
+    return NextResponse.redirect(new URL(ROUTES.public.signIn, request.url))
+  }
+
+  return NextResponse.redirect(
+    new URL(ROUTES.private.emailConfirmation + '/555', request.url)
+  )
 }
