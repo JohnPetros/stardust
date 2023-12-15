7ac1ebb7ffbe194c3a095cb52ba3fee1576c94b4
João Pedro <joaopedro.nc@outlook.com>
2023-12-15 10:44:16 -0300

    ✨ feat (hooks): add usePagination hook

--- src/app/components/Pagination/index.tsx
+++ src/app/components/Pagination/index.tsx
@@ -2,33 +2,25 @@
 import { CaretLeft, CaretRight } from '@phosphor-icons/react'
 
 import PageButton from './PageButton'
+import { usePagination } from './usePagination'
 
-import { calculatePage } from '@/utils/helpers'
 
-interface PaginationProps {
+export interface PaginationProps {
   itemsPerPage: number
   totalItems: number
   offset: number
   setOffset: (offset: number) => void
 }
 
-const MAX_PAGE_BUTTONS = 5
-const SINBLING_PAGE_BUTTONS = (MAX_PAGE_BUTTONS - 1) / 2
-
-export function Pagination({
-  totalItems,
-  itemsPerPage,
-  offset,
-  setOffset,
-}: PaginationProps) {
-  const currentPage = calculatePage(offset, itemsPerPage)
-  const totalPages = Math.ceil(totalItems / itemsPerPage)
-  const firstPage = Math.max(currentPage - SINBLING_PAGE_BUTTONS, 1)
-
-  function handlePageButtonCLick(page: number) {
-    const newOffset = (page - 1) * itemsPerPage
-    setOffset(newOffset)
-  }
+export function Pagination(paginationProps: PaginationProps) {
+  const {
+    currentPage,
+    firstPage,
+    totalPages,
+    maxPageButtons,
+    siblingPageButtons,
+    handlePageButtonCLick,
+  } = usePagination(paginationProps)
 
   return (
     <div className="flex w-full space-x-3">
@@ -48,7 +40,7 @@ export function Pagination({
           1 ...
         </PageButton>
       )}
-      {Array.from({ length: Math.min(MAX_PAGE_BUTTONS, totalPages) }).map(
+      {Array.from({ length: Math.min(maxPageButtons, totalPages) }).map(
         (_, index) => {
           const page = index + firstPage
 
@@ -67,7 +59,7 @@ export function Pagination({
           return ''
         }
       )}
-      {currentPage + SINBLING_PAGE_BUTTONS < totalPages && (
+      {currentPage + siblingPageButtons < totalPages && (
         <PageButton
           isActive={false}
           isVisible={true}
---
+++ src/app/components/Pagination/usePagination.ts
@@ -0,0 +1,31 @@
+import { PaginationProps } from '.'
+
+import { calculatePage } from '@/utils/helpers'
+
+const MAX_PAGE_BUTTONS = 5
+const SINBLING_PAGE_BUTTONS = (MAX_PAGE_BUTTONS - 1) / 2
+
+export function usePagination({
+  totalItems,
+  itemsPerPage,
+  offset,
+  setOffset,
+}: PaginationProps) {
+  const currentPage = calculatePage(offset, itemsPerPage)
+  const totalPages = Math.ceil(totalItems / itemsPerPage)
+  const firstPage = Math.max(currentPage - SINBLING_PAGE_BUTTONS, 1)
+
+  function handlePageButtonCLick(page: number) {
+    const newOffset = (page - 1) * itemsPerPage
+    setOffset(newOffset)
+  }
+
+  return {
+    currentPage,
+    totalPages,
+    firstPage,
+    maxPageButtons: MAX_PAGE_BUTTONS,
+    siblingPageButtons: SINBLING_PAGE_BUTTONS,
+    handlePageButtonCLick,
+  }
+}
