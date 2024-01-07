2a298280bc933baf4b6b69a7ba7418ebefb94911
João Pedro <joaopedro.nc@outlook.com>
2024-01-07 20:16:49 -0300

    🏷️ type: add ResetPasswordFormFields type

--- src/libs/zod.ts
+++ src/libs/zod.ts
@@ -17,23 +17,35 @@ export const passwordSchema = z
   .nonempty(VALIDATION_ERRORS.nonempty)
   .regex(REGEX.password, VALIDATION_ERRORS.password.regex)
 
+export const passwordConfirmationSchema = z.string()
+
 export const signInFormSchema = z.object({
   email: emailSchema,
   password: passwordSchema,
 })
-
-export type SignInFormFields = z.infer<typeof signInFormSchema>
 
 export const signUpFormSchema = z
   .object({
     name: nameSchema,
     email: emailSchema,
     password: passwordSchema,
-    password_confirmation: z.string(),
+    password_confirmation: passwordConfirmationSchema,
   })
   .refine((fields) => fields.password === fields.password_confirmation, {
     path: ['password_confirmation'],
     message: VALIDATION_ERRORS.password_confirmation.equal,
   })
 
+export const resetPasswordFormSchema = z
+  .object({
+    password: passwordSchema,
+    password_confirmation: passwordConfirmationSchema,
+  })
+  .refine((fields) => fields.password === fields.password_confirmation, {
+    path: ['password_confirmation'],
+    message: VALIDATION_ERRORS.password_confirmation.equal,
+  })
+
+export type SignInFormFields = z.infer<typeof signInFormSchema>
 export type SignUpFormFields = z.infer<typeof signUpFormSchema>
+export type ResetPasswordFormFields = z.infer<typeof resetPasswordFormSchema>
