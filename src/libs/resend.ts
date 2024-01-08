03dede25219258b4549cae24e700075b18d76eb9
João Pedro <joaopedro.nc@outlook.com>
2024-01-08 16:15:46 -0300

    ✨ feat (libs): validate resend api key

--- src/libs/resend.ts
+++ src/libs/resend.ts
@@ -2,10 +2,14 @@ import { Resend } from 'resend'
 
 import { Email, EmailProvider } from '@/@types/emailProvider'
 
-const resend = new Resend()
+const API_KEY = process.env.RESEND_API_KEY
+
+if (!API_KEY) throw new Error('Resend API key must be provided')
+
+const resend = new Resend(API_KEY)
 
 export const resendProvider: EmailProvider = {
-  send(email: Email) {
+  async send(email: Email) {
     resend.emails.send({
       from: email.sender,
       to: email.recipient,
