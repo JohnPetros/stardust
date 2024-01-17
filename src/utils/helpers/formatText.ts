eb4d3a7eca2886ae02b1182a55eef809c5176244
João Pedro <joaopedro.nc@outlook.com>
2024-01-17 17:51:33 -0300

    ♻️ refactor: use REGEX constant in formatText helper

--- src/utils/helpers/formatText.ts
+++ src/utils/helpers/formatText.ts
@@ -1,7 +1,6 @@
 import { REGEX } from '../constants'
 
 export function formatText(text: string) {
-  console.log({ text })
   const backticksRegex = REGEX.backticks
-  return text.replace(/`([^`]+)`/g, '<span class="inline-code">$1</span>')
+  return text.replace(backticksRegex, '<span class="inline-code">$1</span>')
 }
