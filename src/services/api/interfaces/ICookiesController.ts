6001aef0a12c45ab29f9149f292d5e6171c72654
João Pedro <joaopedro.nc@outlook.com>
2024-01-17 17:11:59 -0300

    🗃️ file: remove useful cookies api files

--- src/services/api/interfaces/ICookiesController.ts
+++
@@ -1,5 +0,0 @@
-export interface ICookiesController {
-  setCookie(cookieName: string, cookieValue: string): Promise<void>
-  getCookie(cookieName: string): Promise<string | null>
-  deleteCookie(cookieName: string): Promise<void>
-}
