fa7d9bf268bda04c0fed405f28f5618dd37526b0
João Pedro <joaopedro.nc@outlook.com>
2024-01-17 17:16:19 -0300

    🔧 chore: add colors and screens breakpoints schemas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+import { breakpoints } from './src/styles/breakpoints'
+import { colors } from './src/styles/colors'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,56 +15,9 @@ module.exports = {
         code: 'var(--font-roboto-mono)',
       },
 
-      colors: {
-        green: {
-          900: '#07303B',
-          800: '#027558',
-          700: '#20925D',
-          600: '#38C76D',
-          500: '#0FE983',
-          400: '#00FF88',
-          300: '#4affab',
-          200: '#adffd9',
-          100: '#dafff0',
-        },
-        yellow: {
-          900: '#1c1400',
-          700: '#674F00',
-          500: '#B08C18',
-          400: '#FFCE31',
-        },
-        gray: {
-          950: '#0b0e0f',
-          900: '#141A1B',
-          800: '#1E2626',
-          700: '#303030',
-          600: '#595959',
-          500: '#868686',
-          400: '#AFB6C2',
-          300: '#d3d7e1',
-          200: '#EBEBEB',
-          100: '#f2f2f2',
-        },
-        blue: {
-          700: '#022F43',
-          300: '#00D1FF',
-        },
-        red: {
-          700: '#FF3737',
-          300: '#FF8484',
-        },
-        purple: {
-          700: '#514869',
-        },
-      },
+      colors: colors,
 
-      screens: {
-        xs: '440px',
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-      },
+      screens: breakpoints,
     },
   },
   plugins: [],
