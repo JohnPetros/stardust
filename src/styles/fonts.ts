a7dead0aafda40910c1b0bbdfc857b14534c8c38
João Pedro <joaopedro.nc@outlook.com>
2024-01-17 17:15:31 -0300

    refactor: move breakpoints set to a dedicated file

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,5 +1,10 @@
 import { Poppins, Roboto_Mono } from 'next/font/google'
 
+export const fonts = {
+  default: 'var(--font-poppins)',
+  code: 'var(--font-roboto-mono)',
+}
+
 export const poppins = Poppins({
   weight: ['400', '500', '600'],
   subsets: ['latin'],
