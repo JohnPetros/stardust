e6ee562eee36b7e63f7befa158109693fa35a1a0
João Pedro <joaopedro.nc@outlook.com>
2024-01-17 17:49:08 -0300

    ♻️ refactor: remove lesson rewards logic from useLesson hook

--- src/app/(private)/(app)/lesson/components/LessonStar/useLessonStar.ts
+++ src/app/(private)/(app)/lesson/components/LessonStar/useLessonStar.ts
@@ -1,17 +1,14 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
-import { useLesson } from '../../useLesson'
-
 import type { Star } from '@/@types/star'
+import { setCookie } from '@/app/server/actions/setCookie'
 import { useLessonStore } from '@/stores/lessonStore'
-import { formatSecondsToTime } from '@/utils/helpers'
+import { COOKIES, ROUTES } from '@/utils/constants'
 
 export function useLessonStar(star: Star) {
-  const { nextStar, updateUserData } = useLesson(star)
-
   const {
     state: { currentStage, questions, incorrectAnswersAmount, secondsAmount },
     actions: { setTexts, setQuestions, resetState },
@@ -20,15 +17,31 @@ export function useLessonStar(star: Star) {
   const [isTransitionVisible, setIsTransitionVisible] = useState(true)
   const scrollRef = useRef<HTMLDivElement>(null)
 
-  const [coins, setCoins] = useState(0)
-  const [xp, setXp] = useState(0)
-  const [time, setTime] = useState('')
-  const [accurance, setAccurance] = useState('')
-
   const router = useRouter()
 
+  const showRewards = useCallback(async () => {
+    const rewardsPayload = JSON.stringify({
+      star: {
+        seconds: secondsAmount,
+        incorrectAnswers: incorrectAnswersAmount,
+        questions: questions.length,
+        starSlug: star.slug,
+      },
+    })
+
+    await Promise.all([setCookie(COOKIES.rewardsPayload, rewardsPayload)])
+
+    router.push('/congratulations')
+  }, [
+    router,
+    secondsAmount,
+    incorrectAnswersAmount,
+    questions.length,
+    star.slug,
+  ])
+
   function leaveLesson() {
-    router.push('/')
+    router.push(ROUTES.private.home)
   }
 
   useEffect(() => {
@@ -47,51 +60,14 @@ export function useLessonStar(star: Star) {
   }, [star, resetState, setTexts, setQuestions])
 
   useEffect(() => {
-    function getAccurance() {
-      const accurance =
-        ((questions.length - incorrectAnswersAmount) / questions.length) * 100
-      return accurance === 0 ? '100%' : accurance.toFixed(1) + '%'
-    }
-
-    function getCoins() {
-      let maxCoins = nextStar && nextStar.isUnlocked ? 5 : 10
-      for (let i = 0; i < incorrectAnswersAmount; i++) {
-        maxCoins -= nextStar && nextStar.isUnlocked ? 1 : 2
-      }
-      return maxCoins
-    }
-
-    function getXp() {
-      let maxXp = nextStar && nextStar.isUnlocked ? 10 : 20
-      for (let i = 0; i < incorrectAnswersAmount; i++) {
-        maxXp -= nextStar && nextStar.isUnlocked ? 2 : 5
-      }
-
-      return maxXp
-    }
-
     if (currentStage === 'congratulations') {
-      setXp(getXp())
-      setCoins(getCoins())
-      setAccurance(getAccurance())
-      setTime(formatSecondsToTime(secondsAmount))
+      showRewards()
     }
-  }, [
-    currentStage,
-    incorrectAnswersAmount,
-    nextStar,
-    questions.length,
-    secondsAmount,
-  ])
+  }, [currentStage, showRewards])
 
   return {
     isTransitionVisible,
     scrollRef,
-    coins,
-    xp,
-    time,
-    accurance,
     leaveLesson,
-    updateUserData,
   }
 }
