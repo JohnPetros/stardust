a197fb44fa3d41dd4db24f1c9ff6b576aee0efcb
João Pedro <joaopedro.nc@outlook.com>
2024-01-18 13:06:54 -0300

    🏷️ type: add Sound type

--- src/utils/helpers/playSound.ts
+++ src/utils/helpers/playSound.ts
@@ -1,5 +1,17 @@
 'use client'
 
-export function playSound(sound: string) {
+type Sound =
+  | 'asking.wav'
+  | 'crying.wav'
+  | 'denying.wav'
+  | 'end.mp3'
+  | 'star.wav'
+  | 'success.wav'
+  | 'switch.wav'
+  | 'earning.wav'
+  | 'fail.wav'
+  | 'running-code.wav'
+
+export function playSound(sound: Sound) {
   new Audio(`/sounds/${sound}`).play()
 }
