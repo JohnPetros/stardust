ebba4043c875a6ae6dd2edbef044ff960c52494a
João Pedro <joaopedro.nc@outlook.com>
2024-01-19 12:51:42 -0300

    ✨ feat (hooks): add useSpace hook

---
+++ src/app/(private)/(app)/(home)/components/Space/useSpace.ts
@@ -0,0 +1,40 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { useSpaceContext } from '@/contexts/SpaceContext'
+import { useLocalStorage } from '@/hooks/useLocalStorage'
+import { STORAGE } from '@/utils/constants'
+
+export function useSpace() {
+  const localStorage = useLocalStorage()
+  const hasTransitionAnimation = localStorage.hasItem(
+    STORAGE.hasPageAnimationTransition
+  )
+
+  const { lastUnlockedStarPosition, scrollIntoLastUnlockedStar } =
+    useSpaceContext()
+
+  const [isTransitionVisible, setIsTransitionVisible] = useState(
+    hasTransitionAnimation
+  )
+
+  function handleFabClick() {
+    scrollIntoLastUnlockedStar()
+  }
+
+  useEffect(() => {
+    if (!isTransitionVisible) return
+
+    const timeout = setTimeout(() => setIsTransitionVisible(false), 3500)
+    localStorage.removeItem(STORAGE.hasPageAnimationTransition)
+
+    return () => clearTimeout(timeout)
+  }, [isTransitionVisible, localStorage])
+
+  return {
+    isTransitionVisible,
+    lastUnlockedStarPosition,
+    handleFabClick,
+  }
+}
