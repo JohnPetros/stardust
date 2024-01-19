141d601e2b52fe318cf533c48def748b34fd49d1
João Pedro <joaopedro.nc@outlook.com>
2024-01-19 12:53:55 -0300

    🗃️ file: rename home pages to home links

--- src/utils/constants/home-links.ts
+++ src/utils/constants/home-links.ts
@@ -1,34 +1,34 @@
 import { ROUTES } from './routes'
 
-type HomePage = {
+type HomeLink = {
   path: string
   label: string
   icon: string
 }
 
-export const HOME_PAGES: HomePage[] = [
+export const HOME_LINKS: HomeLink[] = [
   {
-    path: ROUTES.private.home,
+    path: ROUTES.private.home.space,
     label: 'Aprender',
     icon: 'learn.svg',
   },
   {
-    path: ROUTES.private.challenges,
+    path: ROUTES.private.home.challenges,
     label: 'Desafios',
     icon: 'challenges.svg',
   },
   {
-    path: ROUTES.private.shop,
+    path: ROUTES.private.home.shop,
     label: 'Loja',
     icon: 'shop.svg',
   },
   {
-    path: ROUTES.private.profile,
+    path: ROUTES.private.home.profile,
     label: 'Perfil',
     icon: 'profile.svg',
   },
   {
-    path: ROUTES.private.ranking,
+    path: ROUTES.private.home.ranking,
     label: 'Ranking',
     icon: 'ranking.svg',
   },
