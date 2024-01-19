c3d53635a8a8719a3ff3867f8b268369c4e88a03
João Pedro <joaopedro.nc@outlook.com>
2024-01-19 15:07:37 -0300

    ✨ feat (ux): add wrap shop page with Suspense

--- src/app/(private)/(app)/(home)/shop/page.tsx
+++ src/app/(private)/(app)/(home)/shop/page.tsx
@@ -1,28 +1,21 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { Suspense } from 'react'
 
 import { AvatarsList } from './components/AvatarsList'
 import { Footer } from './components/Footer'
-import { RocketsSection } from './components/RocketsSection'
+import { RocketsList } from './components/RocketsList'
 
 import { Loading } from '@/app/components/Loading'
 
 export default function Shop() {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 3500)
-  }, [])
-
   return (
     <div className="mx-auto max-w-5xl space-y-12 px-6 pb-[12rem] sm:pb-6">
-      {isLoading && <Loading isSmall={false} />}
-      <RocketsSection />
-      <AvatarsList />
-      <Footer />
+      <Suspense fallback={<Loading isSmall={false} />}>
+        <RocketsList />
+        <AvatarsList />
+        <Footer />
+      </Suspense>
     </div>
   )
 }
