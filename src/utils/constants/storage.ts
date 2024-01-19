dd9d1b538bc8c4d57b2b2705b977ef4ff180930e
João Pedro <joaopedro.nc@outlook.com>
2024-01-19 12:53:13 -0300

    🔧 chore: add STORAGE constant

---
+++ src/utils/constants/storage.ts
@@ -0,0 +1,6 @@
+import { APP_PREFIX } from './app-prefix'
+
+export const STORAGE = {
+  secondsCounter: `${APP_PREFIX}:seconds-counter`,
+  hasPageAnimationTransition: `${APP_PREFIX}:has-page-transition-animation`,
+}
