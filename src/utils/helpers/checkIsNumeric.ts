58c2bbf330c73b8635d4d055ed250a9317b4e6f9
João Pedro <joaopedro.nc@outlook.com>
2024-01-19 12:54:50 -0300

    🗃️ file: rename from checkIsPublicRoute to checkPublicRoute

--- src/utils/helpers/checkIsNumeric.ts
+++
@@ -1,3 +0,0 @@
-export function checkIsNumeric(string: string) {
-  return /^\d+$/.test(string)
-}
--- src/utils/helpers/checkPublicRoute.ts
+++ src/utils/helpers/checkPublicRoute.ts
@@ -1,6 +1,6 @@
 import { ROUTES } from '@/utils/constants/routes'
 
-export function checkIsPublicRoute(route: string) {
+export function checkPublicRoute(route: string) {
   const publicRoutes = Object.values(ROUTES.public)
 
   return publicRoutes.includes(route) || route === '/server'
