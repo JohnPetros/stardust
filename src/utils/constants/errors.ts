5c18d1708cdaacd3b354339b10bb14f9545bb0c5
João Pedro <joaopedro.nc@outlook.com>
2024-01-20 14:12:58 -0300

    ✨ feat (utils): add mdxFailedCompiling error to ERRORS constant

--- src/utils/constants/errors.ts
+++ src/utils/constants/errors.ts
@@ -1,5 +1,7 @@
 export const ERRORS = {
-  userNotFound: 'Usuário não encontrado',
-  rewardsPayloadNotFound: 'Premiações não computadas',
-  categoriesFailedFetching: 'Erro ao buscar categorias',
+  userNotFound: 'Usuário não encontrado.',
+  rewardsPayloadNotFound: 'Premiações não computadas.',
+  categoriesFailedFetching: 'Erro ao buscar categorias.',
+  challengeFailedFetching: 'Erro ao carregar desafio.',
+  mdxFailedCompiling: 'Não foi possível carregar o conteúdo de texto.',
 }
