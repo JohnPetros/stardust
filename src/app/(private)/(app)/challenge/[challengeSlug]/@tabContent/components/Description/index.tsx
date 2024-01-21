79e23a371456cebe66295e7d91ee1ab0e26fb4e6
João Pedro <joaopedro.nc@outlook.com>
2024-01-20 21:28:33 -0300

    ✨ feat (ux): add loading in Desxcription component

--- src/app/(private)/(app)/challenge/[challengeSlug]/@tabContent/components/Description/index.tsx
+++ src/app/(private)/(app)/challenge/[challengeSlug]/@tabContent/components/Description/index.tsx
@@ -2,12 +2,17 @@
 
 import { useDescription } from './useDescription'
 
+import { Loading } from '@/app/components/Loading'
 import { Mdx } from '@/app/components/Mdx'
 
 export function Description() {
-  const { mdx } = useDescription()
+  const { mdx, isLoading } = useDescription()
 
-  return (
+  return isLoading ? (
+    <div className="grid h-full place-content-center">
+      <Loading />
+    </div>
+  ) : (
     <div className="space-y-12 p-6">
       <Mdx>{mdx}</Mdx>
     </div>
--- src/app/(private)/(app)/challenge/[challengeSlug]/@tabContent/components/Description/useDescription.ts
+++ src/app/(private)/(app)/challenge/[challengeSlug]/@tabContent/components/Description/useDescription.ts
@@ -7,6 +7,7 @@ import { useChallengeStore } from '@/stores/challengeStore'
 
 export function useDescription() {
   const [mdx, setMdx] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
   const challenge = useChallengeStore((store) => store.state.challenge)
   const api = useApi()
 
@@ -23,18 +24,20 @@ export function useDescription() {
         )
 
         setMdx(compiledMdxComponents)
-
+        setIsLoading(false)
         return
       }
 
       const compiledDescription = await api.compileDescription(description)
       setMdx(compiledDescription)
+      setIsLoading(false)
     }
 
     fetchMdx()
   }, [challenge, api])
 
   return {
+    isLoading,
     mdx,
   }
 }
