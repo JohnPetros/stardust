abf0dc2eadffe4cb758e1dd6e1a10b6e4345b5d7
João Pedro <joaopedro.nc@outlook.com>
2024-01-20 21:28:09 -0300

    ✨ feat (ui): add Result component

---
+++ src/app/(private)/(app)/challenge/[challengeSlug]/@tabContent/components/Result/index.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { TestCase } from './TestCase'
+import { useResult } from './useResult'
+
+import { VerificationButton } from '@/app/(private)/(app)/lesson/components/Quiz/VerificationButton'
+
+export function Result() {
+  const {
+    challenge,
+    userOutput,
+    results,
+    isAnswerCorrect,
+    isAnswerVerified,
+    handleUserAnswer,
+  } = useResult()
+
+  if (challenge)
+    return (
+      <div className="relative h-full w-full scale-[1] bg-gray-800 blur-[1]">
+        <div className="h-auto space-y-6 p-6">
+          {challenge.test_cases.map((testCase, index) => (
+            <TestCase
+              key={testCase.id}
+              data={testCase}
+              isCorrect={results[index]}
+              userOutput={userOutput[index]}
+            />
+          ))}
+          {challenge.test_cases.map((testCase, index) => (
+            <TestCase
+              key={testCase.id}
+              data={testCase}
+              isCorrect={results[index]}
+              userOutput={userOutput[index]}
+            />
+          ))}
+          {challenge.test_cases.map((testCase, index) => (
+            <TestCase
+              key={testCase.id}
+              data={testCase}
+              isCorrect={results[index]}
+              userOutput={userOutput[index]}
+            />
+          ))}
+        </div>
+        <span className="block h-full w-full bg-gray-800" />
+        <VerificationButton
+          answerHandler={handleUserAnswer}
+          isAnswered={results.length > 0}
+          isAnswerVerified={isAnswerVerified}
+          isAnswerCorrect={isAnswerCorrect}
+        />
+      </div>
+    )
+}
--- src/app/(private)/(app)/challenge/[challengeSlug]/@tabContent/components/Result/useResult.ts
+++ src/app/(private)/(app)/challenge/[challengeSlug]/@tabContent/components/Result/useResult.ts
@@ -1,3 +1,84 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import type { TestCase as TestCaseData } from '@/@types/challenge'
+import { useBreakpoint } from '@/hooks/useBreakpoint'
+import { useChallengeStore } from '@/stores/challengeStore'
+import { compareArrays } from '@/utils/helpers'
+
 export function useResult() {
-  return {}
+  const {
+    state: {
+      challenge,
+      userOutput,
+      results,
+      isAnswerCorrect,
+      isAnswerVerified,
+      tabHandler,
+    },
+    actions: {
+      incrementIncorrectAswersAmount,
+      setIsAnswerVerified,
+      setIsAnswerCorrect,
+      setResults,
+      setIsEnd,
+    },
+  } = useChallengeStore()
+  // const { md: isMobile } = useBreakpoint()
+
+  function handleUserAnswer() {
+    setIsAnswerVerified(!isAnswerVerified)
+
+    const isAnswerCorrect =
+      results.length && results.every((result) => result === true)
+
+    if (isAnswerCorrect) {
+      setIsAnswerCorrect(true)
+      if (isAnswerVerified) setIsEnd(true)
+      return
+    }
+
+    setIsAnswerCorrect(false)
+
+    if (isAnswerVerified) {
+      incrementIncorrectAswersAmount()
+
+      // if (isMobile) tabHandler?.showCodeTab()
+    }
+  }
+
+  useEffect(() => {
+    if (!challenge) return
+
+    const { test_cases } = challenge
+
+    function verifyResult({ expectedOutput }: TestCaseData, index: number) {
+      const userResult = userOutput[index]
+
+      const isCorrect = compareArrays(
+        Array.isArray(userResult) ? userResult : [userResult.toString().trim()],
+        Array.isArray(expectedOutput)
+          ? expectedOutput
+          : [expectedOutput.toString().trim()]
+      )
+
+      return isCorrect
+    }
+
+    if (userOutput.length === test_cases.length) {
+      tabHandler?.showResultTab()
+
+      setResults(test_cases.map(verifyResult))
+    }
+  }, [userOutput, challenge, tabHandler, setResults])
+
+  return {
+    challenge,
+    userOutput,
+    results,
+    isAnswerVerified,
+    isAnswerCorrect,
+    handleUserAnswer,
+  }
 }
