c32639b130abef34ebbfff1248457c26fce12e24
João Pedro <joaopedro.nc@outlook.com>
2024-01-21 20:19:27 -0300

    ✨ feat (utils): add commentsListFailedFetching error

--- src/utils/constants/errors.ts
+++ src/utils/constants/errors.ts
@@ -1,7 +1,8 @@
 export const ERRORS = {
   userNotFound: 'Usuário não encontrado.',
-  rewardsPayloadNotFound: 'Premiações não computadas.',
+  rewardsPayloadNotFound: 'Premiações não processadas.',
   categoriesFailedFetching: 'Erro ao buscar categorias.',
   challengeFailedFetching: 'Erro ao carregar desafio.',
+  commentsListFailedFetching: 'Erro ao carregar lista de comentários.',
   mdxFailedCompiling: 'Não foi possível carregar o conteúdo de texto.',
 }
