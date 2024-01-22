74795270d0e19274dbd032be04c028ba682cc57f
João Pedro <joaopedro.nc@outlook.com>
2024-01-22 17:31:24 -0300

    ✨ feat (utils): add commentsListFailedFetching error

--- src/utils/constants/errors.ts
+++ src/utils/constants/errors.ts
@@ -4,5 +4,6 @@ export const ERRORS = {
   categoriesFailedFetching: 'Erro ao buscar categorias.',
   challengeFailedFetching: 'Erro ao carregar desafio.',
   commentsListFailedFetching: 'Erro ao carregar lista de comentários.',
+  repliesFailedFetching: 'Erro ao carregar as resposta para esse comentário.',
   mdxFailedCompiling: 'Não foi possível carregar o conteúdo de texto.',
 }
