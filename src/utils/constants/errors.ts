1ac19197de57fb580b641bc18273baf4f3a56d33
João Pedro <joaopedro.nc@outlook.com>
2024-01-22 20:59:51 -0300

    ✨ feat (utils): add comment crud errors

--- src/utils/constants/errors.ts
+++ src/utils/constants/errors.ts
@@ -4,6 +4,9 @@ export const ERRORS = {
   categoriesFailedFetching: 'Erro ao buscar categorias.',
   challengeFailedFetching: 'Erro ao carregar desafio.',
   commentsListFailedFetching: 'Erro ao carregar lista de comentários.',
+  commentFailedEdition: 'Não foi possível atualizar esse comentário',
+  commentFailedPost: 'Não foi possível postar esse comentário',
+  commentFailedDeletion: 'Não foi possível deletar esse comentário',
   repliesFailedFetching: 'Erro ao carregar as resposta para esse comentário.',
   mdxFailedCompiling: 'Não foi possível carregar o conteúdo de texto.',
 }
