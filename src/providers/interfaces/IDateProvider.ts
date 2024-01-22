de886ed6bc5440b2dd0d9e95fe89dcf484a588e0
João Pedro <joaopedro.nc@outlook.com>
2024-01-22 13:40:00 -0300

    🏷️ type: update DateFormat type

--- src/providers/interfaces/IDateProvider.ts
+++ src/providers/interfaces/IDateProvider.ts
@@ -1,4 +1,4 @@
-type DateFormat = 'Y-m-d H:i:s' | 'DD MMMM [de] YYYY'
+type DateFormat = 'Y-m-d H:i:s' | 'DD MMMM [de] YYYY' | 'DD/MM/YYYY'
 
 export interface IDateProvider {
   format(date: Date, dateFormat: DateFormat): string
