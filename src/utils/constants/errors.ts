9a5bd6d09cd7f4008957189c44b91ff81209c129
João Pedro <joaopedro.nc@outlook.com>
2024-01-23 09:05:57 -0300

    ✨ feat (utils): add more comment wise errors

--- src/utils/constants/errors.ts
+++ src/utils/constants/errors.ts
@@ -6,7 +6,11 @@ export const ERRORS = {
   commentsListFailedFetching: 'Erro ao carregar lista de comentários.',
   commentFailedEdition: 'Não foi possível atualizar esse comentário',
   commentFailedPost: 'Não foi possível postar esse comentário',
+  commentFailedReply: 'Não foi possível responder esse comentário',
   commentFailedDeletion: 'Não foi possível deletar esse comentário',
+  commentFailedUpvoting: 'Não foi possível dar um upvote nesse comentário',
+  commentFailedDesupvoting:
+    'Não foi possível remover o upvote desse comentário',
   repliesFailedFetching: 'Erro ao carregar as resposta para esse comentário.',
   mdxFailedCompiling: 'Não foi possível carregar o conteúdo de texto.',
 }
