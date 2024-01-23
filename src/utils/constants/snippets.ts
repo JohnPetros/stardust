027c2efc023d5cbcc3d29f692c6c07b5da56dd9f
João Pedro <joaopedro.nc@outlook.com>
2024-01-23 13:59:13 -0300

    ✨ feat (utils): add SNIPPETS constant

---
+++ src/utils/constants/snippets.ts
@@ -0,0 +1,6 @@
+export const SNIPPETS = {
+  strong: '**Insira seu texto em destaque aqui**',
+  code: '<code>Insira seu código aqui</code>',
+  runnableCode: '<Code>Insira seu código executável aqui</Code>',
+  link: '<Link url={Insira a url do link aqui}>o nome do link aqui</Link>',
+}
