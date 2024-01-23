df7b39c6091f0aa3c34826a0d96bf85a24bf8b95
João Pedro <joaopedro.nc@outlook.com>
2024-01-23 11:21:20 -0300

    ✨ feat (ui): add Link mdx component

---
+++ src/app/components/Mdx/Link.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+import NextLink from 'next/link'
+
+type LinkProps = {
+  url: string
+  href: string
+  children: ReactNode
+}
+
+export function Link({ children, href, url }: LinkProps) {
+  return (
+    <NextLink
+      href={href ?? url}
+      target="_blank"
+      className="text-sm font-medium text-green-600"
+    >
+      {children}
+    </NextLink>
+  )
+}
