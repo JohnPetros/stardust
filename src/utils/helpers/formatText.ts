d510d78c75ae7a884e391c6fea618cd73fa89c38
João Pedro <joaopedro.nc@outlook.com>
2024-01-24 17:50:29 -0300

    ♻️ refactor: replace backticks for asterisks on text formatting

--- src/utils/helpers/formatText.ts
+++ src/utils/helpers/formatText.ts
@@ -1,6 +1,8 @@
 import { REGEX } from '../constants'
 
 export function formatText(text: string) {
-  const backticksRegex = REGEX.backticks
-  return text.replace(backticksRegex, '<span class="inline-code">$1</span>')
+  const { betweenTwoAsterisks, betweenFourAsterisks } = REGEX
+  const strongComponent = '<span class="strong">$1</span>'
+
+  return text.replace(betweenTwoAsterisks, strongComponent).replace(betweenFourAsterisks, strongComponent)
 }
