e2b2ea015207dac515f0739dbaa2dd8e0f0d7bf4
João Pedro <joaopedro.nc@outlook.com>
2024-01-26 11:53:13 -0300

    ✨ feat (ui): add StreakBoard component

---
+++ src/app/components/StreakIcon/index.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Lottie from 'lottie-react'
+
+import StreakAnimation from '../../../../public/animations/streak.json'
+
+type StreakIconProps = {
+  size: number
+}
+
+export function StreakIcon({ size }: StreakIconProps) {
+  return (
+    <Lottie
+      animationData={StreakAnimation}
+      style={{ width: size }}
+      loop={false}
+    />
+  )
+}
