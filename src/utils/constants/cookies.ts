d1619656af32d060dc40dfa1152112b0c7ab988b
João Pedro <joaopedro.nc@outlook.com>
2024-01-30 10:42:12 -0300

    ✨ feat; (utils): add challengePanelsOffset to COOKIES constant

--- src/utils/constants/cookies.ts
+++ src/utils/constants/cookies.ts
@@ -3,4 +3,5 @@ import { APP_PREFIX } from './app-prefix'
 export const COOKIES = {
   shouldReturnPassword: `${APP_PREFIX}:should-return-password`,
   rewardsPayload: `${APP_PREFIX}:rewards-payload`,
+  challengePanelsOffset: `${APP_PREFIX}:challenge-panels-offset`,
 }
