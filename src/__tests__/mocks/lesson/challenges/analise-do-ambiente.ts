32a596bbd3e413c8de2d760a6111ef1cfeb6e5c1
João Pedro <joaopedro.nc@outlook.com>
2024-01-30 17:56:55 -0300

    🏷️ type: add Challenge to analise-do-ambiente challenge

--- src/__tests__/mocks/lesson/challenges/analise-do-ambiente.ts
+++ src/__tests__/mocks/lesson/challenges/analise-do-ambiente.ts
@@ -1,11 +1,20 @@
-export const challenge = {
+import { Challenge } from '@/@types/challenge'
+
+export const challenge: Challenge = {
+  id: 'Análise do ambiente',
+  slug: 'analise-do-ambiente',
+  description: '',
+  star_id: '2',
+  created_at: '',
+  categories: [],
+  total_completitions: 0,
+  isCompleted: true,
   title: 'Análise do ambiente',
   difficulty: 'easy',
   downvotes: 0,
   upvotes: 0,
-  total_completitions: 0,
-  created_by: 'Apollo',
-  topic_id: 'f60a0e67-c0b9-401a-a652-c9d5f8042ff1',
+  user_slug: 'apollo',
+  doc_id: 'f60a0e67-c0b9-401a-a652-c9d5f8042ff1',
   code: `var nome = "Datahon"
 var temperatura = 53.5
 var temOxigenio = falso
@@ -16,7 +25,7 @@ var tipoOxigenio
 
 escreva()`,
   function_name: null,
-  tests_cases: [
+  test_cases: [
     {
       id: 1,
       input: [],
@@ -39,13 +48,13 @@ escreva()`,
     {
       type: 'default',
       content:
-        'Isso o seu foguete já fez de antemão, retornando para você dados do planeta como nome, temperatura e se tem ar respirável.',
+        'Isso o seu foguete já fez de antemão, retornando para você dados do planeta como *nome*, *temperatura* e *se tem tipo Oxigênio*.',
       picture: 'panda-olhando-computador.jpg',
     },
     {
       type: 'quote',
       content:
-        'Sua missão é escrever qual o tipo de cada um dos daods na ordem em que são declarados, veja um exemplo:',
+        'Sua missão é escrever qual o tipo de cada um dos dados na ordem em que são declarados, veja um exemplo:',
       picture: 'panda-olhando-computador.jpg',
     },
     {
@@ -54,15 +63,28 @@ escreva()`,
 var nomeEstrela = "Proxima Centauri"
 var temperatura = 100
 var temCorAmarela = falso
-escreva("\${nomeEstrela}: texto, \${temperatura}: numero, \${temCorAmarela}: falso")
+escreva(
+  "\${nomeEstrela}: texto, \${temperatura}: numero, \${temCorAmarela}: falso"
+)
 
-// Resultado: Proxima Centauri: texto, 100: número, falso: lógico`,
+/* Resultado: 
+   Proxima Centauri: texto, 100: número, falso: lógico
+*/
+
+`,
       picture: 'panda.jpg',
+      isRunnable: false,
     },
     {
       type: 'alert',
       content:
-        'Dica: você pode colocar o nome dos tipos em variáveis e concatená-las com os textos usando `interpolação` ou operador de adição.',
+        'Dica: você pode colocar o nome dos tipos em variáveis e concatená-las com os textos usando *interpolação* ou *operador de adição*.',
+      picture: 'panda-sorrindo.jpg',
+    },
+    {
+      type: 'alert',
+      content:
+        'Você pode ver como deve ser o resultado esperado clicando na aba de *resultado* acima',
       picture: 'panda-sorrindo.jpg',
     },
   ],
