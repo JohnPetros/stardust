d8f3b4c9469fc1dced5806dbb7f2cd6ffa3e65be
João Pedro <joaopedro.nc@outlook.com>
2024-01-30 10:42:39 -0300

    ✨ feat (utils): add challengeLayout to STORAGE constant

--- src/utils/constants/storage.ts
+++ src/utils/constants/storage.ts
@@ -5,4 +5,5 @@ export const STORAGE = {
   secondsCounter: `${APP_PREFIX}:seconds-counter`,
   hasPageAnimationTransition: `${APP_PREFIX}:has-page-transition-animation`,
   shouldFormatConsoleOutput: `${APP_PREFIX}:should-format-console-output`,
+  challengeLayout: `${APP_PREFIX}:challenge-layout`,
 }
