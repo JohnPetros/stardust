2adcf38ac0e0e0b0b44c8426b6c800145af1f6e1
João Pedro <joaopedro.nc@outlook.com>
2024-01-30 15:28:28 -0300

    ✨ feat (ux): add challenge layout value to local storage

--- src/stores/challengeStore.ts
+++ src/stores/challengeStore.ts
@@ -1,8 +1,14 @@
-import { create } from 'zustand'
+import { create, StateCreator } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
 import { immer } from 'zustand/middleware/immer'
 
 import type { Challenge } from '@/@types/challenge'
 import type { Vote } from '@/@types/vote'
+
+export type Layout =
+  | 'tabs-right;code_editor-left'
+  | 'tabs-left;code_editor-right'
+  | 'code_editor-full'
 
 type TabHandler = {
   showResultTab: VoidFunction
@@ -21,6 +27,7 @@ type ChallengeState = {
   isEnd: boolean
   incorrectAnswersAmount: number
   isFirstRendering: boolean
+  layout: Layout
   tabHandler: TabHandler | null
 }
 
@@ -35,6 +42,7 @@ type ChallengeActionss = {
   setUserVote: (useVote: Vote) => void
   setIsEnd: (isEnd: boolean) => void
   setTabHandler: (tabHandler: TabHandler) => void
+  setLayout: (layout: Layout) => void
   setIsFirstRendering: (isFirstRendering: boolean) => void
   incrementIncorrectAswersAmount: () => void
   resetState: () => void
@@ -57,78 +65,98 @@ const initialState: ChallengeState = {
   isEnd: false,
   incorrectAnswersAmount: 0,
   tabHandler: null,
+  layout: 'tabs-left;code_editor-right',
   isFirstRendering: true,
 }
+const challengeStore: StateCreator<
+  ChallengeStore,
+  [['zustand/persist', unknown], ['zustand/immer', never]],
+  [],
+  ChallengeStore
+> = (set) => ({
+  state: initialState,
+  actions: {
+    setChallenge(challenge: Challenge) {
+      return set(({ state }: ChallengeStore) => {
+        state.challenge = challenge
+      })
+    },
+    setUserCode: (userCode: string) =>
+      set(({ state }: ChallengeStore) => ({
+        state: { ...state, userCode },
+      })),
+    setUserOutput: (userOutput: string[]) =>
+      set(({ state }: ChallengeStore) => ({
+        state: { ...state, userOutput },
+      })),
+    setUserVote: (userVote: Vote) =>
+      set(({ state }: ChallengeStore) => ({
+        state: { ...state, userVote },
+      })),
+    setResults: (results: boolean[]) =>
+      set(({ state }: ChallengeStore) => ({
+        state: { ...state, results },
+      })),
+    setMdx: (mdx: string) =>
+      set(({ state }: ChallengeStore) => ({
+        state: { ...state, mdx },
+      })),
+    setLayout: (layout: Layout) =>
+      set(({ state }: ChallengeStore) => ({
+        state: { ...state, layout },
+      })),
+    setTabHandler: (tabHandler: TabHandler) =>
+      set(({ state }: ChallengeStore) => ({
+        state: { ...state, tabHandler },
+      })),
+    setIsAnswerVerified: (isAnswerVerified: boolean) =>
+      set(({ state, actions }: ChallengeStore) => ({
+        state: { ...state, isAnswerVerified },
+        actions,
+      })),
+    setIsAnswerCorrect: (isAnswerCorrect: boolean) =>
+      set(({ state, actions }: ChallengeStore) => ({
+        state: { ...state, isAnswerCorrect },
+        actions,
+      })),
+    setIsEnd: (isEnd: boolean) =>
+      set(({ state, actions }: ChallengeStore) => ({
+        state: { ...state, isEnd },
+        actions,
+      })),
+    setIsFirstRendering: (isFirstRendering: boolean) =>
+      set(({ state, actions }: ChallengeStore) => ({
+        state: { ...state, isFirstRendering },
+        actions,
+      })),
+    incrementIncorrectAswersAmount: () =>
+      set(({ state }: ChallengeStore) => {
+        const incorrectAnswersAmount =
+          state.incorrectAnswersAmount === 4
+            ? state.incorrectAnswersAmount
+            : state.incorrectAnswersAmount + 1
+        return {
+          state: { ...state, incorrectAnswersAmount },
+        }
+      }),
+    resetState: () =>
+      set(() => ({
+        state: initialState,
+      })),
+  },
+})
 
-export const useChallengeStore = create<ChallengeStore>()(
-  immer((set) => {
-    return {
-      state: initialState,
-      actions: {
-        setChallenge(challenge: Challenge) {
-          return set(({ state }: ChallengeStore) => {
-            state.challenge = challenge
-          })
+export const useChallengeStore = create(
+  persist(immer(challengeStore), {
+    version: 1,
+    name: 'challenge-store',
+    storage: createJSONStorage(() => localStorage),
+    partialize: (store) => {
+      return {
+        state: {
+          layout: store.state.layout,
         },
-        setUserCode: (userCode: string) =>
-          set(({ state }: ChallengeStore) => ({
-            state: { ...state, userCode },
-          })),
-        setUserOutput: (userOutput: string[]) =>
-          set(({ state }: ChallengeStore) => ({
-            state: { ...state, userOutput },
-          })),
-        setUserVote: (userVote: Vote) =>
-          set(({ state }: ChallengeStore) => ({
-            state: { ...state, userVote },
-          })),
-        setResults: (results: boolean[]) =>
-          set(({ state }: ChallengeStore) => ({
-            state: { ...state, results },
-          })),
-        setMdx: (mdx: string) =>
-          set(({ state }: ChallengeStore) => ({
-            state: { ...state, mdx },
-          })),
-        setTabHandler: (tabHandler: TabHandler) =>
-          set(({ state }: ChallengeStore) => ({
-            state: { ...state, tabHandler },
-          })),
-        setIsAnswerVerified: (isAnswerVerified: boolean) =>
-          set(({ state, actions }: ChallengeStore) => ({
-            state: { ...state, isAnswerVerified },
-            actions,
-          })),
-        setIsAnswerCorrect: (isAnswerCorrect: boolean) =>
-          set(({ state, actions }: ChallengeStore) => ({
-            state: { ...state, isAnswerCorrect },
-            actions,
-          })),
-        setIsEnd: (isEnd: boolean) =>
-          set(({ state, actions }: ChallengeStore) => ({
-            state: { ...state, isEnd },
-            actions,
-          })),
-        setIsFirstRendering: (isFirstRendering: boolean) =>
-          set(({ state, actions }: ChallengeStore) => ({
-            state: { ...state, isFirstRendering },
-            actions,
-          })),
-        incrementIncorrectAswersAmount: () =>
-          set(({ state }: ChallengeStore) => {
-            const incorrectAnswersAmount =
-              state.incorrectAnswersAmount === 4
-                ? state.incorrectAnswersAmount
-                : state.incorrectAnswersAmount + 1
-            return {
-              state: { ...state, incorrectAnswersAmount },
-            }
-          }),
-        resetState: () =>
-          set(() => ({
-            state: initialState,
-          })),
-      },
-    }
+      }
+    },
   })
 )
