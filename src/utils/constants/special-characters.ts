9b28ed93bbdfb9500a8619819763942c445a362e
João Pedro <joaopedro.nc@outlook.com>
2024-02-02 15:46:51 -0300

    ♻️ refactor: remove unecessary characters from special characters constant

--- src/utils/constants/special-characters.ts
+++ src/utils/constants/special-characters.ts
@@ -1,9 +1,9 @@
 export const SPECIAL_CHARACTERS = {
-  '**': '@double-asterisks',
   '{': '@open-bracket',
   '}': '@close-bracket',
-  '(': '@open-parenteses',
-  ')': '@close-parenteses',
   '"': '@quote-mark',
   '\n': '@break-line',
+  '>': '@greater-than',
+  '<': '@less-than',
+  '#': '@hash',
 }
