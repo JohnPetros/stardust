d5d8c4ac90e05b2f372b17e619a4bf982b23db07
João Pedro <joaopedro.nc@outlook.com>
2024-02-02 15:43:20 -0300

    🐛 fix: esnure unlock doc

--- src/app/(private)/(app)/challenge/[challengeSlug]/actions/unlockDoc.ts
+++ src/app/(private)/(app)/challenge/[challengeSlug]/actions/unlockDoc.ts
@@ -5,17 +5,17 @@ import { createServerClient } from '@/services/api/supabase/clients/serverClient
 import { DocsController } from '@/services/api/supabase/controllers/docsController'
 
 export async function unlockDoc(challenge: Challenge, userId: string) {
-  const hasDictionaryTopic = !challenge.isCompleted && challenge.star_id
+  const shouldUnlockDoc = !challenge.isCompleted && Boolean(challenge.star_id)
 
-  if (hasDictionaryTopic && challenge.doc_id) {
+  if (shouldUnlockDoc && challenge.doc_id) {
     const docsController = DocsController(createServerClient())
 
-    const isDictionaryTopicUnlocked = await docsController.checkDocUnlocking(
+    const isDocUnlock = await docsController.checkDocUnlocking(
       challenge.doc_id,
       userId
     )
 
-    if (!isDictionaryTopicUnlocked)
+    if (!isDocUnlock)
       await docsController.addUnlockedDoc(challenge.doc_id, userId)
   }
 }
