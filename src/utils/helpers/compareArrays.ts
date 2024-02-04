74cd79bba07df3da35d515a359ebee7d4f6739e1
João Pedro <joaopedro.nc@outlook.com>
2024-02-03 21:01:36 -0300

    🐛 fix: preventcompareArrays helper apply  lowercase

--- src/utils/helpers/compareArrays.ts
+++ src/utils/helpers/compareArrays.ts
@@ -2,9 +2,7 @@ export function compareArrays(
   array1: Array<string | number>,
   array2: Array<string | number>
 ) {
-  const isEqual =
-    JSON.stringify(array1).toLocaleLowerCase() ===
-    JSON.stringify(array2).toLocaleLowerCase()
+  const isEqual = JSON.stringify(array1) === JSON.stringify(array2)
 
   return isEqual
 }
