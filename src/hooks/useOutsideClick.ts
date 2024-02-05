25d7593b1a6d23803dc3645ad8d9534380b7ae0e
João Pedro <joaopedro.nc@outlook.com>
2024-02-04 21:43:37 -0300

    ✨ feat (hook): add useOutsideClick hook

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,23 @@
+import { type RefObject, useEffect } from 'react'
+
+export function useOutsideClick<Element extends HTMLElement = HTMLElement>(
+  ref: RefObject<Element>,
+  handler: () => void
+) {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      const element = event.target as Node
+
+      const isOutsideClick = ref.current && !ref.current.contains(element)
+
+      if (isOutsideClick) {
+        handler()
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [ref, handler])
+}
