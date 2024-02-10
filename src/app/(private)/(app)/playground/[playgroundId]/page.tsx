bf0c4ff31c1d02783f9b350440497e4fad963f95
João Pedro <joaopedro.nc@outlook.com>
2024-02-10 20:36:52 -0300

    ✨ feat (ui): add NotPublicPlayground component

--- src/app/(private)/(app)/playground/[playgroundId]/page.tsx
+++ src/app/(private)/(app)/playground/[playgroundId]/page.tsx
@@ -1,7 +1,10 @@
-import { _handlePlaygroundPage } from './actions/_handlePlaygroundPage'
+import { _checkPublicPlayground } from './actions/_checkPublicPlayground'
+import { _getPlaygroundById } from './actions/_getPlaygroundById'
+import { NotPublicPlayground } from './components/NotPublicPlayground'
 import { PlaygroundLayout } from './components/PlaygroundLayout'
 
 import { SupabaseServerClient } from '@/services/api/supabase/clients/SupabaseServerClient'
+import { SupabaseAuthController } from '@/services/api/supabase/controllers/SupabaseAuthController'
 import { SupabasePlaygroundsController } from '@/services/api/supabase/controllers/SupabasePlaygroundsController'
 
 type UserPlaygroundPageProps = {
@@ -13,21 +16,27 @@ export default async function PlaygroundPage({
 }: UserPlaygroundPageProps) {
   const supabase = SupabaseServerClient()
   const playgroudsController = SupabasePlaygroundsController(supabase)
+  const authController = SupabaseAuthController(supabase)
 
-  const playground = await _handlePlaygroundPage(
+  const playground = await _getPlaygroundById(
     playgroundId,
     playgroudsController
   )
 
+  const isPlaygroundPublic = await _checkPublicPlayground(
+    playground,
+    authController
+  )
+
+  if (!isPlaygroundPublic) return <NotPublicPlayground />
+
   return (
-    <>
-      <PlaygroundLayout
-        playgroundId={playground.id}
-        playgroundTitle={playground.title}
-        playgroundCode={playground.code ?? ''}
-        playgroundUser={playground.user ?? null}
-        isPlaygroundPublic={playground.isPublic}
-      />
-    </>
+    <PlaygroundLayout
+      playgroundId={playground.id}
+      playgroundTitle={playground.title}
+      playgroundCode={playground.code ?? ''}
+      playgroundUser={playground.user ?? null}
+      isPlaygroundPublic={playground.isPublic}
+    />
   )
 }
