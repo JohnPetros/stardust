e352f585e53a665637e1fb35a2a49bb49ea69314
João Pedro <joaopedro.nc@outlook.com>
2024-02-12 12:25:21 -0300

    ✨ feat (hooks): add useRefreshPage hook

---
+++ src/global/hooks/useRefreshPage.ts
@@ -0,0 +1,15 @@
+import { useEventListener } from './useEventListener'
+
+export function useRefreshPage(
+  onRefreshPage: (() => void | Promise<void>) | null = null,
+  isEnabled: boolean = true
+) {
+  async function handlePageRefresh(event: BeforeUnloadEvent) {
+    if (!isEnabled) return
+
+    event.preventDefault()
+    if (onRefreshPage) await onRefreshPage()
+  }
+
+  useEventListener('beforeunload', handlePageRefresh)
+}
