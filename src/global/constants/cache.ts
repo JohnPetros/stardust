169bb02dd777cb5260dced7017dd25392b7ade62
João Pedro <joaopedro.nc@outlook.com>
2024-02-12 18:46:23 -0300

    ✨ feat (constants): add challengesList cache key

--- src/global/constants/cache.ts
+++ src/global/constants/cache.ts
@@ -2,6 +2,8 @@ export const CACHE = {
   keys: {
     userAchievements: '/user-achievements',
     challengesSummary: '/challenges-summary',
+    challengesList: '/challenges-list',
+    userCompletedChallenges: '/user-completed-challenges',
     commentReplies: '/comment-replies',
     filteredAvatars: '/filtered-avatars',
     filteredRockets: '/filtered-rockets',
