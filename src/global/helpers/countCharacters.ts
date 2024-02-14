5a4250cb6a4adcad2448283d708bd99e570fef4b
João Pedro <joaopedro.nc@outlook.com>
2024-02-14 13:35:08 -0300

    🐛 fix: ensure count characters

--- src/global/helpers/countCharacters.ts
+++ src/global/helpers/countCharacters.ts
@@ -2,7 +2,7 @@ export function countCharacters(characters: string, text: string) {
   let count = 0
 
   for (let i = 0; i < text.length; i++) {
-    if (characters === text.slice(i, characters.length)) count++
+    if (characters === text.slice(i, characters.length + i)) count++
   }
 
   return count
