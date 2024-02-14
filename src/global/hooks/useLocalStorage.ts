5196137e067037d8d16464cc4ee48f5e3e49979c
João Pedro <joaopedro.nc@outlook.com>
2024-02-14 13:35:52 -0300

    ✨ feat (hooks): add useLocalStorage hook

---
+++ src/global/hooks/useLocalStorage.ts
@@ -0,0 +1,41 @@
+import { useCallback } from 'react'
+
+// const IS_SERVER = typeof window === 'undefined'
+
+export function useLocalStorage<Value>(key: string) {
+  function deserialize(value: string) {
+    return JSON.parse(value) as Value
+  }
+
+  function get() {
+    const item = window.localStorage.getItem(key)
+
+    return item && item !== undefined ? deserialize(item) : null
+  }
+
+  const set = useCallback(
+    (value: Value) => {
+      function serialize(value: Value) {
+        return JSON.stringify(value)
+      }
+
+      window.localStorage.setItem(key, serialize(value))
+    },
+    [key]
+  )
+
+  function has() {
+    return Boolean(get())
+  }
+
+  function remove() {
+    window.localStorage.removeItem(key)
+  }
+
+  return {
+    get,
+    set,
+    has,
+    remove,
+  }
+}
