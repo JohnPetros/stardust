a3440528575b9bdf9f579dcc76d9625a0793ec8a
João Pedro <joaopedro.nc@outlook.com>
2024-02-16 18:25:15 -0300

    🚧 in progress: add PreventExitPageAlertDialog component

---
+++ src/global/components/PreventExitPageAlertDialog/usePreventExitPageAlertDialog.ts
@@ -0,0 +1,10 @@
+'use client'
+
+import { useEventListener } from '@/global/hooks/useEventListener'
+
+export function usePreventExitPageAlertDialog(
+  openAlertDialog: VoidFunction | null,
+  isEnabled: boolean
+) {
+  // useEventListener('p')
+}
