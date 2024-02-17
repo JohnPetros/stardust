67c2785685dd554c587a9c801bed21d344e9ee0a
João Pedro <joaopedro.nc@outlook.com>
2024-02-17 14:31:26 -0300

    ♻️ refactor: use useLocalStorage hook on useSecondsCounter hook

--- src/global/hooks/useSecondsCounter.ts
+++ src/global/hooks/useSecondsCounter.ts
@@ -1,19 +1,19 @@
 import { useEffect } from 'react'
 
+import { useLocalStorage } from './useLocalStorage'
+
 import { STORAGE } from '@/global/constants'
 
 export function useSecondsCounter(isEnabled: boolean) {
+  const secondsStorage = useLocalStorage(STORAGE.keys.secondsCounter)
+
   useEffect(() => {
     if (!isEnabled) return
 
     const interval = setInterval(() => {
-      const currentSeconds =
-        Number(localStorage.getItem(STORAGE.keys.secondsCounter)) ?? 0
+      const currentSeconds = Number(secondsStorage.get()) ?? 0
 
-      localStorage.setItem(
-        STORAGE.keys.secondsCounter,
-        String(currentSeconds + 1)
-      )
+      secondsStorage.set(String(currentSeconds + 1))
     }, 1000)
 
     return () => {
