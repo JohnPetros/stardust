783ae4cee3e1e7c146d4acd525a6bc0b520c24f9
JohnPetros <joaopedro.nc@outlook.com>
2024-07-07 19:24:04 -0300

    ⚙️ config: set tailwind css contect mapping

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,7 @@ import { colors } from './src/modules/global/styles/colors'
 module.exports = {
   content: [
     './src/modules/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/global/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/contexts/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/infra/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
