f45b2f10327ed26733ea7c41a8e2f6a6a664cc1e
JohnPetros <joaopedro.nc@outlook.com>
2024-07-08 10:02:45 -0300

    🤡 mocks: add useAuthContext hook

---
+++ src/modules/global/contexts/AuthContext/tests/mocks/useAuthContextMock.ts
@@ -0,0 +1,31 @@
+import { useAuthContext } from '@/modules/global/contexts/AuthContext'
+
+export function useAuthContextMock(
+  returnMock?: Partial<ReturnType<typeof useAuthContext>>
+) {
+  const handleSignInMock = jest.fn()
+  const handleSignUpMock = jest.fn()
+  const handleSignOutMock = jest.fn()
+  const updateUserMock = jest.fn()
+  const mutateUserCacheMock = jest.fn()
+
+  jest.mocked(useAuthContext).mockReturnValue({
+    user: null,
+    isLoading: false,
+    serverSession: null,
+    handleSignIn: handleSignInMock,
+    handleSignUp: handleSignUpMock,
+    handleSignOut: handleSignOutMock,
+    updateUser: updateUserMock,
+    mutateUserCache: mutateUserCacheMock,
+    ...returnMock,
+  })
+
+  return {
+    handleSignInMock,
+    handleSignUpMock,
+    handleSignOutMock,
+    updateUserMock,
+    mutateUserCacheMock,
+  }
+}
