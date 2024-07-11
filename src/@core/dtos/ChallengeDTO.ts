4582947e4b2b5fe8a84d1b24d03faed365fa58bc
JohnPetros <joaopedro.nc@outlook.com>
2024-07-11 14:14:57 -0300

    🏷️ types: add ChallengeDTO

---
+++ src/@core/dtos/ChallengeDTO.ts
@@ -0,0 +1,20 @@
+export type ChallengeDTO = {
+  id: string
+  title: string
+  code: string
+  slug: string
+  // userSlug: string
+  // difficulty: ChallengeDifficulty
+  // createdAt: string
+  // functionName: string | null
+  // texts: Text[]
+  // description: string
+  // starId: string | null
+  // testCases: ChallengeTestCase[]
+  // docId: string | null
+  // upvotesCount: number
+  // downvotesCount: number
+  // totalCompletitions: number
+  // categories?: Pick<Category, 'name'>[]
+  // isCompleted?: boolean
+}
