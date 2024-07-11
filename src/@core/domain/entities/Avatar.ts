0aec78af3d2a1570ad4e3ae9fba8ec24c0df4234
JohnPetros <joaopedro.nc@outlook.com>
2024-07-11 14:26:00 -0300

    👥 entities: add Avatar

---
+++ src/@core/domain/entities/Avatar.ts
@@ -0,0 +1,44 @@
+import type { AvatarDTO } from '@/@core/dtos'
+import { BuyableItem } from '../abstracts'
+
+export type AvatarProps = {
+  id?: string
+  image: string
+  name: string
+  price: number
+}
+
+export class Avatar extends BuyableItem {
+  private props: AvatarProps
+
+  private constructor(props: AvatarProps) {
+    super(props.price, props.id)
+    this.props = props
+  }
+
+  static create(dto: AvatarDTO): Avatar {
+    return new Avatar({
+      id: dto.id,
+      name: dto.name,
+      image: dto.image,
+      price: dto.price,
+    })
+  }
+
+  get dto(): AvatarDTO {
+    return {
+      id: this.id,
+      name: this.name,
+      image: this.image,
+      price: this.price,
+    }
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  get image() {
+    return this.props.image
+  }
+}
