5e618a49b3b63e052ad0c0b889d1c66f6a3a1706
JohnPetros <joaopedro.nc@outlook.com>
2024-07-11 14:34:06 -0300

    🗃️ files: remove useless errors

--- src/@core/errors/users/FetchUserUnlockedStarsIds.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from '../global/BaseError'
-
-export class FetchUserUnlockedStarsIdUnexpectedError extends BaseError {
-  constructor() {
-    super()
-    this.title = 'Fetch User Unlocked Rocket Unexpected Error'
-    this.message = 'Erro inesperado ao buscar estrelas'
-  }
-}
