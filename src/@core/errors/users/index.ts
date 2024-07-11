6c397acd99738a3cd897ec4fcbc5b0184a91b590
JohnPetros <joaopedro.nc@outlook.com>
2024-07-10 21:25:35 -0300

    ⚙️ config: disable noExplicitAny biome rule

--- src/@core/errors/users/index.ts
+++ src/@core/errors/users/index.ts
@@ -1,3 +1,4 @@
 export { SaveUserUnexpectedError } from './SaveUserUnexpectedError'
 export { UpdateUserUnexpectedError } from './UpdateUserUnexpectedError'
 export { UserNotFoundError } from './UserNotFoundError'
+export { SaveUserAcquiredRocketUnexpectedError } from './SaveUserAcquiredRocketUnexpectedError'
