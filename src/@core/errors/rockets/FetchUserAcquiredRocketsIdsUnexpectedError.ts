6777d6f3284f8ca14e8affc1058d956cd521a58b
JohnPetros <joaopedro.nc@outlook.com>
2024-07-12 16:54:53 -0300

    🗃️ folders: rename lib name

--- src/@core/errors/rockets/FetchUserAcquiredRocketsIdsUnexpectedError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from '../global/BaseError'
-
-export class FetchUserAcquiredRocketsIdsUnexpectedError extends BaseError {
-  constructor() {
-    super()
-    this.title = 'Get User Acquired Rockets Ids Id Unexpected Error'
-    this.message = 'Erro inesperado ao buscar foquetes adquiridos'
-  }
-}
