3fefb0dafa8be46bb856c1043f0eaee95aeb613d
JohnPetros <joaopedro.nc@outlook.com>
2024-07-12 16:54:12 -0300

    🧩 structs: add Ids collection

---
+++ src/@core/domain/structs/IdsCollection.ts
@@ -0,0 +1,34 @@
+import { ArrayValidation } from '@/@core/lib/validation'
+import { BaseStruct } from '../abstracts'
+
+type IdsCollectionProps = {
+  value: string[]
+}
+
+export class IdsCollection extends BaseStruct<IdsCollectionProps> {
+  readonly value: string[]
+
+  private constructor(props: IdsCollectionProps) {
+    super(props)
+    this.value = props.value
+  }
+
+  static create(key: string, ids: string[]) {
+    new ArrayValidation(ids, key).id().validate()
+
+    return new IdsCollection({ value: ids })
+  }
+
+  add(id: string) {
+    this.value.push(id)
+  }
+
+  remove(id: string) {
+    const ids = this.value.filter((currentId) => currentId !== id)
+    return new IdsCollection({ value: ids })
+  }
+
+  includes(id: string) {
+    this.value.includes(id)
+  }
+}
