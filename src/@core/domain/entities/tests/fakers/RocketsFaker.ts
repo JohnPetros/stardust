e182f8ee91523199732057fdc24a4bb0a219801b
JohnPetros <joaopedro.nc@outlook.com>
2024-07-14 11:39:50 -0300

    🥸 fakers: add RocketsFaker

---
+++ src/@core/domain/entities/tests/fakers/RocketsFaker.ts
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker'
+import { Rocket } from '../../Rocket'
+import type { RocketDTO } from '@/@core/dtos'
+
+export class RocketsFaker {
+  static fake(baseDTO?: Partial<RocketDTO>): Rocket {
+    return Rocket.create({
+      id: faker.string.uuid(),
+      name: faker.person.firstName(),
+      image: faker.image.avatar(),
+      price: faker.number.int({ max: 100 }),
+      slug: faker.lorem.slug(),
+      ...baseDTO,
+    })
+  }
+
+  static fakeMany(count?: number): Rocket[] {
+    return Array.from({ length: count ?? 10 }).map(() => RocketsFaker.fake())
+  }
+}
