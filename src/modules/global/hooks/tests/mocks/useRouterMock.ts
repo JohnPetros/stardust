88ab65363e6d3811351d7c2b74ff2704f9f60efd
JohnPetros <joaopedro.nc@outlook.com>
2024-07-14 15:46:04 -0300

    🤡 mocks: getCurrentroute function of useRouterMock

--- src/modules/global/hooks/tests/mocks/useRouterMock.ts
+++ src/modules/global/hooks/tests/mocks/useRouterMock.ts
@@ -4,13 +4,16 @@ jest.mock('@/modules/global/hooks/useRouter')
 
 export function useRouterMock(returnMock?: Partial<ReturnType<typeof useRouter>>) {
   const goToMock = jest.fn()
+  const getCurrentrouteMock = jest.fn()
 
   jest.mocked(useRouter).mockReturnValue({
     goTo: goToMock,
+    getCurrentroute: getCurrentrouteMock,
     ...returnMock,
   })
 
   return {
     goToMock,
+    getCurrentrouteMock,
   }
 }
