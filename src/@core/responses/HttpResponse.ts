1128f24417b290dabecf3f5d4895dbede91102b9
JohnPetros <joaopedro.nc@outlook.com>
2024-07-16 21:31:55 -0300

    📨 response: make possible to throw error in HttpResponse

--- src/@core/responses/HttpResponse.ts
+++ src/@core/responses/HttpResponse.ts
@@ -1,9 +1,23 @@
-export class HttpResponse {
-  readonly body: unknown
+import { AppError } from '../errors/global/AppError'
+
+export class HttpResponse<Response = unknown> {
+  readonly body: Response
   readonly statusCode: number
 
-  constructor(body: unknown, statusCode = 200) {
+  constructor(body: Response, statusCode = 200) {
     this.body = body
     this.statusCode = statusCode
+  }
+
+  throwError() {
+    throw new AppError(this.errorMessage)
+  }
+
+  get isError() {
+    return this.statusCode >= 500
+  }
+
+  get errorMessage() {
+    return String(this.body)
   }
 }
