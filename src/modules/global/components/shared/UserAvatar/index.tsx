ef466826a80d0ee6cb0958fec9a8f3623da32366
JohnPetros <joaopedro.nc@outlook.com>
2024-07-17 08:36:04 -0300

    ♻️ refactor: remove next image from avatar component

--- src/modules/global/components/shared/UserAvatar/index.tsx
+++ src/modules/global/components/shared/UserAvatar/index.tsx
@@ -1,36 +1,26 @@
 'use client'
 
 import * as Avatar from '@radix-ui/react-avatar'
-import Image from 'next/image'
 
 import { useApi } from '@/infra/api'
 
 type UserAvatarProps = {
-  avatarUrl: string
-  avatarName: string
+  avatarImage: string
   size: number
 }
 
-export function UserAvatar({ avatarName, avatarUrl, size }: UserAvatarProps) {
+export function UserAvatar({ avatarImage, size }: UserAvatarProps) {
   const api = useApi()
-  const avatarImage = api.fetchImage('avatars', avatarUrl)
+  const avatarImageSrc = api.fetchImage('avatars', avatarImage)
 
   return (
-    <div
-      style={{ width: size, height: size, borderRadius: '50%' }}
-      className='relative grid place-content-center overflow-hidden border border-green-700 bg-gray-300 shrink-0'
-    >
-      <Avatar.Root>
-        <Avatar.Image src={avatarImage} asChild>
-          <Image
-            src={avatarImage}
-            fill
-            alt={avatarName}
-            className='skeleton'
-            onLoadingComplete={(image) => image.classList.remove('skeleton')}
-          />
-        </Avatar.Image>
-      </Avatar.Root>
-    </div>
+    <Avatar.Root>
+      <Avatar.Image
+        src={avatarImageSrc}
+        className='skeleton relative grid place-content-center overflow-hidden border border-green-700 bg-gray-300 shrink-0 rounded-full'
+        style={{ width: size, height: size, borderRadius: '50%' }}
+        onLoad={(element) => element.currentTarget.classList.remove('skeleton')}
+      />
+    </Avatar.Root>
   )
 }
