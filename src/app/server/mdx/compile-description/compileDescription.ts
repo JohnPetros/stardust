fdceb37cdce068c53d0ac884d4ff0419227c3baf
JohnPetros <joaopedro.nc@outlook.com>
2024-07-21 11:15:49 -0300

    🗃️ files: remove useless helpers

--- src/app/server/mdx/compile-description/compileDescription.ts
+++
@@ -1,7 +0,0 @@
-import { serialize } from 'next-mdx-remote/serialize'
-
-export default async function compileDescription(description: string) {
-  const mdx = await serialize(description)
-
-  return mdx
-}
