4506143c2305c1b1638a9e4686869c14f180de59
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-01 23:51:42 +0000

    🚨 errors: add InvalidRewardingPayloadError

--- src/@core/errors/validation/LevelInvalidValueError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from '../global/BaseError'
-
-export class LevelInvalidValueError extends BaseError {
-  constructor(value: number) {
-    super()
-    this.title = 'Level Invalid Value Error'
-    this.message = `Valor de nível: ${value}, deveria ser maior ou igual a 1`
-  }
-}
