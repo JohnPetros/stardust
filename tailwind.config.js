d7a23c588bc413df3fb144c82a74791f1b0a91d8
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-07 03:30:13 +0000

    ⚙️ config: set tailwind content to ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/modules/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
