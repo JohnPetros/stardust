9ca72721a39921f0489dbf7f298182adf8f63529
JohnPetros <joaopedro.nc@outlook.com>
2024-08-13 09:25:43 -0300

    🏷 types: add ChallengeCompletionStatus

--- src/@core/dtos/ChallengeDTO.ts
+++ src/@core/dtos/ChallengeDTO.ts
@@ -1,17 +1,19 @@
+import type { ChallengeCategoryDTO } from './ChallengeCategoryDTO'
+
 export type ChallengeDTO = {
   id?: string
   title: string
   code: string
   slug: string
   difficulty: string
+  docId?: string | null
+  functionName?: string | null
   // userSlug: string
   // createdAt: string
-  // functionName: string | null
   // texts: Text[]
   // description: string
   // starId: string | null
   // testCases: ChallengeTestCase[]
-  // docId: string | null
   // upvotesCount: number
   // downvotesCount: number
   // totalCompletitions: number
