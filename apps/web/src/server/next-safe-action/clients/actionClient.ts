aeda936039e42df6d05f090bc51ba6cf375303bd
JohnPetros <joaopedro.nc@outlook.com>
2024-12-23 15:18:49 -0300

    ⚙️ config: setup next safe action clients

---
+++ apps/web/src/server/next-safe-action/clients/actionClient.ts
@@ -0,0 +1,14 @@
+import { AppError } from '@stardust/core/global/errors'
+import { createSafeActionClient, DEFAULT_SERVER_ERROR_MESSAGE } from 'next-safe-action'
+
+export const actionClient = createSafeActionClient({
+  handleServerError(error) {
+    if (error instanceof AppError) {
+      console.error('Action error title:', error.title)
+      console.error('Action error message:', error.message)
+      return error.message
+    }
+
+    return DEFAULT_SERVER_ERROR_MESSAGE
+  },
+})
---
+++ apps/web/src/server/next-safe-action/clients/authActionClient.ts
@@ -0,0 +1,16 @@
+import { SupabaseServerActionClient } from '@/api/supabase/clients'
+import { actionClient } from './actionClient'
+import { SupabaseAuthService, SupabaseProfileService } from '@/api/supabase/services'
+
+export const authActionClient = actionClient.use(async ({ next }) => {
+  const supabase = SupabaseServerActionClient()
+  const authService = SupabaseAuthService(supabase)
+  const authResponse = await authService.fetchUserId()
+  if (authResponse.isFailure) authResponse.throwError()
+
+  const profileService = SupabaseProfileService(supabase)
+  const profileResponse = await profileService.fetchUserById(authResponse.body)
+  if (profileResponse.isFailure) profileResponse.throwError()
+
+  return next({ ctx: { user: profileResponse.body } })
+})
