3d97044a2dbb2f1f8cbe68dae3b002a94d46991a
JohnPetros <joaopedro.nc@outlook.com>
2024-12-23 15:18:24 -0300

    📟 actions: observe new unlocker achievements

---
+++ apps/web/src/server/actions/profile/ObserveNewUnlockedAchievementsAction.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import type { UserDto } from '@stardust/core/global/dtos'
+import type { AchievementDto } from '@stardust/core/profile/dtos'
+import { ObserveNewUnlockedAchievementsUseCase } from '@stardust/core/profile/use-cases'
+import type { IAction, IActionServer, IProfileService } from '@stardust/core/interfaces'
+
+type Response = {
+  userDto: UserDto
+  newUnlockedAchievementsDto: AchievementDto[]
+}
+
+export const ObsverNewUnlockedAchievementsAction = (
+  profileService: IProfileService,
+): IAction<void, Response> => {
+  return {
+    async handle(actionServer: IActionServer) {
+      const userDto = await actionServer.getUser()
+
+      const useCase = new ObserveNewUnlockedAchievementsUseCase(profileService)
+      const { user, newUnlockedAchievements } = await useCase.do({
+        userDto,
+      })
+
+      return {
+        userDto: user.dto,
+        newUnlockedAchievementsDto: newUnlockedAchievements.map(
+          (achievement) => achievement.dto,
+        ),
+      }
+    },
+  }
+}
---
+++ apps/web/src/server/actions/profile/index.ts
@@ -0,0 +1,1 @@
+export { ObsverNewUnlockedAchievementsAction } from './ObserveNewUnlockedAchievementsAction'
