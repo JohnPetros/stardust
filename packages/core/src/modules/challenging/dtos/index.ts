e36014886b26e6b682de4e3cf127ee59bbd3b8cb
JohnPetros <joaopedro.nc@outlook.com>
2024-12-28 13:58:45 -0300

    🏷️ types: add TestCaseDto

--- packages/core/src/modules/challenging/dtos/index.ts
+++ packages/core/src/modules/challenging/dtos/index.ts
@@ -1,3 +1,4 @@
 export type { ChallengeDto } from './ChallengeDto'
 export type { ChallengeCategoryDto } from './ChallengeCategoryDto'
 export type { DocDto } from './DocDto'
+export type { TestCaseDto } from './TestCaseDto'
