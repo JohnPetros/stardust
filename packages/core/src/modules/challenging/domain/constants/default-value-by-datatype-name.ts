b4e373e3697273550957508d936f56af3eb59373
JohnPetros <joaopedro.nc@outlook.com>
2025-01-11 10:11:31 -0300

    🪨 constants: add DEFAULT_VALUE_BY_DATA_TYPE_NAME

---
+++ packages/core/src/modules/challenging/domain/constants/default-value-by-datatype-name.ts
@@ -0,0 +1,9 @@
+import type { DataTypeName } from '#challenging/types'
+
+export const DEFAULT_VALUE_BY_DATA_TYPE_NAME: Record<DataTypeName, unknown> = {
+  boolean: true,
+  string: '',
+  number: 0,
+  array: [''],
+  undefined: undefined,
+} as const
---
+++ packages/core/src/modules/challenging/domain/constants/index.ts
@@ -0,0 +1,1 @@
+export { DEFAULT_VALUE_BY_DATA_TYPE_NAME } from './default-value-by-datatype-name'
