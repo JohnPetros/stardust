b90a88cce1c13491c4177cb4fd6901c33fada240
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-01-13 23:07:49 +0000

    🚨 errors: enhance validation error message print

--- packages/core/src/modules/global/errors/ValidationError.ts
+++ packages/core/src/modules/global/errors/ValidationError.ts
@@ -7,7 +7,9 @@ type FieldError = {
 
 export class ValidationError extends AppError {
   constructor(readonly fieldErrors: FieldError[]) {
-    const message = fieldErrors.map((field) => `${field.messages.join(', ')}`).join('; ')
+    const message = fieldErrors
+      .map((field) => `${field.name} -> ${field.messages.join(', ')}`)
+      .join('; ')
 
     super(message, 'Validation Error')
   }
