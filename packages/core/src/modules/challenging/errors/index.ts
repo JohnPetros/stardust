0327e830b9b168b38fa1875c72f3b50fdccc67cb
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-01-13 23:05:32 +0000

    🚨 errors: add NotChallengeAuthorError

--- packages/core/src/modules/challenging/errors/index.ts
+++ packages/core/src/modules/challenging/errors/index.ts
@@ -1,3 +1,4 @@
 export { ChallengeNotFoundError } from './ChallengeNotFoundError'
 export { InsufficientInputsError } from './InsufficientInputsError'
 export { ChallengeWithoutTestCaseError } from './ChallengeWithoutTestCaseError'
+export { NotChallengeAuthorError } from './NotChallengeAuthorError'
