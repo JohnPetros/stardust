6aa9ed68828c333451d81aa7654c3571231a332d
JohnPetros <joaopedro.nc@outlook.com>
2025-01-14 13:34:52 -0300

    🧱 widgets: add RowsSkeleton for profile crafts table

---
+++ apps/web/src/ui/profile/widgets/pages/Profile/CraftsTable/TabsGroup/RowsSkeleton/index.tsx
@@ -0,0 +1,10 @@
+export function RowsSkeleton() {
+  return (
+    <ul>
+      <li className='w-full h-32 bg-gray-800 animate-pulse' />
+      <li className='w-full h-32 bg-gray-900 animate-pulse' />
+      <li className='w-full h-32 bg-gray-800 animate-pulse' />
+      <li className='w-full h-32 bg-gray-900 animate-pulse' />
+    </ul>
+  )
+}
