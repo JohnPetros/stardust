22dbe86119ab88a07cb706c3e5c410e79f692fba
JohnPetros <joaopedro.nc@outlook.com>
2025-01-14 19:23:41 -0300

    🖥️ hooks: add useEditSnippetAction

---
+++ apps/web/src/ui/playground/hooks/useEditSnippetAction.ts
@@ -0,0 +1,42 @@
+import { useCallback } from 'react'
+import { useAction } from 'next-safe-action/hooks'
+
+import type { SnippetDto } from '@stardust/core/playground/dtos'
+
+import type { ActionParams } from '@/server/next-safe-action/types'
+import { playgroundActions } from '@/server/next-safe-action'
+import { useToastContext } from '@/ui/global/contexts/ToastContext'
+
+type EditSnippetActionProps = {
+  onSuccess: (snippetDto: SnippetDto) => void
+  onError: VoidFunction
+}
+
+const action = playgroundActions.editSnippet
+
+export function useEditSnippetAction({ onSuccess, onError }: EditSnippetActionProps) {
+  const toast = useToastContext()
+  const { executeAsync, isPending, hasErrored } = useAction(action, {
+    onError: ({ error }) => {
+      if (error.serverError) toast.show(error.serverError)
+      onError()
+    },
+    onSuccess: ({ data }) => {
+      if (data) onSuccess(data)
+    },
+  })
+
+  const editSnippet = useCallback(
+    async (params: ActionParams<typeof action>) => {
+      const result = await executeAsync(params)
+      return result?.data
+    },
+    [executeAsync],
+  )
+
+  return {
+    isEditing: isPending,
+    isEditFailure: hasErrored,
+    editSnippet,
+  }
+}
