a7891502652ed11af318e15994a8046f13fc9711
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-02-09 14:49:46 +0000

    🐛 fix; ensure planets fetching on load space page has cache

--- apps/web/src/app/api/space/planets/route.ts
+++ apps/web/src/app/api/space/planets/route.ts
@@ -1,15 +1,13 @@
 import { NextHttp } from '@/api/next/NextHttp'
 import { runApiRoute } from '@/api/next/utils'
-import { SupabaseRouteHandlerClient } from '@/api/supabase/clients'
+import { SupabaseServerClient } from '@/api/supabase/clients'
 import { FetchPlanetsController } from '@/api/controllers/space'
 import { SupabaseSpaceService } from '@/api/supabase/services'
-
-export const dynamic = 'force-dynamic'
 
 export async function GET() {
   return runApiRoute(async () => {
     const http = await NextHttp()
-    const supabase = SupabaseRouteHandlerClient()
+    const supabase = SupabaseServerClient()
     const spaceService = SupabaseSpaceService(supabase)
     const controller = FetchPlanetsController(spaceService)
     return await controller.handle(http)
