fcc39bad2fa62e99c7f501f3754a3795f17229c2
JohnPetros <joaopedro.nc@outlook.com>
2025-02-14 00:15:52 +0000

    🖥️ hooks: increase ANIMATION_DURATION_BETWEEN_TEXTS value to use in useEndingPage

--- apps/web/src/ui/lesson/widgets/pages/Ending/useEndingPage.ts
+++ apps/web/src/ui/lesson/widgets/pages/Ending/useEndingPage.ts
@@ -2,7 +2,7 @@ import { type RefObject, useEffect, useState } from 'react'
 
 import type { AnimatedProgressBarRef } from '@/ui/global/widgets/components/AnimatedProgressBar/types'
 
-const ANIMATION_DURATION_BETWEEN_TEXTS = 7 // seconds
+const ANIMATION_DURATION_BETWEEN_TEXTS = 30 // seconds
 const LAST_THANK_INDEX = 3
 
 export function useEndingPage(progressBarRef: RefObject<AnimatedProgressBarRef>) {
