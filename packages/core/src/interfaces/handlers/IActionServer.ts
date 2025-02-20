e42b5e5a498bcd9f7f229412d8d7449ed0bba117
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-02-20 20:33:55 +0000

    📟 server: redirect user to page not found if they have not completed the star which challenge belongs to

--- packages/core/src/interfaces/handlers/IActionServer.ts
+++ packages/core/src/interfaces/handlers/IActionServer.ts
@@ -2,8 +2,8 @@ import type { UserDto } from '#global/dtos'
 
 export interface IActionServer<Request = void> {
   getRequest(): Request
-  redirect(route: string): unknown
-  notFound(): unknown
+  redirect(route: string): never
+  notFound(): never
   setCookie(key: string, value: string, duration?: number): Promise<void>
   getCookie(key: string): Promise<string | null>
   deleteCookie(key: string): Promise<void>
