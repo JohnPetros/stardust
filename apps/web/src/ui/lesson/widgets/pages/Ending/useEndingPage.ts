26ab78b588b82703a570362b07e040f658433054
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-02-21 00:26:12 +0000

    🐛 fix: ensure render the next text in lesson ending page instead the last one

--- apps/web/src/ui/lesson/widgets/pages/Ending/useEndingPage.ts
+++ apps/web/src/ui/lesson/widgets/pages/Ending/useEndingPage.ts
@@ -1,19 +1,19 @@
 import { type RefObject, useEffect, useState } from 'react'
 
 import type { AnimatedProgressBarRef } from '@/ui/global/widgets/components/AnimatedProgressBar/types'
+import { useSleep } from '@/ui/global/hooks/useSleep'
 
 const ANIMATION_DURATION_BETWEEN_TEXTS = 30 // seconds
 const LAST_THANK_INDEX = 3
 
 export function useEndingPage(progressBarRef: RefObject<AnimatedProgressBarRef>) {
   const [activeThankIndex, setActiveThankIndex] = useState(0)
+  const { sleep } = useSleep()
 
-  function nextText() {
+  async function nextText() {
     setActiveThankIndex((activeThankIndex) => activeThankIndex + 1)
-    console.log({ activeThankIndex })
-    setTimeout(() => {
-      progressBarRef.current?.fill(100, ANIMATION_DURATION_BETWEEN_TEXTS)
-    }, 250)
+    await sleep(300)
+    progressBarRef.current?.fill(100, ANIMATION_DURATION_BETWEEN_TEXTS)
   }
 
   function handleProgressBarAnimationEnd() {
