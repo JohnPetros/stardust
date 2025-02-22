9fedc5f9b28cc1bf096210dd915c761f7bd2fbb6
João Pedro Carvalho <joaopedro.nc@outlook.com>
2025-02-22 17:27:41 +0000

    🪨 constants: add node env to ENV

--- apps/web/src/constants/env.ts
+++ apps/web/src/constants/env.ts
@@ -1,12 +1,16 @@
 import { StringValidation } from '@stardust/core/libs'
 
+console.log('NODE_ENV', process.env.NODE_ENV)
+
 const ENV = {
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
-  supabaseCdnUrl: process.env.NEXT_PUBLIC_SUPABASE_CDN_URL ?? '',
-  appHost: process.env.NEXT_PUBLIC_APP_HOST ?? '',
+  mode: process.env.NODE_ENV,
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  supabaseCdnUrl: process.env.NEXT_PUBLIC_SUPABASE_CDN_URL,
+  appHost: process.env.NEXT_PUBLIC_APP_HOST,
 }
 
+new StringValidation(ENV.mode, 'App Mode').validate()
 new StringValidation(ENV.supabaseUrl, 'Supabase Url').validate()
 new StringValidation(ENV.supabaseKey, 'Supabase Key').validate()
 new StringValidation(ENV.supabaseCdnUrl, 'Supabase CDN Url').url().validate()
