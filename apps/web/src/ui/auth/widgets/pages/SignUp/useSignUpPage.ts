5b3dd09bd75c7249a7d124b51836f4a98f5ee7cb
JohnPetros <joaopedro.nc@outlook.com>
2025-02-22 18:22:01 -0300

    🖥️ ui: increase sign up form subtmit delay for 2.5 seconds

--- apps/web/src/ui/auth/widgets/pages/SignUp/useSignUpPage.ts
+++ apps/web/src/ui/auth/widgets/pages/SignUp/useSignUpPage.ts
@@ -30,7 +30,7 @@ export function useSignUpPage() {
 
   async function handleFormSubmit({ email, password, name }: SignUpFormFields) {
     await signUp(email, password, name)
-    await sleep(1500)
+    await sleep(2500)
     setUserCredentials({ email, password })
   }
 
