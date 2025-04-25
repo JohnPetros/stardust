c1666cf9411c0040d8021d6993f3a56a341eaa6f
João Pedro Carvalho <joaopcarvalho.cds@gmail.com>
2025-04-25 22:47:40 +0000

    (core) ⚙️ config: add jest config

---
+++ packages/core/jest.config.ts
@@ -0,0 +1,17 @@
+const path = require('node:path')
+const root = path.resolve(__dirname, '..', 'core')
+
+
+/** @type {import('jest').Config} */
+module.exports = {
+  rootDir: root,
+  testEnvironment: 'node',
+  displayName: 'core-tests',
+  verbose: true,
+  transformIgnorePatterns: ['!node_modules/(?!@src/*.)'],
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  preset: 'ts-jest',
+}
