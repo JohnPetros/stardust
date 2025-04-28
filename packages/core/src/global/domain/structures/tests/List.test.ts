63dad96823c9e2a0a1e1d471b6ddd685d42aca0a
João Pedro Carvalho <joaopcarvalho.cds@gmail.com>
2025-04-28 20:13:25 +0000

    🧪 tests: List structure

--- packages/core/src/global/domain/structures/tests/List.test.ts
+++ packages/core/src/global/domain/structures/tests/List.test.ts
@@ -1,6 +1,6 @@
 import { List } from '../List'
 
-describe('List struct', () => {
+describe('List structure', () => {
   it('should add an item', () => {
     const list = List.create([1, 2, 3])
 
@@ -27,7 +27,7 @@ describe('List struct', () => {
     expect(list.swap(4, 1).items).toEqual([4, 2, 3, 1])
   })
 
-  it('should verify if is equal to other list', () => {
+  it('should verify if it is equal to another list', () => {
     let list = List.create([1, 2, 3, 4])
     let otherList = List.create([1, 2, 3, 4])
 
@@ -71,15 +71,53 @@ describe('List struct', () => {
     expect(list.includesList(otherList).isTrue).toBeTruthy()
   })
 
-  it('should return the length', () => {
+  it('should get an item by index, returning a falllback value if it is not found', () => {
+    const list = List.create([1, 2, 3, 4])
+
+    expect(list.getByIndex(0, 'fallback value')).toBe(1)
+    expect(list.getByIndex(1, 'fallback value')).toBe(2)
+    expect(list.getByIndex(2, 'fallback value')).toBe(3)
+    expect(list.getByIndex(3, 'fallback value')).toBe(4)
+    expect(list.getByIndex(4, 'fallback value')).toBe('fallback value')
+  })
+
+  it('should determine if the list is empty', () => {
+    expect(List.create([1, 2, 3, 4]).isEmpty().value).toBeFalsy()
+    expect(List.create([]).isEmpty().value).toBeTruthy()
+  })
+
+  it('should become empty', () => {
+    let list = List.create([1, 2, 3, 4])
+    expect(list.hasItems.value).toBeTruthy()
+
+    list = list.becomeEmpty()
+    expect(list.hasItems.value).toBeFalsy()
+  })
+
+  it('should return true if the list includes at least one item from the given array', () => {
+    expect(List.create([1, 2, 3, 4]).includesSome([4, 5, 6, 7]).value).toBeTruthy()
+    expect(List.create([1, 2, 3, 4]).includesSome([5, 6, 7, 8]).value).toBeFalsy()
+  })
+
+  it('should return true if all items in the list are equal to a given value', () => {
+    expect(List.create([3, 3, 3]).hasAllEqualTo(3).value).toBeTruthy()
+    expect(List.create([3, 2, 3]).hasAllEqualTo(3).value).toBeFalsy()
+  })
+
+  it('should return the length of the list', () => {
     const list = List.create([1, 2, 3, 4])
 
     expect(list.lengthTruthy).toBe(4)
   })
 
-  it('should return the length only considering the truthy items', () => {
-    const list = List.create(['1', '2', '', '4', ''])
-
-    expect(list.lengthTruthy).toBe(3)
+  it('should return the length of the list considering only the truthy values', () => {
+    const list = List.create([1, true, '', 0])
+    expect(list.lengthTruthy).toBe(2)
   })
+
+  it('should return true if the ist has items', () => {
+    expect(List.create([1, true, '', 0]).hasItems.value).toBeTruthy()
+    expect(List.create([]).hasItems.value).toBeFalsy()
+  })
+
 })
