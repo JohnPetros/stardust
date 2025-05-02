9178397d9e25dd4ee1e74630027e177ce5e097d9
JohnPetros <joaopedro.nc@outlook.com>
2025-05-02 01:06:31 -0300

    🧪 test: Reward user use case

--- packages/core/src/profile/use-cases/tests/RewardUserUseCase.test.ts
+++ packages/core/src/profile/use-cases/tests/RewardUserUseCase.test.ts
@@ -1,191 +1,56 @@
-// import { ProfileServiceMock, SpaceServiceMock } from '#mocks/services'
-// import { StarsFaker, PlanetsFaker, UsersFaker } from '#fakers/entities'
-// import { StarRewardingPayloadFaker } from '#fakers/structures'
-// import { RewardUserUseCase } from '../RewardUserUseCase'
+import type { UsersRepository } from '#profile/interfaces/UsersRepository'
+import { _RewardUserUseCase } from '../_RewardUserUseCase'
+import { UsersFaker } from '#profile/domain/entities/fakers/UsersFaker'
+import { UsersRepositoryMock } from '#profile/interfaces/mocks/UsersRepositoryMock'
 
-// let profileServiceMock: ProfileServiceMock
-// let spaceServiceMock: SpaceServiceMock
-// let useCase: RewardUserUseCase
+let usersRepositoryMock: UsersRepository
+let useCase: _RewardUserUseCase
 
-// describe('Reward User Use Case', () => {
-//   beforeEach(() => {
-//     profileServiceMock = new ProfileServiceMock()
-//     spaceServiceMock = new SpaceServiceMock()
-//     useCase = new RewardUserUseCase(profileServiceMock)
-//   })
+describe('Reward user use case', () => {
+  beforeEach(() => {
+    usersRepositoryMock = new UsersRepositoryMock()
+    useCase = new _RewardUserUseCase(usersRepositoryMock)
+  })
 
-//   const UseCaseFixture = async (
-//     questionsCount = 5,
-//     incorrectAnswersCount = 0,
-//     secondsCount = 60,
-//   ) => {
-//     const currentStar = StarsFaker.fakeDto({ number: 1 })
-//     const nextStar = StarsFaker.fakeDto({ number: 2 })
-//     await spaceServiceMock.savePlanet(
-//       PlanetsFaker.fake({ stars: [currentStar, nextStar] }),
-//     )
+  it('should make the user earn xp, coins and update today status', async () => {
+    const newCoins = 5
+    const newXp = 10
+    const user = UsersFaker.fake({ xp: 0, coins: 0 })
+    usersRepositoryMock.add(user)
 
-//     return await useCase.do({
-//       userDto: UsersFaker.fakeDto({ coins: 0, xp: 0 }),
-//       rewardingPayloadDto: StarRewardingPayloadFaker.fakeDto({
-//         questionsCount,
-//         incorrectAnswersCount,
-//         secondsCount,
-//         starId: currentStar.id,
-//       }),
-//     })
-//   }
+    await useCase.do({ newCoins, newXp, userDto: user.dto })
 
-//   const UseCaseWithNextStarUnlockedFixture = async (
-//     questionsCount: number,
-//     incorrectAnswersCount: number,
-//   ) => {
-//     const currentStar = StarsFaker.fakeDto({ number: 1 })
-//     const nextStar = StarsFaker.fakeDto({ number: 2 })
-//     await spaceServiceMock.savePlanet(
-//       PlanetsFaker.fake({ stars: [currentStar, nextStar] }),
-//     )
+    const updatedUser = await usersRepositoryMock.findById(user.id)
 
-//     return await useCase.do({
-//       userDto: UsersFaker.fakeDto({
-//         coins: 0,
-//         xp: 0,
-//         unlockedStarsIds: [currentStar.id, nextStar.id],
-//       }),
-//       rewardingPayloadDto: StarRewardingPayloadFaker.fakeDto({
-//         questionsCount,
-//         incorrectAnswersCount,
-//         starId: currentStar.id,
-//       }),
-//     })
-//   }
+    expect(updatedUser?.xp.value).toBe(newXp)
+    expect(updatedUser?.coins.value).toBe(newCoins)
+    expect(updatedUser?.weekStatus.todayStatus).toBe('done')
+  })
 
-//   it('should calculate the new coins and xp based on the count of questions they answered', async () => {
-//     const questionsCount = 5
-//     const incorrectAnswersCount = 0
+  it('should return the new level, streak and week status if the user achieved them', async () => {
+    let response = await useCase.do({
+      newCoins: 0,
+      newXp: 10000,
+      userDto: UsersFaker.fakeDto({ streak: 0, xp: 0, coins: 0 }),
+    })
 
-//     const { newCoins, newXp } = await UseCaseFixture(
-//       questionsCount,
-//       incorrectAnswersCount,
-//     )
+    expect(response.newLevel).not.toBeNull()
+    expect(response.newStreak).not.toBeNull()
+    expect(response.newWeekStatus).not.toBeNull()
 
-//     expect(newCoins).toBe(10)
-//     expect(newXp).toBe(20)
-//   })
+    response = await useCase.do({
+      newCoins: 0,
+      newXp: 0,
+      userDto: UsersFaker.fakeDto({
+        streak: 0,
+        xp: 0,
+        coins: 0,
+        weekStatus: ['done', 'done', 'done', 'done', 'done', 'done', 'done'],
+      }),
+    })
 
-//   it('should decrease the calculated new coins and xp amount according to the count of incorrect answers that the user made', async () => {
-//     const questionsCount = 5
-//     const incorrectAnswersCount = 2
-
-//     const { newCoins, newXp } = await UseCaseFixture(
-//       questionsCount,
-//       incorrectAnswersCount,
-//     )
-
-//     expect(newCoins).toBe(6)
-//     expect(newXp).toBe(12)
-//   })
-
-//   it('should divide the calculated new coins and xp amount by 2 if the next star is already unlocked', async () => {
-//     const questionsCount = 5
-//     const incorrectAnswersCount = 0
-
-//     const { newCoins, newXp } = await UseCaseWithNextStarUnlockedFixture(
-//       questionsCount,
-//       incorrectAnswersCount,
-//     )
-
-//     expect(newCoins).toBe(5)
-//     expect(newXp).toBe(10)
-//   })
-
-//   it('should get the accuracy percentage of questions answered correctly', async () => {
-//     let questionsCount = 5
-//     let incorrectAnswersCount = 0
-
-//     const response1 = await UseCaseFixture(questionsCount, incorrectAnswersCount)
-
-//     expect(response1.accuracyPercentage).toBe(100)
-
-//     questionsCount = 5
-//     incorrectAnswersCount = 2
-
-//     const response2 = await UseCaseFixture(questionsCount, incorrectAnswersCount)
-
-//     expect(response2.accuracyPercentage).toBe(60)
-//   })
-
-//   it('should get the time based on the seconds count that the user took to complete the questions', async () => {
-//     let secondsCount = 30
-//     const response1 = await UseCaseFixture(5, 0, secondsCount)
-
-//     expect(response1.time).toBe('00:30')
-
-//     secondsCount = 60
-//     const response2 = await UseCaseFixture(5, 0, secondsCount)
-
-//     expect(response2.time).toBe('01:00')
-
-//     secondsCount = 75
-//     const response3 = await UseCaseFixture(5, 0, secondsCount)
-
-//     expect(response3.time).toBe('01:15')
-//   })
-
-//   it('should fetch and unlock the next star', async () => {
-//     const currentStar = StarsFaker.fakeDto({ number: 1 })
-//     const nextStar = StarsFaker.fakeDto({ number: 2 })
-
-//     await spaceServiceMock.savePlanet(
-//       PlanetsFaker.fake({ stars: [currentStar, nextStar] }),
-//     )
-
-//     await useCase.do({
-//       userDto: UsersFaker.fakeDto({ unlockedStarsIds: [currentStar.id] }),
-//       rewardingPayloadDto: StarRewardingPayloadFaker.fakeDto({
-//         starId: currentStar.id,
-//       }),
-//     })
-
-//     expect(spaceServiceMock.unlockedStarsIds.includes(nextStar.id)).toBeTruthy()
-//   })
-
-//   it('should fetch and unlock the next star from the next planet if the current star is the last one of its own planet', async () => {
-//     const currentStar = StarsFaker.fakeDto()
-//     const nextStar = StarsFaker.fakeDto()
-
-//     await Promise.all([
-//       spaceServiceMock.savePlanet(
-//         PlanetsFaker.fake({ position: 1, stars: [currentStar] }),
-//       ),
-//       spaceServiceMock.savePlanet(PlanetsFaker.fake({ position: 2, stars: [nextStar] })),
-//     ])
-
-//     await useCase.do({
-//       userDto: UsersFaker.fakeDto(),
-//       rewardingPayloadDto: StarRewardingPayloadFaker.fakeDto({
-//         starId: currentStar.id,
-//       }),
-//     })
-
-//     expect(spaceServiceMock.unlockedStarsIds.includes(nextStar.id)).toBeTruthy()
-//   })
-
-//   it('should not throw any error at all even if the current star is the last one of the last planet', async () => {
-//     const currentStar = StarsFaker.fakeDto()
-
-//     await spaceServiceMock.savePlanet(PlanetsFaker.fake({ stars: [currentStar] }))
-
-//     const { newCoins, newXp } = await useCase.do({
-//       userDto: UsersFaker.fakeDto({ coins: 0, xp: 0 }),
-//       rewardingPayloadDto: StarRewardingPayloadFaker.fakeDto({
-//         questionsCount: 5,
-//         incorrectAnswersCount: 0,
-//         starId: currentStar.id,
-//       }),
-//     })
-
-//     expect(newCoins).toBe(10)
-//     expect(newXp).toBe(20)
-//   })
-// })
+    expect(response.newLevel).toBeNull()
+    expect(response.newStreak).toBeNull()
+    expect(response.newWeekStatus).toBeNull()
+  })
+})
