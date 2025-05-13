a9cc9904ae2e6f106f798c0f6c9440457caa5ad7
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-13 16:54:40 -0300

    ✨ use case: update user

---
+++ packages/core/src/profile/use-cases/VerifyUserEmailInUseUseCase.ts
@@ -0,0 +1,15 @@
+import { Email } from '#global/domain/structures/Email'
+import type { UseCase } from '#global/interfaces/UseCase'
+import { UserEmailAlreadyInUseError } from '../errors'
+import type { UsersRepository } from '../interfaces'
+
+export class VerifyUserEmailInUseUseCase implements UseCase<string, Promise<void>> {
+  constructor(private readonly repository: UsersRepository) {}
+
+  async execute(email: string): Promise<void> {
+    const user = await this.repository.hasWithEmail(Email.create(email))
+    if (user) {
+      throw new UserEmailAlreadyInUseError()
+    }
+  }
+}
