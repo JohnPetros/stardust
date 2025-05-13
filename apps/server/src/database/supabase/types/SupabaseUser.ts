cfe63106a98c4ab4757dcdeac719cc19cc1edc23
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-13 16:50:19 -0300

    (server) 💾 database: add types for supabase tables

---
+++ apps/server/src/database/supabase/types/SupabaseUser.ts
@@ -0,0 +1,19 @@
+import type { Database } from './Database'
+import type { SupabaseAvatar } from './SupabaseAvatar'
+import type { SupabaseRocket } from './SupabaseRocket'
+import type { SupabaseTier } from './SupabaseTier'
+
+export type SupabaseUser = Database['public']['Tables']['users']['Row'] & {
+  avatar?: SupabaseAvatar | null
+  rocket?: SupabaseRocket | null
+  tier?: SupabaseTier | null
+  users_unlocked_stars?: { star_id: string }[]
+  users_unlocked_achievements?: { achievement_id: string }[]
+  users_rescuable_achievements?: { achievement_id: string }[]
+  users_acquired_rockets?: { rocket_id: string }[]
+  users_acquired_avatars?: { avatar_id: string }[]
+  users_completed_challenges?: { challenge_id: string }[]
+  users_upvoted_solutions?: { solution_id: string }[]
+  users_upvoted_comments?: { comment_id: string }[]
+  users_completed_planets?: { planet_id: string | null }[]
+}
