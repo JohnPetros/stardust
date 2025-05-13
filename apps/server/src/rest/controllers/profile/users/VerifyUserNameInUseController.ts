569e2021c77b89114fd56a8c10a09b047770081a
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-13 16:56:56 -0300

    (server) 📶 rest: add VerifyUserNameInUseController

---
+++ apps/server/src/rest/controllers/profile/users/VerifyUserNameInUseController.ts
@@ -0,0 +1,22 @@
+import type { Controller } from '@stardust/core/global/interfaces'
+import type { Http } from '@stardust/core/global/interfaces'
+import type { RestResponse } from '@stardust/core/global/responses'
+import type { UsersRepository } from '@stardust/core/profile/interfaces'
+import { VerifyUserNameInUseUseCase } from '@stardust/core/profile/use-cases'
+
+type Schema = {
+  body: {
+    name: string
+  }
+}
+
+export class VerifyUserNameInUseController implements Controller<Schema> {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async handle(http: Http<Schema>): Promise<RestResponse> {
+    const { name } = await http.getBody()
+    const useCase = new VerifyUserNameInUseUseCase(this.usersRepository)
+    const updatedUser = await useCase.execute(name)
+    return http.sendJson(updatedUser)
+  }
+}
