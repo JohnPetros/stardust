2574f1a4965797dcdde977391d192c45b7239942
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-16 17:39:15 -0300

    (server) 📶 rest: add RescueAchievementController

---
+++ apps/server/src/rest/controllers/profile/achievements/RescueAchievementController.ts
@@ -0,0 +1,31 @@
+import type { Controller, Http } from '@stardust/core/global/interfaces'
+import type { RestResponse } from '@stardust/core/global/responses'
+import type {
+  AchievementsRepository,
+  UsersRepository,
+} from '@stardust/core/profile/interfaces'
+import { RescueAchievementUseCase } from '@stardust/core/profile/use-cases'
+
+type Schema = {
+  routeParams: {
+    userId: string
+    achievementId: string
+  }
+}
+
+export class RescueAchievementController implements Controller<Schema> {
+  constructor(
+    private readonly achievementsRepository: AchievementsRepository,
+    private readonly usersRepository: UsersRepository,
+  ) {}
+
+  async handle(http: Http<Schema>): Promise<RestResponse> {
+    const { userId, achievementId } = http.getRouteParams()
+    const useCase = new RescueAchievementUseCase(
+      this.achievementsRepository,
+      this.usersRepository,
+    )
+    const response = await useCase.execute(achievementId, userId)
+    return http.sendJson(response)
+  }
+}
