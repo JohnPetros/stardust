7787db63908a7240e4a49b0dc33657e5a537aac3
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-20 17:23:18 -0300

    ✨ use case: Update Ranking Position

---
+++ packages/core/src/profile/use-cases/UpdateRankingPositionUseCase.ts
@@ -0,0 +1,18 @@
+import type { UsersRepository } from '../interfaces'
+import { Id, Integer } from '#global/domain/structures/index'
+
+export class UpdateRankingPositionUseCase {
+  constructor(private readonly repository: UsersRepository) {}
+
+  async execute(tierId: string) {
+    const users = await this.repository.findByTierOrderedByXp(Id.create(tierId))
+    let rankingPosition = Integer.create(1)
+
+    for (const user of users) {
+      user.updateRankingPosition(rankingPosition)
+      rankingPosition = rankingPosition.increment()
+    }
+
+    await this.repository.replaceMany(users)
+  }
+}
