c8651fd0fb562ef1728d3e71fc72f93f61fe1502
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-20 17:19:34 -0300

    ✨ use case: Reach First Tier

---
+++ packages/core/src/ranking/use-cases/ReachFirstTierUseCase.ts
@@ -0,0 +1,36 @@
+import { OrdinalNumber } from '#global/domain/structures/OrdinalNumber'
+import type { EventBroker } from '#global/interfaces/EventBroker'
+import type { UseCase } from '#global/interfaces/UseCase'
+import type { TiersRepository } from '../interfaces'
+import { FirstTierReachedEvent } from '../domain/events'
+import { TierNotFoundError } from '../domain/errors'
+
+type Request = {
+  user: {
+    id: string
+    name: string
+    email: string
+  }
+  firstStarId: string
+}
+
+export class ReachFirstTierUseCase implements UseCase<Request, void> {
+  constructor(
+    private readonly repository: TiersRepository,
+    private readonly eventBroker: EventBroker,
+  ) {}
+
+  async execute({ user, firstStarId }: Request) {
+    const tier = await this.repository.findByPosition(OrdinalNumber.create(1))
+    if (!tier) {
+      throw new TierNotFoundError()
+    }
+
+    const event = new FirstTierReachedEvent({
+      user,
+      firstTierId: tier.id.value,
+      firstStarId: firstStarId,
+    })
+    await this.eventBroker.publish(event)
+  }
+}
