fcaa28c83db7fb89832e05f150a989b1edec6cb8
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-21 18:12:02 -0300

    (server) 🎞️ queue:  add UnlockFirstStarJob

---
+++ apps/server/src/queue/jobs/space/UnlockFirstStarJob.ts
@@ -0,0 +1,32 @@
+import type { PlanetsRepository } from '@stardust/core/space/interfaces'
+import type { EventBroker } from '@stardust/core/global/interfaces'
+import type { Job, Amqp } from '@stardust/core/global/interfaces'
+import { UnlockFirstStarUseCase } from '@stardust/core/space/use-cases'
+import type { UserSignedUpEvent } from '@stardust/core/auth/events'
+import type { EventPayload } from '@stardust/core/global/types'
+
+type Payload = EventPayload<typeof UserSignedUpEvent>
+
+export class UnlockFirstStarJob implements Job<Payload> {
+  static readonly KEY = 'space/unlock.first.star.job'
+
+  constructor(
+    private readonly planetsRepository: PlanetsRepository,
+    private readonly eventBroker: EventBroker,
+  ) {}
+
+  async handle(amqp: Amqp<Payload>) {
+    const payload = amqp.getPayload()
+    const useCase = new UnlockFirstStarUseCase(this.planetsRepository, this.eventBroker)
+
+    await amqp.run(
+      async () =>
+        await useCase.execute({
+          userId: payload.userId,
+          userName: payload.userName,
+          userEmail: payload.userEmail,
+        }),
+      UnlockFirstStarUseCase.name,
+    )
+  }
+}
