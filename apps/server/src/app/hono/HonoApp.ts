a0639e2962e6f5b898b91a4840b2b8c1a9f386b4
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-21 18:22:55 -0300

    ⚙️ config: register Inngest route using Hono

--- apps/server/src/app/hono/HonoApp.ts
+++ apps/server/src/app/hono/HonoApp.ts
@@ -1,14 +1,22 @@
 import { type Context, type Next, Hono } from 'hono'
 import { serve } from '@hono/node-server'
+import { setCookie } from 'hono/cookie'
+import { serve as serveInngest } from 'inngest/hono'
+import { parseCookieHeader } from '@supabase/ssr'
+import { createServerClient } from '@supabase/ssr'
+import type { SupabaseClient } from '@supabase/supabase-js'
 
 import type { RestResponse } from '@stardust/core/global/responses'
 
 import { ENV } from '@/constants'
 import { ProfileRouter } from './routers'
-import { parseCookieHeader } from '@supabase/ssr'
-import { createServerClient } from '@supabase/ssr'
-import { setCookie } from 'hono/cookie'
-import type { SupabaseClient } from '@supabase/supabase-js'
+import { inngest } from '@/queue/inngest/client'
+import {
+  ProfileFunctions,
+  SpaceFunctions,
+  ShopFunctions,
+  RankingFunctions,
+} from '@/queue/inngest/functions'
 
 declare module 'hono' {
   interface ContextVariableMap {
@@ -28,7 +36,7 @@ export class HonoApp {
         port: ENV.port,
       },
       (info) => {
-        console.log(`Server is running on ${ENV.baseUrl}:${info.port} 🔥`)
+        console.log(`🏢 Server is running on ${ENV.baseUrl}:${info.port}`)
       },
     )
   }
@@ -36,10 +44,31 @@ export class HonoApp {
   private registerRoutes() {
     const profileRouter = new ProfileRouter(this)
     this.hono.route('/', profileRouter.registerRoutes())
+    this.registerInngestRoute()
   }
 
   private registerMiddlewares() {
     this.hono.use('*', this.createSupabaseClient())
+  }
+
+  private registerInngestRoute() {
+    this.hono.on(['GET', 'PUT', 'POST'], '/inngest', (context) => {
+      const supabase = context.get('supabase')
+      const profileFunctions = new ProfileFunctions(inngest)
+      const spaceFunctions = new SpaceFunctions(inngest)
+      const shopFunctions = new ShopFunctions(inngest)
+      const rankingFunctions = new RankingFunctions(inngest)
+
+      return serveInngest({
+        client: inngest,
+        functions: [
+          ...profileFunctions.getFunctions(supabase),
+          ...spaceFunctions.getFunctions(supabase),
+          ...shopFunctions.getFunctions(supabase),
+          ...rankingFunctions.getFunctions(supabase),
+        ],
+      })(context)
+    })
   }
 
   private createSupabaseClient() {
