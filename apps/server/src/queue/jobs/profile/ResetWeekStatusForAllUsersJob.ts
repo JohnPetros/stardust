353e5202367952cc42101b33fdfde87329252eae
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-21 18:13:16 -0300

    (server) 🎞️ queue:  add ResetWeekStatusForAllUsersJob

---
+++ apps/server/src/queue/jobs/profile/ResetWeekStatusForAllUsersJob.ts
@@ -0,0 +1,15 @@
+import type { Amqp } from '@stardust/core/global/interfaces'
+import type { UsersRepository } from '@stardust/core/profile/interfaces'
+import { ResetWeekStatusForAllUsersUseCase } from '@stardust/core/profile/use-cases'
+
+export class ResetWeekStatusForAllUsersJob {
+  static readonly KEY = 'profile/reset.week.status.job'
+  static readonly CRON_EXPRESSION = 'TZ=America/Sao_Paulo 3 0 * * 0'
+
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async handle(amqp: Amqp) {
+    const useCase = new ResetWeekStatusForAllUsersUseCase(this.usersRepository)
+    await amqp.run(async () => useCase.execute(), ResetWeekStatusForAllUsersUseCase.name)
+  }
+}
