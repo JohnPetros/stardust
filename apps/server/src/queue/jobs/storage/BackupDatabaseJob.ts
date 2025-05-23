6d7ceebf5e190bc78815d61d87a07d888b465acb
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-23 20:36:10 -0300

    (server) 🎞️ queue: add BackupDatabaseJob

---
+++ apps/server/src/queue/jobs/storage/BackupDatabaseJob.ts
@@ -0,0 +1,18 @@
+import type { DatabaseProvider, Job } from '@stardust/core/global/interfaces'
+import type { StorageProvider } from '@stardust/core/storage/interfaces'
+import { BackupDatabaseUseCase } from '@stardust/core/storage/use-cases'
+
+export class BackupDatabaseJob implements Job {
+  static readonly KEY = 'storage/backup-database'
+  static readonly CRON_EXPRESSION = 'TZ=America/Sao_Paulo 0 0 * * 0'
+
+  constructor(
+    private readonly databaseProvider: DatabaseProvider,
+    private readonly storageProvider: StorageProvider,
+  ) {}
+
+  async handle(): Promise<void> {
+    const useCase = new BackupDatabaseUseCase(this.databaseProvider, this.storageProvider)
+    await useCase.execute()
+  }
+}
