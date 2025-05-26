de069e9bf8e67315aeaa26925662d11b81b2946f
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-26 17:34:07 -0300

    (server) ⚙️ config: add  SpaceRouter

---
+++ apps/server/src/app/hono/routers/space/SpaceRouter.ts
@@ -0,0 +1,18 @@
+import { Hono } from 'hono'
+
+import { HonoRouter } from '../../HonoRouter'
+import { StarsRouter } from './StarsRouter'
+import { PlanetsRouter } from './PlanetsRouter'
+
+export class SpaceRouter extends HonoRouter {
+  private readonly router = new Hono().basePath('/space')
+
+  registerRoutes(): Hono {
+    const starsRouter = new StarsRouter(this.app)
+    const planetsRouter = new PlanetsRouter(this.app)
+
+    this.router.route('/', starsRouter.registerRoutes())
+    this.router.route('/', planetsRouter.registerRoutes())
+    return this.router
+  }
+}
