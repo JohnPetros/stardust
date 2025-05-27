acb9e4acd47eb3f8804010bd0c55806e41869eaa
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-27 18:21:43 -0300

    (server) 📶 rest: add FetchAvatarsListController

---
+++ apps/server/src/rest/controllers/shop/FetchAvatarsListController.ts
@@ -0,0 +1,25 @@
+import type { Http } from '@stardust/core/global/interfaces'
+import type { RestResponse } from '@stardust/core/global/responses'
+import type { AvatarsRepository } from '@stardust/core/shop/interfaces'
+import type { Controller } from '@stardust/core/global/interfaces'
+import { ListAvatarsUseCase } from '@stardust/core/shop/use-cases'
+
+type Schema = {
+  queryParams: {
+    search: string
+    order: string
+    page: number
+    itemsPerPage: number
+  }
+}
+
+export class FetchAvatarsListController implements Controller<Schema> {
+  constructor(private readonly avatarsRepository: AvatarsRepository) {}
+
+  async handle(http: Http<Schema>): Promise<RestResponse> {
+    const request = http.getQueryParams()
+    const useCase = new ListAvatarsUseCase(this.avatarsRepository)
+    const response = await useCase.execute(request)
+    return http.send(response)
+  }
+}
