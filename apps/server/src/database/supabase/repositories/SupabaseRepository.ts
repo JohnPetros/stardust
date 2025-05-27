5d8b1b10a5cf052c29eb9e54c1b4616eab99e590
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-27 18:16:26 -0300

    (server) 💾 db: implement findMany method in RocketsRepository using Supabase

--- apps/server/src/database/supabase/repositories/SupabaseRepository.ts
+++ apps/server/src/database/supabase/repositories/SupabaseRepository.ts
@@ -2,4 +2,13 @@ import type { SupabaseClient } from '@supabase/supabase-js'
 
 export abstract class SupabaseRepository {
   constructor(protected readonly supabase: SupabaseClient) {}
+
+  protected calculateQueryRange(page: number, itemsPerPage: number) {
+    const offset = (page - 1) * itemsPerPage
+
+    return {
+      from: offset,
+      to: offset + itemsPerPage - 1,
+    }
+  }
 }
--- packages/core/src/shop/interfaces/RocketsRepository.ts
+++ packages/core/src/shop/interfaces/RocketsRepository.ts
@@ -1,7 +1,11 @@
 import type { Id, Integer } from '#global/domain/structures/index'
 import type { Rocket } from '../domain/entities'
+import type { ShopItemsListingParams } from '../domain/types'
 
 export interface RocketsRepository {
   findById(id: Id): Promise<Rocket | null>
+  findMany(
+    params: ShopItemsListingParams,
+  ): Promise<{ rockets: Rocket[]; totalRocketsCount: number }>
   findAllByPrice(price: Integer): Promise<Rocket[]>
 }
