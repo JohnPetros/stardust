a8b9051a71c88fd60e7f7c28343ec0174c6cf2ac
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-27 18:22:16 -0300

    (server) 📶 rest: add AcquireAvatarController

---
+++ apps/server/src/rest/controllers/profile/users/AcquireAvatarController.ts
@@ -0,0 +1,35 @@
+import type { Controller } from '@stardust/core/global/interfaces'
+import type { Http } from '@stardust/core/global/interfaces'
+import type { RestResponse } from '@stardust/core/global/responses'
+import type { UsersRepository } from '@stardust/core/profile/interfaces'
+import { AcquireAvatarUseCase } from '@stardust/core/profile/use-cases'
+
+type Schema = {
+  routeParams: {
+    userId: string
+  }
+  body: {
+    avatarId: string
+    avatarName: string
+    avatarImage: string
+    avatarPrice: number
+  }
+}
+
+export class AcquireAvatarController implements Controller<Schema> {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async handle(http: Http<Schema>): Promise<RestResponse> {
+    const { userId } = http.getRouteParams()
+    const { avatarId, avatarName, avatarImage, avatarPrice } = await http.getBody()
+    const useCase = new AcquireAvatarUseCase(this.usersRepository)
+    const user = await useCase.execute({
+      userId,
+      avatarId,
+      avatarName,
+      avatarImage,
+      avatarPrice,
+    })
+    return http.send(user)
+  }
+}
