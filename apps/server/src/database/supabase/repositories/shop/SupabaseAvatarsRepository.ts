6a7c584a85a1d538c0f3a16afd38ac3fc6001880
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-27 18:16:02 -0300

    (server) 💾 db: implement findMany method in AvatarsRepository using Supabase

--- apps/server/src/database/supabase/repositories/shop/SupabaseAvatarsRepository.ts
+++ apps/server/src/database/supabase/repositories/shop/SupabaseAvatarsRepository.ts
@@ -5,6 +5,7 @@ import type { Id, Integer } from '@stardust/core/global/structures'
 import { SupabaseRepository } from '../SupabaseRepository'
 import { SupabasePostgreError } from '../../errors'
 import { SupabaseAvatarMapper } from '../../mappers/shop'
+import type { ShopItemsListingParams } from '@stardust/core/shop/types'
 
 export class SupabaseAvatarsRepository
   extends SupabaseRepository
@@ -35,5 +36,35 @@ export class SupabaseAvatarsRepository
     }
 
     return data.map(SupabaseAvatarMapper.toEntity)
+  }
+
+  async findMany({ search, page, itemsPerPage, order }: ShopItemsListingParams) {
+    let query = this.supabase.from('avatars').select('*', {
+      count: 'exact',
+      head: false,
+    })
+
+    if (search && search.value.length > 1) {
+      query = query.ilike('name', `%${search}%`)
+    }
+
+    const range = this.calculateQueryRange(page.value, itemsPerPage.value)
+
+    query = query
+      .order('price', { ascending: order.value === 'ascending' })
+      .range(range.from, range.to)
+
+    const { data, count, error } = await query
+
+    if (error) {
+      throw new SupabasePostgreError(error)
+    }
+
+    const avatars = data.map(SupabaseAvatarMapper.toEntity)
+
+    return {
+      avatars,
+      totalAvatarsCount: count ?? avatars.length,
+    }
   }
 }
--- packages/core/src/shop/interfaces/AvatarsRepository.ts
+++ packages/core/src/shop/interfaces/AvatarsRepository.ts
@@ -1,7 +1,11 @@
 import type { Id, Integer } from '#global/domain/structures/index'
 import type { Avatar } from '../domain/entities'
+import type { ShopItemsListingParams } from '../domain/types'
 
 export interface AvatarsRepository {
   findById(id: Id): Promise<Avatar | null>
+  findMany(
+    params: ShopItemsListingParams,
+  ): Promise<{ avatars: Avatar[]; totalAvatarsCount: number }>
   findAllByPrice(price: Integer): Promise<Avatar[]>
 }
