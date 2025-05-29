0634cadd95483daf1fa1a6918a72c10d51f5b7f7
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-29 15:30:03 -0300

    (server) ⚙️ config: define tsup configuration

---
+++ apps/server/tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, type Options } from 'tsup'
+
+export default defineConfig((options: Options) => {
+  return {
+    entry: ['src/main.ts'],
+    format: ['cjs'],
+    clean: true,
+    sourcemap: false,
+    outDir: 'build',
+    external: ['@stardust/core', '@stardust/validation'],
+    esbuildOptions(options) {
+      options.loader = {
+        ...options.loader,
+        '.toml': 'empty',
+        '.sql': 'empty',
+      }
+    },
+    ...options,
+  }
+})
