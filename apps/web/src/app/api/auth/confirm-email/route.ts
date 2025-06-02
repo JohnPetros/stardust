fad08ab04feeca477d1c32deaabcb54f6160f794
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-06-01 21:06:38 -0300

    (web) ♻️ refactor: use NextServerRestClient in api routes for auth module

--- apps/web/src/app/api/auth/confirm-email/route.ts
+++ apps/web/src/app/api/auth/confirm-email/route.ts
@@ -3,9 +3,9 @@ import { z } from 'zod'
 
 import { NextHttp } from '@/rest/next/NextHttp'
 import { runApiRoute } from '@/rest/next/utils'
-import { SupabaseRouteHandlerClient } from '@/rest/supabase/clients'
 import { ConfirmEmailController } from '@/rest/controllers/auth'
-import { SupabaseAuthService } from '@/rest/supabase/services'
+import { NextServerRestClient } from '@/rest/next/NextServerRestClient'
+import { AuthService } from '@/rest/services/AuthService'
 
 export const dynamic = 'force-dynamic'
 
@@ -20,8 +20,8 @@ type Schema = z.infer<typeof schema>
 export async function GET(request: NextRequest) {
   return runApiRoute(async () => {
     const http = await NextHttp<Schema>({ request, schema })
-    const supabase = SupabaseRouteHandlerClient()
-    const authService = SupabaseAuthService(supabase)
+    const restClient = await NextServerRestClient({ isCacheEnabled: false })
+    const authService = AuthService(restClient)
     const controller = ConfirmEmailController(authService)
     return controller.handle(http)
   })
