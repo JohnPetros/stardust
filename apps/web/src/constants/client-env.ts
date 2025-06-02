89e87d4435450c36ce89b88c42537c086b134d70
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-06-01 21:00:11 -0300

    (web) 🪨 constants: add serverAppUrl to CLIENT_ENV

--- apps/web/src/constants/client-env.ts
+++ apps/web/src/constants/client-env.ts
@@ -5,13 +5,15 @@ const CLIENT_ENV = {
   supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
   supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
   supabaseCdnUrl: process.env.NEXT_PUBLIC_SUPABASE_CDN_URL ?? '',
-  appHost: process.env.NEXT_PUBLIC_APP_HOST ?? '',
+  webAppUrl: process.env.NEXT_PUBLIC_WEB_APP_URL ?? '',
+  serverAppUrl: process.env.NEXT_PUBLIC_SERVER_APP_URL ?? '',
 }
 
 new StringValidation(CLIENT_ENV.mode, 'App Mode').validate()
 new StringValidation(CLIENT_ENV.supabaseUrl, 'Supabase Url').validate()
 new StringValidation(CLIENT_ENV.supabaseKey, 'Supabase Key').validate()
 new StringValidation(CLIENT_ENV.supabaseCdnUrl, 'Supabase CDN Url').url().validate()
-new StringValidation(CLIENT_ENV.appHost, 'App Url').url().validate()
+new StringValidation(CLIENT_ENV.webAppUrl, 'App Url').url().validate()
+new StringValidation(CLIENT_ENV.serverAppUrl, 'Server App Url').url().validate()
 
 export { CLIENT_ENV }
