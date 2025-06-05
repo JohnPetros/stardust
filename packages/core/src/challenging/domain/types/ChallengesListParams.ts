a3f52c25d3b52678215e26858930f89ba5c5b9a7
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-06-05 15:36:59 -0300

    (core) ♻️ refactor: rename ListOder to  ListingOrder

--- packages/core/src/challenging/domain/types/ChallengesListParams.ts
+++ packages/core/src/challenging/domain/types/ChallengesListParams.ts
@@ -1,11 +1,15 @@
 import type { ChallengeDifficultyLevel } from '.'
-import type { ListOrder, ListParam, PaginationParams } from '../../../global/domain/types'
+import type {
+  ListingOrder,
+  ListParam,
+  PaginationParams,
+} from '../../../global/domain/types'
 
 export type ChallengesListParams = {
   difficultyLevel: ListParam<ChallengeDifficultyLevel>
   title: string
   categoriesIds: string[]
   userId: string | null
-  postOrder: ListParam<ListOrder>
-  upvotesCountOrder: ListParam<ListOrder>
+  postOrder: ListParam<ListingOrder>
+  upvotesCountOrder: ListParam<ListingOrder>
 } & PaginationParams
