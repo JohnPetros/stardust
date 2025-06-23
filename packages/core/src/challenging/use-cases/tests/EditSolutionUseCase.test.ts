3c0cf616af5119de7bcd40fbef21025e1676e0cc
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-06-23 16:11:57 -0300

    🧪 test: Edit Solution Use Case

---
+++ packages/core/src/challenging/use-cases/tests/EditSolutionUseCase.test.ts
@@ -0,0 +1,92 @@
+import { mock, type Mock } from 'ts-jest-mocker'
+
+import type { SolutionsRepository } from '#challenging/interfaces/index'
+import { EditSolutionUseCase } from '../EditSolutionUseCase'
+import { SolutionNotFoundError } from '#challenging/domain/errors/SolutionNotFoundError'
+import { SolutionsFaker } from '#challenging/domain/entities/fakers/SolutionsFaker'
+import { SolutionTitleAlreadyInUseError } from '#challenging/domain/errors/SolutionTitleAlreadyInUseError'
+
+describe('Edit Solution Use Case', () => {
+  let repository: Mock<SolutionsRepository>
+  let useCase: EditSolutionUseCase
+
+  beforeEach(() => {
+    repository = mock<SolutionsRepository>()
+    repository.findById.mockImplementation()
+    repository.replace.mockImplementation()
+
+    useCase = new EditSolutionUseCase(repository)
+  })
+
+  it('should throw an error if the solution does not exist', () => {
+    expect(
+      useCase.execute({
+        solutionId: '',
+        solutionTitle: '',
+        solutionContent: '',
+      }),
+    ).rejects.toThrow(SolutionNotFoundError)
+  })
+
+  it('should throw an error if the solution title is already in use', () => {
+    const solution = SolutionsFaker.fake()
+    repository.findById.mockResolvedValue(solution)
+    repository.findBySlug.mockResolvedValue(solution)
+
+    expect(
+      useCase.execute({
+        solutionId: solution.id.value,
+        solutionTitle: solution.title.value,
+        solutionContent: solution.content.value,
+      }),
+    ).rejects.toThrow(SolutionTitleAlreadyInUseError)
+  })
+
+  it('should set the solution title and content', async () => {
+    const solution = SolutionsFaker.fake()
+    const solutionTitle = 'fake-title'
+    const solutionContent = 'fake-content'
+    repository.findById.mockResolvedValue(solution)
+
+    await useCase.execute({
+      solutionId: solution.id.value,
+      solutionTitle,
+      solutionContent,
+    })
+
+    expect(solution.title.value).toEqual(solutionTitle)
+    expect(solution.content.value).toEqual(solutionContent)
+  })
+
+  it('should replace the solution in the repository', async () => {
+    const solution = SolutionsFaker.fake()
+    const solutionTitle = 'fake-title'
+    const solutionContent = 'fake-content'
+    repository.findById.mockResolvedValue(solution)
+
+    await useCase.execute({
+      solutionId: solution.id.value,
+      solutionTitle,
+      solutionContent,
+    })
+
+    expect(repository.replace).toHaveBeenCalledWith(solution)
+  })
+
+  it('should return the solution dto', async () => {
+    const solution = SolutionsFaker.fake()
+    const solutionTitle = 'fake-title'
+    const solutionContent = 'fake-content'
+    solution.title = solutionTitle
+    solution.content = solutionContent
+    repository.findById.mockResolvedValue(solution)
+
+    const response = await useCase.execute({
+      solutionId: solution.id.value,
+      solutionTitle,
+      solutionContent,
+    })
+
+    expect(response).toEqual(solution.dto)
+  })
+})
