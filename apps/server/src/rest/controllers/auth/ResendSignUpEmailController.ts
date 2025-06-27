7481f898dc68185cc2c08fd19c9e78940eef92df
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-06-27 16:35:21 -0300

    🚧 wip: email confirmation bug fix

--- apps/server/src/rest/controllers/auth/ResendSignUpEmailController.ts
+++ apps/server/src/rest/controllers/auth/ResendSignUpEmailController.ts
@@ -4,6 +4,7 @@ import type { RestResponse } from '@stardust/core/global/responses'
 import type { AuthService } from '@stardust/core/auth/interfaces'
 
 import { Email } from '@stardust/core/global/structures'
+import { ENV } from '@/constants/env'
 
 type Schema = {
   body: {
@@ -16,6 +17,7 @@ export class ResendSignUpEmailController implements Controller<Schema> {
 
   async handle(http: Http<Schema>): Promise<RestResponse> {
     const { email } = await http.getBody()
-    return await this.authService.resendSignUpEmail(Email.create(email))
+    await this.authService.resendSignUpEmail(Email.create(email))
+    return http.send({ webAppUrl: ENV.webAppUrl })
   }
 }
