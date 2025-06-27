501cdfea59d41b5944c7db52f29e7fd8aa1596e1
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-06-27 16:42:01 -0300

    [server] 🐛 fix:  correct ResendSignUpEmailController's rest response

--- apps/server/src/rest/controllers/auth/ResendSignUpEmailController.ts
+++ apps/server/src/rest/controllers/auth/ResendSignUpEmailController.ts
@@ -4,7 +4,6 @@ import type { RestResponse } from '@stardust/core/global/responses'
 import type { AuthService } from '@stardust/core/auth/interfaces'
 
 import { Email } from '@stardust/core/global/structures'
-import { ENV } from '@/constants/env'
 
 type Schema = {
   body: {
@@ -17,7 +16,6 @@ export class ResendSignUpEmailController implements Controller<Schema> {
 
   async handle(http: Http<Schema>): Promise<RestResponse> {
     const { email } = await http.getBody()
-    await this.authService.resendSignUpEmail(Email.create(email))
-    return http.send({ webAppUrl: ENV.webAppUrl })
+    return await this.authService.resendSignUpEmail(Email.create(email))
   }
 }
