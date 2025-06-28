628c85352b08ba4dc89f47a1eca1b5dc9011a362
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-06-28 09:49:43 -0300

    [server] 💾 db: implement DocsRepository using Supabase

--- apps/server/src/database/index.ts
+++ apps/server/src/database/index.ts
@@ -1,2 +1,7 @@
+export * from './supabase/repositories/challenging'
+export * from './supabase/repositories/documentation'
+export * from './supabase/repositories/forum'
 export * from './supabase/repositories/profile'
+export * from './supabase/repositories/ranking'
+export * from './supabase/repositories/shop'
 export * from './supabase/repositories/space'
