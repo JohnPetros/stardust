f5590bc7bf7a9ad8b6ca8afa5e2e509281420c3f
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-02 14:37:46 -0300

    📑 interfaces: add SnippetsRepository

--- packages/core/src/playground/interfaces/PlaygroundService.ts
+++ packages/core/src/playground/interfaces/PlaygroundService.ts
@@ -1,7 +1,7 @@
 import type { Id } from '#global/domain/structures/index'
 import type { Snippet } from '../domain/entities'
 import type { SnippetsListParams } from '../domain/types'
-import type { SnippetDto } from '../domain/dtos'
+import type { SnippetDto } from '../domain/entities/dtos'
 import type { PaginationResponse, RestResponse } from '#global/responses/index'
 
 export interface PlaygroundService {
@@ -9,7 +9,7 @@ export interface PlaygroundService {
   fetchSnippetsList(
     params: SnippetsListParams,
   ): Promise<RestResponse<PaginationResponse<SnippetDto>>>
-  saveSnippet(snippet: Snippet): Promise<RestResponse>
-  updateSnippet(snippet: Snippet): Promise<RestResponse>
+  createSnippet(snippet: Snippet): Promise<RestResponse>
+  editSnippet(snippet: Snippet): Promise<RestResponse>
   deleteSnippet(snippetId: Id): Promise<RestResponse>
 }
