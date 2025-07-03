3f812f24184a4178ca7bd3947d405a63cc153608
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-03 16:35:45 -0300

    [server] 📶 rest: add ValidationMiddleware

--- apps/server/src/app/hono/middlewares/index.ts
+++ apps/server/src/app/hono/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { AuthMiddleware } from './AuthMiddleware'
 export { SpaceMiddleware } from './SpaceMiddleware'
 export { ChallengingMiddleware } from './ChallengingMiddleware'
+export { ValidationMiddleware } from './ValidationMiddleware'
