329dc63c862530d401a5d08edc1a02c0bbd9f0d3
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-08 17:59:15 -0300

    📶 rest(web): refresh auth session in VerifyAuthRoutesController when needed

--- apps/web/src/rest/controllers/auth/VerifyAuthRoutesController.ts
+++ apps/web/src/rest/controllers/auth/VerifyAuthRoutesController.ts
@@ -1,7 +1,12 @@
 import type { Controller, Http } from '@stardust/core/global/interfaces'
 import type { AuthService } from '@stardust/core/auth/interfaces'
+import type { SessionDto } from '@stardust/core/auth/structures/dtos'
 
-import { ROUTES } from '@/constants'
+import { COOKIES, ROUTES } from '@/constants'
+import { cookieActions } from '@/rpc/next-safe-action'
+import { RestResponse } from '@stardust/core/global/responses'
+import { HTTP_STATUS_CODE } from '@stardust/core/global/constants'
+import { Text } from '@stardust/core/global/structures'
 
 const PUBLIC_ROUTES = [
   ROUTES.landing,
@@ -11,6 +16,32 @@ const PUBLIC_ROUTES = [
 ]
 
 export const VerifyAuthRoutesController = (authService: AuthService): Controller => {
+  async function refreshAuthSession(http: Http) {
+    const refreshToken = await cookieActions.getCookie(COOKIES.refreshToken.key)
+    if (!refreshToken?.data) {
+      return new RestResponse<SessionDto>({ statusCode: HTTP_STATUS_CODE.unauthorized })
+    }
+
+    const response = await authService.refreshSession(Text.create(refreshToken.data))
+
+    if (response.isSuccessful) {
+      await Promise.all([
+        http.setCookie(
+          COOKIES.accessToken.key,
+          response.body.accessToken,
+          response.body.durationInSeconds,
+        ),
+        http.setCookie(
+          COOKIES.refreshToken.key,
+          response.body.refreshToken,
+          response.body.durationInSeconds,
+        ),
+      ])
+    }
+
+    return response
+  }
+
   return {
     async handle(http: Http) {
       const currentRoute = http.getCurrentRoute()
@@ -21,6 +52,11 @@ export const VerifyAuthRoutesController = (authService: AuthService): Controller
       const hasSession = response.isSuccessful
       const isRootRoute = currentRoute === ROUTES.landing
       const isSignInRoute = currentRoute === ROUTES.auth.signIn
+
+      if (!hasSession) {
+        const response = await refreshAuthSession(http)
+        if (response.isSuccessful) return http.redirect(currentRoute)
+      }
 
       if (!hasSession && !isPublicRoute) {
         return http.redirect(ROUTES.auth.signIn)
