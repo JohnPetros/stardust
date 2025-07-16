cf3147c228ba61d2362903b25a1046dcaaa8d3b3
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-16 19:01:11 -0300

    ⚙️ config(studio): setup app and install initial dependnecies

---
+++ apps/studio/src/ui/global/widgets/pages/Dashboard/DashboardPageView.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/ui/shadcn/components/button'
+
+export const DashboardPageView = () => {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
