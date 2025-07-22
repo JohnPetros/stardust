d5e99a7809aaf45b2deb8c984f6542dffae5fb79
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-22 17:19:46 -0300

    📶 rest(studio): add AuthService

--- apps/studio/src/rest/index.ts
+++
@@ -1,8 +0,0 @@
-import { ENV } from '@/constants'
-import { AxiosRestClient } from './axios/AxiosRestClient'
-import { SpaceService } from './services'
-
-const restClient = AxiosRestClient()
-restClient.setBaseUrl(ENV.serverAppUrl)
-
-export const spaceService = SpaceService(restClient)
