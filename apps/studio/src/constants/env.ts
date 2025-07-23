8dc2787857ab7c24440b9ee976897269c13d94ba
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-23 17:48:26 -0300

    🪨 constants: add supabaseCdnUrl to ENV

--- apps/studio/src/constants/env.ts
+++ apps/studio/src/constants/env.ts
@@ -2,10 +2,12 @@ import { z } from 'zod'
 
 const env = {
   serverAppUrl: import.meta.env.VITE_SERVER_APP_URL,
+  supabaseCdnUrl: import.meta.env.VITE_SUPABASE_CDN_URL,
 }
 
 const envSchema = z.object({
   serverAppUrl: z.string().url(),
+  supabaseCdnUrl: z.string().url(),
 })
 
 export const ENV = envSchema.parse(env)
