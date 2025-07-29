232d34a5b8259c36182ee83cd79c137f0ce573cc
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-29 19:08:16 -0300

    📶 rest(studio): implement StorageService's uploadFile and removeFile

--- apps/studio/src/rest/services/StorageService.ts
+++ apps/studio/src/rest/services/StorageService.ts
@@ -1,6 +1,7 @@
 import type { StorageService as IStorageService } from '@stardust/core/storage/interfaces'
 import type { RestClient } from '@stardust/core/global/interfaces'
-import type { FilesListingParams } from '@stardust/core/storage/types'
+import type { FilesListingParams, StorageFolder } from '@stardust/core/storage/types'
+import type { Text } from '@stardust/core/global/structures'
 
 export const StorageService = (restClient: RestClient): IStorageService => {
   return {
@@ -9,6 +10,16 @@ export const StorageService = (restClient: RestClient): IStorageService => {
       restClient.setQueryParam('itemsPerPage', String(params.itemsPerPage.value))
       restClient.setQueryParam('search', params.search.value)
       return await restClient.get(`/storage/${params.folder}`)
+    },
+
+    async uploadFile(folder: StorageFolder, file: File) {
+      const formData = new FormData()
+      formData.append('file', file)
+      return await restClient.postFormData(`/storage/${folder}`, formData)
+    },
+
+    async removeFile(folder: StorageFolder, fileName: Text) {
+      return await restClient.delete(`/storage/${folder}/${fileName.value}`)
     },
   }
 }
--- packages/core/src/storage/interfaces/StorageService.ts
+++ packages/core/src/storage/interfaces/StorageService.ts
@@ -1,6 +1,9 @@
-import type { PaginationResponse, RestResponse } from '#global/responses'
-import type { FilesListingParams } from '../types'
+import type { PaginationResponse, RestResponse } from '@stardust/core/global/responses'
+import type { FilesListingParams, StorageFolder } from '../types'
+import type { Text } from '#global/domain/structures/Text'
 
 export interface StorageService {
   listFiles(params: FilesListingParams): Promise<RestResponse<PaginationResponse<string>>>
+  uploadFile(folder: StorageFolder, file: File): Promise<RestResponse<string>>
+  removeFile(folder: StorageFolder, filename: Text): Promise<RestResponse>
 }
