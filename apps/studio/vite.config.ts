bee244059321e0e3d622b5e5e9ffdad51841595d
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-05 13:56:49 -0300

    ⚙️ config: polyfill node process in vite config

--- apps/studio/vite.config.ts
+++ apps/studio/vite.config.ts
@@ -2,7 +2,15 @@ import { reactRouter } from '@react-router/dev/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    nodePolyfills({
+      include: ['process'],
+    }),
+  ],
 })
