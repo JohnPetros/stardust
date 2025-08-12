aa8fa9b32e1321f7658da224f1f72579bf966769
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-12 20:37:33 -0300

    🧪 test(web): signInPageView

--- apps/web/src/ui/auth/widgets/components/Link/index.tsx
+++ apps/web/src/ui/auth/widgets/components/Link/index.tsx
@@ -2,15 +2,17 @@ import NextLink from 'next/link'
 import type { ReactNode } from 'react'
 
 interface LinkProps {
+  testId: string
   children: ReactNode
   href: string
 }
 
-export function Link({ children, href }: LinkProps) {
+export function Link({ children, href, testId }: LinkProps) {
   return (
     <NextLink
       className='p-2 text-sm text-gray-300 transition-colors duration-200 hover:text-gray-100'
       href={href}
+      data-testid={testId}
     >
       {children}
     </NextLink>
--- apps/web/src/ui/auth/widgets/pages/SignIn/AnimatedHero/index.tsx
+++ apps/web/src/ui/auth/widgets/pages/SignIn/AnimatedHero/index.tsx
@@ -1,34 +1,3 @@
-'use client'
+import { AnimatedHeroView } from './AnimatedHeroView'
 
-import { AnimatePresence, type Variants, motion } from 'motion/react'
-import { Hero } from '../../../components/Hero'
-
-const variants: Variants = {
-  hidden: {
-    opacity: 0,
-    x: '75vw',
-    transition: {
-      duration: 2,
-    },
-  },
-}
-
-type AnimatedHeroProps = {
-  isVisible: boolean
-}
-
-export function AnimatedHero({ isVisible }: AnimatedHeroProps) {
-  return (
-    <AnimatePresence>
-      {!isVisible && (
-        <motion.div
-          variants={variants}
-          exit='hidden'
-          className='hidden bg-gray-800 lg:grid lg:place-content-center'
-        >
-          <Hero />
-        </motion.div>
-      )}
-    </AnimatePresence>
-  )
-}
+export const AnimatedHero = AnimatedHeroView
