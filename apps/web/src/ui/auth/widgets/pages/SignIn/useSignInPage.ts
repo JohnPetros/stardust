7d915aa1ef102398d8e06d50e5e4e476ba7124da
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-12 20:36:27 -0300

    🧪 test(web): useSignInPage

--- apps/web/src/ui/auth/widgets/pages/SignIn/useSignInPage.ts
+++ apps/web/src/ui/auth/widgets/pages/SignIn/useSignInPage.ts
@@ -5,27 +5,30 @@ import { type RefObject, useEffect, useState } from 'react'
 import { Slug } from '@stardust/core/global/structures'
 
 import { ROUTES } from '@/constants'
-import { NextRestClient } from '@/rest/next/NextRestClient'
 import type { AnimationRef } from '@/ui/global/widgets/components/Animation/types'
-import { useAuthContext } from '@/ui/auth/contexts/AuthContext'
 import { useToastContext } from '@/ui/global/contexts/ToastContext'
 import { ROCKET_ANIMATION_DELAY } from '@/ui/auth/constants'
 import { useRouter } from '@/ui/global/hooks/useRouter'
-import { useQueryStringParam } from '@/ui/global/hooks/useQueryStringParam'
 import { useSleep } from '@/ui/global/hooks/useSleep'
 import type { SignInFormFields } from './SignInForm/types'
 
-export function useSignInPage(
-  url: string,
-  rocketAnimationRef: RefObject<AnimationRef | null>,
-) {
+type Params = {
+  rocketAnimationRef: RefObject<AnimationRef | null>
+  error: string
+  nextRoute: string
+  handleSignIn: (email: string, password: string) => Promise<boolean>
+}
+
+export function useSignInPage({
+  rocketAnimationRef,
+  error,
+  nextRoute,
+  handleSignIn,
+}: Params) {
   const [isRocketVisible, setIsRocketVisible] = useState(false)
-  const [nextRoute] = useQueryStringParam('nextRoute')
   const { sleep } = useSleep()
-  const { handleSignIn } = useAuthContext()
   const toast = useToastContext()
   const router = useRouter()
-  NextRestClient()
 
   async function handleFormSubmit({ email, password }: SignInFormFields) {
     const isSuccess = await handleSignIn(email, password)
@@ -48,16 +51,13 @@ export function useSignInPage(
   }
 
   useEffect(() => {
-    if (url.includes('error=')) {
-      const errorMessage = url.split('error=').at(-1)
+    if (error) {
+      const errorMessage = error.split('error=').at(-1)
       if (!errorMessage) return
 
-      toast.show(Slug.deslugify(errorMessage), {
-        type: 'error',
-        seconds: 3.5,
-      })
+      toast.showError(Slug.deslugify(errorMessage), 3.5)
     }
-  }, [url, toast])
+  }, [error, toast])
 
   return {
     rocketAnimationRef,
