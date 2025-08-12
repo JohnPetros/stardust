c5916a7bdce34925c60eba2edbba9c867056b4fb
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-12 18:04:44 -0300

    🗃️ ftree(web):  remove all texts and questions mocks from __tests__ folder

--- apps/web/src/constants/client-env.ts
+++ apps/web/src/constants/client-env.ts
@@ -2,16 +2,12 @@ import { StringValidation } from '@stardust/core/global/libs'
 
 const CLIENT_ENV = {
   mode: process.env.NODE_ENV,
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
   supabaseCdnUrl: process.env.NEXT_PUBLIC_SUPABASE_CDN_URL ?? '',
   webAppUrl: process.env.NEXT_PUBLIC_WEB_APP_URL ?? '',
   serverAppUrl: process.env.NEXT_PUBLIC_SERVER_APP_URL ?? '',
 }
 
 new StringValidation(CLIENT_ENV.mode, 'App Mode').validate()
-new StringValidation(CLIENT_ENV.supabaseUrl, 'Supabase Url').validate()
-new StringValidation(CLIENT_ENV.supabaseKey, 'Supabase Key').validate()
 new StringValidation(CLIENT_ENV.supabaseCdnUrl, 'Supabase CDN Url').url().validate()
 new StringValidation(CLIENT_ENV.webAppUrl, 'App Url').url().validate()
 new StringValidation(CLIENT_ENV.serverAppUrl, 'Server App Url').url().validate()
