3fe5c1642d17705d229b1c234cc915b2917ff61f
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-19 19:00:14 -0300

    ♻️ refactor(web): add isDefaultAudioDisabled prop and toggleAudioDisability method to AudioContext

--- apps/web/src/ui/global/contexts/AudioContext/index.tsx
+++ apps/web/src/ui/global/contexts/AudioContext/index.tsx
@@ -1,26 +1,21 @@
 'use client'
 
-import { createContext, type ReactNode } from 'react'
+import { createContext, type PropsWithChildren } from 'react'
 
 import type { AudioContextValue } from './types'
 import { useAudioContextProvider } from './useAudioContextProvider'
 
-type AudioContextProps = {
-  children: ReactNode
+type Props = {
+  isDefaultAudioDisabled: boolean
 }
 
 export const AudioContext = createContext({} as AudioContextValue)
 
-export const AudioContextProvider = ({ children }: AudioContextProps) => {
-  const { playAudio } = useAudioContextProvider()
+export const AudioContextProvider = ({
+  children,
+  isDefaultAudioDisabled,
+}: PropsWithChildren<Props>) => {
+  const contextValue = useAudioContextProvider(isDefaultAudioDisabled)
 
-  return (
-    <AudioContext.Provider
-      value={{
-        playAudio,
-      }}
-    >
-      {children}
-    </AudioContext.Provider>
-  )
+  return <AudioContext.Provider value={contextValue}>{children}</AudioContext.Provider>
 }
