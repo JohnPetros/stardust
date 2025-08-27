6796d32eea04e988bc907b5f0c5819ca2ef09e67
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-27 20:54:05 -0300

    📑 interface: enhance AuthService interface with social account methods for improved authentication options

--- packages/core/src/auth/interfaces/AuthService.ts
+++ packages/core/src/auth/interfaces/AuthService.ts
@@ -3,12 +3,16 @@ import type { RestResponse } from '#global/responses/RestResponse'
 import type { Password } from '../domain/structures'
 import type { AccountDto } from '../domain/entities/dtos'
 import type { SessionDto } from '../domain/structures/dtos'
+import type { Account } from '../domain/entities'
 
 export interface AuthService {
   fetchAccount(): Promise<RestResponse<AccountDto>>
+  fetchSocialAccount(): Promise<RestResponse<AccountDto>>
   signIn(email: Email, password: Password): Promise<RestResponse<SessionDto>>
   signUp(email: Email, password: Password): Promise<RestResponse<AccountDto>>
+  signUpWithSocialAccount(socialAccount: Account): Promise<RestResponse<AccountDto>>
   signOut(): Promise<RestResponse>
+  signInWithGoogle(): Promise<RestResponse<{ url: string }>>
   resendSignUpEmail(email: Email): Promise<RestResponse>
   requestSignUp(email: Email, password: Password, name: Name): Promise<RestResponse>
   requestPasswordReset(email: Email): Promise<RestResponse>
