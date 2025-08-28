22ec6b036de2eff149732acafdfa42878a2bf48f
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-28 18:02:31 -0300

    📶 rest(server): add FetchSocialAccountController to retrieve social account information

---
+++ apps/server/src/rest/controllers/auth/FetchSocialAccountController.ts
@@ -0,0 +1,12 @@
+import type { Controller } from '@stardust/core/global/interfaces'
+import type { Http } from '@stardust/core/global/interfaces'
+import type { RestResponse } from '@stardust/core/global/responses'
+import type { AuthService } from '@stardust/core/auth/interfaces'
+
+export class FetchSocialAccountController implements Controller {
+  constructor(private readonly authService: AuthService) {}
+
+  async handle(_: Http): Promise<RestResponse> {
+    return await this.authService.fetchSocialAccount()
+  }
+}
