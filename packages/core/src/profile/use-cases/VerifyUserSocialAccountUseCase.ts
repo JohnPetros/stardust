7ee7c4820ab94558fb7060bb62520e7aea011213
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-28 18:15:48 -0300

    📑 interface: implement VerifyUserSocialAccount use case and middleware for social account verification

---
+++ packages/core/src/profile/use-cases/VerifyUserSocialAccountUseCase.ts
@@ -0,0 +1,75 @@
+import type { UseCase } from '#global/interfaces/index'
+import type { UsersRepository } from '#profile/interfaces/index'
+import { AccountProvider, Email, Id, Name } from '#global/domain/structures/index'
+import { UserSocialAccountAlreadyInUseError } from '../errors'
+import { AppError } from '#global/domain/errors/AppError'
+
+type Request = {
+  socialAccountId: string
+  socialAccountName: string
+  socialAccountEmail: string
+  socialAccountProvider: string
+}
+
+type Response = Promise<{ deduplicatedUserName: string }>
+
+export class VerifyUserSocialAccountUseCase implements UseCase<Request, Response> {
+  constructor(private readonly repository: UsersRepository) {}
+
+  async execute({
+    socialAccountId,
+    socialAccountName,
+    socialAccountEmail,
+    socialAccountProvider,
+  }: Request) {
+    const accountId = Id.create(socialAccountId)
+    const accountProvider = AccountProvider.create(socialAccountProvider)
+    const accountEmail = Email.create(socialAccountEmail)
+
+    await this.findUserBySocialAccount(accountId, accountProvider)
+    await this.findUserByEmail(accountEmail, accountId, accountProvider)
+
+    let name = Name.create(socialAccountName)
+    let user = await this.repository.findByName(name)
+
+    while (user) {
+      name = name.deduplicate()
+      user = await this.repository.findByName(name)
+    }
+
+    return { deduplicatedUserName: name.value }
+  }
+
+  private async findUserBySocialAccount(
+    socialAccountId: Id,
+    socialAccountProvider: AccountProvider,
+  ) {
+    switch (socialAccountProvider.value) {
+      case 'google': {
+        const user = await this.repository.findByGoogleAccountId(socialAccountId)
+        if (user) throw new UserSocialAccountAlreadyInUseError()
+        break
+      }
+      case 'github': {
+        const user = await this.repository.findByGithubAccountId(socialAccountId)
+        if (user) throw new UserSocialAccountAlreadyInUseError()
+        break
+      }
+      default:
+        throw new AppError('Provedor de conta social não suportado')
+    }
+  }
+
+  private async findUserByEmail(
+    email: Email,
+    socialAccountId: Id,
+    socialAccountProvider: AccountProvider,
+  ) {
+    const user = await this.repository.findByEmail(email)
+    if (user) {
+      user.setSocialAccountId(socialAccountId, socialAccountProvider)
+      await this.repository.replace(user)
+      throw new UserSocialAccountAlreadyInUseError()
+    }
+  }
+}
