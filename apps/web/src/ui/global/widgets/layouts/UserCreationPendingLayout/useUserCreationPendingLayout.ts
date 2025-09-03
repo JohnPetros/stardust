79f735d235dff905ec07fce0bc85b2368fc3784e
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-03 17:24:49 -0300

    ♻️ refactor(web): enhance useUserCreationPendingLayout to manage user state with useEffect for improved responsiveness

--- apps/web/src/ui/global/widgets/layouts/UserCreationPendingLayout/useUserCreationPendingLayout.ts
+++ apps/web/src/ui/global/widgets/layouts/UserCreationPendingLayout/useUserCreationPendingLayout.ts
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-export function useUserCreationPendingLayout(isUserCreated: boolean) {
-  const [isUserCreationPending, setIsUserCreationPending] = useState(!isUserCreated)
+export function useUserCreationPendingLayout(user: User | null) {
+  const [isUserCreationPending, setIsUserCreationPending] = useState(user === null)
 
   function handleUserCreated() {
     setIsUserCreationPending(false)
   }
+
+  useEffect(() => {
+    setIsUserCreationPending(user === null)
+  }, [user])
 
   return {
     isUserCreationPending,
