5cbca42ea34967c73f5fc727f0e9e956998c1e46
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-04 16:18:55 -0300

    🖥️ ui(web): improve AlertView layout by restructuring flex properties and enhancing child component alignment

--- apps/web/src/ui/global/widgets/components/Mdx/Alert/AlertView.tsx
+++ apps/web/src/ui/global/widgets/components/Mdx/Alert/AlertView.tsx
@@ -20,28 +20,30 @@ export const AlertView = ({
 }: AlertProps) => {
   return (
     <Animation hasAnimation={hasAnimation}>
-      <div className='not-prose flex w-full flex-col items-center'>
+      <div className='not-prose flex w-full flex-col'>
         {title && (
           <div className='mb-4'>
             <Title>{title}</Title>
           </div>
         )}
         <div>
-          <div className='flex w-full items-center'>
+          <div className='flex w-full flex-col gap-4 md:gap-2 md:items-center md:justify-center md:flex-row'>
             {picture && <Picture url={picture} />}
-            <span className='mr-3 block'>
-              <Image
-                src='/icons/alert.svg'
-                width={32}
-                height={32}
-                alt=''
-                className='skeleton'
-                onLoadingComplete={(image) => image.classList.remove('skeleton')}
-              />
-            </span>
-            <Content type='alert' hasAnimation={hasAnimation}>
-              {children}
-            </Content>
+            <div className='flex w-full items-center'>
+              <span className='mr-3 block'>
+                <Image
+                  src='/icons/alert.svg'
+                  width={32}
+                  height={32}
+                  alt=''
+                  className='skeleton'
+                  onLoadingComplete={(image) => image.classList.remove('skeleton')}
+                />
+              </span>
+              <Content type='alert' hasAnimation={hasAnimation}>
+                {children}
+              </Content>
+            </div>
           </div>
         </div>
       </div>
