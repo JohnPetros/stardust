81cb9dff3b17547b76a939b663b72095de7cca49
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-05 19:53:53 -0300

    📮 validation: introduce accountProviderSchema for account validation and remove deprecated accountSchema

--- packages/validation/src/modules/auth/schemas/accountSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-import { booleanSchema, emailSchema, idSchema, nameSchema } from '../../global/schemas'
-
-export const accountSchema = z.object({
-  id: idSchema.optional(),
-  email: emailSchema,
-  name: nameSchema,
-  isAuthenticated: booleanSchema,
-})
