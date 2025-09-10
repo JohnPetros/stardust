f53b0deb1a2f783e301d8c4a9555865788271be0
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-10 19:13:40 -0300

    ✨ use case: enhance GetNextStarUseCase to include user information and publish PlanetCompletedEvent

--- packages/core/src/space/use-cases/tests/GetNextStarUseCase.test.ts
+++ packages/core/src/space/use-cases/tests/GetNextStarUseCase.test.ts
@@ -1,33 +1,40 @@
 import { mock, type Mock } from 'ts-jest-mocker'
 
 import type { StarsRepository, PlanetsRepository } from '#space/interfaces/index'
+import type { EventBroker } from '#global/interfaces/EventBroker'
 import { GetNextStarUseCase } from '../GetNextStarUseCase'
 import { StarNotFoundError } from '#space/domain/errors/StarNotFoundError'
 import { Id } from '#global/domain/structures/Id'
 import { StarsFaker } from '#space/domain/entities/tests/fakers/StarsFaker'
 import { PlanetNotFoundError } from '#space/domain/errors/PlanetNotFoundError'
 import { PlanetsFaker } from '#space/domain/entities/tests/fakers/PlanetsFaker'
+import { PlanetCompletedEvent } from '#space/domain/events/PlanetCompletedEvent'
 
 describe('Get Next Star Use Case', () => {
   let starsRepository: Mock<StarsRepository>
   let planetsRepository: Mock<PlanetsRepository>
+  let eventBroker: Mock<EventBroker>
   let useCase: GetNextStarUseCase
+  const userName = 'John Doe'
+  const userSlug = 'john-doe'
 
   beforeEach(() => {
     starsRepository = mock<StarsRepository>()
     planetsRepository = mock<PlanetsRepository>()
+    eventBroker = mock<EventBroker>()
+    eventBroker.publish.mockImplementation()
     starsRepository.findById.mockImplementation()
     planetsRepository.findByStar.mockImplementation()
     planetsRepository.findByPosition.mockImplementation()
-    useCase = new GetNextStarUseCase(starsRepository, planetsRepository)
+    useCase = new GetNextStarUseCase(starsRepository, planetsRepository, eventBroker)
   })
 
   it('should throw an error if the current star is not found', () => {
     starsRepository.findById.mockResolvedValue(null)
 
-    expect(useCase.execute({ currentStarId: Id.create().value })).rejects.toThrow(
-      StarNotFoundError,
-    )
+    expect(
+      useCase.execute({ currentStarId: Id.create().value, userName, userSlug }),
+    ).rejects.toThrow(StarNotFoundError)
   })
 
   it('should throw an error if the current planet is not found', () => {
@@ -35,9 +42,9 @@ describe('Get Next Star Use Case', () => {
     starsRepository.findById.mockResolvedValue(star)
     planetsRepository.findByStar.mockResolvedValue(null)
 
-    expect(useCase.execute({ currentStarId: star.id.value })).rejects.toThrow(
-      PlanetNotFoundError,
-    )
+    expect(
+      useCase.execute({ currentStarId: star.id.value, userName, userSlug }),
+    ).rejects.toThrow(PlanetNotFoundError)
   })
 
   it('should try to find the next star in the next planet if the current star is the last one of the current planet', async () => {
@@ -46,7 +53,7 @@ describe('Get Next Star Use Case', () => {
     starsRepository.findById.mockResolvedValue(star)
     planetsRepository.findByStar.mockResolvedValue(planet)
 
-    await useCase.execute({ currentStarId: star.id.value })
+    await useCase.execute({ currentStarId: star.id.value, userName, userSlug })
 
     expect(planetsRepository.findByPosition).toHaveBeenCalledWith(
       planet.position.increment(),
@@ -60,7 +67,11 @@ describe('Get Next Star Use Case', () => {
     starsRepository.findById.mockResolvedValue(currentStar)
     planetsRepository.findByStar.mockResolvedValue(planet)
 
-    const response = await useCase.execute({ currentStarId: currentStar.id.value })
+    const response = await useCase.execute({
+      currentStarId: currentStar.id.value,
+      userName,
+      userSlug,
+    })
 
     expect(response).toEqual(nextStar.dto)
   })
@@ -74,12 +85,40 @@ describe('Get Next Star Use Case', () => {
     planetsRepository.findByStar.mockResolvedValue(planet)
     planetsRepository.findByPosition.mockResolvedValue(nextPlanet)
 
-    const response = await useCase.execute({ currentStarId: currentStar.id.value })
+    const response = await useCase.execute({
+      currentStarId: currentStar.id.value,
+      userName,
+      userSlug,
+    })
 
     expect(planetsRepository.findByPosition).toHaveBeenCalledWith(
       planet.position.increment(),
     )
     expect(response).toEqual(nextStar.dto)
+  })
+
+  it('should publish an event when the current planet is completed', async () => {
+    const currentStar = StarsFaker.fake()
+    const nextStar = StarsFaker.fake()
+    const planet = PlanetsFaker.fake({ stars: [currentStar.dto] })
+    const nextPlanet = PlanetsFaker.fake({ stars: [nextStar.dto] })
+    starsRepository.findById.mockResolvedValue(currentStar)
+    planetsRepository.findByStar.mockResolvedValue(planet)
+    planetsRepository.findByPosition.mockResolvedValue(nextPlanet)
+
+    await useCase.execute({
+      currentStarId: currentStar.id.value,
+      userName,
+      userSlug,
+    })
+
+    expect(eventBroker.publish).toHaveBeenCalledWith(
+      new PlanetCompletedEvent({
+        userSlug,
+        userName,
+        planetName: planet.name.value,
+      }),
+    )
   })
 
   it('should return null if no next star is found', async () => {
@@ -88,7 +127,11 @@ describe('Get Next Star Use Case', () => {
     starsRepository.findById.mockResolvedValue(currentStar)
     planetsRepository.findByStar.mockResolvedValue(planet)
 
-    const response = await useCase.execute({ currentStarId: currentStar.id.value })
+    const response = await useCase.execute({
+      currentStarId: currentStar.id.value,
+      userName,
+      userSlug,
+    })
 
     expect(response).toBeNull()
   })
