d7d18c668f64aba3a219dc3d8ed37426b62c209e
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-10 19:12:46 -0300

    📶 rest(server): implement DiscordNotificationService for sending user notifications

---
+++ apps/server/src/rest/services/DiscordNotificationService.ts
@@ -0,0 +1,26 @@
+import type { RestClient } from '@stardust/core/global/interfaces'
+import type { RestResponse } from '@stardust/core/global/responses'
+import type { NotificationService } from '@stardust/core/notification/interfaces'
+
+export class DiscordNotificationService implements NotificationService {
+  constructor(private readonly restClient: RestClient) {}
+
+  async sendPlanetCompletedNotification(
+    userSlug: string,
+    userName: string,
+    planetName: string,
+  ): Promise<RestResponse> {
+    return await this.restClient.post('/', {
+      content: `🎉 Um usuário concluiu o planeta ${planetName} 🪐, seu nome é ${userName}. Link do perfil: https://stardust-app.com.br/profile/${userSlug}`,
+    })
+  }
+
+  async sendSpaceCompletedNotification(
+    userSlug: string,
+    userName: string,
+  ): Promise<RestResponse> {
+    return await this.restClient.post('/', {
+      content: `🎉 Um usuário concluiu todo o espaço de exploração 🌌, seu nome é ${userName}. Link do perfil: https://stardust-app.com.br/profile/${userSlug}`,
+    })
+  }
+}
