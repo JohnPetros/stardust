bb55412dab0da2fa70fbed8935aee666ad881441
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-23 16:52:03 -0300

    ♻️ refactor(lsp): remove useCodeRunner hook and introduce useLsp for LSP-based code execution

--- apps/studio/src/ui/global/hooks/useCodeRunner.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo } from 'react'
-
-import { ExecutorDeCodigoDelegua } from '@stardust/code-runner'
-
-export function useCodeRunner() {
-  return useMemo(() => new ExecutorDeCodigoDelegua(), [])
-}
