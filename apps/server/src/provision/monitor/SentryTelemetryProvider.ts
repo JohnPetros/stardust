b546413f919ebb27dcb9b950ce74188fde8aa7e0
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-30 17:28:26 -0300

    🧰 provision(server): add Sentry telemetry provider and integrate Sentry DSN in environment configuration

---
+++ apps/server/src/provision/monitor/SentryTelemetryProvider.ts
@@ -0,0 +1,28 @@
+import { ENV } from '@/constants'
+import * as Sentry from '@sentry/node'
+
+import type { TelemetryProvider } from '@stardust/core/global/interfaces'
+import { AppError } from '@stardust/core/global/errors'
+
+export class SentryTelemetryProvider implements TelemetryProvider {
+  private readonly sentry: Sentry.NodeClient
+
+  constructor() {
+    const sentry = Sentry.init({
+      dsn: ENV.sentryDsn,
+    })
+    if (!sentry) throw new AppError('Failed to initialize Sentry')
+
+    console.log('🔍 Sentry initialized')
+
+    this.sentry = sentry
+  }
+
+  trackError(error: Error): void {
+    this.sentry.captureException(error, {
+      data: {
+        accountId: '123',
+      },
+    })
+  }
+}
---
+++ apps/server/src/provision/monitor/index.ts
@@ -0,0 +1,1 @@
+export { SentryTelemetryProvider } from './SentryTelemetryProvider'
