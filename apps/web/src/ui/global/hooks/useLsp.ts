32b2ef0f55a8262a479a93624c6a8462170b72da
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-09 19:57:59 -0300

    🖥️ ui(web): update useLsp hook to include DELEGUA_SNIPPETS for enhanced functionality

--- apps/web/src/ui/global/hooks/useLsp.ts
+++ apps/web/src/ui/global/hooks/useLsp.ts
@@ -1,12 +1,13 @@
 import { useMemo } from 'react'
 
-import { DELEGUA_DOCUMENTACOES, DeleguaLsp } from '@stardust/lsp'
+import { DELEGUA_DOCUMENTACOES, DELEGUA_SNIPPETS, DeleguaLsp } from '@stardust/lsp'
 
 export function useLsp() {
   return useMemo(() => {
     return {
       lspProvider: new DeleguaLsp(),
       documentations: DELEGUA_DOCUMENTACOES,
+      snippets: DELEGUA_SNIPPETS,
     }
   }, [])
 }
