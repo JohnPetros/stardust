7f03c26fbf6feea1440942b75b1ee315909dbd04
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-18 18:39:08 -0300

    📶 rest(server): add routes for editing star name, availability, and type in StarsRouter

--- apps/server/src/app/hono/routers/space/StarsRouter.ts
+++ apps/server/src/app/hono/routers/space/StarsRouter.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono'
 import { z } from 'zod'
 
-import { idSchema, stringSchema } from '@stardust/validation/global/schemas'
+import {
+  booleanSchema,
+  idSchema,
+  nameSchema,
+  stringSchema,
+} from '@stardust/validation/global/schemas'
 
-import { FetchStarController } from '@/rest/controllers/space/stars'
+import {
+  EditStarAvailabilityController,
+  EditStarNameController,
+  EditStarTypeController,
+  FetchStarController,
+} from '@/rest/controllers/space/stars'
 import { SupabaseStarsRepository } from '@/database'
 import { HonoRouter } from '../../HonoRouter'
 import { HonoHttp } from '../../HonoHttp'
@@ -54,9 +64,90 @@ export class StarsRouter extends HonoRouter {
     )
   }
 
+  private registerEditStarNameRoute(): void {
+    this.router.patch(
+      '/:starId/name',
+      this.authMiddleware.verifyAuthentication,
+      this.validationMiddleware.validate(
+        'param',
+        z.object({
+          starId: idSchema,
+        }),
+      ),
+      this.validationMiddleware.validate(
+        'json',
+        z.object({
+          name: nameSchema,
+        }),
+      ),
+      async (context) => {
+        const http = new HonoHttp(context)
+        const repository = new SupabaseStarsRepository(http.getSupabase())
+        const controller = new EditStarNameController(repository)
+        const response = await controller.handle(http)
+        return http.sendResponse(response)
+      },
+    )
+  }
+
+  private registerEditStarAvailabilityRoute(): void {
+    this.router.patch(
+      '/:starId/availability',
+      this.authMiddleware.verifyAuthentication,
+      this.validationMiddleware.validate(
+        'param',
+        z.object({
+          starId: idSchema,
+        }),
+      ),
+      this.validationMiddleware.validate(
+        'json',
+        z.object({
+          isAvailable: booleanSchema,
+        }),
+      ),
+      async (context) => {
+        const http = new HonoHttp(context)
+        const repository = new SupabaseStarsRepository(http.getSupabase())
+        const controller = new EditStarAvailabilityController(repository)
+        const response = await controller.handle(http)
+        return http.sendResponse(response)
+      },
+    )
+  }
+
+  private registerEditStarTypeRoute(): void {
+    this.router.patch(
+      '/:starId/type',
+      this.authMiddleware.verifyAuthentication,
+      this.validationMiddleware.validate(
+        'param',
+        z.object({
+          starId: idSchema,
+        }),
+      ),
+      this.validationMiddleware.validate(
+        'json',
+        z.object({
+          isChallenge: booleanSchema,
+        }),
+      ),
+      async (context) => {
+        const http = new HonoHttp(context)
+        const repository = new SupabaseStarsRepository(http.getSupabase())
+        const controller = new EditStarTypeController(repository)
+        const response = await controller.handle(http)
+        return http.sendResponse(response)
+      },
+    )
+  }
+
   registerRoutes(): Hono {
     this.registerFetchStarBySlugRoute()
     this.registerFetchStarByIdRoute()
+    this.registerEditStarNameRoute()
+    this.registerEditStarAvailabilityRoute()
+    this.registerEditStarTypeRoute()
     return this.router
   }
 }
