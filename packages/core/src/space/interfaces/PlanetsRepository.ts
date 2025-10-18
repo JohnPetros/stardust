c563c6fcc148dde02dc8f837106adbc10f385ccc
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-18 18:37:27 -0300

    💾 db(server): add isAvailable and isChallenge properties to Star and Planet entities; add findById method for planets

--- packages/core/src/space/interfaces/PlanetsRepository.ts
+++ packages/core/src/space/interfaces/PlanetsRepository.ts
@@ -3,6 +3,7 @@ import type { Id } from '../../main'
 import type { Planet } from '../domain/entities'
 
 export interface PlanetsRepository {
+  findById(id: Id): Promise<Planet | null>
   findAll(): Promise<Planet[]>
   findByPosition(position: OrdinalNumber): Promise<Planet | null>
   findByStar(starId: Id): Promise<Planet | null>
