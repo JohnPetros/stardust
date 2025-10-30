22f5562ebf10bc47b5f5a51e963a6d80322d3ea9
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-30 18:30:07 -0300

    🖥️ ui(web): conditionally render Google Analytics in RootLayoutView based on server environment

--- apps/web/src/ui/global/widgets/layouts/Root/RootLayoutView.tsx
+++ apps/web/src/ui/global/widgets/layouts/Root/RootLayoutView.tsx
@@ -5,14 +5,15 @@ import '@/ui/global/styles/global.css'
 import { roboto_mono, poppins } from '@/constants/fonts'
 import { ClientProviders } from './ClientProviders'
 import { ServerProviders } from './ServerProviders'
+import { SERVER_ENV } from '@/constants/server-env'
 
 export const RootLayoutView = ({ children }: PropsWithChildren) => {
   return (
     <html lang='pt-BR' className='scroll-smooth'>
-      <GoogleAnalytics gaId="G-S77CE2QV3E" />
       <body
         className={`${roboto_mono.variable} ${poppins.variable} relative h-screen bg-gray-900 font-normal`}
       >
+     {SERVER_ENV.mode === 'production' && <GoogleAnalytics gaId="G-S77CE2QV3E" />}
         <ServerProviders>
           <ClientProviders>{children}</ClientProviders>
         </ServerProviders>
