cda532e70e4f691f8095621f4736b839bd30d328
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-30 18:38:34 -0300

    🪨 constants(web): remove unused environment variables from serverEnv and schema

--- apps/web/src/constants/server-env.ts
+++ apps/web/src/constants/server-env.ts
@@ -4,16 +4,10 @@ import { appModeSchema } from '@stardust/validation/global/schemas'
 
 const serverEnv = {
   mode: process.env.MODE,
-  inngestSigningKey: process.env.INNGEST_SIGNING_KEY,
-  inngestEventKey: process.env.INNGEST_EVENT_KEY,
-  googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
 }
 
 const schema = z.object({
   mode: appModeSchema,
-  inngestSigningKey: z.string(),
-  inngestEventKey: z.string(),
-  googleAnalyticsId: z.string(),
 })
 
 export const SERVER_ENV = schema.parse(serverEnv)
