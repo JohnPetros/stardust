7c24cb668e6e7837d2e463c91d189423f9b725a2
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-30 18:04:38 -0300

    🖥️ ui(web): integrate Google Analytics into RootLayoutView for enhanced tracking

--- apps/web/src/ui/global/widgets/layouts/Root/RootLayoutView.tsx
+++ apps/web/src/ui/global/widgets/layouts/Root/RootLayoutView.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from 'react'
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 import '@/ui/global/styles/global.css'
 import { roboto_mono, poppins } from '@/constants/fonts'
@@ -8,6 +9,7 @@ import { ServerProviders } from './ServerProviders'
 export const RootLayoutView = ({ children }: PropsWithChildren) => {
   return (
     <html lang='pt-BR' className='scroll-smooth'>
+      <GoogleAnalytics gaId="G-S77CE2QV3E" />
       <body
         className={`${roboto_mono.variable} ${poppins.variable} relative h-screen bg-gray-900 font-normal`}
       >
