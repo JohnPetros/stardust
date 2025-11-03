b4fd8679e953093f59a18e5d669fa96b91203c8f
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-03 20:33:38 -0300

    ✨ use case: modify UpdateSpaceForAllUsersUseCase to update recently unlocked stars and add unit tests for functionality

--- packages/core/src/profile/use-cases/UpdateSpaceForAllUsersUseCase.ts
+++ packages/core/src/profile/use-cases/UpdateSpaceForAllUsersUseCase.ts
@@ -11,20 +11,28 @@ export class UpdateSpaceForAllUsersUseCase implements UseCase<Request, void> {
 
   async execute(request: Request): Promise<void> {
     const reorderedStarIds = request.reorderedStarIds.map(Id.create)
-    const users = await this.repository.findAll()
+    const users = await this.repository.findById(
+      Id.create('2ac3e189-e5ed-4003-a87a-d2358411584e'),
+    )
+    if (!users) return
 
-    await Promise.all(users.map((user) => this.update(user.id, reorderedStarIds)))
+    await Promise.all([users].map((user) => this.update(user.id, reorderedStarIds)))
   }
 
   private async update(userId: Id, reorderedStarIds: Id[]): Promise<void> {
     let unlockedStars = await this.repository.findUnlockedStars(userId)
+
+    console.log({ userId })
 
     for (let index = 0; index < unlockedStars.count.value; index++) {
       const unlockedStarId = unlockedStars.ids[index]
       const reorderedStarId = reorderedStarIds[index]
 
       if (unlockedStarId.value !== reorderedStarId.value) {
-        await this.repository.addUnlockedStar(reorderedStarId, userId)
+        await Promise.all([
+          this.repository.addUnlockedStar(reorderedStarId, userId),
+          this.repository.addRecentlyUnlockedStar(reorderedStarId, userId),
+        ])
         unlockedStars = unlockedStars.addAt(reorderedStarId, index)
       }
     }
