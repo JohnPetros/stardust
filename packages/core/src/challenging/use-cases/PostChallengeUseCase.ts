e5002e6ecb9ce50574a6624b6aba72667a88f481
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-12 18:04:59 -0300

    ♻️ refactor: replace ChallengingService with ChallengesRepository in PostChallengeUseCase for improved challenge management

--- packages/core/src/challenging/use-cases/PostChallengeUseCase.ts
+++ packages/core/src/challenging/use-cases/PostChallengeUseCase.ts
@@ -1,8 +1,9 @@
-import type { ChallengingService } from '../interfaces'
-import { Slug } from '#global/domain/structures/Slug'
+import type { ChallengesRepository } from '../interfaces'
+import type { Slug } from '#global/domain/structures/Slug'
 import type { UseCase } from '#global/interfaces/UseCase'
 import type { ChallengeDto } from '../domain/entities/dtos'
 import { Challenge } from '../domain/entities'
+import { ChallengeAlreadyExistsError } from '../domain/errors'
 
 type Request = {
   challengeDto: ChallengeDto
@@ -11,33 +12,17 @@ type Request = {
 type Response = Promise<ChallengeDto>
 
 export class PostChallengeUseCase implements UseCase<Request, Response> {
-  constructor(private readonly challengingService: ChallengingService) {}
+  constructor(private readonly repository: ChallengesRepository) {}
 
   async execute({ challengeDto }: Request) {
     const challenge = Challenge.create(challengeDto)
-    await this.fetchChallenge(challenge.slug.value)
-    await this.saveChallenge(challenge)
-    await this.saveChallengeCategories(challenge)
+    await this.findChallenge(challenge.slug)
+    await this.repository.add(challenge)
     return challenge.dto
   }
 
-  private async fetchChallenge(challengeSlug: string) {
-    const response = await this.challengingService.fetchChallengeBySlug(
-      Slug.create(challengeSlug),
-    )
-    if (response.isSuccessful) response.throwError()
-  }
-
-  private async saveChallenge(challenge: Challenge) {
-    const response = await this.challengingService.saveChallenge(challenge)
-    if (response.isFailure) response.throwError()
-  }
-
-  private async saveChallengeCategories(challenge: Challenge) {
-    const response = await this.challengingService.saveChallengeCategories(
-      challenge.id,
-      challenge.categories,
-    )
-    if (response.isFailure) response.throwError()
+  private async findChallenge(challengeSlug: Slug) {
+    const challenge = await this.repository.findBySlug(challengeSlug)
+    if (challenge) throw new ChallengeAlreadyExistsError()
   }
 }
