12dc9c4e81f7be7f9038640aa7c79317e4e3d46d
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-12 18:08:47 -0300

    📮 validation: update challengeSchema to include author, position, and new categories/difficulty level schemas

--- packages/validation/src/modules/challenging/schemas/challengeSchema.ts
+++ packages/validation/src/modules/challenging/schemas/challengeSchema.ts
@@ -1,58 +1,29 @@
 import z from 'zod'
-import { nameSchema } from '../../global/schemas/nameSchema'
 import { contentSchema } from '../../global/schemas/contentSchema'
 import { idSchema } from '../../global/schemas/idSchema'
-import { dataTypeNameSchema } from './dataTypeNameSchema'
 import { booleanSchema } from '../../global/schemas/booleanSchema'
-import { codeVariableNameSchema, titleSchema } from '../../global/schemas'
+import { ordinalNumberSchema, titleSchema } from '../../global/schemas'
+import { challengeCategoriesSchema } from './challengeCategoriesSchema'
+import { challengeDifficultyLevelSchema } from './challengeDifficultyLevelSchema'
 
 export const challengeSchema = z.object({
   title: titleSchema,
-  function: z.object({
-    name: codeVariableNameSchema,
-    params: z
-      .array(
-        z.object({
-          name: codeVariableNameSchema,
-          dataTypeName: dataTypeNameSchema,
-        }),
-      )
-      .min(1),
-  }),
   description: contentSchema,
+  author: z.object({
+    id: idSchema,
+  }),
   code: contentSchema,
   testCases: z
     .array(
       z.object({
-        inputs: z.array(
-          z
-            .object({
-              value: z.unknown(),
-            })
-            .transform((input) => {
-              if (Array.isArray(input?.value))
-                return {
-                  value: input.value.filter((item) => typeof item !== 'undefined'),
-                }
-              return input
-            }),
-        ),
-        expectedOutput: z.object({
-          dataTypeName: dataTypeNameSchema,
-          value: z.unknown(),
-        }),
+        inputs: z.array(z.unknown()),
+        expectedOutput: z.unknown().readonly(),
         isLocked: booleanSchema,
+        position: ordinalNumberSchema,
       }),
     )
     .min(3, 'Deve haver pelo menos 3 testes casos'),
-  categories: z
-    .array(
-      z.object({
-        id: idSchema,
-        name: nameSchema,
-      }),
-    )
-    .min(1),
-  difficultyLevel: z.enum(['easy', 'medium', 'hard', 'any']),
+  categories: challengeCategoriesSchema,
+  difficultyLevel: challengeDifficultyLevelSchema,
   isPublic: booleanSchema,
 })
