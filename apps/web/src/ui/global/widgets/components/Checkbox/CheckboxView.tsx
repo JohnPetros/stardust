ec92d3474a173aab6da2fbb6049540461f2ef800
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-13 14:42:31 -0300

    ♻️ refactor(web): rename Checkbox component import and update onChange handler for improved readability

--- apps/web/src/ui/global/widgets/components/Checkbox/CheckboxView.tsx
+++ apps/web/src/ui/global/widgets/components/Checkbox/CheckboxView.tsx
@@ -1,4 +1,4 @@
-import * as C from '@radix-ui/react-checkbox'
+import * as Checkbox from '@radix-ui/react-checkbox'
 import { type ClassNameValue, twMerge } from 'tailwind-merge'
 
 import { AnimatedIndicator } from './AnimatedIndicator'
@@ -13,19 +13,19 @@ type Props = {
 
 export const CheckboxView = ({ id, isChecked, className, onChange }: Props) => {
   return (
-    <C.Root
+    <Checkbox.Root
       className={twMerge(
         'h-[18px] w-[18px] rounded-md border-2 border-green-400',
         isChecked ? 'bg-green-400' : ' border-gray-500 bg-transparent',
         className,
       )}
-      checked={isChecked}
-      onCheckedChange={onChange}
       id={id}
+      checked={isChecked}
+      onCheckedChange={() => onChange?.(!isChecked)}
     >
       <AnimatedIndicator>
         <Icon name='check' size={14} className='text-green-900' weight='bold' />
       </AnimatedIndicator>
-    </C.Root>
+    </Checkbox.Root>
   )
 }
