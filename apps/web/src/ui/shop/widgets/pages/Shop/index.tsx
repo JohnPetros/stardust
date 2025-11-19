47d64d155a739375b14d3da71f5331df8c2d7ce5
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-19 20:50:45 -0300

    🖥️ ui(web): refactor ShopPage component into ShopPageView for improved structure and readability

--- apps/web/src/ui/shop/widgets/pages/Shop/index.tsx
+++ apps/web/src/ui/shop/widgets/pages/Shop/index.tsx
@@ -1,15 +1,3 @@
-import { InsigniasList } from './InsiginiasList'
-import { RocketsList } from './RocketsList'
-import { AvatarsList } from './AvatarsList'
-import { Footer } from './Footer'
+import { ShopPageView } from './ShopPageView'
 
-export const ShopPage = () => {
-  return (
-    <main className='mx-auto max-w-5xl space-y-12 px-6 py-1 pb-[12rem] sm:pb-6'>
-      <InsigniasList />
-      <RocketsList />
-      <AvatarsList />
-      <Footer />
-    </main>
-  )
-}
+export const ShopPage = ShopPageView
