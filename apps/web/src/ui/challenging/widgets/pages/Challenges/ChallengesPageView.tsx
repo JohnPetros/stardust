51a1fe8bfaf3f39b1b4ad601b483a5bb22642217
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-24 15:58:43 -0300

    🖥️ ui(web): refactor ChallengesPageView to remove isChallengePostingEnabled prop and integrate BackPageLink and PostChallengeLink components

--- apps/web/src/ui/challenging/widgets/pages/Challenges/ChallengesPageView.tsx
+++ apps/web/src/ui/challenging/widgets/pages/Challenges/ChallengesPageView.tsx
@@ -1,37 +1,21 @@
-import Link from 'next/link'
-
 import type { ChallengeCategoryDto } from '@stardust/core/challenging/entities/dtos'
 
-import { ROUTES } from '@/constants'
-import { Icon } from '@/ui/global/widgets/components/Icon'
 import { ChallengesFilters } from './ChallengesFilters'
 import { ChallengesList } from './ChallengesList'
 import { WarningMessage } from './WarningMessage'
+import { PostChallengeLink } from './PostChallengeLink'
+import { BackPageLink } from './BackPageLink'
 
 type Props = {
   categoriesDto: ChallengeCategoryDto[]
-  isChallengePostingEnabled: boolean
 }
 
-export const ChallengesPageView = ({
-  categoriesDto,
-  isChallengePostingEnabled,
-}: Props) => {
+export const ChallengesPageView = ({ categoriesDto }: Props) => {
   return (
     <div className='relative mx-auto mt-10 max-w-2xl px-6 pb-40 md:px-0'>
-      <Link href={ROUTES.space} className='absolute -top-8 text-green-400'>
-        <Icon name='arrow-left' />
-      </Link>
+      <BackPageLink />
       <WarningMessage />
-      {isChallengePostingEnabled && (
-        <Link
-          href={ROUTES.challenging.challenge()}
-          className='flex items-center w-max text-green-400 px-3 ml-auto mt-3 text-sm'
-        >
-          Criar seu próprio desafio para outros usuários
-          <Icon name='simple-arrow-right' size={16} />
-        </Link>
-      )}
+      <PostChallengeLink />
       <div className='mt-6'>
         <ChallengesFilters categoriesDto={categoriesDto} />
         <ChallengesList />
--- apps/web/src/ui/challenging/widgets/pages/Challenges/index.tsx
+++ apps/web/src/ui/challenging/widgets/pages/Challenges/index.tsx
@@ -1,8 +1,5 @@
-'use client'
-
 import type { ChallengeCategoryDto } from '@stardust/core/challenging/entities/dtos'
 
-import { useAuthContext } from '@/ui/global/hooks/useAuthContext'
 import { ChallengesPageView } from './ChallengesPageView'
 
 type Props = {
@@ -10,12 +7,5 @@ type Props = {
 }
 
 export const ChallengesPage = ({ categoriesDto }: Props) => {
-  const { user } = useAuthContext()
-
-  return (
-    <ChallengesPageView
-      categoriesDto={categoriesDto}
-      isChallengePostingEnabled={user?.hasEngineerRole.value ?? false}
-    />
-  )
+  return <ChallengesPageView categoriesDto={categoriesDto} />
 }
