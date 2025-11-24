a121dbd36c95294349a6269a6eb8eb2bbcb8c858
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-24 15:58:50 -0300

    🖥️ ui(web): enhance ChallengeCard component to include user authentication context and manage challenge completion status

--- apps/web/src/ui/challenging/widgets/pages/Challenges/ChallengesList/ChallengeCard/index.tsx
+++ apps/web/src/ui/challenging/widgets/pages/Challenges/ChallengesList/ChallengeCard/index.tsx
@@ -1,3 +1,32 @@
+import { useAuthContext } from '@/ui/global/hooks/useAuthContext'
 import { ChallengeCardView } from './ChallengeCardView'
+import type { ChallengeCategory } from '@stardust/core/challenging/entities'
+import type { ChallengeDifficultyLevel } from '@stardust/core/challenging/types'
+import type { Id } from '@stardust/core/global/structures'
 
-export const ChallengeCard = ChallengeCardView
+type Props = {
+  id: Id
+  slug: string
+  title: string
+  difficultyLevel: ChallengeDifficultyLevel
+  categories: ChallengeCategory[]
+  upvotesCount: number
+  downvotesCount: number
+  completionsCount: number
+  authorSlug: string
+  authorName: string
+}
+
+export const ChallengeCard = (props: Props) => {
+  const { user } = useAuthContext()
+
+  if (user)
+    return (
+      <ChallengeCardView
+        {...props}
+        isCompleted={user.hasCompletedChallenge(props.id).isTrue}
+      />
+    )
+
+  return <ChallengeCardView {...props} isCompleted={false} />
+}
