74010a91c24f64a87f3ff40b304e55a6737fa07e
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-24 15:57:49 -0300

    🖥️ ui(web): add isAccountAuthenticated prop to ChallengesFiltersView for conditional rendering of completion status filter

--- apps/web/src/ui/challenging/widgets/pages/Challenges/ChallengesFilters/index.tsx
+++ apps/web/src/ui/challenging/widgets/pages/Challenges/ChallengesFilters/index.tsx
@@ -5,12 +5,14 @@ import { ChallengeCategory } from '@stardust/core/challenging/entities'
 
 import { ChallengesFiltersView } from './ChallengesFiltersView'
 import { useChallengesFilter } from './useChallengesFilters'
+import { useAuthContext } from '@/ui/global/hooks/useAuthContext'
 
 type Props = {
   categoriesDto: ChallengeCategoryDto[]
 }
 
 export const ChallengesFilters = ({ categoriesDto }: Props) => {
+  const { account } = useAuthContext()
   const categories = categoriesDto.map(ChallengeCategory.create)
   const {
     tags,
@@ -24,6 +26,7 @@ export const ChallengesFilters = ({ categoriesDto }: Props) => {
     <ChallengesFiltersView
       categories={categories}
       tags={tags}
+      isAccountAuthenticated={account?.isAuthenticated.isTrue ?? false}
       handleTitleChange={handleTitleChange}
       handleCompletionStatusChange={handleCompletionStatusChange}
       handleDifficultyLevelChange={handleDifficultyLevelChange}
