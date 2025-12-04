9996c23afd8f371df6dea5f0669dfef3f365ae6d
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-04 20:44:16 -0300

    📶 rest(server): add GetCompletedChallengesKpiController to handle KPI retrieval for completed challenges

---
+++ apps/server/src/rest/controllers/profile/users/GetCompletedChallengesKpiController.ts
@@ -0,0 +1,14 @@
+import type { Controller, Http } from '@stardust/core/global/interfaces'
+import type { RestResponse } from '@stardust/core/global/responses'
+import type { UsersRepository } from '@stardust/core/profile/interfaces'
+import { GetCompletedChallengesKpiUseCase } from '@stardust/core/profile/use-cases'
+
+export class GetCompletedChallengesKpiController implements Controller {
+  constructor(private readonly repository: UsersRepository) {}
+
+  async handle(http: Http): Promise<RestResponse> {
+    const useCase = new GetCompletedChallengesKpiUseCase(this.repository)
+    const response = await useCase.execute()
+    return http.send(response)
+  }
+}
