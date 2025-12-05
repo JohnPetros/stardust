8218f30fb0f6fd23293aea8afa4e158d02137bac
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-05 19:03:24 -0300

    🖥️ ui(studio): implement client-side data loading and enhance DashboardPage with KPI metrics

--- apps/studio/src/ui/global/widgets/pages/Dashboard/DashboardPageView.tsx
+++ apps/studio/src/ui/global/widgets/pages/Dashboard/DashboardPageView.tsx
@@ -1,10 +1,23 @@
-import { Button } from '@/ui/shadcn/components/button'
+import type { Kpi } from '@stardust/core/global/structures'
 
-export const DashboardPageView = () => {
+import { KpiCard } from './KpiCard'
+
+type Props = {
+  postedChallengesKpi: Kpi
+  completedChallengesKpi: Kpi
+  createdUsersKpi: Kpi
+  unlockedStarsKpi: Kpi
+}
+
+export const DashboardPageView = ({ postedChallengesKpi, completedChallengesKpi, createdUsersKpi, unlockedStarsKpi }: Props) => {
   return (
-    <div>
-      <h1>Dashboard Page</h1>
-      <Button>Click me</Button>
+    <div className='p-6'>
+      <div className='grid grid-cols-4 gap-4 mt-3'>
+        <KpiCard title='Usuários criados' kpi={createdUsersKpi} />
+        <KpiCard title='Desafios publicados' kpi={postedChallengesKpi} />
+        <KpiCard title='Desafios completados' kpi={completedChallengesKpi} />
+        <KpiCard title='Estrelas desbloqueadas' kpi={unlockedStarsKpi} />
+      </div>
     </div>
   )
 }
