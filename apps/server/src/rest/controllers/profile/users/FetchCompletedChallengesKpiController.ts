6b3eaad90c4d48f15a372c07d09e2508ce2d39e2
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-05 18:58:16 -0300

    ♻️ refactor(server): rename KPI controllers and routes for consistency

--- apps/server/src/rest/controllers/profile/users/FetchCompletedChallengesKpiController.ts
+++ apps/server/src/rest/controllers/profile/users/FetchCompletedChallengesKpiController.ts
@@ -1,13 +1,13 @@
 import type { Controller, Http } from '@stardust/core/global/interfaces'
 import type { RestResponse } from '@stardust/core/global/responses'
 import type { UsersRepository } from '@stardust/core/profile/interfaces'
-import { GetCreatedUsersKpiUseCase } from '@stardust/core/profile/use-cases'
+import { GetCompletedChallengesKpiUseCase } from '@stardust/core/profile/use-cases'
 
-export class GetCreatedUsersKpiController implements Controller {
+export class FetchCompletedChallengesKpiController implements Controller {
   constructor(private readonly repository: UsersRepository) {}
 
   async handle(http: Http): Promise<RestResponse> {
-    const useCase = new GetCreatedUsersKpiUseCase(this.repository)
+    const useCase = new GetCompletedChallengesKpiUseCase(this.repository)
     const response = await useCase.execute()
     return http.send(response)
   }
--- apps/server/src/rest/controllers/profile/users/FetchCreatedUsersKpiController.ts
+++ apps/server/src/rest/controllers/profile/users/FetchCreatedUsersKpiController.ts
@@ -1,13 +1,13 @@
 import type { Controller, Http } from '@stardust/core/global/interfaces'
 import type { RestResponse } from '@stardust/core/global/responses'
 import type { UsersRepository } from '@stardust/core/profile/interfaces'
-import { GetUnlockedStarsKpiUseCase } from '@stardust/core/profile/use-cases'
+import { GetCreatedUsersKpiUseCase } from '@stardust/core/profile/use-cases'
 
-export class GetUnlockedStarsKpiController implements Controller {
+export class FetchCreatedUsersKpiController implements Controller {
   constructor(private readonly repository: UsersRepository) {}
 
   async handle(http: Http): Promise<RestResponse> {
-    const useCase = new GetUnlockedStarsKpiUseCase(this.repository)
+    const useCase = new GetCreatedUsersKpiUseCase(this.repository)
     const response = await useCase.execute()
     return http.send(response)
   }
