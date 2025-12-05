9636836dba44bd71ce9220e019d8b3ad24bdc73e
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-05 17:11:09 -0300

    📶 rest(server): update GetPostedChallengesKpiController to use GetPostedChallengesKpiUseCase for KPI retrieval

--- apps/server/src/rest/controllers/challenging/challenges/GetPostedChallengesKpiController.ts
+++ apps/server/src/rest/controllers/challenging/challenges/GetPostedChallengesKpiController.ts
@@ -1,13 +1,13 @@
 import type { Controller, Http } from '@stardust/core/global/interfaces'
 import type { RestResponse } from '@stardust/core/global/responses'
 import type { ChallengesRepository } from '@stardust/core/challenging/interfaces'
-import { GetPostedChallengesUseCase } from '@stardust/core/challenging/use-cases'
+import { GetPostedChallengesKpiUseCase } from '@stardust/core/challenging/use-cases'
 
 export class GetPostedChallengesKpiController implements Controller {
   constructor(private readonly repository: ChallengesRepository) {}
 
   async handle(http: Http): Promise<RestResponse> {
-    const useCase = new GetPostedChallengesUseCase(this.repository)
+    const useCase = new GetPostedChallengesKpiUseCase(this.repository)
     const response = await useCase.execute()
     return http.send(response)
   }
