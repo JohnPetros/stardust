d045e1808bb3b4112e3ea82a73aca2118bb50e76
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-06 15:14:55 -0300

    🌐 domain: introduce Platform class for platform type validation and creation

---
+++ packages/core/src/profile/domain/structures/Platform.ts
@@ -0,0 +1,28 @@
+import { ValidationError } from '#global/domain/errors/ValidationError'
+import { StringValidation } from '#global/libs/index'
+
+export type PlatformName = 'web' | 'mobile'
+
+export class Platform {
+  readonly name: PlatformName
+
+  private constructor(name: PlatformName) {
+    this.name = name
+  }
+
+  static create(name: string): Platform {
+    if (!Platform.isPlatformName(name)) {
+      throw new ValidationError([
+        { name: 'platform', messages: ['Platform is not valid'] },
+      ])
+    }
+
+    return new Platform(name)
+  }
+
+  static isPlatformName(name: string): name is PlatformName {
+    new StringValidation(name, 'Platform Name').oneOf(['web', 'mobile']).validate()
+
+    return true
+  }
+}
