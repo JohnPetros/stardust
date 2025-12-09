69404717db595e6c8ee2836b01ca799cdc48727a
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-09 20:14:04 -0300

    📶 rest(server): implement CreateAchievementController for handling achievement creation requests

---
+++ apps/server/src/rest/controllers/profile/achievements/CreateAchievementController.ts
@@ -0,0 +1,19 @@
+import type { AchievementDto } from '@stardust/core/profile/entities/dtos'
+import type { AchievementsRepository } from '@stardust/core/profile/interfaces'
+import type { Controller, Http } from '@stardust/core/global/interfaces'
+import { CreateAchievementUseCase } from '@stardust/core/profile/use-cases'
+
+type Schema = {
+  body: AchievementDto
+}
+
+export class CreateAchievementController implements Controller<Schema> {
+  constructor(private readonly repository: AchievementsRepository) {}
+
+  async handle(http: Http<Schema>) {
+    const achievementDto = await http.getBody()
+    const useCase = new CreateAchievementUseCase(this.repository)
+    const response = await useCase.execute({ achievementDto })
+    return http.send(response)
+  }
+}
