c2af0c9c0c27615efbb63daee0543ba145cc34ea
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-10 17:33:11 -0300

    ✨ use case: add ReorderAchievementsUseCase for reordering achievements based on provided IDs

---
+++ packages/core/src/profile/use-cases/ReorderAchievementsUseCase.ts
@@ -0,0 +1,35 @@
+import { OrdinalNumber } from '#global/domain/structures/OrdinalNumber'
+import type { UseCase } from '#global/interfaces/UseCase'
+import type { AchievementsRepository } from '../interfaces/AchievementsRepository'
+import type { Achievement } from '../domain/entities/Achievement'
+import type { AchievementDto } from '../domain/entities/dtos/AchievementDto'
+import { AchievementNotFoundError } from '../domain/errors'
+
+type Request = {
+  achievementIds: string[]
+}
+
+type Response = Promise<AchievementDto[]>
+
+export class ReorderAchievementsUseCase implements UseCase<Request, Response> {
+  constructor(private readonly repository: AchievementsRepository) {}
+
+  async execute({ achievementIds }: Request): Response {
+    const achievements = await this.repository.findAll()
+    const reorderedAchievements: Achievement[] = []
+
+    for (let number = 1; number <= achievementIds.length; number++) {
+      const achievementId = achievementIds[number - 1]
+      const achievement = achievements.find(
+        (achievement) => achievement.id.value === achievementId,
+      )
+      if (!achievement) throw new AchievementNotFoundError()
+
+      achievement.position = OrdinalNumber.create(number)
+      reorderedAchievements.push(achievement)
+    }
+
+    await this.repository.replaceMany(reorderedAchievements)
+    return reorderedAchievements.map((achievement) => achievement.dto)
+  }
+}
