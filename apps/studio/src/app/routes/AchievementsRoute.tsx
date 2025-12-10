5916ea7f48d805bfb46dfba2367b5994b9c2cfdf
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-10 16:53:53 -0300

    ♻️ refactor(studio): update AchievementsRoute to use new AchievementsPage component and implement clientLoader for fetching achievements

--- apps/studio/src/app/routes/AchievementsRoute.tsx
+++ apps/studio/src/app/routes/AchievementsRoute.tsx
@@ -1,8 +1,24 @@
-import { AchievementsPage } from '@/ui/global/widgets/pages/Achievements'
+import type { Route } from '../+types/root'
+
+import { AchievementsPage } from '@/ui/profile/widgets/pages/AchievementsPage'
 import { AuthMiddleware } from '../middlewares/AuthMiddleware'
 import { RestMiddleware } from '../middlewares/RestMiddleware'
+import { restContext } from '../contexts/RestContext'
 
 export const clientMiddleware = [AuthMiddleware, RestMiddleware]
+
+export const clientLoader = async ({ context }: Route.LoaderArgs) => {
+  const { profileService } = context.get(restContext)
+  const response = await profileService.fetchAllAchievements()
+
+  if (response.isFailure) {
+    response.throwError()
+  }
+
+  return {
+    achievementsDto: response.body,
+  }
+}
 
 const AchievementsRoute = () => {
   return <AchievementsPage />
