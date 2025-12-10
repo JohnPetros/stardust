343657a46c2cf39057f954ccfae1f7e322f3023c
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-10 16:55:48 -0300

    🖥️ ui(studio): implement AchievementsPage component with view and logic for managing achievements

---
+++ apps/studio/src/ui/profile/widgets/pages/AchievementsPage/index.tsx
@@ -0,0 +1,29 @@
+import { useLoaderData } from 'react-router'
+import { AchievementsPageView } from './AchievementsPageView'
+import type { clientLoader } from '@/app/routes/AchievementsRoute'
+import { useAchievementsPage } from './useAchievementsPage'
+import { useRest } from '@/ui/global/hooks/useRest'
+import { useToastProvider } from '@/ui/global/hooks/useToastProvider'
+import { useUiProvider } from '@/ui/global/hooks/useUiProvider'
+
+export const AchievementsPage = () => {
+  const { achievementsDto } = useLoaderData<typeof clientLoader>()
+  const { profileService } = useRest()
+  const toastProvider = useToastProvider()
+  const uiProvider = useUiProvider()
+  const { handleCreateAchievement, handleUpdate, handleDelete } = useAchievementsPage({
+    achievementsDto,
+    profileService,
+    toastProvider,
+    uiProvider,
+  })
+
+  return (
+    <AchievementsPageView
+      achievements={achievementsDto}
+      onCreateAchievement={handleCreateAchievement}
+      onUpdate={handleUpdate}
+      onDelete={handleDelete}
+    />
+  )
+}
