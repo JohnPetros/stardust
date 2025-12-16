96b178b2464ed826471fae040986420ca197d149
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-16 12:45:49 -0300

    📶 rest(studio): extend ShopService interface with rocket and avatar management methods

---
+++ apps/studio/src/rest/services/ShopService.ts
@@ -0,0 +1,49 @@
+import type { ShopService as IShopService } from '@stardust/core/shop/interfaces'
+import type { RestClient } from '@stardust/core/global/interfaces'
+import type { ShopItemsListingParams } from '@stardust/core/shop/types'
+import type { PaginationResponse } from '@stardust/core/global/responses'
+import type { AvatarDto, RocketDto } from '@stardust/core/shop/entities/dtos'
+
+export const ShopService = (restClient: RestClient): IShopService => {
+  return {
+    async fetchInsigniasList() {
+      return await restClient.get('/shop/insignias')
+    },
+
+    async fetchRocketsList({
+      search,
+      page,
+      itemsPerPage,
+      order,
+    }: ShopItemsListingParams) {
+      restClient.setQueryParam('search', search?.value ?? '')
+      restClient.setQueryParam('page', page.value.toString())
+      restClient.setQueryParam('itemsPerPage', itemsPerPage.value.toString())
+      restClient.setQueryParam('order', order.value)
+      const response =
+        await restClient.get<PaginationResponse<RocketDto>>('/shop/rockets')
+      restClient.clearQueryParams()
+      return response
+    },
+
+    async fetchAvatarsList({
+      search,
+      page,
+      itemsPerPage,
+      order,
+    }: ShopItemsListingParams) {
+      restClient.setQueryParam('search', search?.value ?? '')
+      restClient.setQueryParam('page', page.value.toString())
+      restClient.setQueryParam('itemsPerPage', itemsPerPage.value.toString())
+      restClient.setQueryParam('order', order.value)
+      const response =
+        await restClient.get<PaginationResponse<AvatarDto>>('/shop/avatars')
+      restClient.clearQueryParams()
+      return response
+    },
+
+    async createRocket(rocket: RocketDto) {
+      return await restClient.post<RocketDto>('/shop/rockets', rocket)
+    },
+  }
+}
--- packages/core/src/shop/interfaces/ShopService.ts
+++ packages/core/src/shop/interfaces/ShopService.ts
@@ -1,5 +1,6 @@
 import type { PaginationResponse } from '#global/responses/PaginationResponse'
 import type { RestResponse } from '#global/responses/RestResponse'
+import type { Id } from '#global/domain/structures/Id'
 import type { ShopItemsListingParams } from '../domain/types'
 import type { AvatarDto, InsigniaDto, RocketDto } from '../domain/entities/dtos'
 
@@ -11,4 +12,10 @@ export interface ShopService {
   fetchAvatarsList(
     params: ShopItemsListingParams,
   ): Promise<RestResponse<PaginationResponse<AvatarDto>>>
+  createRocket(rocket: RocketDto): Promise<RestResponse<RocketDto>>
+  updateRocket(rocket: RocketDto): Promise<RestResponse<RocketDto>>
+  deleteRocket(rocketId: Id): Promise<RestResponse>
+  createAvatar(avatar: AvatarDto): Promise<RestResponse<AvatarDto>>
+  updateAvatar(avatar: AvatarDto): Promise<RestResponse<AvatarDto>>
+  deleteAvatar(avatarId: Id): Promise<RestResponse>
 }
