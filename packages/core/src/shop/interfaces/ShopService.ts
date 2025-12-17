c71c2eb1aaafd7ac07bd567f705b80c29b230f33
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-17 18:19:49 -0300

    📶 rest(studio): add CRUD operations for insignias in ShopService

--- packages/core/src/shop/interfaces/ShopService.ts
+++ packages/core/src/shop/interfaces/ShopService.ts
@@ -3,16 +3,20 @@ import type { RestResponse } from '#global/responses/RestResponse'
 import type { Id } from '#global/domain/structures/Id'
 import type { ShopItemsListingParams } from '../domain/types'
 import type { AvatarDto, InsigniaDto, RocketDto } from '../domain/entities/dtos'
-import type { Avatar, Rocket } from '../domain/entities'
+import type { Avatar, Insignia, Rocket } from '../domain/entities'
 
 export interface ShopService {
   fetchInsigniasList(): Promise<RestResponse<InsigniaDto[]>>
+
   fetchRocketsList(
     params: ShopItemsListingParams,
   ): Promise<RestResponse<PaginationResponse<RocketDto>>>
   fetchAvatarsList(
     params: ShopItemsListingParams,
   ): Promise<RestResponse<PaginationResponse<AvatarDto>>>
+  createInsignia(insignia: Insignia): Promise<RestResponse<InsigniaDto>>
+  updateInsignia(insignia: Insignia): Promise<RestResponse<InsigniaDto>>
+  deleteInsignia(insigniaId: Id): Promise<RestResponse>
   createRocket(rocket: Rocket): Promise<RestResponse<RocketDto>>
   updateRocket(rocket: Rocket): Promise<RestResponse<RocketDto>>
   deleteRocket(rocketId: Id): Promise<RestResponse>
