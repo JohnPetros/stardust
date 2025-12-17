bfb107994c2d8e8d8757ea455741c3f57e45bbd6
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-17 11:51:35 -0300

    ♻️ refactor: streamline avatar and rocket creation logic with Logical handling

--- packages/core/src/shop/use-cases/CreateAvatarUseCase.ts
+++ packages/core/src/shop/use-cases/CreateAvatarUseCase.ts
@@ -2,6 +2,7 @@ import type { UseCase } from '#global/interfaces/index'
 import type { AvatarsRepository } from '../interfaces/index'
 import type { AvatarDto } from '../domain/entities/dtos'
 import { Avatar } from '../domain/entities/index'
+import { Logical } from '#global/domain/structures/index'
 
 type Request = {
   avatarDto: AvatarDto
@@ -13,12 +14,19 @@ export class CreateAvatarUseCase implements UseCase<Request, Response> {
   constructor(private readonly repository: AvatarsRepository) {}
 
   async execute({ avatarDto }: Request) {
-    const avatar = Avatar.create({
-      ...avatarDto,
-      isAcquiredByDefault: false,
-      isSelectedByDefault: false,
-    })
+    const avatar = Avatar.create(avatarDto)
+    if (avatar.isSelectedByDefault.isTrue) {
+      await this.updateCurrentSelectedAvatarByDefault()
+    }
     await this.repository.add(avatar)
     return avatar.dto
+  }
+
+  async updateCurrentSelectedAvatarByDefault(): Promise<void> {
+    const avatar = await this.repository.findSelectedByDefault()
+    if (avatar) {
+      avatar.isSelectedByDefault = Logical.createAsFalse()
+      await this.repository.replace(avatar)
+    }
   }
 }
--- packages/core/src/shop/use-cases/CreateRocketUseCase.ts
+++ packages/core/src/shop/use-cases/CreateRocketUseCase.ts
@@ -1,6 +1,7 @@
 import type { UseCase } from '#global/interfaces/index'
 import type { RocketsRepository } from '../interfaces/index'
 import type { RocketDto } from '../domain/entities/dtos'
+import { Logical } from '#global/domain/structures/index'
 import { Rocket } from '../domain/entities/index'
 
 type Request = {
@@ -13,12 +14,19 @@ export class CreateRocketUseCase implements UseCase<Request, Response> {
   constructor(private readonly repository: RocketsRepository) {}
 
   async execute({ rocketDto }: Request) {
-    const rocket = Rocket.create({
-      ...rocketDto,
-      isAcquiredByDefault: false,
-      isSelectedByDefault: false,
-    })
+    const rocket = Rocket.create(rocketDto)
+    if (rocket.isSelectedByDefault.isTrue) {
+      await this.updateCurrentSelectedRocketByDefault()
+    }
     await this.repository.add(rocket)
     return rocket.dto
+  }
+
+  async updateCurrentSelectedRocketByDefault(): Promise<void> {
+    const rocket = await this.repository.findSelectedByDefault()
+    if (rocket) {
+      rocket.isSelectedByDefault = Logical.createAsFalse()
+      await this.repository.replace(rocket)
+    }
   }
 }
