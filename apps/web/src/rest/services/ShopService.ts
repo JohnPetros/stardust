39ad800df4ab5fdccebc677fc7cb7942ae74f967
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-17 11:57:32 -0300

    📶 rest(web): enhance ShopService with insignias fetching and avatar management methods

--- apps/web/src/rest/services/ShopService.ts
+++ apps/web/src/rest/services/ShopService.ts
@@ -1,37 +1,77 @@
 import type { ShopService as IShopService } from '@stardust/core/shop/interfaces'
 import type { RestClient } from '@stardust/core/global/interfaces'
 import type { ShopItemsListingParams } from '@stardust/core/shop/types'
+import type { PaginationResponse } from '@stardust/core/global/responses'
+import type { AvatarDto, RocketDto } from '@stardust/core/shop/entities/dtos'
+import type { Id } from '@stardust/core/global/structures'
+import type { Avatar, Rocket } from '@stardust/core/shop/entities'
 
 export const ShopService = (restClient: RestClient): IShopService => {
   return {
+    async fetchInsigniasList() {
+      return await restClient.get('/shop/insignias')
+    },
+
     async fetchRocketsList({
       search,
+      page,
       itemsPerPage,
       order,
-      page,
     }: ShopItemsListingParams) {
-      if (search) restClient.setQueryParam('search', search?.value)
-      restClient.setQueryParam('order', order.value)
-      restClient.setQueryParam('itemsPerPage', itemsPerPage.value.toString())
+      restClient.setQueryParam('search', search?.value ?? '')
       restClient.setQueryParam('page', page.value.toString())
-      return await restClient.get('/shop/rockets')
+      restClient.setQueryParam('itemsPerPage', itemsPerPage.value.toString())
+      restClient.setQueryParam('order', order.value)
+      const response =
+        await restClient.get<PaginationResponse<RocketDto>>('/shop/rockets')
+      restClient.clearQueryParams()
+      return response
     },
 
     async fetchAvatarsList({
       search,
+      page,
       itemsPerPage,
       order,
-      page,
     }: ShopItemsListingParams) {
-      if (search) restClient.setQueryParam('search', search?.value)
-      restClient.setQueryParam('order', order.value)
-      restClient.setQueryParam('itemsPerPage', itemsPerPage.value.toString())
+      restClient.setQueryParam('search', search?.value ?? '')
       restClient.setQueryParam('page', page.value.toString())
-      return await restClient.get('/shop/avatars')
+      restClient.setQueryParam('itemsPerPage', itemsPerPage.value.toString())
+      restClient.setQueryParam('order', order.value)
+      const response =
+        await restClient.get<PaginationResponse<AvatarDto>>('/shop/avatars')
+      restClient.clearQueryParams()
+      return response
     },
 
-    async fetchInsigniasList() {
-      return await restClient.get('/shop/insignias')
+    async createRocket(rocket: Rocket) {
+      return await restClient.post<RocketDto>('/shop/rockets', rocket.dto)
+    },
+
+    async updateRocket(rocket: Rocket) {
+      return await restClient.put<RocketDto>(
+        `/shop/rockets/${rocket.id.value}`,
+        rocket.dto,
+      )
+    },
+
+    async deleteRocket(rocketId: Id) {
+      return await restClient.delete(`/shop/rockets/${rocketId.value}`)
+    },
+
+    async createAvatar(avatar: Avatar) {
+      return await restClient.post<AvatarDto>('/shop/avatars', avatar.dto)
+    },
+
+    async updateAvatar(avatar: Avatar) {
+      return await restClient.put<AvatarDto>(
+        `/shop/avatars/${avatar.id.value}`,
+        avatar.dto,
+      )
+    },
+
+    async deleteAvatar(avatarId: Id) {
+      return await restClient.delete(`/shop/avatars/${avatarId.value}`)
     },
   }
 }
