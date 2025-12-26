070f54aafc44a41b03b8112c05e0807722916142
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-26 16:59:13 -0300

    ✨ use case: update GetGuideUseCase to return guide DTO and add type annotation for execute method

--- packages/core/src/manual/use-cases/GetGuideUseCase.ts
+++ packages/core/src/manual/use-cases/GetGuideUseCase.ts
@@ -1,6 +1,8 @@
+import type { GuideDto } from '../domain/entities/dtos'
 import type { UseCase } from '#global/interfaces/UseCase'
 import { Id } from '#global/domain/structures/Id'
 import { GuideNotFoundError } from '../domain/errors'
+import type { GuidesRepository } from '../interfaces'
 
 type Request = {
   guideId: string
@@ -11,11 +13,11 @@ type Response = Promise<GuideDto>
 export class GetGuideUseCase implements UseCase<Request, Response> {
   constructor(private readonly repository: GuidesRepository) {}
 
-  async execute({ guideId }: Request) {
+  async execute({ guideId }: Request): Response {
     const guide = await this.repository.findById(Id.create(guideId))
     if (!guide) {
       throw new GuideNotFoundError()
     }
-    return guide
+    return guide.dto
   }
 }
