66fdd77b7b026bc1c9d82c5cf19e8157fb4caa78
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-29 13:42:21 -0300

    ♻️ refactor(web): modify fetchChatMessages to return a mock response instead of making an API call

--- apps/web/src/rest/services/ConversationService.ts
+++ apps/web/src/rest/services/ConversationService.ts
@@ -1,10 +1,12 @@
 import type { ConversationService as IConversationService } from '@stardust/core/conversation/interfaces'
 import type { RestClient } from '@stardust/core/global/interfaces'
+import { RestResponse } from '@stardust/core/global/responses'
 
 export const ConversationService = (restClient: RestClient): IConversationService => {
   return {
     async fetchChatMessages(chatId) {
-      return await restClient.get(`/conversation/chat/${chatId.value}/messages`)
+      return new RestResponse({body: []})
+      // return await restClient.get(`/conversation/chat/${chatId.value}/messages`)
     },
   }
 }
