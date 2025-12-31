3514fd7cc545b14f835923ebe771935b62ddd0b1
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-31 14:30:33 -0300

    📶 rest(web): add SetAccessTokenController to handle access token management via cookies

---
+++ apps/web/src/rest/controllers/auth/SetAccessTokenController.ts
@@ -0,0 +1,22 @@
+import type { Controller, Http } from '@stardust/core/global/interfaces'
+import type { AuthService } from '@stardust/core/auth/interfaces'
+
+import { COOKIES } from '@/constants'
+
+export const SetAccessTokenController = (): Controller => {
+return {
+    async handle(http: Http) {
+      const header = http.getHeader('Authorization')
+      if (!header) return http.pass()
+
+      const token = header.split(' ')[1]
+
+      http.setCookie(
+        COOKIES.accessToken.key,
+        token,
+        COOKIES.accessToken.durationInSeconds,
+      )
+      return http.pass()
+    },
+  }
+}
--- apps/web/src/rest/controllers/auth/index.ts
+++ apps/web/src/rest/controllers/auth/index.ts
@@ -1,3 +1,4 @@
 export { VerifyAuthRoutesController } from './VerifyAuthRoutesController'
 export { ConfirmEmailController } from './ConfirmEmailController'
 export { ConfirmPasswordResetController } from './ConfirmPasswordResetController'
+export { SetAccessTokenController } from './SetAccessTokenController'
