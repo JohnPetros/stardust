50a32b6fcb865d25ad566983597cf03e18350b36
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-12-31 14:38:14 -0300

    📶 rest(server): introduce EmbeddingsStorageRouter and FilesStorageRouter for managing embeddings and file operations

---
+++ apps/server/src/app/hono/routers/storage/FilesStorageRouter.ts
@@ -0,0 +1,98 @@
+import { Hono } from 'hono'
+import { z } from 'zod'
+
+import { storageFolderSchema } from '@stardust/validation/storage/schemas'
+import {
+  itemsPerPageSchema,
+  pageSchema,
+  searchSchema,
+  stringSchema,
+} from '@stardust/validation/global/schemas'
+
+import {
+  FetchImagesListController,
+  RemoveFileController,
+  UploadFileController,
+} from '@/rest/controllers/storage'
+import { SupabaseStorageProvider } from '@/provision/storage'
+import { HonoRouter } from '../../HonoRouter'
+import { HonoHttp } from '../../HonoHttp'
+import { AuthMiddleware, ValidationMiddleware } from '../../middlewares'
+
+export class FilesStorageRouter extends HonoRouter {
+  private readonly router = new Hono().basePath('/files')
+  private readonly authMiddleware = new AuthMiddleware()
+  private readonly validationMiddleware = new ValidationMiddleware()
+
+  private registerUploadFileRoute(): void {
+    this.router.post(
+      '/:folder',
+      this.authMiddleware.verifyAuthentication,
+      this.validationMiddleware.validate(
+        'param',
+        z.object({ folder: storageFolderSchema }),
+      ),
+      async (context) => {
+        const http = new HonoHttp(context)
+        const storageProvider = new SupabaseStorageProvider()
+        const controller = new UploadFileController(storageProvider)
+        const response = await controller.handle(http)
+        return http.sendResponse(response)
+      },
+    )
+  }
+
+  private registerlistFilesRoute(): void {
+    this.router.get(
+      '/:folder',
+      this.authMiddleware.verifyAuthentication,
+      this.validationMiddleware.validate(
+        'param',
+        z.object({ folder: storageFolderSchema }),
+      ),
+      this.validationMiddleware.validate(
+        'query',
+        z.object({
+          page: pageSchema,
+          itemsPerPage: itemsPerPageSchema,
+          search: searchSchema,
+        }),
+      ),
+      async (context) => {
+        const http = new HonoHttp(context)
+        const storageProvider = new SupabaseStorageProvider()
+        const controller = new FetchImagesListController(storageProvider)
+        const response = await controller.handle(http)
+        return http.sendResponse(response)
+      },
+    )
+  }
+
+  private registerRemoveFileRoute(): void {
+    this.router.delete(
+      '/:folder/:fileName',
+      this.authMiddleware.verifyAuthentication,
+      this.validationMiddleware.validate(
+        'param',
+        z.object({
+          fileName: stringSchema,
+          folder: storageFolderSchema,
+        }),
+      ),
+      async (context) => {
+        const http = new HonoHttp(context)
+        const storageProvider = new SupabaseStorageProvider()
+        const controller = new RemoveFileController(storageProvider)
+        const response = await controller.handle(http)
+        return http.sendResponse(response)
+      },
+    )
+  }
+
+  registerRoutes(): Hono {
+    this.registerUploadFileRoute()
+    this.registerRemoveFileRoute()
+    this.registerlistFilesRoute()
+    return this.router
+  }
+}
