6517a410ceb6421b8c991d741a9286fdef241680
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-04 18:52:27 -0300

    📶 rest(server): add verifyGodAccount method to AuthMiddleware for god account validation

--- apps/server/src/app/hono/middlewares/AuthMiddleware.ts
+++ apps/server/src/app/hono/middlewares/AuthMiddleware.ts
@@ -1,13 +1,22 @@
 import type { Context, Next } from 'hono'
 
 import { SupabaseAuthService } from '@/rest/services/SupabaseAuthService'
-import { VerifyAuthenticationController } from '@/rest/controllers/auth'
+import {
+  VerifyAuthenticationController,
+  VerifyGodAccountController,
+} from '@/rest/controllers/auth'
 import { HonoHttp } from '../HonoHttp'
 
 export class AuthMiddleware {
   async verifyAuthentication(context: Context, next: Next) {
     const authService = new SupabaseAuthService(context.get('supabase'))
     const controller = new VerifyAuthenticationController(authService)
+    const http = new HonoHttp(context, next)
+    await controller.handle(http)
+  }
+
+  async verifyGodAccount(context: Context, next: Next) {
+    const controller = new VerifyGodAccountController()
     const http = new HonoHttp(context, next)
     await controller.handle(http)
   }
