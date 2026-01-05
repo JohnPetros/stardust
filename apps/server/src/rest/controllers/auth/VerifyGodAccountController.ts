bb612491003af75815e3576ee7d0fa9288f8d49c
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-05 18:14:13 -0300

    📶 rest(server): replace AuthError with NotGodAccountError in VerifyGodAccountController for improved error handling

--- apps/server/src/rest/controllers/auth/VerifyGodAccountController.ts
+++ apps/server/src/rest/controllers/auth/VerifyGodAccountController.ts
@@ -1,5 +1,5 @@
 import { ENV } from '@/constants'
-import { AuthError } from '@stardust/core/global/errors'
+import { NotGodAccountError } from '@stardust/core/global/errors'
 import type { Controller, Http } from '@stardust/core/global/interfaces'
 import type { RestResponse } from '@stardust/core/global/responses'
 
@@ -7,7 +7,7 @@ export class VerifyGodAccountController implements Controller {
   async handle(http: Http): Promise<RestResponse> {
     const accountId = await http.getAccountId()
     if (accountId !== ENV.godAccountId) {
-      throw new AuthError('Esta conta não é o administrador do sistema')
+      throw new NotGodAccountError()
     }
     return http.pass()
   }
