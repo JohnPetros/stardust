38e0774427714500cb940cc8dbb76ec56cb68fa5
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-07 20:32:13 -0300

    ♻️ refactor(core): update LspProvider interface methods to return Promises for improved asynchronous handling

--- packages/core/src/global/interfaces/provision/LspProvider.ts
+++ packages/core/src/global/interfaces/provision/LspProvider.ts
@@ -3,15 +3,15 @@ import type { LspResponse } from '../../responses'
 
 export interface LspProvider {
   run(code: string): Promise<LspResponse>
-  addInputs(codeInput: CodeInput, code: string): string
-  addFunctionCall(functionParams: unknown[], code: string): string
+  addInputs(codeInput: CodeInput, code: string): Promise<string>
+  addFunctionCall(functionParams: unknown[], code: string): Promise<string>
   buildFunction(functionName: string, functionParamsNames: string[]): string
   getFunctionName(code: string): string | null
   getFunctionParamsNames(code: string): string[]
   getInput(code: string): string | null
-  translateToLsp(jsCode: unknown): string
-  translateToJs(codeRunnerCode: string): string
+  translateToLsp(jsCode: unknown): Promise<string>
+  translateToJs(codeRunnerCode: string): Promise<string>
   getInputsCount(code: string): number
-  performSyntaxAnalysis(code: string): LspResponse
-  performSemanticAnalysis(code: string): LspResponse
+  performSyntaxAnalysis(code: string): Promise<LspResponse>
+  performSemanticAnalysis(code: string): Promise<LspResponse>
 }
