3eccc785a329373ce03188cf19ecd5f581a0a23d
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-10 19:47:57 -0300

    🌐 domain: enhance Period class to validate date ranges and ensure start date is before end date

--- packages/core/src/global/domain/structures/Period.ts
+++ packages/core/src/global/domain/structures/Period.ts
@@ -1,3 +1,6 @@
+import { Datetime, DateValidation } from '#global/libs/index'
+import { ValidationError } from '../errors'
+
 export class Period {
   private constructor(
     readonly startDate: Date,
@@ -5,6 +8,20 @@ export class Period {
   ) {}
 
   static create(startDate: string, endDate: string) {
-    return new Period(new Date(startDate), new Date(endDate))
+    const start = new Datetime(startDate).date()
+    const end = new Datetime(endDate).date()
+    new DateValidation(end, 'data final do Período').validate()
+    new DateValidation(start, 'data inicial do Período').validate()
+
+    if (start.getTime() > end.getTime()) {
+      throw new ValidationError([
+        {
+          name: 'Data inicial',
+          messages: ['Data final deve ser maior que a data inicial'],
+        },
+      ])
+    }
+
+    return new Period(start, end)
   }
 }
