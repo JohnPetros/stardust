e18aba497b37fb953aa568c747d256eaa1a8befb
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-10 19:47:18 -0300

    🖥️ ui(studio): add Calendar and DateRangePicker components for enhanced date selection functionality

---
+++ apps/studio/src/ui/shadcn/components/date-range-picker.tsx
@@ -0,0 +1,41 @@
+'use client'
+import * as React from 'react'
+import type { DateRange } from 'react-day-picker'
+import { Calendar } from './calendar'
+import { Datetime } from '@stardust/core/global/libs'
+
+type Props = {
+  startDate?: Date
+  endDate?: Date
+  onChange: (range: { startDate?: Date; endDate?: Date }) => void
+}
+
+export const DateRangePicker = ({ startDate, endDate, onChange }: Props) => {
+  const dateRange: DateRange | undefined = React.useMemo(() => {
+    if (!startDate && !endDate) return undefined
+    return {
+      from: new Datetime(startDate).addHours(3),
+      to: new Datetime(endDate).addHours(3),
+    }
+  }, [startDate, endDate])
+
+  function handleSelect(range: DateRange | undefined) {
+    if (range?.to && range?.from) {
+      onChange({
+        startDate: range?.from,
+        endDate: range?.to,
+      })
+    }
+  }
+
+  return (
+    <Calendar
+      mode='range'
+      defaultMonth={dateRange?.from}
+      selected={dateRange}
+      onSelect={handleSelect}
+      numberOfMonths={2}
+      className='rounded-lg border shadow-sm'
+    />
+  )
+}
