8c0a0de52dd0ef7f1dce90744d224bb42db7ea7c
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-27 19:52:07 -0300

    🌐 domain: add optional lastUnlockedStarId to UsersListingParams for enhanced user data tracking

--- packages/core/src/profile/domain/types/UsersListingParams.ts
+++ packages/core/src/profile/domain/types/UsersListingParams.ts
@@ -2,6 +2,7 @@ import type { Period } from '#global/domain/structures/Period'
 import type { InsigniaRole } from '#global/domain/structures/InsigniaRole'
 import type { ListingOrder } from '#global/domain/structures/ListingOrder'
 import type { FilteringParams } from '#global/domain/types/FilteringParams'
+import type { Id } from '#global/domain/structures/Id'
 import type { SpaceCompletionStatus } from '../structures'
 
 export type UsersListingParams = {
@@ -12,5 +13,6 @@ export type UsersListingParams = {
   completedChallengeCountOrder: ListingOrder
   spaceCompletionStatus: SpaceCompletionStatus
   insigniaRoles: InsigniaRole[]
+  lastUnlockedStarId?: Id
   creationPeriod?: Period
 } & FilteringParams
