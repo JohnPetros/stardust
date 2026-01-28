d5159fdadf5c7e7adf161801f93348dfa9fc52df
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-28 16:55:52 -0300

    🌐 domain: implement FeedbackReportSentEvent class for handling feedback report sent events in the reporting module

---
+++ packages/core/src/reporting/domain/events/FeedbackReportSentEvent.ts
@@ -0,0 +1,18 @@
+import { Event } from '#global/domain/abstracts/Event'
+import type { AuthorAggregateDto } from '#global/domain/aggregates/dtos/index'
+
+type Payload = {
+  feedbackReportId: string
+  feedbackReportContent: string
+  feedbackReportIntent: string
+  feedbackReportSentAt: string
+  author: AuthorAggregateDto
+}
+
+export class FeedbackReportSentEvent extends Event<Payload> {
+  static readonly _NAME = 'reporting/feedback-report.sent'
+
+  constructor(readonly payload: Payload) {
+    super(FeedbackReportSentEvent._NAME, payload)
+  }
+}
