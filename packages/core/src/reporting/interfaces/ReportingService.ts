5f1ad281abe48606141cb19ccc2091e4122ce741
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-02 15:41:09 -0300

    📶 rest: add ReportingService to manage feedback reports and update RestContext accordingly

--- packages/core/src/reporting/interfaces/ReportingService.ts
+++ packages/core/src/reporting/interfaces/ReportingService.ts
@@ -1,12 +1,12 @@
-import type { RestResponse } from '#global/responses/index'
+import type { PaginationResponse, RestResponse } from '#global/responses/index'
 import type { FeedbackReport } from '../domain/entities/FeedbackReport'
 import type { FeedbackReportsListingParams } from '../domain/types'
-import type { Id } from '../../main'
+import type { FeedbackReportDto, Id } from '../../main'
 
 export interface ReportingService {
   sendFeedbackReport(feedbackReport: FeedbackReport): Promise<RestResponse<void>>
   listFeedbackReports(
     params: FeedbackReportsListingParams,
-  ): Promise<RestResponse<FeedbackReport[]>>
+  ): Promise<RestResponse<PaginationResponse<FeedbackReportDto>>>
   deleteFeedbackReport(feedbackId: Id): Promise<RestResponse>
 }
