256d159f0588e13a961e0be9365f8846403b6cca
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-06 11:23:02 -0300

    ✨ use case: update PostChallenge use case

--- packages/core/src/challenging/use-cases/PostChallengeUseCase.ts
+++ packages/core/src/challenging/use-cases/PostChallengeUseCase.ts
@@ -2,8 +2,10 @@ import type { ChallengesRepository } from '../interfaces'
 import type { Slug } from '#global/domain/structures/Slug'
 import type { UseCase } from '#global/interfaces/UseCase'
 import type { ChallengeDto } from '../domain/entities/dtos'
+import type { Broker } from '#global/interfaces/Broker'
 import { Challenge } from '../domain/entities'
 import { ChallengeAlreadyExistsError } from '../domain/errors'
+import { ChallengePostedEvent } from '../domain/events'
 
 type Request = {
   challengeDto: ChallengeDto
@@ -12,12 +14,24 @@ type Request = {
 type Response = Promise<ChallengeDto>
 
 export class PostChallengeUseCase implements UseCase<Request, Response> {
-  constructor(private readonly repository: ChallengesRepository) {}
+  constructor(
+    private readonly repository: ChallengesRepository,
+    private readonly broker: Broker,
+  ) {}
 
   async execute({ challengeDto }: Request) {
-    const challenge = Challenge.create(challengeDto)
+    const challenge = Challenge.create({
+      ...challengeDto,
+      isPublic: false,
+    })
     await this.findChallenge(challenge.slug)
     await this.repository.add(challenge)
+    const event = new ChallengePostedEvent({
+      challengeSlug: challenge.slug.value,
+      challengeTitle: challenge.title.value,
+      challengeAuthor: challenge.author.dto,
+    })
+    await this.broker.publish(event)
     return challenge.dto
   }
 
