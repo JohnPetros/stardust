cbd442f7674eb62fcb0db4e3bf9046ad052ccbf5
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-27 19:43:34 -0300

    📑 interface: update ChallengesRepository and CacheProvider contracts

--- packages/core/src/global/interfaces/provision/CacheProvider.ts
+++ packages/core/src/global/interfaces/provision/CacheProvider.ts
@@ -6,5 +6,6 @@ export interface CacheProvider {
   get(key: string): Promise<string | null>
   set(key: string, value: string | number, options?: CacheOptions): Promise<void>
   getListItem(key: string, itemIndex: number): Promise<string | null>
+  deleteListItem(key: string, itemIndex: number): Promise<void>
   delete(key: string): Promise<void>
 }
