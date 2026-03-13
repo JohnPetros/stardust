118e9979df9e103b8a0bec950f2eff1265c5c72e
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-03-13 15:40:17 -0300

    ⚙️ config(studio): refactor Vite configuration to use a plugins array for better readability

--- apps/studio/vite.config.ts
+++ apps/studio/vite.config.ts
@@ -1,18 +1,21 @@
 import { reactRouter } from '@react-router/dev/vite'
+import type { PluginOption } from 'vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import EnvironmentPlugin from 'vite-plugin-environment'
 
+const plugins = [
+  tailwindcss(),
+  reactRouter(),
+  tsconfigPaths(),
+  nodePolyfills({
+    include: ['process'],
+  }),
+  EnvironmentPlugin('all'),
+] as unknown as PluginOption[]
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
-    nodePolyfills({
-      include: ['process'],
-    }),
-    EnvironmentPlugin('all'),
-  ],
+  plugins,
 })
