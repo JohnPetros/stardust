e0f1c1301148ee09113d8ca560ada265065b1886
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-03-25 17:02:36 -0300

    📮 validation: accept optional additional instructions in source schema

--- packages/validation/src/modules/challenging/schemas/challengeSourceSchema.ts
+++ packages/validation/src/modules/challenging/schemas/challengeSourceSchema.ts
@@ -5,4 +5,5 @@ import { idSchema, urlSchema } from '../../global/schemas'
 export const challengeSourceSchema = z.object({
   url: urlSchema,
   challengeId: idSchema.optional().nullable(),
+  additionalInstructions: z.string().optional().nullable(),
 })
