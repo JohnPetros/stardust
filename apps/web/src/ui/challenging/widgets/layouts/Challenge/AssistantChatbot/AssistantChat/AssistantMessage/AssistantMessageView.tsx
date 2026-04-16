be3e35deeb463fe4c34da86c59ced3e1643680a1
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-04-16 15:33:59 -0300

    🖥️ ui: support attributes in assistant code blocks

--- apps/web/src/ui/challenging/widgets/layouts/Challenge/AssistantChatbot/AssistantChat/AssistantMessage/AssistantMessageView.tsx
+++ apps/web/src/ui/challenging/widgets/layouts/Challenge/AssistantChatbot/AssistantChat/AssistantMessage/AssistantMessageView.tsx
@@ -15,7 +15,7 @@ type Block = {
 const parseStream = (rawText: string): Block[] => {
   const blocks: Block[] = []
 
-  const regex = /<(Text|Code|Alert|Quote)>([\s\S]*?)(?:<\/\1>|$)/g
+  const regex = /<(Text|Code|Alert|Quote)(?:\s+[^>]*)?>([\s\S]*?)(?:<\/\1>|$)/g
 
   let lastIndex = 0
   let match: RegExpExecArray | null
