21753678fbe71ea74030c716d0f2145006855b2b
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-04-18 17:03:14 -0300

    📮 validation: extend name schema length

--- packages/validation/src/modules/global/schemas/nameSchema.ts
+++ packages/validation/src/modules/global/schemas/nameSchema.ts
@@ -1,4 +1,4 @@
 import { GLOBAL_ERROR_MESSAGES } from '../constants'
 import { stringSchema } from './stringSchema'
 
-export const nameSchema = stringSchema.min(3, GLOBAL_ERROR_MESSAGES.name.min)
+export const nameSchema = stringSchema.min(3, GLOBAL_ERROR_MESSAGES.name.min).max(100)
