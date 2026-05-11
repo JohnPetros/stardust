fc8214783bf3ba8a2493f418997eb41ac092fd8a
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-05-11 18:34:07 -0300

    💾 db: remove unused next star and delete user routines

---
+++ apps/server/supabase/migrations/20260511210000_remove_next_star_function_and_view.sql
@@ -0,0 +1,7 @@
+drop function if exists "public"."get_next_star_from_next_planet"(uuid, character varying);
+
+drop function if exists "public"."delete_user"(uuid);
+
+drop procedure if exists "public"."delete_user"(character varying);
+
+drop view if exists "public"."next_star_from_next_planet";
