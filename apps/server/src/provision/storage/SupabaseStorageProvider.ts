fc37305be4bdcb53066d3488466ebc9aa9f08519
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-05-14 14:48:58 -0300

    🐛 fix: restore supabase storage bucket name

--- apps/server/src/provision/storage/SupabaseStorageProvider.ts
+++ apps/server/src/provision/storage/SupabaseStorageProvider.ts
@@ -8,7 +8,7 @@ import { Text } from '@stardust/core/global/structures'
 import type { ManyItems } from '@stardust/core/global/types'
 
 export class SupabaseStorageProvider implements StorageProvider {
-  private static readonly BUCKET_NAME = 'supabase-bucket'
+  private static readonly BUCKET_NAME = 'images'
 
   constructor(private readonly supabase: SupabaseClient) {}
 
